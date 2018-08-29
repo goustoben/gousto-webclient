2e2feb6fb6b9eb5adb1540e267efdc32b99a7b47
Renato Ferreira <renatojoey@gmail.com>
2018-08-29 16:32:39 +0100

    TECH-8900 wrong indentation

--- src/__tests__/middlewares/tracking/snowplow/getHelp.test.js
+++ src/__tests__/middlewares/tracking/snowplow/getHelp.test.js
@@ -4,18 +4,18 @@ import actions from 'actions/actionTypes'
 describe('snowplow get help tracking events', () => {
 	describe('order issue', () => {
 		const action = {
-            type: actions.GET_HELP_ORDER_ISSUE_SELECTED,
-            issue: 'ingredients'
-        }
+			type: actions.GET_HELP_ORDER_ISSUE_SELECTED,
+			issue: 'ingredients'
+		}
 
-        test('orderIssueSelected works correctly', () => {
-            expect(getHelpTracking.orderIssueSelected(action)).toEqual({
-                data: {
-                    order_issue: 'ingredients',
-                },
-                seCategory: 'Order Get Help',
-                type: 'OrderIssue Selected'
-            })
-        })
+		test('orderIssueSelected works correctly', () => {
+			expect(getHelpTracking.orderIssueSelected(action)).toEqual({
+				data: {
+					order_issue: 'ingredients',
+				},
+				seCategory: 'Order Get Help',
+				type: 'OrderIssue Selected'
+			})
+		})
 	})
 })
