991f75b73fb8fe89970ce5faec9fafd48b220018
Ana Pasparan <ana.pasparan@gousto.co.uk>
2018-08-29 17:13:10 +0100

    change component

--- src/src/routes/Account/MyDeliveries/OrdersList/OrderSkipRecovery/OrderSkipRecovery.js
+++ src/src/routes/Account/MyDeliveries/OrdersList/OrderSkipRecovery/OrderSkipRecovery.js
@@ -3,47 +3,37 @@ import React, { PropTypes } from 'react'
 import css from './OrderSkipRecovery.css'
 import ModalComponent, { ModalTitle, ModalContent, ModalFooter } from 'ModalComponent'
 
+const skipCancelOrder = (orderId, dayId, orderType, cancelPendingOrder, cancelProjectedOrder) => {
 
-class OrderSkipRecovery extends React.PureComponent {
-
-	skipCancelOrder = () => {
-		const { orderId, dayId, orderType, cancelPendingOrder, cancelProjectedOrder } = this.props
-
-		if (orderType === 'pending') {
-			cancelPendingOrder(orderId)
-		} else {
-			cancelProjectedOrder(dayId)
-		}
-	}
-
-	render() {
-		const { visible, orderId, orderType, boxNumber, skipRecovery, keepOrder } = this.props
-
-		return (
-			<ModalComponent visible={visible}>
-				<ModalTitle>
-					<div className={css.orderSkipRecoveryMessage}>Are you sure you want to {(orderType === 'pending') ? 'cancel' : 'skip'}?</div>
-				</ModalTitle>
-				{(boxNumber < 10 && skipRecovery) &&
-					<ModalContent>
-						<div className={css.valuePropositionTitle}>Not in on your delivery date?</div>
-						<div className={css.valuePropositionDescription}>Change your delivery day easily for any box you can choose recipes for.</div>
-					</ModalContent>
-				}
-				<ModalFooter>
-					<button
-						className={css.keepButton}
-						onClick={() => keepOrder({ orderId, status: orderType })}
-					>Keep Box</button>
-					<div className={css.skipAnyWay} onClick={() => this.skipCancelOrder()}>
-						{orderType === 'pending' ? 'Cancel' : 'Skip'} anyway
-					</div>
-				</ModalFooter>
-			</ModalComponent>
-		)
+	if (orderType === 'pending') {
+		cancelPendingOrder(orderId)
+	} else {
+		cancelProjectedOrder(dayId)
 	}
 }
 
+const OrderSkipRecovery = ({ visible, dayId, orderId, orderType, boxNumber, skipRecovery, keepOrder, cancelPendingOrder, cancelProjectedOrder }) => (
+	<ModalComponent visible={visible}>
+		<ModalTitle>
+			<div className={css.orderSkipRecoveryMessage}>Are you sure you want to {(orderType === 'pending') ? 'cancel' : 'skip'}?</div>
+		</ModalTitle>
+		{(boxNumber < 10 && skipRecovery) &&
+			<ModalContent>
+				<div className={css.valuePropositionTitle}>Not in on your delivery date?</div>
+				<div className={css.valuePropositionDescription}>Change your delivery day easily for any box you can choose recipes for.</div>
+			</ModalContent>
+		}
+		<ModalFooter>
+			<button
+				className={css.keepButton}
+				onClick={() => keepOrder({ orderId, status: orderType })}
+			>Keep Box</button>
+			<div className={css.skipAnyWay} onClick={() => skipCancelOrder(orderId, dayId, orderType, cancelPendingOrder, cancelProjectedOrder)}>
+				{orderType === 'pending' ? 'Cancel' : 'Skip'} anyway
+					</div>
+		</ModalFooter>
+	</ModalComponent>
+)
 
 OrderSkipRecovery.propTypes = {
 	visible: PropTypes.bool,
