7f34790a2f5d6720c44a65a3b25f6c08ce3652ee
Renato Ferreira <renatojoey@gmail.com>
2018-09-04 17:20:22 +0100

    TECH-8902 GetHelpLayout component created and implemented in OrdersIssue

---
+++ src/src/layouts/GetHelpLayout/GetHelpLayout.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react'
+
+import { PageContent, PageHeader } from 'Page'
+
+import css from './GetHelpLayout.css'
+
+const GetHelpLayout = ({ title, body, children }) => {
+	const bodyContent = []
+	const footerContent = []
+
+	React.Children.forEach(children, child => {
+		if (child.type.name === 'BottomBar') {
+			footerContent.push(child)
+		} else {
+			bodyContent.push(child)
+		}
+	})
+
+	return (
+		<div className={css.rootContainer}>
+			<div className={css.header}>
+				<PageHeader title={title} />
+			</div>
+			<PageContent className={css.pageContent}>
+				<p className={css.bodyDescription}>
+					{body}
+				</p>
+				<div className={css.bodyContent}>
+					{bodyContent}
+				</div>
+				{footerContent}
+			</PageContent>
+		</div>
+	)
+}
+
+GetHelpLayout.propTypes = {
+	title: PropTypes.string.isRequired,
+	body: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired,
+}
+
+export default GetHelpLayout
--- src/src/routes/GetHelp/OrderIssue/OrderIssue.js
+++ src/src/routes/GetHelp/OrderIssue/OrderIssue.js
@@ -1,40 +1,21 @@
 import React, { PropTypes } from 'react'
 
 import List from './List'
-import { PageContent, PageHeader } from 'Page'
 import { BottomBar } from 'BottomBar'
-import Button from 'Button'
-import Link from 'Link'
+import BottomButton from '../components/BottomButton'
 import { client as routes } from 'config/routes'
 
-import css from './OrderIssue.css'
+import GetHelpLayout from 'layouts/GetHelpLayout'
 
 const OrderIssue = ({ content: { title, body, buttonCopy }, categories }) => (
-	<div className={css.rootContainer}>
-		<div className={css.header}>
-			<PageHeader title={title} />
-		</div>
-		<PageContent className={css.pageContent}>
-			<p className={css.copy}>
-				{body}
-			</p>
-			<div className={css.issuesContainer}>
-				<List categories={categories} />
-			</div>
-			<BottomBar>
-				<Button color="secondary" width="no-auto" className={css.button} areChildrenInSegment>
-					<Link
-						noDecoration
-						className={css.buttonChild}
-						clientRouted={false}
-						to={routes.myGousto}
-					>
-						{buttonCopy}
-					</Link>
-				</Button>
-			</BottomBar>
-		</PageContent>
-	</div>
+	<GetHelpLayout title={title} body={body}>
+		<List categories={categories} />
+		<BottomBar>
+			<BottomButton color="secondary" url={routes.myGousto} clientRouted={false}>
+				{buttonCopy}
+			</BottomButton>
+		</BottomBar>
+	</GetHelpLayout>
 )
 
 OrderIssue.propTypes = {
