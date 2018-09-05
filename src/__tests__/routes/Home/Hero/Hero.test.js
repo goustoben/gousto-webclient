800e2b6e42ac71681f2d3d36a999a496b598b27c
Mark Gono <markgono@gmail.com>
2018-09-05 17:08:14 +0100

    update hero tests - variant tests

--- src/__tests__/routes/Home/Hero/Hero.test.js
+++ src/__tests__/routes/Home/Hero/Hero.test.js
@@ -1,11 +1,10 @@
-import sinon from 'sinon'
-
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Hero from 'routes/Home/Hero/Hero'
-
 import CTA from 'routes/Home/CTA'
+import ContentMask from 'ContentMask'
+
+import Hero from 'routes/Home/Hero/Hero'
 
 describe('Hero', () => {
 	test('should include a main header', () => {
@@ -19,9 +18,27 @@ describe('Hero', () => {
 	})
 
 	test('should have a CTA button', () => {
-		const redirect = sinon.spy()
+		const redirect = jest.fn()
 
 		const wrapper = shallow(<Hero redirect={redirect} />)
 		expect(wrapper.find(CTA)).toHaveLength(1)
+	})
+
+	describe('variants', () => {
+		test('should display default variant by default', () => {
+			const wrapper = shallow(<Hero />)
+
+			expect(wrapper.find('div').at(0).prop('className')).toBe('container--default')
+			expect(wrapper.find('div').at(1).prop('className')).toBe('textContainer--default')
+			expect(wrapper.contains(<ContentMask fillColor="Coconut" />)).toBe(true)
+		})
+
+		test('should display rebrand variant when variant prop is rebrand', () => {
+			const wrapper = shallow(<Hero variant="rebrand" />)
+
+			expect(wrapper.find('div').at(0).prop('className')).toBe('container--rebrand')
+			expect(wrapper.find('div').at(1).prop('className')).toBe('textContainer--rebrand')
+			expect(wrapper.contains(<ContentMask fillColor="Coconut" />)).toBe(false)
+		})
 	})
 })
