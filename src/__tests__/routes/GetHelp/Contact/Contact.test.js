348adc0e82ee70382ff00d91bcf17b55604d9480
David Torres <david.torres@gousto.co.uk>
2018-09-20 09:48:36 +0100

    TECH-8961 add one more test

--- src/__tests__/routes/GetHelp/Contact/Contact.test.js
+++ src/__tests__/routes/GetHelp/Contact/Contact.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import { zendesk } from 'config/routes'
+import { telephone } from 'config/company'
 
 import Contact from 'routes/GetHelp/Contact/Contact'
 
@@ -26,33 +27,24 @@ describe('<Contact />', () => {
 		getHelpLayout = wrapper.find('GetHelpLayout')
 	})
 
-	test('layout is rendering correctly', () => {
-		const BottomBar = getHelpLayout.find('BottomBar')
+	describe('render', () => {
+		test('layout is rendering correctly', () => {
+			const BottomBar = getHelpLayout.find('BottomBar')
 
-		expect(getHelpLayout).toHaveLength(1)
-		expect(BottomBar).toHaveLength(1)
-		expect(BottomBar.find('BottomButton')).toHaveLength(2)
+			expect(getHelpLayout).toHaveLength(1)
+			expect(BottomBar).toHaveLength(1)
+			expect(BottomBar.find('BottomButton')).toHaveLength(2)
 
-	})
+		})
 
-	test('header is rendering correctly', () => {
-		expect(getHelpLayout.prop('title')).toBe(content.title)
-	})
+		test('header is rendering correctly', () => {
+			expect(getHelpLayout.prop('title')).toBe(content.title)
+		})
 
-	test('body description is redering correctly', () => {
-		expect(getHelpLayout.prop('body')).toBe(content.body)
-	})
+		test('body description is redering correctly', () => {
+			expect(getHelpLayout.prop('body')).toBe(content.body)
+		})
 
-	test('bottom bar buttons is rendering correctly', () => {
-		const BottomBar = getHelpLayout.find('BottomBar')
-		const Button1 = BottomBar.find('BottomButton').at(0)
-		const Button2 = BottomBar.find('BottomButton').at(1)
-
-		expect(Button1.text()).toContain(content.button1Copy)
-		expect(Button2.text()).toContain(content.button2Copy)
-	})
-
-	describe('items', () => {
 		test('items are rendered', () => {
 			const items = wrapper.find('Item')
 
@@ -62,6 +54,17 @@ describe('<Contact />', () => {
 			expect(items.at(2).text()).toContain('test phoneItem')
 		})
 
+		test('bottom bar buttons is rendering correctly', () => {
+			const BottomBar = getHelpLayout.find('BottomBar')
+			const Button1 = BottomBar.find('BottomButton').at(0)
+			const Button2 = BottomBar.find('BottomButton').at(1)
+
+			expect(Button1.text()).toContain(content.button1Copy)
+			expect(Button2.text()).toContain(content.button2Copy)
+		})
+	})
+
+	describe('behaviour', () => {
 		test('live chat opens the chat when clicked', () => {
 			const openLiveChatSpy = jest.fn()
 			const chatItem = wrapper.find('Item').at(0).find('ItemExecutable')
@@ -81,6 +84,13 @@ describe('<Contact />', () => {
 			const linkTo = wrapper.find('Item').at(1).find('a').prop('href')
 
 			expect(linkTo).toContain(zendesk.link)
+		})
+
+		test('phone number is shown when phone item is clicked', () => {
+			const phoneItem = wrapper.find('Item').at(2).find('ItemExpandable')
+			phoneItem.simulate('click')
+
+			expect(phoneItem.text()).toContain(telephone.number)
 		})
 	})
 })
