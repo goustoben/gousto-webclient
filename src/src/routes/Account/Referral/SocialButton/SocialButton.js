b1e2409038aa6a250eb6c31aaa884e422aeffc8a
Mark Gono <markgono@gmail.com>
2018-09-25 23:33:43 +0100

    add raf socialbutton component

---
+++ src/src/routes/Account/Referral/SocialButton/SocialButton.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react'
+import Svg from 'Svg'
+import css from './SocialButton.css'
+
+const SocialButton = ({ text, type, onClick }) => (
+	<div className={`${css[`button-${type}`]}`} onClick={onClick}>
+		<Svg fileName={`icon-${type}`} className={css.icon} />
+		<span className={css.text}>{text}</span>
+	</div>
+)
+
+SocialButton.propTypes = {
+	paymentDate: PropTypes.string,
+	text: PropTypes.string,
+	type: PropTypes.string,
+	onClick: PropTypes.func,
+}
+
+export default SocialButton
