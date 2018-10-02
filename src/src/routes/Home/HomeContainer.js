e63b6fc45d86b02ccfb404b1c3e7a59fb0369f82
Mark Gono <markgono@gmail.com>
2018-10-02 14:45:57 +0100

    use defaultvariant as the fallback for the home variant value

--- src/src/routes/Home/HomeContainer.js
+++ src/src/routes/Home/HomeContainer.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state, props) => ({
 	enableStorystream: state.features.getIn(['enableStorystream', 'value'], false),
 	moduleOrder: state.features.getIn(['hp_module_order', 'value']),
 	isAuthenticated: state.auth.get('isAuthenticated'),
-	variant: (props.location && props.location.query) ? getKnownVariant(props.location.query.variant) : 'default'
+	variant: (props.location && props.location.query) ? getKnownVariant(props.location.query.variant) : defaultVariant,
 })
 
 const HomeContainer = connect(
