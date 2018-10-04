980c9d81586ee29832ce8868ce9254d4f04fd32f
Ana Pasparan <ana.pasparan@gousto.co.uk>
2018-10-04 17:00:22 +0100

    fix jest

--- src/src/validations/regularExpressions.js
+++ src/src/validations/regularExpressions.js
@@ -1,3 +1,3 @@
 export default {
-	name: new RegExp('^[a-zA-ZÀ-ž]+(-[a-zA-ZÀ-ž]+)?$'),
+	name: new RegExp('^[a-zA-ZÀ-ž]{1,}(\\-[a-zA-ZÀ-ž]{1,}){0,1}(\\s[a-zA-ZÀ-ž]{1,}(\\-[a-zA-ZÀ-ž]{1,}){0,1}){0,}$'),
 }
