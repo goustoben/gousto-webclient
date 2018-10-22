af9318ced775202fc41d61d4329deb4331af1b88
Jan Stehlik <jan.stehlik@gousto.co.uk>
2018-10-22 11:36:15 +0100

    importing PropTypes from prop-types liibrary rather than react lib

--- src/src/routes/Account/Referral/UserRAFLink/UserRAFLink.js
+++ src/src/routes/Account/Referral/UserRAFLink/UserRAFLink.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import css from './UserRAFLink.css'
 import actions from 'actions/user'
