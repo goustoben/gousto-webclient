0476237667e7de0eb335735b24a0ec118d5440aa
David Torres <david.torres@gousto.co.uk>
2018-10-22 16:10:51 +0100

    TECH-9120 fix all remaining broken tests after React16 rebase

--- src/__tests__/routes/GetHelp/GetHelp.test.js
+++ src/__tests__/routes/GetHelp/GetHelp.test.js
@@ -1,20 +1,35 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import Helmet from 'react-helmet'
 
 import GetHelp from 'routes/GetHelp/GetHelp'
 
 describe('<GetHelp />', () => {
+	const location = {
+		query: {
+			orderId: '7',
+		},
+	}
+	const storeGetHelpOrderIdSpy = jest.fn()
+	const wrapper = mount(
+		<GetHelp
+			location={location}
+			storeGetHelpOrderId={storeGetHelpOrderIdSpy}
+		>
+			<div className="test" />
+		</GetHelp>
+	)
+
 	describe('rendering', () => {
 		test('component is redering correctly', () => {
-			const wrapper = shallow(
-				<GetHelp>
-					<div className="test" />
-				</GetHelp>
-			)
-
 			expect(wrapper.find(Helmet)).toHaveLength(1)
 			expect(wrapper.contains(<div className="test" />)).toBe(true)
+		})
+	})
+
+	describe('behaviour', () => {
+		test('storeGetHelpOrderId action is called with the order id', () => {
+			expect(storeGetHelpOrderIdSpy).toHaveBeenCalledWith('7')
 		})
 	})
 })
