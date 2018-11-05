04f822e1d9377eacb8e9164ecb5928e606177910
Renato Ferreira <renatojoey@gmail.com>
2018-11-05 13:00:15 +0000

    TECH-9204 adding ingredients to the FE

--- src/src/routes/GetHelp/Ingredients/Ingredients.logic.js
+++ src/src/routes/GetHelp/Ingredients/Ingredients.logic.js
@@ -5,6 +5,8 @@ import { List } from '../components/List'
 import { ItemExpandable } from 'goustouicomponents'
 import { IngredientsPresentation } from './Ingredients.presentation'
 
+import css from './InputCheck'
+
 const propTypes = {
 	content: PropTypes.shape({
 		title: PropTypes.string.isRequired,
@@ -14,16 +16,43 @@ const propTypes = {
 	}).isRequired,
 }
 
+const InputCheck = ({ id, label, onChange }) => (
+	<div className={css.inputCheckContainer}>
+		<input
+			id="inputCheck"
+			type="checkbox"
+			onChange={() => {
+				onChange(id)
+			}}
+		/>
+		<div className={css.inputCheckMask}></div>
+		<label className={css.inputCheckLabel} htmlFor="inputCheck">
+			{label}
+		</label>
+	</div>
+)
+
 const RecipeList = ({ recipes }) => {
+	const changeFn = (id) => {
+		console.log('>>>', id)
+	}
 	const items = Object.keys(recipes).map((id) => {
 		const recipe = recipes[id]
+		const ingredients = recipe.ingredients.map((ingredient) => (
+			<InputCheck
+				key={ingredient.id}
+				id={ingredient.id}
+				label={ingredient.label}
+				onChange={changeFn}
+			/>
+		))
 
 		return (
 			<ItemExpandable
 				key={recipe.id}
 				label={recipe.title}
 			>
-				<div />
+				{ingredients}
 			</ItemExpandable>
 		)
 	})
@@ -45,7 +74,9 @@ const Ingredients = ({ content, recipes }) => {
 			buttonLeftUrl={buttonLeftUrl}
 			buttonRightUrl={buttonRightUrl}
 		>
-			<RecipeList recipes={recipes} />
+			<div>
+				<RecipeList recipes={recipes} />
+			</div>
 		</IngredientsPresentation>
 	)
 }
