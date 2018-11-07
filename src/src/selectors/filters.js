9474ced52ad6b0f9051487292f425191c1b9f61e
Ellie <Chrome@MG515.local>
2018-11-07 09:33:40 +0000

    Removed selector JFY logic

--- src/src/selectors/filters.js
+++ src/src/selectors/filters.js
@@ -1,22 +1,10 @@
 import { textReducer } from 'utils/text'
-import { getCollectionIdWithName } from 'utils/collections'
 import config from 'config/recipes'
 
-export const getCurrentCollectionId = state => {
-	console.log('getting current collection id')
-	if (state.features.get('justforyou').get('value')) {
-		console.log('has just for you feature flag')
-
-		return getCollectionIdWithName(state, 'recommendations')
-	}
-
-	return state.filters.get('currentCollectionId')
-}
+export const getCurrentCollectionId = state => state.filters.get('currentCollectionId')
 export const getCurrentDietTypes = state => state.filters.get('dietTypes')
 export const getCurrentTotalTime = state => state.filters.get('totalTime')
 export const getDietaryAttributes = state => state.filters.get('dietaryAttributes')
-
-
 
 export const getShortTitle = (menuCollections, currentCollectionId) => {
 	if (menuCollections.getIn([currentCollectionId, 'slug'], '') === 'recommendations') {
