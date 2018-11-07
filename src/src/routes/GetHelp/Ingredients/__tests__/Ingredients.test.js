375a5d724a0f538a5bd5369a1753f7a90aad67a2
Renato Ferreira <renatojoey@gmail.com>
2018-11-07 15:18:12 +0000

    TECH-9204 tests fix

--- src/src/routes/GetHelp/Ingredients/__tests__/Ingredients.test.js
+++ src/src/routes/GetHelp/Ingredients/__tests__/Ingredients.test.js
@@ -10,12 +10,12 @@ describe('<Ingredients />', () => {
 		button1Copy: 'Back',
 		button2Copy: 'Done',
 	}
-	const recipes = {
-		1: { id: 1, title: 'test 1' },
-		2: { id: 2, title: 'test 2' },
-		3: { id: 3, title: 'test 3' },
-		4: { id: 4, title: 'test 4' },
-	}
+	const recipes = [
+		{ id: 1, title: 'test 1', ingredients: [{ id: 1, label: 'test' }] },
+		{ id: 2, title: 'test 2', ingredients: [{ id: 2, label: 'test' }] },
+		{ id: 3, title: 'test 3', ingredients: [{ id: 3, label: 'test' }] },
+		{ id: 4, title: 'test 4', ingredients: [{ id: 4, label: 'test' }] },
+	]
 	let wrapper
 	let getHelpLayout
 
