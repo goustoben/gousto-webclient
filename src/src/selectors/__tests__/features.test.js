89e3e5c973c110cf8362a63938911483fe64449c
Ellie <Chrome@MG515.local>
2018-11-08 17:25:54 +0000

    Added check for undefined features to feature selectors

--- src/src/selectors/__tests__/features.test.js
+++ src/src/selectors/__tests__/features.test.js
@@ -2,79 +2,102 @@ import Immutable from 'immutable'
 
 import { isCollectionsFeatureEnabled, isJustForYouFeatureEnabled } from 'selectors/features'
 
-describe('isCollectionsFeatureEnabled', () => {
-	describe('when collections feature is set to true', () => {
-		const state = {
-			features: Immutable.fromJS({
-				collections: {
-					value: true
-				}
-			})
-		}
 
-		it('should return true', () => {
-			expect(isCollectionsFeatureEnabled(state)).toBe(true)
-		})
+describe('when features are undefined', () => {
+	const state = {}
+
+	it('isCollectionsFeatureEnabled should return false', () => {
+		expect(isCollectionsFeatureEnabled(state)).toBe(false)
 	})
 
-	describe('when forceCollections feature is set to true', () => {
-		const state = {
-			features: Immutable.fromJS({
-				forceCollections: {
-					value: true
-				}
-			})
-		}
-
-		it('should return true', () => {
-			expect(isCollectionsFeatureEnabled(state)).toBe(true)
-		})
-	})
-
-	describe('when both collections and forceCollections are false', () => {
-		const state = {
-			features: Immutable.fromJS({
-				collections: {
-					value: false
-				},
-				forceCollections: {
-					value: false
-				}
-			})
-		}
-
-		it('should return false', () => {
-			expect(isCollectionsFeatureEnabled(state)).toBe(false)
-		})
+	it('isJustForYouFeatureEnabled should return false', () => {
+		expect(isJustForYouFeatureEnabled(state)).toBe(false)
 	})
 })
 
-describe('isJustForYouFeatureEnabled', () => {
-	describe('when justforyou feature is enabled', () => {
-		const state = {
-			features: Immutable.fromJS({
-				justforyou: {
-					value: true
-				}
-			})
-		}
+describe('when features are defined', () => {
+	let state = {}
 
-		it('should return true', () => {
-			expect(isJustForYouFeatureEnabled(state)).toBe(true)
+	beforeEach(() => {
+		state = {
+			features: Immutable.Map({})
+		}
+	})
+
+	describe('isCollectionsFeatureEnabled', () => {
+		describe('when collections feature is set to true', () => {
+			beforeEach(() => {
+				state.features = Immutable.fromJS({
+					collections: {
+						value: true
+					}
+				})
+			})
+
+			it('should return true', () => {
+				expect(isCollectionsFeatureEnabled(state)).toBe(true)
+			})
+		})
+
+		describe('when forceCollections feature is set to true', () => {
+			beforeEach(() => {
+				state.features = Immutable.fromJS({
+					forceCollections: {
+						value: true
+					}
+				})
+			})
+
+			it('should return true', () => {
+				expect(isCollectionsFeatureEnabled(state)).toBe(true)
+			})
+		})
+
+		describe('when both collections and forceCollections are false', () => {
+			beforeEach(() => {
+				state.features = Immutable.fromJS({
+						collections: {
+							value: false
+						},
+						forceCollections: {
+							value: false
+						}
+					})
+				})
+
+			it('should return false', () => {
+				expect(isCollectionsFeatureEnabled(state)).toBe(false)
+			})
 		})
 	})
 
-	describe('when justforyou feature is disabled', () => {
-		const state = {
-			features: Immutable.fromJS({
-				justforyou: {
-					value: false
-				}
-			})
-		}
+	describe('isJustForYouFeatureEnabled', () => {
+		describe('when justforyou feature is enabled', () => {
+			beforeEach(() => {
+				state.features = Immutable.fromJS({
+						justforyou: {
+							value: true
+						}
+					})
+				})
 
-		it('should return false', () => {
-			expect(isJustForYouFeatureEnabled(state)).toBe(false)
+			it('should return true', () => {
+				expect(isJustForYouFeatureEnabled(state)).toBe(true)
+			})
+		})
+
+		describe('when justforyou feature is disabled', () => {
+			beforeEach(() => {
+				state.features = Immutable.fromJS({
+						justforyou: {
+							value: false
+						}
+					})
+				})
+
+			it('should return false', () => {
+				expect(isJustForYouFeatureEnabled(state)).toBe(false)
+			})
 		})
 	})
 })
--- src/src/selectors/features.js
+++ src/src/selectors/features.js
@@ -1,5 +1,5 @@
 export const isCollectionsFeatureEnabled = state => (
-	state.features.getIn(['collections', 'value']) || state.features.getIn(['forceCollections', 'value'])
+	state.features ? (state.features.getIn(['collections', 'value']) || state.features.getIn(['forceCollections', 'value'])) : false
 )
 
-export const isJustForYouFeatureEnabled = state => state.features.getIn(['justforyou', 'value'])
+export const isJustForYouFeatureEnabled = state => (state.features ? state.features.getIn(['justforyou', 'value']) : false)
