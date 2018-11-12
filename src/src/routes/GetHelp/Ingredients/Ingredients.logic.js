b26ef364d57055a4e908a95823e9dd645539722b
Renato Ferreira <renatojoey@gmail.com>
2018-11-12 15:06:36 +0000

    TECH-9189 Continue button integrated with ingredients selection; API call

--- src/src/routes/GetHelp/Ingredients/Ingredients.logic.js
+++ src/src/routes/GetHelp/Ingredients/Ingredients.logic.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { client } from 'config/routes'
+import { validateIngredients } from 'apis/getHelp'
 import { IngredientsPresentation } from './Ingredients.presentation'
 import { RecipeList } from '../components/RecipeList'
 
+import css from './Ingredients.css'
+
 const propTypes = {
+  order: PropTypes.shape({
+    id: PropTypes.string.isRequired
+  }),
+  recipes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      title: PropTypes.string,
+      ingredients: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          label: PropTypes.string.isRequired,
+        })
+      )
+    })
+  ),
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    accessToken: PropTypes.string.isRequired,
+  }),
   content: PropTypes.shape({
     title: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
@@ -13,19 +35,72 @@ const propTypes = {
   }).isRequired,
 }
 
-const Ingredients = ({ content, recipes }) => {
-  const buttonLeftUrl = client.getHelp.index
-  const buttonRightUrl = `${client.getHelp.index}/${client.getHelp.refund}`
+class Ingredients extends PureComponent {
+  state = {
+    selectedIngredients: new Map()
+  }
 
-  return (
-    <IngredientsPresentation
-      content={content}
-      buttonLeftUrl={buttonLeftUrl}
-      buttonRightUrl={buttonRightUrl}
-    >
-      <RecipeList recipes={recipes}/>
-    </IngredientsPresentation>
-  )
+  changeHandler = (checkboxId, isChecked) => {
+    const { selectedIngredients } = this.state
+    const newSelectedIngredients = new Map(selectedIngredients)
+
+    if (isChecked) {
+      newSelectedIngredients.set(checkboxId, isChecked)
+    } else {
+      newSelectedIngredients.delete(checkboxId)
+    }
+
+    this.setState({
+      ...this.state,
+      selectedIngredients: newSelectedIngredients
+    })
+  }
+
+  continueClickHandler = () => {
+    // POST -> /validate-ingredients -> {customer_id: int, order_id: int, ingredients: array}
+    // 200 -> {'valid': True}
+
+    const { order, user } = this.props
+    const { selectedIngredients } = this.state
+    const ingredients = []
+
+    selectedIngredients.forEach((value, ingredientId) => {
+      ingredients.push(ingredientId)
+    })
+
+    validateIngredients(
+      user.accessToken,
+      {
+        customer_id: Number(user.id),
+        order_id: Number(order.id),
+        ingredients
+      }
+    )
+
+  }
+
+  render() {
+    const { content, recipes } = this.props
+    const { selectedIngredients } = this.state
+    const buttonLeftUrl = client.getHelp.index
+    const cssButton = css.button
+
+    return (
+      <IngredientsPresentation
+        content={content}
+        buttonLeftUrl={buttonLeftUrl}
+        cssButton={cssButton}
+        cannotContinue={selectedIngredients.size < 1}
+        continueClick={this.continueClickHandler}
+      >
+        <RecipeList
+          recipes={recipes}
+          selectedIngredients={selectedIngredients}
+          onChange={this.changeHandler}
+        />
+      </IngredientsPresentation>
+    )
+  }
 }
 
 Ingredients.propTypes = propTypes
--- src/src/routes/GetHelp/Ingredients/IngredientsContainer.js
+++ src/src/routes/GetHelp/Ingredients/IngredientsContainer.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux'
 import { Ingredients } from './Ingredients.logic'
 
 const mapStateToProps = (state) => ({
+  order: state.getHelp.get('order').toJS(),
   recipes: state.getHelp.get('recipes').toJS(),
+  user: {
+    id: state.user.get('id'),
+    accessToken: state.auth.get('accessToken'),
+  },
   content: {
     title: state.content.get('get-help_contact_pageheader_header')
     || 'Get help with your box',
@@ -12,7 +17,7 @@ const mapStateToProps = (state) => ({
     button1Copy: state.content.get('get-help_orderissues_pagecontent_button1copy')
     || 'back',
     button2Copy: state.content.get('get-help_orderissues_pagecontent_button2copy')
-    || 'done',
+    || 'continue',
   }
 })
 
