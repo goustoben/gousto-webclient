831b8fc50140d08a15085af519f6f298d7d2c204
David Torres <david.torres@gousto.co.uk>
2018-11-13 17:11:05 +0000

    TECH-9189 move ingredients behaviour tests from RecipeList to Ingredients, as the state has been moved there

--- src/src/routes/GetHelp/Ingredients/__tests__/Ingredients.test.js
+++ src/src/routes/GetHelp/Ingredients/__tests__/Ingredients.test.js
@@ -12,7 +12,11 @@ describe('<Ingredients />', () => {
   }
   const recipes = [
     { id: '1', title: 'test 1', ingredients: [{ id: '1', label: 'test' }] },
-    { id: '2', title: 'test 2', ingredients: [{ id: '2', label: 'test' }] },
+    {
+      id: '2',
+      title: 'test 2',
+      ingredients: [{ id: '2', label: 'test' }, { id: '2222', label: 'test2' }]
+    },
     { id: '3', title: 'test 3', ingredients: [{ id: '3', label: 'test' }] },
     { id: '4', title: 'test 4', ingredients: [{ id: '4', label: 'test' }] },
   ]
@@ -106,6 +110,31 @@ describe('<Ingredients />', () => {
         />
       )
       getHelpLayout = wrapper.find('GetHelpLayout')
+    })
+
+    test('ingredients are unselected by default', () => {
+      const secondRecipe = getHelpLayout.find('Recipe').at(1)
+      secondRecipe.find('Item').simulate('click')
+      const ingredientsCheckboxes = wrapper.find('input[type="checkbox"]')
+
+      expect(ingredientsCheckboxes).toHaveLength(2)
+      expect(ingredientsCheckboxes.at(0).prop('checked')).toBeFalsy()
+      expect(ingredientsCheckboxes.at(1).prop('checked')).toBeFalsy()
+    })
+
+    test('ingredients retain the selection state when thy are collapsed and then expanded', () => {
+      const secondRecipe = wrapper.find('Recipe').at(1)
+      secondRecipe.find('Item').simulate('click')
+      let ingredientsCheckboxes = wrapper.find('input[type="checkbox"]')
+      ingredientsCheckboxes.at(1).simulate('change')
+
+      expect(ingredientsCheckboxes.at(1).prop('checked')).toBeTruthy()
+
+      secondRecipe.find('Item').simulate('click')
+      secondRecipe.find('Item').simulate('click')
+      ingredientsCheckboxes = wrapper.find('input[type="checkbox"]')
+
+      expect(ingredientsCheckboxes.at(1).prop('checked')).toBeTruthy()
     })
 
     test('the Continue button is enabled only when one or more ingredients are selected', () => {
