1ccf92a61bc09cb1d96e405431f09f3ba0942d4c
Mark Gono <markgono@gmail.com>
2018-11-16 10:56:41 +0000

    add checkoutframe component

---
+++ src/src/routes/Checkout/Components/CheckoutPayment/CheckoutFrame/CheckoutFrame.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { publicKey } from '../config'
+
+/* global Frames */
+export class CheckoutFrame extends React.Component {
+  componentDidMount() {
+    const { paymentButton, paymentForm } = this
+
+    Frames.init({
+      publicKey,
+      containerSelector: '.frames-container',
+      cardValidationChanged: () => {
+        paymentButton.disabled = !Frames.isCardValid()
+      },
+      cardSubmitted: () => {
+        paymentButton.disabled = true
+      },
+      cardTokenised: (e) => {
+        const { cardToken } = e.data
+        Frames.addCardToken(paymentForm, cardToken)
+        paymentForm.submit(() => false)
+      },
+      cardTokenisationFailed: (err) => {}
+    })
+    paymentForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      Frames.submitCard()
+    })
+  }
+
+  setPaymentButtonRef = element => {
+    this.paymentButton = element
+  }
+
+  setPaymentFormRef = element => {
+    this.paymentForm = element
+  }
+
+  submitCard = e => {
+    e.preventDefault()
+    document.forms['payment-form'].submit()
+  }
+
+  render() { 
+    return (
+      <form ref={this.setPaymentFormRef} id="payment-form" name="payment-form" method="POST" action="https://merchant.com/charge-card">
+        <div className="frames-container" />
+        <button ref={this.setPaymentButtonRef} type="submit" onClick={this.submitCard}>Submit Checkout Request</button>
+      </form>
+    )
+  }
+}
