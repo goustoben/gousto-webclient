8043bb06af506dfa81e048b35ffdf6063fca7677
Mark Gono <markgono@gmail.com>
2018-11-19 16:44:36 +0000

    edit billingaddress - remove unreachable statement, remove billingaddress

--- src/src/routes/Checkout/Components/BillingAddress/BillingAddress.js
+++ src/src/routes/Checkout/Components/BillingAddress/BillingAddress.js
@@ -9,7 +9,6 @@ import Address from './AddressContainer'
 export class BillingAddress extends React.PureComponent {
   static propTypes = {
     deliveryAddress: PropTypes.object,
-    billingAddress: PropTypes.object,
     formValues: PropTypes.object,
     sectionName: PropTypes.string,
     form: PropTypes.string,
@@ -27,14 +26,14 @@ export class BillingAddress extends React.PureComponent {
   }
 
   render() {
-    const { formValues, sectionName, billingAddress, deliveryAddress, asyncValidate, form, receiveRef, scrollToFirstMatchingRef } = this.props
+    const { formValues, sectionName, deliveryAddress, asyncValidate, form, receiveRef, scrollToFirstMatchingRef } = this.props
     const isBillingAddressDifferent = formValues && formValues[sectionName] && formValues[sectionName].isBillingAddressDifferent
 
     // TODO: Tidy up conditions
     return (
       <div>
         <p>Billing Address</p>
-        {!isBillingAddressDifferent && <p className={css.textSMWithBottomMargin}>{showAddress(!isBillingAddressDifferent ? deliveryAddress : billingAddress)}</p>}
+        {!isBillingAddressDifferent && <p className={css.textSMWithBottomMargin}>{showAddress(deliveryAddress)}</p>}
         <p>
           <span
             data-testing="checkout_payment_toggle"
