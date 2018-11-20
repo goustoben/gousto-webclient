731025228e57a53a094b742834496c6efd94dce3
Mark Gono <markgono@gmail.com>
2018-11-20 21:30:17 +0000

    remove comments

--- src/src/routes/Checkout/Components/CheckoutPayment/form.js
+++ src/src/routes/Checkout/Components/CheckoutPayment/form.js
@@ -2,7 +2,6 @@ import { addPrefix } from 'validations/util'
 import deliveryRules from 'validations/delivery'
 import { validationRules } from 'validations/card-checkout'
 
-// TODO: Duplicate code - only rules are different
 export const getValidationRules = (sectionName) => (
   (formValues) => {
     let rules = addPrefix(sectionName, validationRules)
