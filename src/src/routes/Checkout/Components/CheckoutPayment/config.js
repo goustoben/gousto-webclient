f0de79eb5d4d3284d04017c6b013fb97c02e7306
Ana Pasparan <ana.pasparan@gousto.co.uk>
2018-11-26 09:34:26 +0000

    use env pk for checkout

--- src/src/routes/Checkout/Components/CheckoutPayment/config.js
+++ src/src/routes/Checkout/Components/CheckoutPayment/config.js
@@ -1,4 +1,4 @@
 export const formName = 'checkout'
 export const sectionName = 'payment'
 export const deliveryAddressSectionName = 'delivery'
-export const publicKey = 'pk_test_756717b0-978f-4e01-84f8-055732e25cba'
+export const publicKey = process.env.npm_config_gousto2frontend_checkoutcom_pk || ''
