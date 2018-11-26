510283838222af3f2383dc3b8a7cc24b2a381cde
Ana Pasparan <ana.pasparan@gousto.co.uk>
2018-11-26 11:45:11 +0000

    add env variable for checkout

--- src/src/routes/Checkout/Components/CheckoutPayment/config.js
+++ src/src/routes/Checkout/Components/CheckoutPayment/config.js
@@ -1,4 +1,5 @@
 export const formName = 'checkout'
 export const sectionName = 'payment'
 export const deliveryAddressSectionName = 'delivery'
-export const publicKey = process.env.npm_config_gousto2frontend_checkoutcom_pk || ''
+
+export const publicKey = __CHECKOUT_PK__ || ''
