404fe0210a093aa42fe42f9cf2aeda1abe5d4f37
Ellie <Chrome@mg515.home>
2018-12-05 14:43:03 +0000

    Added error class to dropdown

--- src/src/components/Form/Dropdown/DropdownInput.js
+++ src/src/components/Form/Dropdown/DropdownInput.js
@@ -1,131 +1,168 @@
 import React from 'react'
-import css from './DropdownInput.css'
-import globalCss from './reactSelect.css' // eslint-disable-line no-unused-vars
+import PropTypes from 'prop-types'
 import Select from 'react-select'
 import classNames from 'classnames'
 import restrictProps from 'utils/restrictProps'
 import InputWrapper from 'Form/InputWrapper'
+import formsCss from 'styles/forms.css'
+import globalCss from './reactSelect.css' // eslint-disable-line no-unused-vars
+import css from './DropdownInput.css'
 
 export class DropdownInput extends React.Component {
 
-	static propTypes = {
-	  additionalProps: React.PropTypes.object,
-	  options: React.PropTypes.array,
-	  onChange: React.PropTypes.func,
-	  color: React.PropTypes.oneOf(['primary', 'secondary']),
-	  value: React.PropTypes.any,
-	  subLabelClassName: React.PropTypes.string,
-	  labelClassName: React.PropTypes.string,
-	  required: React.PropTypes.bool,
-	  className: React.PropTypes.string,
-	  uppercase: React.PropTypes.bool,
-	  dataTesting: React.PropTypes.string,
-	}
+  static propTypes = {
+    additionalProps: PropTypes.object,
+    options: PropTypes.array,
+    onChange: PropTypes.func,
+    color: PropTypes.oneOf(['primary', 'secondary']),
+    value: PropTypes.any,
+    subLabelClassName: PropTypes.string,
+    labelClassName: PropTypes.string,
+    required: PropTypes.bool,
+    className: PropTypes.string,
+    uppercase: PropTypes.bool,
+    dataTesting: PropTypes.string,
+    error: PropTypes.bool,
+  }
 
-	static defaultProps = {
-	  additionalProps: {},
-	  options: [],
-	  required: false,
-	  color: 'primary',
-	  uppercase: false,
-	}
+  static defaultProps = {
+    additionalProps: {},
+    options: [],
+    required: false,
+    color: 'primary',
+    uppercase: false,
+    error: 'false'
+  }
 
-	handleChange = (obj) => {
-	  if (this.props.onChange) {
-	    this.props.onChange(JSON.parse(obj.value))
-	  }
-	}
+  handleChange = (obj) => {
+    const { onChange } = this.props
 
-	nativeChanged = (e) => {
-	  this.handleChange(e.target)
-	}
+    if (onChange) {
+      onChange(JSON.parse(obj.value))
+    }
+  }
 
-	mapToSelect = (options) => (
-	  options.map((option) => ({
-	    value: JSON.stringify(option.value),
-	    label: (
-				<span className={classNames(this.props.labelClassName, (option.labelClassName) ? option.labelClassName : '', { [css.disabled]: option.disabled })}>
-					{option.label}
-					<span className={classNames(css.subLabel, this.props.subLabelClassName)}> {option.subLabel}</span>
-					{option.icon ? <span className={css[`icon-${option.icon}`]}></span> : null}
-				</span>),
-	    disabled: option.disabled,
-	  }))
-	)
+  nativeChanged = (e) => {
+    this.handleChange(e.target)
+  }
 
-	mapToNative = (options) => (
-	  options.map(
-	    (option) => (
-				<option key={JSON.stringify(option.value)} value={JSON.stringify(option.value)} disabled={option.disabled}>
-					{option.label} {(typeof option.subLabel === 'string') ? option.subLabel : ''}
-				</option>)
-	  )
-	)
+  mapToSelect = (options) => {
+    const { labelClassName, subLabelClassName } = this.props
 
-	renderNative = (options) => (
-		<div>
-			<select
-			  {...this.props.additionalProps}
-			  className={classNames(css.native, css[this.props.color], this.props.uppercase && css.selectuppercase)}
-			  onChange={this.nativeChanged}
-			  value={JSON.stringify(this.props.value)}
-			  required={this.props.required}
-			  data-testing={this.props.dataTesting}
-			>
-				{this.mapToNative(options)}
-			</select>
-			<span className={classNames(css.nativeSelectArrow, css[this.props.color])}></span>
-		</div>
-	)
+    return (
+      options.map((option) => ({
+        value: JSON.stringify(option.value),
+        label: (
+          <span className={classNames(labelClassName, (option.labelClassName) ? option.labelClassName : '', { [css.disabled]: option.disabled })}>
+            {option.label}
+            <span className={classNames(css.subLabel, subLabelClassName)}> {option.subLabel}</span>
+            {option.icon ? <span className={css[`icon-${option.icon}`]}></span> : null}
+          </span>),
+        disabled: option.disabled,
+      }))
+    )
+  }
 
-	renderSelect = (options, props) => (
-		<div className={classNames(css.select, css[this.props.color], this.props.uppercase && css.selectuppercase)}>
-			<Select
-			  {...this.props.additionalProps}
-			  options={this.mapToSelect(options)}
-			  onChange={this.handleChange}
-			  value={JSON.stringify(this.props.value)}
-			  required={this.props.required}
-			  {...props}
-			  data-testing={this.props.dataTesting}
-			/>
-		</div>
-	)
+  mapToNative = (options) => (
+    options.map(
+      (option) => (
+        <option key={JSON.stringify(option.value)} value={JSON.stringify(option.value)} disabled={option.disabled}>
+          {option.label} {(typeof option.subLabel === 'string') ? option.subLabel : ''}
+        </option>)
+    )
+  )
 
-	render = () => {
-	  const defaultProps = {
-	    clearable: false,
-	    searchable: false,
-	    placeholder: '',
-	  }
-	  const ourProps = [
-	    'options',
-	    'onChange',
-	    'className',
-	    'value',
-	    'required',
-	    'color',
-	    'defaultValue',
-	    'subLabelClassName',
-	  ]
+  renderNative = (options ) => {
+    const {error, uppercase, additionalProps, value, required, dataTesting, color } = this.props
 
-	  const selectProps = restrictProps(this.props, ourProps, defaultProps)
-	  const defaultOptions = []
+    const className = classNames(css.native, {
+      [css.primary]: !error && color == 'primary',
+      [css.secondary]: !error && color == 'secondary',
+      [formsCss.inputError]: error,
+      [css.selectuppercase] : uppercase,
+    })
 
-	  selectProps.className = this.props.className ? classNames(this.props.className, css.dropdown) : css.dropdown
+    return (
+      <div>
+        <select
+          {...additionalProps}
+          className={className}
+          onChange={this.nativeChanged}
+          value={JSON.stringify(value)}
+          required={required}
+          data-testing={dataTesting}
+        >
+          {this.mapToNative(options)}
+        </select>
+        <span className={classNames(css.nativeSelectArrow, css[color])}></span>
+      </div>
+    )
+  }
 
-	  return (
-			<div className={css.container}>
-				<span className={css.mobileOnly}>
-					{this.renderNative(this.props.options || defaultOptions)}
-				</span>
-				<span className={css.mobileHide}>
-					{this.renderSelect(this.props.options || defaultOptions, selectProps)}
-				</span>
-			</div>
-	  )
-	}
+  renderSelect = (options, props) => {
+    const {error, uppercase, additionalProps, value, required, dataTesting, color } = this.props
 
+    const className = classNames(css.select, {
+      [css.primary]: !error && color == 'primary',
+      [css.secondary]: !error && color == 'secondary',
+      [css.error]: error,
+      [css.selectuppercase] : uppercase,
+    })
+
+    return (
+      <div>
+        <Select
+          {...additionalProps}
+          className={className}
+          options={this.mapToSelect(options)}
+          onChange={this.handleChange}
+          value={JSON.stringify(value)}
+          required={required}
+          {...props}
+          data-testing={dataTesting}
+        />
+      </div>
+    )
+  }
+
+  render = () => {
+    const defaultProps = {
+      clearable: false,
+      searchable: false,
+      placeholder: '',
+    }
+    const ourProps = [
+      'options',
+      'onChange',
+      'className',
+      'value',
+      'required',
+      'color',
+      'defaultValue',
+      'subLabelClassName',
+      'error'
+    ]
+
+    const { options, className } = this.props
+
+    const selectProps = restrictProps(this.props, ourProps, defaultProps)
+    const defaultOptions = []
+
+    selectProps.className = classNames(className, css.dropdown, {
+      [formsCss.inputError]: true
+    })
+
+    return (
+      <div className={css.container}>
+        <span className={css.mobileOnly}>
+          {this.renderNative(options || defaultOptions)}
+        </span>
+        <span className={css.mobileHide}>
+          {this.renderSelect(options || defaultOptions, selectProps)}
+        </span>
+      </div>
+    )
+  }
 }
 
 export default InputWrapper(DropdownInput)
