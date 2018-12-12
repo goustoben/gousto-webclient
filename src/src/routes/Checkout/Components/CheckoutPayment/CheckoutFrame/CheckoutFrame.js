cce0ae6ab7357fa39edd004a997a24149782019b
Jan Stehlik <jan.stehlik@gousto.co.uk>
2018-12-12 15:09:24 +0000

    reverted check

--- src/src/routes/Checkout/Components/CheckoutPayment/CheckoutFrame/CheckoutFrame.js
+++ src/src/routes/Checkout/Components/CheckoutPayment/CheckoutFrame/CheckoutFrame.js
@@ -71,7 +71,7 @@ class CheckoutFrame extends React.Component {
       Frames.unblockFields()
     }
 
-    if (hasPropUpdated(isSubmitCardEnabled, prevProps.isSubmitCardEnabled)){
+    if (hasPropUpdated(isSubmitCardEnabled, prevProps.isSubmitCardEnabled) && isSubmitCardEnabled){
       this.submitCard()
     }
 
