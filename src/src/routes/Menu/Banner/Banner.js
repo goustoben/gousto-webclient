44bb15eba3a02b632c0ef99d9aaadbe3b2d90295
Mark Gono <markgono@gmail.com>
2019-01-07 10:08:32 +0000

    remove menu christmas banner component

--- src/src/routes/Menu/Banner/Banner.js
+++ src/src/routes/Menu/Banner/Banner.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import moment from 'moment'
 
 import { JoeWicksBanner } from '../JoeWicksBanner'
-import { ChristmasBanner } from '../ChristmasBanner'
 
-export const Banner = ({ switchoverDate }) => {
-  const now = moment()
-  const switchoverTime = moment(switchoverDate)
-
-  return (now.isSameOrAfter(switchoverTime, 'day')) ? (
-    <JoeWicksBanner />
-  ) : (
-    <ChristmasBanner />
-  )
-}
+export const Banner = () => (
+  <JoeWicksBanner />
+)
--- src/src/routes/Menu/ChristmasBanner/index.js
+++
@@ -1,1 +0,0 @@
-export { ChristmasBanner } from './ChristmasBanner'
