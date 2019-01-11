930886685c46aa70f2d9541e731310b1857371de
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-01-11 11:30:22 +0000

    change component type for HowItWorks logic

--- src/src/routes/Account/Referral/HowItWorks/HowItWorks.logic.js
+++ src/src/routes/Account/Referral/HowItWorks/HowItWorks.logic.js
@@ -1,25 +1,23 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Immutable from 'immutable'
 import { replaceLinkToTermsAndCondition } from 'utils/sanitizeText'
 import { rafTermsLink } from '../config'
 import { HowItWorksPresentation } from './HowItWorks.presentation'
 
-class HowItWorks extends PureComponent {
-
-  render() {
-    const { details } = this.props
-    const sanitizeLastStep = replaceLinkToTermsAndCondition(details.last(), rafTermsLink)
-    const detailsSteps = details.pop().push(sanitizeLastStep)
-  
-    return (
-      <HowItWorksPresentation detailsSteps={detailsSteps} />
-    )
-  }
-}
-
-HowItWorks.propTypes = {
+const propTypes = {
   details: PropTypes.instanceOf(Immutable.List),
 }
+
+const HowItWorks = ({ details }) => {
+  const sanitizeLastStep = replaceLinkToTermsAndCondition(details.last(), rafTermsLink)
+  const detailsSteps = details.pop().push(sanitizeLastStep)
+
+  return (
+    <HowItWorksPresentation detailsSteps={detailsSteps} />
+  )
+}
+
+HowItWorks.propTypes = propTypes
 
 export { HowItWorks }
