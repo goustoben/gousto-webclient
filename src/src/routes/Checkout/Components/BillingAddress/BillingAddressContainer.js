8a134929accc7bb9f8c3d14d5688f50cbb81242b
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-01-15 12:34:12 +0000

    add form name for mobile

--- src/src/routes/Checkout/Components/BillingAddress/BillingAddressContainer.js
+++ src/src/routes/Checkout/Components/BillingAddress/BillingAddressContainer.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux'
 import { change, getFormValues } from 'redux-form'
 
+import { getDeliveryFormName } from 'selectors/checkout'
 import { form, sectionName } from './config'
 
 import { BillingAddress } from './BillingAddress'
 
-const getDeliveryAddress = (state) => (
-  state.form.delivery ? state.form.delivery.values.delivery : {}
-)
+const getDeliveryAddress = (state) => {
+  const deliveryFormName = getDeliveryFormName(state)
+  const address = state.form[deliveryFormName] ? state.form[deliveryFormName].values.delivery : {}
+  
+  return address
+}
 
 const mapStateToProps = (state) => {
   const formValues = getFormValues(form)(state)
