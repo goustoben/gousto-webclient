c11d424df10eebeaa10545dc473a1d87c0897828
Ellie <Chrome@MG515.local>
2019-01-23 16:29:00 +0000

    Changed cooking time icon and fixed linting warnings

--- src/src/components/Recipe/CookingTime/CookingTime.js
+++ src/src/components/Recipe/CookingTime/CookingTime.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import css from './CookingTime.css'
+import PropTypes from 'prop-types'
+import Svg from 'Svg'
+
 import { getCookingTime } from 'utils/recipe'
+import css from './CookingTime.css'
+
+const blankSpace = String.fromCharCode(160)
 
 const CookingTime = ({ time }) => (
-	<div>
-		<span className={css.icon}></span><span className={css.description}>&nbsp;Takes {getCookingTime(time)}</span>
-	</div>
+  <div>
+    <Svg
+      fileName="icon-time"
+      className={css.icon}
+    />
+    <span className={css.description}>{blankSpace}Takes {getCookingTime(time)}</span>
+  </div>
 )
 
 CookingTime.propTypes = {
-  time: React.PropTypes.number.isRequired,
+  time: PropTypes.number.isRequired,
 }
 
 export default CookingTime
