9afff591763cda6ef15a85241fcfc296c42187c6
Michele Riso <michele.riso@gousto.co.uk>
2019-01-24 09:48:26 +0000

    Changing the way PM2 start as a different user

--- images/nodejs/nodejs-entrypoint.sh
+++ images/nodejs/nodejs-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pm2 startup -u $(id -u ${USER}) # I need to run pm2 as the runtime user (root or 1001)
+pm2 startup -u 1001 # I need to run pm2 as the runtime user (root or 1001)
 echo "AFTER STARTUP"
 pm2 start process-docker.json --no-daemon
