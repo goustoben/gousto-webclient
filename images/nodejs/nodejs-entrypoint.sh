2bfd3b30c1023843dbc53f84fc111bc647bd3634
Michele Riso <michele.riso@gousto.co.uk>
2019-01-24 14:49:49 +0000

    Nginx configuration to run as different users

--- images/nodejs/nodejs-entrypoint.sh
+++ images/nodejs/nodejs-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 pm2 startup -u 1001 # I need to run pm2 as the runtime user (root or 1001)
-echo "AFTER STARTUP"
+chown -R 1001:1001 /.pm2
+chown -R 1001:1001 /var/log/pm2
 pm2 start process-docker.json --no-daemon
