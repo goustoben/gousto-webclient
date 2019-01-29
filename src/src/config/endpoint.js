67287719fee96dff2cbb2f7e23f7144b4c7fe19f
Alex Kelly <alex.kelly@MG641.local>
2019-01-29 14:25:42 +0000

    WIP - added back local env and endpoint files pushed changes by mistake

--- src/src/config/endpoint.js
+++ src/src/config/endpoint.js
@@ -1,9 +1,53 @@
 function endpoint(service, version = '') {
-  if (service === 'core') {
-    return 'https://staging-api.gousto.info'
+  let protocol
+  let domain
+
+  if (service === 'webclient') {
+    protocol = __CLIENT_PROTOCOL__
+    domain = `${__ENV__}-${service}.${__DOMAIN__}`
+    if (__SERVER__) {
+      protocol = 'http'
+    } else if (__ENV__ === 'production') {
+      domain = `www.${__DOMAIN__}`
+    }
+  } else {
+    if (__ENV__ !== 'local') {
+      if (__SERVER__) {
+        protocol = 'http'
+        domain = `${__ENV__}-${service}`
+        domain += `.${__DOMAIN__}`
+      } else {
+        protocol = __CLIENT_PROTOCOL__
+        domain = `${__ENV__}-api.${__DOMAIN__}`
+        if (service !== 'core') {
+          domain += `/${service}`
+        }
+        if (version) {
+          domain += `/${version}`
+        }
+      }
+    } else {
+      protocol = 'http'
+
+      if (service === 'core') {
+        if (__CLIENT__) {
+          domain = `api.${__DOMAIN__}`
+        } else {
+          domain = `api.${__DOMAIN__}:80`
+        }
+      } else {
+        if (__CLIENT__) {
+          domain = `api.${__DOMAIN__}/${service}/${version}`
+        } else {
+          domain = `api.${__DOMAIN__}:80/${service}/${version}`
+        }
+      }
+    }
   }
 
-  return `https://staging-api.gousto.info/${service}/${version}`
+  const url = `${protocol}://${domain}`
+
+  return url
 }
 
 export default endpoint
