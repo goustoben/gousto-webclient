9384c050e434bb50b851c24476e987e62fd6cf31
Sofiane Sadi <sofiane.sadi@gousto.co.uk>
2019-01-30 17:26:53 +0000

    Remove image moved to devbox

--- images/nodejs/nodejs-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pm2 startup -u 1001 # I need to run pm2 as the runtime user (root or 1001)
-chown -R 1001:1001 /.pm2
-chown -R 1001:1001 /var/log/pm2
-pm2 start process-docker.json --no-daemon
