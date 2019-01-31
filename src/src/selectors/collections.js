d8ba42bd1eeb8c6fe3db9d786c0996240f3d5c68
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-01-31 16:56:28 +0000

    TECH-10103 add tracking for details screen

--- src/src/selectors/collections.js
+++ src/src/selectors/collections.js
@@ -1,1 +1,12 @@
+import { getCurrentCollectionId } from './filters'
+import { getMenuRecipes } from './root'
+
 export const hasJustForYouCollection = ({ menuCollections }) => menuCollections.some(collection => collection.get('slug') === 'recommendations')
+
+export const getRecipePosition = (state, recipeId) => {
+  const menuCollectionRecipes = getMenuRecipes(state)
+  const currentCollection = getCurrentCollectionId(state)
+  const indexOfRecipe = menuCollectionRecipes.get(currentCollection) && menuCollectionRecipes.get(currentCollection).indexOf(recipeId)
+
+  return (indexOfRecipe+1) || null
+}
