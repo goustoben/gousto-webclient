a6b97dacc7ff32bd184a308de23e816d49a01d2d
Alex Kelly <alex.kelly@MG641.local>
2019-02-01 17:51:05 +0000

    creating tests for the ticket

--- src/__tests__/reducers/features.test.js
+++ src/__tests__/reducers/features.test.js
@@ -50,6 +50,10 @@ describe('features reducers', () => {
           experiment: false,
           value: false,
         },
+        unavailableSlots: {
+          experiment: false,
+          value: [],
+        },
       })
       const result = features.features(state, action)
       expect(Immutable.is(expected, result)).toEqual(true)
@@ -90,6 +94,9 @@ describe('features reducers', () => {
         queueIt: {
           experiment: false,
           value: false,
+        },unavailableSlots: {
+          experiment: false,
+          value: [],
         },
       })
       const result = features.features(state, action)
@@ -139,6 +146,9 @@ describe('features reducers', () => {
         queueIt: {
           experiment: false,
           value: false,
+        },unavailableSlots: {
+          experiment: false,
+          value: [],
         },
       })
       const result = features.features(state, action)
