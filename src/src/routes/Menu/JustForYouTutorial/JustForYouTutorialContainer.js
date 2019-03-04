ccb2dbf112dcba54deae61dd072e1ee9972df530
Mark Gono <markgono@gmail.com>
2019-03-04 10:06:37 +0000

    edit jfy tutorial container - remove console log

--- src/src/routes/Menu/JustForYouTutorial/JustForYouTutorialContainer.js
+++ src/src/routes/Menu/JustForYouTutorial/JustForYouTutorialContainer.js
@@ -4,13 +4,9 @@ import { incrementTutorialViewed } from 'actions/tutorial'
 
 import { JustForYouTutorial } from './JustForYouTutorial'
 
-const mapStateToProps = (state) => {
-  console.log('TCL: mapStateToProps -> state.tutorial.getIn([visibile justforyou], false)', state.tutorial.getIn(['visible', 'justforyou'], false)) //eslint-disable-line
-  
-  return {
-    showTutorial: state.tutorial && state.tutorial.getIn(['visible', 'justforyou'], false),
-  }
-}
+const mapStateToProps = (state) => ({
+  showTutorial: state.tutorial && state.tutorial.getIn(['visible', 'justforyou'], false),
+})
 
 const mapDispatchToProps = {
   incrementTutorialViewed,
