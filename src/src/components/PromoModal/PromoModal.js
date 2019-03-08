53f3beeacf8164d7ea7202f54bc71f627c132c1b
Ellie <Chrome@MG515.local>
2019-03-08 13:50:28 +0000

    Updates the promo banner to force close modal

--- src/src/components/PromoModal/PromoModal.js
+++ src/src/components/PromoModal/PromoModal.js
@@ -1,30 +1,44 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import Overlay from 'Overlay'
 import ModalPanel from 'Modal/ModalPanel'
 import { Button } from 'goustouicomponents'
 import PromoModalBody from './PromoModalBody'
 import css from './PromoModal.css'
 import AgeVerify from './AgeVerify'
 
-const PromoModal = ({ text, title, error, buttonText, promoApply, close, needsAgeVerification, isAgeVerified, pending, justApplied }) => (
-	<ModalPanel closePortal={(error || justApplied) ? close : promoApply} closePortalFromButton={(error || justApplied) ? close : promoApply} disableOverlay>
-		<div className={css.body} data-testing="promoModal">
-			<PromoModalBody title={title} text={text} error={error} />
-			<span className={css.buttonContainer}>
-				{needsAgeVerification && !error ? <AgeVerify /> : null}
-				<Button
-				  className={css.buttonSegment}
-				  data-testing="promoModalButton"
-				  disabled={(needsAgeVerification && !isAgeVerified && !error) || pending}
-				  onClick={(error || justApplied) ? close : promoApply}
-				  pending={pending}
-				>
-					{buttonText}
-				</Button>
-			</span>
-		</div>
-	</ModalPanel>
-)
+class PromoModal extends React.Component {
+
+  handleClick = () => {
+    const { error, promoApply, justApplied } = this.props
+
+    return (error || justApplied) ? Overlay.forceCloseAll() : promoApply()
+  }
+
+  render() {
+    const { text, title, error, buttonText, needsAgeVerification, isAgeVerified, pending } = this.props
+
+    return (
+      <ModalPanel closePortal={() => this.handleClick()} closePortalFromButton={() => this.handleClick()} disableOverlay>
+        <div className={css.body} data-testing="promoModal">
+          <PromoModalBody title={title} text={text} error={error} />
+          <span className={css.buttonContainer}>
+            {needsAgeVerification && !error ? <AgeVerify /> : null}
+            <Button
+              className={css.buttonSegment}
+              data-testing="promoModalButton"
+              disabled={(needsAgeVerification && !isAgeVerified && !error) || pending}
+              onClick={() => this.handleClick()}
+              pending={pending}
+            >
+              {buttonText}
+            </Button>
+          </span>
+        </div>
+      </ModalPanel>
+    )
+  }
+}
 
 PromoModal.propTypes = {
   text: PropTypes.string,
