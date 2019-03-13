54ec3033b5d1f2f916701a02ca7ef5ed392f951a
Alex Kelly <alex.kelly@gousto.co.uk>
2019-03-13 17:20:13 +0000

    Refactors the logic

--- src/src/routes/OrderConfirmation/OrderConfirmationContainer.js
+++ src/src/routes/OrderConfirmation/OrderConfirmationContainer.js
@@ -4,15 +4,13 @@ import { orderDetails } from 'actions/order'
 import { locationQuery } from 'selectors/routing.js'
 import OrderConfirmation from './OrderConfirmation'
 
-const mapStateToProps = (state, ownProps) => {
-  const { orderId } = ownProps.params
+const mapStateToProps = (state) => {
   const order = state.basket.get('orderDetails')
   const locationQueryParam = locationQuery(state)
 
   const showHeader = state.temp.get('showHeader') || (locationQueryParam && locationQueryParam['order_action'])
 
   return ({
-    orderNumber: orderId,
     showHeader,
     order,
   })
--- src/src/routes/OrderConfirmation/helper.js
+++ src/src/routes/OrderConfirmation/helper.js
@@ -3,8 +3,8 @@ import moment from 'moment'
 export const getHeaderDetails = (order) => {
   const { humanDeliveryDate, whenCutoff } = order
   const { deliveryStart, deliveryEnd } = order.deliverySlot
-  const deliveryStartFormat = moment(deliveryStart, 'HH:mm').format('h a')
-  const deliveryEndFormat = moment(deliveryEnd, 'HH:mm').format('h a')
+  const deliveryStartFormat = moment(deliveryStart, 'HH:mm:ss').format('h a')
+  const deliveryEndFormat = moment(deliveryEnd, 'HH:mm:ss').add(1, 'seconds').format('h a')
   const cutOffTimeFormat = moment(whenCutoff).add(1, 'seconds').format('h a')
   const cutoffDayFormat = moment(whenCutoff).format('dddd Do MMMM')
 
