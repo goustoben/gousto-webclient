572071e4bbb6a234c5dcc88c91e501acc2c11427
Mark Gono <markgono@gmail.com>
2019-03-13 12:13:13 +0000

    edit subscription action - dispatch basketNumPortions change if box data exists

--- src/src/actions/subscription.js
+++ src/src/actions/subscription.js
@@ -1,20 +1,27 @@
 /* eslint no-use-before-define: ["error", { "functions": false }] */
 import { fetchSubscription } from 'apis/subscription'
+import { basketNumPortionChange } from './basket'
 import actionTypes from './actionTypes'
 
-const subActions = {
-  subscriptionLoadData,
-}
-
-export function subscriptionLoadData() {
-  return async (dispatch, getState) => {
+export const subscriptionLoadData = () => (
+  async (dispatch, getState) => {
     const accessToken = getState().auth.get('accessToken')
     const { data = {} } = await fetchSubscription(accessToken)
+    const { box } = data
+
     dispatch({
       type: actionTypes.SUBSCRIPTION_LOAD_DATA,
       data,
     })
+
+    if (data && box && box.numPortions) {
+      dispatch(basketNumPortionChange(box.numPortions))
+    }
   }
+)
+
+export const subActions = {
+  subscriptionLoadData,
 }
 
 export default subActions
