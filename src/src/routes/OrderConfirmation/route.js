d0b4bf75d3db1c1e9ac7d3c4246c3dba53a97970
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-03-20 10:54:06 +0000

    parseInt the param

--- src/src/routes/OrderConfirmation/route.js
+++ src/src/routes/OrderConfirmation/route.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route } from 'react-router'
-import DOMPurify from 'dompurify'
 import config from 'config/routes'
 import { checkValidSession } from 'utils/routes'
 import { orderDetails } from 'actions/orderConfirmation'
@@ -11,7 +10,7 @@ export default (store) => {
   const onEnterHandler = (routes, replace, next) => {
     const redirectTo = config.client.orderConfirmation
     const path = store.getState().routing.locationBeforeTransitions.pathname
-    const orderId = DOMPurify.sanitize(path.split('/order-confirmation/')[1])
+    const orderId = parseInt(path.split('/order-confirmation/')[1])
     store.dispatch(orderDetails(orderId))
 
     checkValidSession(store, redirectTo)(routes, replace, next)
