1f04f7f2be2ac03a6aa4afc5bb3a8dba0b26ffcb
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-03-20 09:12:00 +0000

    sanitise order id param

--- src/src/routes/OrderConfirmation/route.js
+++ src/src/routes/OrderConfirmation/route.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router'
+import DOMPurify from 'dompurify'
 import config from 'config/routes'
 import { checkValidSession } from 'utils/routes'
 import { orderDetails } from 'actions/orderConfirmation'
@@ -10,7 +11,7 @@ export default (store) => {
   const onEnterHandler = (routes, replace, next) => {
     const redirectTo = config.client.orderConfirmation
     const path = store.getState().routing.locationBeforeTransitions.pathname
-    const orderId = path.split('/order-confirmation/')[1]
+    const orderId = DOMPurify.sanitize(path.split('/order-confirmation/')[1])
     store.dispatch(orderDetails(orderId))
 
     checkValidSession(store, redirectTo)(routes, replace, next)
