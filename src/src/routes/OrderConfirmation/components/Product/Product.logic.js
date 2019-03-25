a915a4dd37092e9f94949d2a58660ad44d58ebdd
Alex Kelly <alex.kelly@gousto.co.uk>
2019-03-25 17:15:39 +0000

    Updates product logic to show pop ups for age verification

--- src/src/routes/OrderConfirmation/components/Product/Product.logic.js
+++ src/src/routes/OrderConfirmation/components/Product/Product.logic.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import { AgeVerificationPopUp } from 'Product/AgeVerification/AgeVerificationPopUp'
 import { ProductPresentation } from './Product.presentation'
+import css from './Product.css'
 
-const Product = (props) => {
-  const { images } = props
-  const imgSource = images && images['400']['src']
+class Product extends PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      showAgeVerification: false
+    }
+  }
+  closePopUp = () => {
+    this.setState({
+      showAgeVerification: false
+    })
+  }
 
-  return(
-    <ProductPresentation {...props} imgSource={imgSource} />
-  )
+  onAddProduct = (productId) => {
+    const { ageVerified, ageRestricted } = this.props
+    const ageVerificationRequired = !ageVerified && ageRestricted
+
+    if(ageVerificationRequired) {
+      this.setState({
+        showAgeVerification: true
+      })
+    }
+  }
+
+  render() {
+    const { images, ageVerified, ageRestricted } = this.props
+    const imgSource = images && images['400']['src']
+    const ageVerificationRequired = !ageVerified && ageRestricted
+    const { showAgeVerification } = this.state
+
+    return(
+      <div className={css.productCardContainer}>
+        <ProductPresentation onAdd={this.onAddProduct} {...this.props} imgSource={imgSource} ageVerificationRequired={ageVerificationRequired} />
+        <AgeVerificationPopUp visible={showAgeVerification} close={this.closePopUp} />
+      </div>
+    )
+  }
 }
 
 export { Product }
