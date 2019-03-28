e22bd0dfbfc9ac4c30894e5f2ade08aec0f9ad9a
Ellie <Chrome@MG515.local>
2019-03-28 14:54:52 +0000

    Updates product actions tests

--- src/src/actions/__tests__/products.test.js
+++ src/src/actions/__tests__/products.test.js
@@ -1,27 +1,34 @@
 import Immutable from 'immutable'
+import actionTypes from 'actions/actionTypes'
+import { fetchProducts } from 'apis/products'
 import productActions from '../products'
 import statusActions from '../status'
-import actionTypes from 'actions/actionTypes'
 
 const { productsLoadProducts} = productActions
 
 jest.mock('apis/products', () => ({
-  fetchProducts: jest.fn().mockReturnValue(
-    Promise.resolve({ data: ['1', '2'] })
-  )}))
+  fetchProducts: jest.fn()
+}))
 
 describe('productsLoadProducts', () => {
-  let fetchProductsMock0
   let dispatchSpy
   let getStateSpy
   const statusPendingSpy = jest.spyOn(statusActions, 'pending')
   const statusErrorSpy = jest.spyOn(statusActions, 'error')
 
   beforeEach(() => {
+    fetchProducts.mockReturnValue(Promise.resolve(
+      { data: [
+        {id:'1', isForSale: true},
+        {id:'2', isForSale: true}
+      ]}
+    ))
+
     dispatchSpy = jest.fn()
     getStateSpy = () => ({
       auth: Immutable.Map({ accessToken: 'access-token' }),
       products: Immutable.OrderedMap({}),
+      productsStock: Immutable.OrderedMap({1:1000, 2:1000}),
     })
   })
 
@@ -29,70 +36,68 @@ describe('productsLoadProducts', () => {
     jest.clearAllMocks()
   })
 
-  test('should dispatch status "pending" true for PRODUCTS_RECEIVE action before fetching products', async function() {
+  test('should dispatch status "pending" true for PRODUCTS_RECEIVE action before fetching products', async () => {
     await productsLoadProducts()(dispatchSpy, getStateSpy)
 
-    expect(statusPendingSpy).toHaveBeenCalledWith(actionTypes.PRODUCTS_RECEIVE, true)
+    expect(statusPendingSpy.mock.calls[0]).toEqual([actionTypes.PRODUCTS_RECEIVE, true])
   })
 
-  // test('should dispatch status "pending" false for PRODUCTS_RECEIVE action after fetching products', async function() {
-  //   await productActions.productsLoadProducts()(dispatchSpy, getStateSpy)
+  test('should dispatch status "pending" false for PRODUCTS_RECEIVE action after fetching products', async () => {
+    await productsLoadProducts()(dispatchSpy, getStateSpy)
 
-  //   expect(statusPendingSpy.args[1]).to.deep.equal([actionTypes.PRODUCTS_RECEIVE, false])
-  // })
+    expect(statusPendingSpy.mock.calls[1]).toEqual([actionTypes.PRODUCTS_RECEIVE, false])
+  })
 
-  // test('should dispatch status "error" true for PRODUCTS_RECEIVE action if an error occurs while fetching products', async function() {
-  //   fetchProductsMock.returns(new Promise(function() { throw new Error('error!') }))
+  test('should dispatch status "error" true for PRODUCTS_RECEIVE action if an error occurs while fetching products', async () => {
+    fetchProducts.mockReturnValue(Promise.reject( new Error('error!') ))
 
-  //   productActions = require('inject-loader?apis/products&./status&!actions/products')({
-  //     'apis/products': { fetchProducts: fetchProductsMock },
-  //     './status': { pending: statusPendingSpy, error: statusErrorSpy },
-  //   }).default
+    await productsLoadProducts()(dispatchSpy, getStateSpy)
 
-  //   await productActions.productsLoadProducts()(dispatchSpy, getStateSpy)
+    expect(statusErrorSpy.mock.calls[0]).toEqual([actionTypes.PRODUCTS_RECEIVE, new Error('error!').message])
+  })
 
-  //   expect(statusErrorSpy.args[0]).to.deep.equal([actionTypes.PRODUCTS_RECEIVE, new Error('error!').message])
-  // })
+  test('should fetch in stock products by default if none have been fetched', async () => {
+    await productsLoadProducts()(dispatchSpy, getStateSpy)
 
-  // test('should fetch products by default if none have been fetched', async function() {
-  //   await productActions.productsLoadProducts()(dispatchSpy, getStateSpy)
+    const dispatchSpyCalls = dispatchSpy.mock.calls[1]
+    const cutoffDate = undefined
+    expect(dispatchSpyCalls[0]).toEqual({
+      type: actionTypes.PRODUCTS_RECEIVE,
+      products: [
+        {id:'1', isForSale: true},
+        {id:'2', isForSale: true}
+      ],
+      cutoffDate: cutoffDate,
+    })
+    expect(fetchProducts).toHaveBeenCalledWith('access-token', cutoffDate)
+  })
 
-  //   const dispatchSpyCalls = dispatchSpy.args[1]
-  //   expect(dispatchSpyCalls[0]).to.deep.equal({
-  //     type: actionTypes.PRODUCTS_RECEIVE,
-  //     products: ['1', '2'],
-  //     cutoffDate: undefined,
-  //   })
+  test('should not fetch products by default if there are products in product store & no cutoffDate is passed in', async () => {
+    getStateSpy = () => ({
+      auth: Immutable.fromJS({ accessToken: 'accessToken' }),
+      products: Immutable.fromJS({
+        1: { id: '1', title: 'Title 1' },
+      }),
+    })
 
-  //   const fetchProductsMockCalls = fetchProductsMock.args[0]
-  //   expect(fetchProductsMockCalls[0]).to.equal('accessToken')
-  // })
+    await productsLoadProducts()(dispatchSpy, getStateSpy)
 
-  // test('should not fetch products by default if there are products in product store & no cutoffDate is passed in', async function() {
-  //   getStateSpy.returns({
-  //     auth: Immutable.fromJS({ accessToken: 'accessToken' }),
-  //     products: Immutable.fromJS({
-  //       1: { id: '1', title: 'Title 1' },
-  //     }),
-  //   })
+    expect(fetchProducts).not.toHaveBeenCalled()
+  })
 
-  //   await productActions.productsLoadProducts()(dispatchSpy, getStateSpy)
+  test('should fetch products for given cutoff date when cutoffDate is passed in', async () => {
+    await productsLoadProducts('whenCutoff timestamp')(dispatchSpy, getStateSpy)
 
-  //   expect(fetchProductsMock).to.not.have.been.called
-  // })
+    const dispatchSpyCalls = dispatchSpy.mock.calls[1]
+    expect(dispatchSpyCalls[0]).toEqual({
+      type: actionTypes.PRODUCTS_RECEIVE,
+      products: [
+        {id:'1', isForSale: true},
+        {id:'2', isForSale: true}
+      ],
+      cutoffDate: 'whenCutoff timestamp',
+    })
 
-  // test('should fetch products for given cutoff date when cutoffDate is passed in', async function() {
-  //   await productActions.productsLoadProducts('whenCutoff timestamp')(dispatchSpy, getStateSpy)
-
-  //   const dispatchSpyCalls = dispatchSpy.args[1]
-  //   expect(dispatchSpyCalls[0]).to.deep.equal({
-  //     type: actionTypes.PRODUCTS_RECEIVE,
-  //     products: ['1', '2'],
-  //     cutoffDate: 'whenCutoff timestamp',
-  //   })
-
-  //   const fetchProductsMockCalls = fetchProductsMock.args[0]
-  //   expect(fetchProductsMockCalls[0]).to.equal('accessToken')
-  //   expect(fetchProductsMockCalls[1]).to.equal('whenCutoff timestamp')
-  // })
+    expect(fetchProducts).toHaveBeenCalledWith('access-token','whenCutoff timestamp')
+  })
 })
