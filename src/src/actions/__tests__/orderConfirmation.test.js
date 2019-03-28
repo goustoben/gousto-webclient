126932a65e8e2d72532de1fb944c8d835d0de159
Ellie <ellie.crome@gousto.co.uk>
2019-03-28 15:51:33 +0000

    Removes mockImplementationOnce from tests

--- src/src/actions/__tests__/orderConfirmation.test.js
+++ src/src/actions/__tests__/orderConfirmation.test.js
@@ -1,5 +1,5 @@
 import Immutable from 'immutable'
-import orderApi from 'apis/orders'
+import { fetchOrder } from 'apis/orders'
 import { orderDetails } from '../orderConfirmation'
 import productActions from '../products'
 
@@ -9,7 +9,9 @@ jest.mock('../products', () => ({
   productsLoadCategories: jest.fn()
 }))
 
-jest.mock('apis/orders')
+jest.mock('apis/orders', () => ({
+  fetchOrder: jest.fn()
+}))
 
 describe('orderDetails', () => {
   const dispatchSpy = jest.fn()
@@ -21,27 +23,28 @@ describe('orderDetails', () => {
     jest.clearAllMocks()
   })
   test('should get the order details for the orderId given', async () => {
-    await orderApi.fetchOrder.mockImplementationOnce(jest.fn().mockReturnValueOnce(
-      new Promise((resolve, reject) => { resolve({ data: { id: '1234', whenCutOff: '2019-04-12 19:00:00' } }) })
-    ))
-    const fetchOrderSpy = jest.spyOn(orderApi, 'fetchOrder')
+    fetchOrder.mockReturnValue(
+      Promise.resolve({ data: { id: '1234', whenCutOff: '2019-04-12 19:00:00' } })
+    )
+
     await orderDetails('1234')(dispatchSpy, getStateSpy)
-    expect(fetchOrderSpy).toHaveBeenCalled()
+    expect(fetchOrder).toHaveBeenCalled()
   })
 
   test('should fetch the products for the given cutoff date', async () => {
-    await orderApi.fetchOrder.mockImplementationOnce(jest.fn().mockReturnValueOnce(
-      new Promise((resolve, reject) => { resolve({ data: { id: '1234', whenCutOff: '2019-04-12 19:00:00' } }) })
-    ))
+    fetchOrder.mockReturnValue(
+      Promise.resolve({ data: { id: '1234', whenCutOff: '2019-04-12 19:00:00' } })
+    )
+
     const productsLoadProductsSpy = jest.spyOn(productActions, 'productsLoadProducts')
     await orderDetails('1234')(dispatchSpy, getStateSpy)
     expect(productsLoadProductsSpy).toHaveBeenCalledWith('2019-04-12 19:00:00')
   })
 
   test('should fetch the products for the given cutoff date', async () => {
-    await orderApi.fetchOrder.mockImplementationOnce(jest.fn().mockReturnValueOnce(
-      new Promise((resolve, reject) => { reject(reject) })
-    ))
+    fetchOrder.mockReturnValue(
+      Promise.reject(new Error('error'))
+    )
     const productsLoadProductsSpy = jest.spyOn(productActions, 'productsLoadProducts')
     await orderDetails('1234')(dispatchSpy, getStateSpy)
     expect(productsLoadProductsSpy).not.toHaveBeenCalled()
