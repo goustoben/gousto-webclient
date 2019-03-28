74b55eec9c9b3cc5739e450718a701020805ef20
Alex Kelly <alex.kelly@gousto.co.uk>
2019-03-28 17:23:42 +0000

    Adds test for basketOrderItemsLoadSpy

--- src/src/actions/__tests__/orderConfirmation.test.js
+++ src/src/actions/__tests__/orderConfirmation.test.js
@@ -2,6 +2,7 @@ import Immutable from 'immutable'
 import { fetchOrder } from 'apis/orders'
 import { orderDetails } from '../orderConfirmation'
 import productActions from '../products'
+import basketActions from '../basket'
 
 jest.mock('../products', () => ({
   productsLoadProducts: jest.fn(),
@@ -11,6 +12,10 @@ jest.mock('../products', () => ({
 
 jest.mock('apis/orders', () => ({
   fetchOrder: jest.fn()
+}))
+
+jest.mock('../basket', () => ({
+  basketOrderItemsLoad: jest.fn()
 }))
 
 describe('orderDetails', () => {
@@ -41,7 +46,22 @@ describe('orderDetails', () => {
     expect(productsLoadProductsSpy).toHaveBeenCalledWith('2019-04-12 19:00:00')
   })
 
-  test('should fetch the products for the given cutoff date', async () => {
+  test('should fetch the basket order items for the given order', async () => {
+    fetchOrder.mockReturnValue(
+      Promise.resolve({ data: { id: '1234', whenCutOff: '2019-04-12 19:00:00' } })
+    )
+
+    const order = Immutable.Map ({
+      "id": "1234", 
+      "whenCutOff": "2019-04-12 19:00:00"
+    })
+    
+    const basketOrderItemsLoadSpy = jest.spyOn(basketActions, 'basketOrderItemsLoad')
+    await orderDetails('1234')(dispatchSpy, getStateSpy)
+    expect(basketOrderItemsLoadSpy).toHaveBeenCalledWith('1234', order)
+  })
+
+  test('should not fetch the products if order details are not retrieved', async () => {
     fetchOrder.mockReturnValue(
       Promise.reject(new Error('error'))
     )
