e0340d21c120cd31e6a7e8462a367e18fdfc2b98
Alex Kelly <alex.kelly@gousto.co.uk>
2019-04-01 15:37:45 +0100

    Moves product object to config file and refactors tests

--- src/src/routes/OrderConfirmation/components/Product/__tests__/Product.test.js
+++ src/src/routes/OrderConfirmation/components/Product/__tests__/Product.test.js
@@ -2,35 +2,15 @@ import React from 'react'
 import Immutable from 'immutable'
 import { mount } from 'enzyme'
 import { Product } from '..'
+import { mockProduct } from './config'
 
 describe('Product component', () => {
   let wrapper
 
   describe('render', () => {
     beforeEach(() => {
-      const product = {
-        "id": "0009468c-33e9-11e7-b485-02859a19531d",
-        "title": "Borsao Macabeo",
-        "description": "A flavoursome Summer wine made from the indigenous Macabeo grape in northern Spain. A balanced, modern white with flavours of ripe peach, zesty lemon and nutty undertones, it leaves the palate with a clean and fruity finish.",
-        "listPrice": "6.95",
-        "isForSale": true,
-        "ageRestricted": true,
-        "boxLimit": 2,
-        "images": {
-          "400": {
-            "src": "https://production-media.gousto.co.uk/cms/product-image-landscape/YAddOns-WhiteWines-Borsao_013244-x400.jpg",
-            "url": "https://production-media.gousto.co.uk/cms/product-image-landscape/YAddOns-WhiteWines-Borsao_013244-x400.jpg",
-            "width": 400
-          },
-        }
-      }
-      const basket = Immutable.fromJS({
-        products: {
-          "0009468c-33e9-11e7-b485-02859a19531d": 1,
-        }
-      })
-
-      wrapper = mount(<Product product={product} basket={basket} ageVerified />)
+      
+      wrapper = mount(<Product product={mockProduct} ageVerified />)
     })
     
     test('should render image of product', () => {
@@ -52,33 +32,13 @@ describe('Product component', () => {
 
   describe('add/remove product', () => {
     beforeEach(() => {
-      const product = {
-        "id": "0009468c-33e9-11e7-b485-02859a19531d",
-        "title": "Borsao Macabeo",
-        "description": "A flavoursome Summer wine made from the indigenous Macabeo grape in northern Spain. A balanced, modern white with flavours of ripe peach, zesty lemon and nutty undertones, it leaves the palate with a clean and fruity finish.",
-        "listPrice": "6.95",
-        "isForSale": true,
-        "ageRestricted": true,
-        "boxLimit": 2,
-        "images": {
-          "400": {
-            "src": "https://production-media.gousto.co.uk/cms/product-image-landscape/YAddOns-WhiteWines-Borsao_013244-x400.jpg",
-            "url": "https://production-media.gousto.co.uk/cms/product-image-landscape/YAddOns-WhiteWines-Borsao_013244-x400.jpg",
-            "width": 400
-          },
-        },
-      }
-      const basket = Immutable.fromJS({
-        products: {}
-      })
-
-      wrapper = mount(<Product product={product} basket={basket} ageVerified limitReached={false} basketProductAdd={jest.fn()} basketProductRemove={jest.fn()} />)
+      wrapper = mount(<Product product={mockProduct} ageVerified limitReached={false} basketProductAdd={jest.fn()} basketProductRemove={jest.fn()} />)
     })
     test('should change the qty of the product', async() => {
       wrapper.setProps({
         basket: Immutable.fromJS({
           products: {
-            "0009468c-33e9-11e7-b485-02859a19531d": 1,
+            "1234": 1,
           }
         })
       })
