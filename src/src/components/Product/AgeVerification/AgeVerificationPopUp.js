0075dc0d61d258fc8c59a640cb01968725552dda
Ellie <ellie.crome@gousto.co.uk>
2019-04-05 09:55:58 +0100

    Updates the age verification pop up for underage

--- src/src/components/Product/AgeVerification/AgeVerificationPopUp.js
+++ src/src/components/Product/AgeVerification/AgeVerificationPopUp.js
@@ -1,47 +1,65 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Modal, { ModalTitle, ModalContent, ModalFooter } from 'ModalComponent'
 import ModalPanel from 'Modal/ModalPanel'
 import { Button } from 'goustouicomponents'
 import css from './AgeVerification.css'
+class AgeVerificationPopUp extends PureComponent {
 
-const AgeVerificationPopUp = ({isVisible, onClose, ageVerified}) => {
-  return (
+  renderFooter = () => {
+    const { onAgeConfirmation, isOver18 } = this.props
+
+    if(isOver18) {
+      return (
+      <ModalFooter className={css.ageVerificationFooter}>
+        <Button fill={false}>
+          Close
+        </Button>
+      </ModalFooter>
+      )
+    }
+
+    return (
+    <ModalFooter className={css.ageVerificationFooter}>
+      <Button className={css.noAgeVerificationButton} fill={false} onClick={() => onAgeConfirmation(false)} >
+        No, i&#8242;m under 18
+      </Button>
+      <Button className={css.yesAgeVerificationButton} fill color={'primary'} onClick={() => onAgeConfirmation(true)} >
+        Yes, I&#8242;m over 18
+      </Button>
+    </ModalFooter>)
+  }
+
+  getContent = () => {
+    const { isOver18 } = this.props
+
+    return isOver18 ? 'Sorry, 18 is the minimum legal age required for this item' : ' To add this item to your order, please confirm you are over 18.'
+  }
+
+  render() {
+    const {isVisible, onClose} = this.props
+
+    return (
     <Modal visible={isVisible}>
       <ModalPanel closePortal={onClose} className={css.ageVerificationModal} disableOverlay>
         <ModalTitle className={css.ageVerificationTitle}>
           Over 18?
         </ModalTitle>
         <ModalContent className={css.ageVerificationContent}>
-          To add this item to your order, please confirm you are over 18.
+          {this.getContent()}
         </ModalContent>
-          {ageVerified ? (
-            <ModalFooter className={css.ageVerificationFooter}>
-              <Button fill={false}>
-                Close
-              </Button>
-            </ModalFooter>
-          ) : (
-            <ModalFooter className={css.ageVerificationFooter}>
-              <Button className={css.noAgeVerificationButton} fill={false} >
-                No, i&#8242;m under 18
-              </Button>
-              <Button className={css.yesAgeVerificationButton} fill color={'primary'} >
-                Yes, I&#8242;m over 18
-              </Button>
-            </ModalFooter>
-          )}
+          {this.renderFooter()}
       </ModalPanel>
     </Modal>
-  )
-}
-
-AgeVerificationPopUp.defaultProps = {
-  ageVerified: false,
+    )
+  }
 }
 
 AgeVerificationPopUp.propTypes = {
-  ageVerified: PropTypes.bool,
+  isVisible: PropTypes.bool,
+  isOver18: PropTypes.bool,
+  onClose: PropTypes.func,
+  onAgeConfirmation: PropTypes.func
 }
 
 export { AgeVerificationPopUp }
