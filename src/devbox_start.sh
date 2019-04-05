5906b23bfba339b42cae9bb0d307a43164672b86
Federico Busetti <federico.busetti@gousto.co.uk>
2019-04-05 16:17:38 +0100

    Make dev image not dependant on devbox init containers

---
+++ src/devbox_start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ORIGIN="/home/docker/artifacts"
+DEST="/home/docker/app"
+
+PATHS=(
+    '/dist'
+    '/node_modules'
+    '/public'
+    '/process-docker.json'
+    '/manifest.json'
+)
+
+for VALUE in "${PATHS[@]}" ; do
+    if [[ ! -e "${DEST}${VALUE}" ]]; then
+        cp -R ${ORIGIN}${VALUE} ${DEST}${VALUE}
+    fi
+done
+
+pm2-runtime start process-docker.json
