f696c04224c8a75b391df44b3c76560dc4e17ab4
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-04-05 14:20:32 +0100

    Add propTypes to product list presentation component

--- src/src/routes/OrderConfirmation/components/ProductList/ProductList.presentation.js
+++ src/src/routes/OrderConfirmation/components/ProductList/ProductList.presentation.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Immutable from 'immutable'
+
 import { Product } from '../Product'
 import css from './ProductList.css'
+
+const propTypes = {
+  basket: PropTypes.instanceOf(Immutable.Map),
+  productsCategories: PropTypes.instanceOf(Immutable.Map),
+  products: PropTypes.shape({
+    id: PropTypes.string,
+  }),
+  ageVerified: PropTypes.bool,
+  basketProductAdd: PropTypes.func,
+  basketProductRemove: PropTypes.func,
+  isLimitReached: PropTypes.func,
+}
 
 const ProductListPresentation = ({ products, basket, productsCategories, ageVerified, basketProductAdd, basketProductRemove, isLimitReached }) => (
   <div className={css.productList}>
@@ -24,5 +39,7 @@ const ProductListPresentation = ({ products, basket, productsCategories, ageVeri
     }
   </div>
 )
+
+ProductListPresentation.propTypes = propTypes
 
 export { ProductListPresentation }
