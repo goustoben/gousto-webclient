5bc22927a79267c818b8bfcce37effa73a30c955
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-04-08 13:36:41 +0100

    update test for utils/basket to match jest synax

--- src/__tests__/utils/basket.test.js
+++ src/__tests__/utils/basket.test.js
@@ -1,13 +1,20 @@
-import { expect } from 'enzyme'
 import Immutable from 'immutable' /* eslint-disable new-cap */
-import { basketSum, okRecipes, getProductsQtyInCategory } from 'utils/basket'
+import { basketSum, okRecipes, getProductsQtyInCategory, limitReached, getProductLimitReached } from 'utils/basket'
+import * as basketProductLimits from 'utils/basketProductLimits'
+
+jest.mock('utils/basketProductLimits', () => ({
+  getAllBasketProducts: jest.fn(),
+  getFirstProductCategoryAtLimit: jest.fn(),
+  getProductItemLimitReached: jest.fn(),
+  productsOverallLimitReached: jest.fn(),
+}))
 
 describe('basket utils', function() {
   describe('basketSum', function() {
     it('sum up the number of recipes', function() {
       const basket = Immutable.fromJS({ '1': 1, '22': 1, '23': 2 })
 
-      expect(basketSum(basket)).to.equal(4)
+      expect(basketSum(basket)).toEqual(4)
     })
   })
 
@@ -23,7 +30,7 @@ describe('basket utils', function() {
       const numPortions = 2
 
       const result = okRecipes(recipes, menuRecipes, menuRecipeStock, numPortions)
-      expect(Immutable.is(result, Immutable.Map({ 456: 3 }))).to.be.true
+      expect(Immutable.is(result, Immutable.Map({ 456: 3 }))).toBe(true)
     })
 
     it('should only return recipes that are in the current menu', function() {
@@ -37,7 +44,7 @@ describe('basket utils', function() {
       const numPortions = 2
 
       const result = okRecipes(recipes, menuRecipes, menuRecipeStock, numPortions)
-      expect(Immutable.is(result, Immutable.Map({ 678: 3 }))).to.be.true
+      expect(Immutable.is(result, Immutable.Map({ 678: 3 }))).toBe(true)
     })
 
     it('should only return recipes that are in stock and in the current menu', function() {
@@ -51,7 +58,7 @@ describe('basket utils', function() {
       const numPortions = 2
 
       const result = okRecipes(recipes, menuRecipes, menuRecipeStock, numPortions)
-      expect(Immutable.is(result, Immutable.Map({ 456: 3 }))).to.be.true
+      expect(Immutable.is(result, Immutable.Map({ 456: 3 }))).toBe(true)
     })
 
     it('should handle no stock', function() {
@@ -63,7 +70,7 @@ describe('basket utils', function() {
 
       const result = okRecipes(recipes, menuRecipes, menuRecipeStock, numPortions)
 
-      expect(Immutable.is(result, Immutable.Map({}))).to.be.true
+      expect(Immutable.is(result, Immutable.Map({}))).toBe(true)
     })
 
     it('should account for recipes that I have taken the last stock for', function() {
@@ -77,13 +84,11 @@ describe('basket utils', function() {
       const numPortions = 2
 
       const result = okRecipes(recipes, menuRecipes, menuRecipeStock, numPortions)
-      expect(Immutable.is(result, Immutable.Map({ 456: 3 }))).to.be.true
+      expect(Immutable.is(result, Immutable.Map({ 456: 3 }))).toBe(true)
     })
   })
 
   describe('limitReached', function() {
-    let limitReached
-
     function defaultLimitReached(maxRecipesNum) {
       const recipes = Immutable.Map({ 456: 3, 567: 3, 678: 3 })
       const numPortions = 2
@@ -102,21 +107,8 @@ describe('basket utils', function() {
       return limitReached(basket, menuRecipes, menuRecipeStock, false, maxRecipesNum)
     }
 
-    beforeEach(function() {
-      limitReached = require('inject-loader?config!utils/basket')({
-        config: {
-          basket: {
-            maxRecipesNum: 5,
-          },
-          menu: {
-            stockThreshold: 0,
-          },
-        },
-      }).limitReached
-    })
-
     it('should return true if the number of recipes in the basket is greater than config.basket.maxRecipesNum', function() {
-      expect(defaultLimitReached()).to.be.true
+      expect(defaultLimitReached()).toBe(true)
     })
 
     it('should return false if the number of recipes in the basket is less than config.basket.maxRecipesNum', function() {
@@ -134,11 +126,11 @@ describe('basket utils', function() {
         678: { 2: 3000, 3: 1750 },
       })
       const result = limitReached(basket, menuRecipes, menuRecipeStock)
-      expect(result).to.be.false
+      expect(result).toBe(false)
     })
 
     it('should disregard stock and the menu if the naive argument is true', function() {
-      expect(defaultLimitReached(12)).to.be.false
+      expect(defaultLimitReached(12)).toBe(false)
     })
 
     it('should use new maximum recipe count if maxRecipesNum defined', function() {
@@ -156,7 +148,7 @@ describe('basket utils', function() {
         678: { 2: 3000, 3: 1750 },
       })
       const result = limitReached(basket, menuRecipes, menuRecipeStock, false, 12)
-      expect(result).to.be.false
+      expect(result).toBe(false)
     })
 
   })
@@ -173,14 +165,19 @@ describe('basket utils', function() {
         'product-3': { categories: [{ id: 'cat-3' }, { id: 'cat-4' }] },
       })
 
-      let result = getProductsQtyInCategory('cat-2', basket, products)
-      expect(result).to.equal(1)
+      let result = getProductsQtyInCategory('cat-2', basket, products, false)
+      expect(result).toEqual(1)
 
-      result = getProductsQtyInCategory('cat-3', basket, products)
-      expect(result).to.equal(3)
+      result = getProductsQtyInCategory('cat-3', basket, products, false)
+      expect(result).toEqual(3)
     })
 
     it('should include gift products by default', function() {
+      basketProductLimits.getAllBasketProducts.mockReturnValueOnce(Immutable.fromJS({
+        'product-3': 3,
+        'product-2': 1,
+      }))
+
       const basket = Immutable.fromJS({
         products: { 'product-2': 1, 'product-3': 2 },
         gifts: { 'product-3': 1 },
@@ -191,11 +188,11 @@ describe('basket utils', function() {
         'product-3': { categories: [{ id: 'cat-3' }, { id: 'cat-4' }] },
       })
 
-      let result = getProductsQtyInCategory('cat-2', basket, products)
-      expect(result).to.equal(1)
+      let result = getProductsQtyInCategory('cat-2', basket, products, false)
+      expect(result).toEqual(1)
 
       result = getProductsQtyInCategory('cat-3', basket, products)
-      expect(result).to.equal(4)
+      expect(result).toEqual(4)
     })
 
     it('should NOT include gift products if includeGiftProducts is set to false', function() {
@@ -210,76 +207,76 @@ describe('basket utils', function() {
       })
 
       let result = getProductsQtyInCategory('cat-2', basket, products, false)
-      expect(result).to.equal(1)
+      expect(result).toEqual(1)
 
       result = getProductsQtyInCategory('cat-3', basket, products, false)
-      expect(result).to.equal(3)
+      expect(result).toEqual(3)
     })
   })
 
   describe('getProductLimitReached', function() {
-    it('should return object containing type "box" & value of max products allowed when total products limit reached', function() {
-      const productsOverallLimitReachedSpy = sinon.stub().returns(true)
-      const getProductLimitReached = require('inject-loader?config&utils/basketProductLimits!utils/basket')({
-        config: {
-          basket: {
-            maxProductsNum: 10,
-          },
+    let basket
+    let products
+    let productsCategories
+
+    beforeEach(() => {
+      basket = Immutable.fromJS({
+        products: { 'product-2': 1, 'product-3': 2 },
+        gifts: { 'product-3': 1 },
+      })
+
+      products = Immutable.fromJS({
+        'product-2': { categories: [{ id: 'cat-2' }, { id: 'cat-3' }] },
+        'product-3': { categories: [{ id: 'cat-3' }, { id: 'cat-4' }] },
+      })
+
+      productsCategories = Immutable.fromJS({
+        'cat-2' : { 
+          id: 'cat-2',
+          boxLimit: 2,
         },
-        'utils/basketProductLimits': { productsOverallLimitReached: productsOverallLimitReachedSpy },
-      }).getProductLimitReached
+        'cat-3' : { 
+          id: 'cat-3',
+          boxLimit: 3,
+        }
+      })
+    })
+    it('should return object containing type "box" & value of max products allowed when total products limit reached', function() {
+      basketProductLimits.productsOverallLimitReached.mockReturnValueOnce(true)
+      const productsOverallLimitReachedSpy = jest.spyOn(basketProductLimits , 'productsOverallLimitReached')
 
-      const result = getProductLimitReached('productId', 'basket', 'products', 'productsCategories')
+      const result = getProductLimitReached('product-3', basket, products, productsCategories)
 
-      expect(productsOverallLimitReachedSpy).to.have.been.calledOnce
-      expect(productsOverallLimitReachedSpy.args[0][0]).to.equal('basket')
-      expect(result).to.deep.equal({ type: 'box', value: 10 })
+      expect(productsOverallLimitReachedSpy.mock.calls.length).toBe(1)
+      expect(productsOverallLimitReachedSpy.mock.calls[0][0]).toEqual(basket)
+      expect(result).toEqual({ type: 'box', value: 10 })
     })
 
     it('should return object containing type "item" & value equal to max of the specific item allowed when total products limit reached', function() {
-      const productsOverallLimitReachedSpy = sinon.stub().returns(false)
-      const getProductItemLimitReachedSpy = sinon.stub().returns(3)
-      const getProductLimitReached = require('inject-loader?config&utils/basketProductLimits!utils/basket')({
-        config: {
-          basket: {
-            maxProductsNum: 10,
-          },
-        },
-        'utils/basketProductLimits': {
-          productsOverallLimitReached: productsOverallLimitReachedSpy,
-          getProductItemLimitReached: getProductItemLimitReachedSpy,
-        },
-      }).getProductLimitReached
+      basketProductLimits.productsOverallLimitReached.mockReturnValueOnce(false)
+      basketProductLimits.getProductItemLimitReached.mockReturnValueOnce(3)
+      const getProductItemLimitReachedSpy = jest.spyOn(basketProductLimits, 'getProductItemLimitReached')
 
-      const result = getProductLimitReached('productId', 'basket', 'products', 'productsCategories')
+      const result = getProductLimitReached('product-3', basket, products, productsCategories)
 
-      expect(getProductItemLimitReachedSpy).to.have.been.calledOnce
-      expect(getProductItemLimitReachedSpy.args[0]).to.deep.equal(['productId', 'basket', 'products'])
-      expect(result).to.deep.equal({ type: 'item', value: 3 })
+      expect(getProductItemLimitReachedSpy.mock.calls.length).toEqual(1)
+      expect(getProductItemLimitReachedSpy.mock.calls[0]).toEqual([
+        'product-3', basket, products
+      ])
+      expect(result).toEqual({ type: 'item', value: 3 })
     })
 
     it('should return object containing type "category" & value equal to title of the first category which has reached its limit', function() {
-      const productsOverallLimitReachedSpy = sinon.stub().returns(false)
-      const getProductItemLimitReachedSpy = sinon.stub().returns(false)
-      const getFirstProductCategoryAtLimitSpy = sinon.stub().returns('category name')
-      const getProductLimitReached = require('inject-loader?config&utils/basketProductLimits!utils/basket')({
-        config: {
-          basket: {
-            maxProductsNum: 10,
-          },
-        },
-        'utils/basketProductLimits': {
-          productsOverallLimitReached: productsOverallLimitReachedSpy,
-          getProductItemLimitReached: getProductItemLimitReachedSpy,
-          getFirstProductCategoryAtLimit: getFirstProductCategoryAtLimitSpy,
-        },
-      }).getProductLimitReached
+      basketProductLimits.productsOverallLimitReached.mockReturnValueOnce(false)
+      basketProductLimits.getProductItemLimitReached.mockReturnValueOnce(false)
+      basketProductLimits.getFirstProductCategoryAtLimit.mockReturnValueOnce('category name')
+      const getFirstProductCategoryAtLimitSpy = jest.spyOn(basketProductLimits, 'getFirstProductCategoryAtLimit')
+      
+      const result = getProductLimitReached('product-3', basket, products, productsCategories)
 
-      const result = getProductLimitReached('productId', 'basket', 'products', 'productsCategories')
-
-      expect(getFirstProductCategoryAtLimitSpy).to.have.been.calledOnce
-      expect(getFirstProductCategoryAtLimitSpy.args[0]).to.deep.equal(['productId', 'basket', 'products', 'productsCategories'])
-      expect(result).to.deep.equal({ type: 'category', value: 'category name' })
+      expect(getFirstProductCategoryAtLimitSpy.mock.calls.length).toBe(1)
+      expect(getFirstProductCategoryAtLimitSpy.mock.calls[0]).toEqual(['product-3', basket, products, productsCategories])
+      expect(result).toEqual({ type: 'category', value: 'category name' })
     })
   })
 })
