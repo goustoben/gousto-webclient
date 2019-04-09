f6a5b4f86b15e06383318998011c6dc0ce198735
Safeer Hayat <safeer.hayat@MG648.local>
2019-04-09 17:45:46 +0100

    Moves navbar into orderConfirmation
    Programmatically gets category list
    
    Adds dropdown version of navbar for mobile

--- src/src/routes/OrderConfirmation/OrderConfirmation.js
+++ src/src/routes/OrderConfirmation/OrderConfirmation.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import Immutable from 'immutable'
 import { AgeVerificationPopUp } from 'Product/AgeVerification'
 import Overlay from 'Overlay'
+import { Dropdown } from 'goustouicomponents'
 import css from './OrderConfirmation.css'
 import { OrderConfirmationHeader } from './OrderConfirmationHeader'
 import { ProductList } from './components/ProductList'
+import { Navbar } from './components/Navbar'
 
 const propTypes = {
   showHeader: PropTypes.bool.isRequired,
@@ -36,6 +38,7 @@ const defaultProps = {
   showHeader: false,
   headerDetails: {}
 }
+
 class OrderConfirmation extends PureComponent {
   constructor() {
     super()
@@ -59,6 +62,52 @@ class OrderConfirmation extends PureComponent {
     userVerifyAge(isOver18, true)
   }
 
+  state={
+    selectedCategory: 'All Products'
+  }
+
+  getCategoriesList = () => {
+    const { products } = this.props
+
+    const categoriesList = Object.keys(products).reduce((categoryProducts, nextProduct) => {
+      const productProps = products[nextProduct]
+      const productCategory = productProps.categories[0].title
+      if (!categoryProducts.includes(productCategory)) categoryProducts.push(productProps.categories[0].title)
+  
+      return categoryProducts
+    }, []).map(category => {
+      return {id: category, label: category}
+    })
+
+    categoriesList.unshift({id: 'All Products', label: 'All Products'})
+
+    return categoriesList
+  }
+
+  getChosenCategoryProducts = (chosenCategory) => {
+    const { products, productsFilteredByCategory } = this.props
+    
+    this.setState({
+      selectedCategory: chosenCategory
+    })
+
+    let chosenCategoryProducts
+
+    if (chosenCategory == 'All Products') {
+      chosenCategoryProducts = products
+    } else {
+      chosenCategoryProducts = Object.keys(products).reduce((categoryProducts, productKey) => {
+        const productProps = products[productKey]
+        const productCategory = productProps.categories[0].title
+        chosenCategory == productCategory ? categoryProducts.push(productProps) : null
+    
+        return categoryProducts
+      }, [])
+    }
+    
+    productsFilteredByCategory(chosenCategoryProducts)
+  }
+
   render() {
     const {
       headerDetails,
@@ -70,10 +119,13 @@ class OrderConfirmation extends PureComponent {
       filteredProducts,
       basketProductAdd,
       basketProductRemove,
-      productsFilteredByCategory
+      productsFilteredByCategory,
     } = this.props
     const { showAgeVerification, hasConfirmedAge } = this.state
     const isUnderAge = hasConfirmedAge && !ageVerified
+
+    const { selectedCategory } = this.state
+    const categoriesList = this.getCategoriesList()
 
     return (
       <div>
@@ -85,6 +137,12 @@ class OrderConfirmation extends PureComponent {
         </Overlay>
         <div className={css.marketPlaceWrapper}>
           <h3 className={css.marketPlaceTitle}>The Gousto Market</h3>
+          <div className={css.navbar}>
+            <Navbar items={categoriesList} onClick={this.getChosenCategoryProducts} active={selectedCategory}/>
+          </div>
+          <div className={css.dropdown}>
+            <Dropdown options={categoriesList} groupedOptions={[]} optionSelected={selectedCategory} onChange={this.getChosenCategoryProducts}/>  
+          </div>  
           <section className={css.marketPlaceContent}>
             <ProductList
               products={products}
