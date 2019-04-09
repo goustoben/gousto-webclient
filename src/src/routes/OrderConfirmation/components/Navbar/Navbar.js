2275178e6e62a82c9e3a47110d60d4c23aa147da
Safeer Hayat <safeer.hayat@MG648.local>
2019-04-09 17:48:36 +0100

    Adds styling to navbar
    Changes format of props in navbar to match dropdown

--- src/src/routes/OrderConfirmation/components/Navbar/Navbar.js
+++ src/src/routes/OrderConfirmation/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import classnames from 'classnames'
+import css from './Navbar.css'
 
-const Navbar = ({items, onClick}) => (
-  <ul>
+const Navbar = ({items, onClick, active}) => (
+  <ul className={css.navbarContainer}>
     {items.map(item => (
-      <li key={item} >
-        <button type="button" onClick={onClick}>{item}</button>
+      <li className={css.navbarItems} key={item.id} >
+        <button className={active == item.label ? classnames(css.navbarButton, css.active) : css.navbarButton} type="button" onClick={() => onClick(item.label)}>{item.label}</button>
       </li>
     ))}
   </ul>
