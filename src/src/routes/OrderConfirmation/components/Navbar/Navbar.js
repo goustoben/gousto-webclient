fd649d9b070020af501a3d55a836da2a15e16c97
Safeer Hayat <safeer.hayat@MG648.local>
2019-04-11 14:46:58 +0100

    Adds proptypes to Navbar component

--- src/src/routes/OrderConfirmation/components/Navbar/Navbar.js
+++ src/src/routes/OrderConfirmation/components/Navbar/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import css from './Navbar.css'
 
-const Navbar = ({items, onClick, active}) => (
+const propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  active: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+const Navbar = ({ items, onClick, active }) => (
   <ul className={css.navbarContainer}>
     {items.map(item => (
       <li className={css.navbarItems} key={item.id} >
@@ -11,5 +18,7 @@ const Navbar = ({items, onClick, active}) => (
     ))}
   </ul>
 )
+
+Navbar.propTypes = propTypes
 
 export { Navbar }
