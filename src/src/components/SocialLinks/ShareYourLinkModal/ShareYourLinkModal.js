9997a8796f39ee267abfbb238b15ef193682aa4f
Safeer Hayat <saf_hayat94@hotmail.com>
2019-04-17 15:24:46 +0100

    Updates references to moved files

--- src/src/components/SocialLinks/ShareYourLinkModal/ShareYourLinkModal.js
+++ src/src/components/SocialLinks/ShareYourLinkModal/ShareYourLinkModal.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import ModalPanel from 'Modal/ModalPanel'
 import Svg from 'Svg'
 import actionTypes from 'actions/actionTypes'
-import { UserRAFLink } from '../UserRAFLink'
-import ReferAFriend from '../ReferAFriend'
+import { UserRAFLink } from 'components/UserRAFLink'
+import ReferAFriend from 'components/ReferAFriend'
 import { LinkRow } from './LinkRow'
 import css from './ShareYourLinkModal.css'
-import { getFacebookReferralLink, getWhatsappReferralLink, getTextMessageReferralLink} from '../socialReferralHelper'
+import { getFacebookReferralLink, getWhatsappReferralLink, getTextMessageReferralLink } from '../socialReferralHelper'
 
 class ShareYourLinkModal extends React.PureComponent {
   state = { isEmailFormOpen: false }
@@ -25,7 +25,7 @@ class ShareYourLinkModal extends React.PureComponent {
     const { isEmailFormOpen } = this.state
     !isEmailFormOpen && trackingReferFriendSocialSharing(actionTypes.REFER_FRIEND_LINK_SHARE, 'ReferFriendLink Share', 'Email')
 
-    this.setState(function(prevState) {
+    this.setState(function (prevState) {
       return { isEmailFormOpen: !prevState.isEmailFormOpen }
     })
   }
@@ -35,47 +35,47 @@ class ShareYourLinkModal extends React.PureComponent {
     const { isEmailFormOpen } = this.state
 
     return (
-    <ModalPanel
-      closePortal={onClose}
-      className={css.modal}
-      disableOverlay
-    >
-      <div className={`${css.title} ${css.row}`}>
-        <span>Share Via</span>
-      </div>
-
-      <div className={`${css.row} ${isEmailFormOpen && css.emailFormOpen}`}>
-        <div className={css.emailHeader} onClick={this.toggleEmailModal}>
-          <div className={css.iconWrapper}>
-            <Svg fileName='icon-email-colour' className={css.icon} />
-          </div>
-          <span>E-mail</span>
-        </div>
-
-        <div className={`${css.emailForm} ${!isEmailFormOpen && css.hideContent}`}>
-          { isEmailFormOpen && <ReferAFriend/> }
-        </div>
-      </div>
-
-      <LinkRow onClick={() => { getTextMessageReferralLink(referralCode, userFirstName, rafOffer, trackingReferFriendSocialSharing)}} svgName='icon-text-message-colour' rowName='Text Message'/>
-      <LinkRow onClick={() => { getWhatsappReferralLink(referralCode, userFirstName, rafOffer, trackingReferFriendSocialSharing)}} svgName='icon-whatsapp-colour' rowName='Whatsapp'/>
-      <LinkRow onClick={() => { getFacebookReferralLink(referralCode, userFirstName, trackingReferFriendSocialSharing)}} svgName='icon-facebook-colour' rowName='Facebook' />
-      
-      <UserRAFLink
-        classContainer={css.row}
-        classLinkContainer={css.rafLinkWrapper}
-        referralCode={referralCode}
-        trackingReferFriend={trackingReferFriend}
-        isModal
+      <ModalPanel
+        closePortal={onClose}
+        className={css.modal}
+        disableOverlay
       >
-        <div>
-          <div className={css.iconWrapper}>
-            <Svg fileName='icon-link-colour' className={css.icon} />
-          </div>
-          <span>Copy link</span>
+        <div className={`${css.title} ${css.row}`}>
+          <span>Share Via</span>
         </div>
-      </UserRAFLink>
-    </ModalPanel>
+
+        <div className={`${css.row} ${isEmailFormOpen && css.emailFormOpen}`}>
+          <div className={css.emailHeader} onClick={this.toggleEmailModal}>
+            <div className={css.iconWrapper}>
+              <Svg fileName='icon-email-colour' className={css.icon} />
+            </div>
+            <span>E-mail</span>
+          </div>
+
+          <div className={`${css.emailForm} ${!isEmailFormOpen && css.hideContent}`}>
+            {isEmailFormOpen && <ReferAFriend />}
+          </div>
+        </div>
+
+        <LinkRow onClick={() => { getTextMessageReferralLink(referralCode, userFirstName, rafOffer, trackingReferFriendSocialSharing) }} svgName='icon-text-message-colour' rowName='Text Message' />
+        <LinkRow onClick={() => { getWhatsappReferralLink(referralCode, userFirstName, rafOffer, trackingReferFriendSocialSharing) }} svgName='icon-whatsapp-colour' rowName='Whatsapp' />
+        <LinkRow onClick={() => { getFacebookReferralLink(referralCode, userFirstName, trackingReferFriendSocialSharing) }} svgName='icon-facebook-colour' rowName='Facebook' />
+
+        <UserRAFLink
+          classContainer={css.row}
+          classLinkContainer={css.rafLinkWrapper}
+          referralCode={referralCode}
+          trackingReferFriend={trackingReferFriend}
+          isModal
+        >
+          <div>
+            <div className={css.iconWrapper}>
+              <Svg fileName='icon-link-colour' className={css.icon} />
+            </div>
+            <span>Copy link</span>
+          </div>
+        </UserRAFLink>
+      </ModalPanel>
     )
   }
 
