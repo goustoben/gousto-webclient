d14a6aacf724dacad89c8a90da5a137d1f651689
Alex Kelly <alex.kelly@gousto.co.uk>
2019-04-17 10:28:24 +0100

    Updates tests for banner and gels

--- src/src/routes/Menu/Banner/__tests__/Banner.test.js
+++ src/src/routes/Menu/Banner/__tests__/Banner.test.js
@@ -13,15 +13,21 @@ describe('Banner', () => {
   test('should render a banner with no gels if no imageName and fileName', () => {
     wrapper = shallow(<Banner type={'test-css'}/>)
     expect(wrapper.find('.test-css')).toHaveLength(1)
+    expect(wrapper.find('.gelPortrait')).toHaveLength(0)
+    expect(wrapper.find('.gelText')).toHaveLength(0)
   })
 
   test('should render the banner and the imageName gel only', () => {
     wrapper = shallow(<Banner imageName={'test.jpg'} type={'banner-with-image'}/>)
+    expect(wrapper.find('.banner-with-image')).toHaveLength(1)
     expect(wrapper.find('.gelPortrait')).toHaveLength(1)
+    expect(wrapper.find('.gelText')).toHaveLength(0)
   })
   
   test('should render the banner and both imageName gel and fileName gels', () => {
     wrapper = shallow(<Banner imageName={'test.jpg'} type={'banner-with-gel'} color='red' fileName="gel-text"/>)
+    expect(wrapper.find('.banner-with-gel')).toHaveLength(1)
+    expect(wrapper.find('.gelPortrait')).toHaveLength(1)
     expect(wrapper.find('.gelText')).toHaveLength(1)
   })
 })
