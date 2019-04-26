4c49e14ccfb584cf23324e6ee8aae8ec2b97023d
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-04-26 11:28:22 +0100

    remove unnecessary selector

--- src/__tests__/components/NoResultsPage/NoResultsPage.test.js
+++ src/__tests__/components/NoResultsPage/NoResultsPage.test.js
@@ -7,7 +7,7 @@ describe('NoResultsPage', () => {
 
   test('render NoResultPage', () => {
     wrapper = mount(<NoResultsPage imageName={'0-result'} title={'Title'} description={'Description'} />)
-    expect(wrapper.find('Svg.image').find('div')).toHaveLength(1)
+    expect(wrapper.find('Svg').find('div')).toHaveLength(1)
     expect(wrapper.find('h2').text()).toBe('Title')
     expect(wrapper.find('div.description').text()).toBe('Description')
   })
