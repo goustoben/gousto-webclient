e5ac8212f6686ecae1a1996b515b4d6339bb6ce6
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-05-07 12:55:58 +0100

    remove unnecessary functions from the container

--- src/src/routes/Menu/FilterTag/FilterTagContainer.js
+++ src/src/routes/Menu/FilterTag/FilterTagContainer.js
@@ -1,16 +1,13 @@
 import { connect } from 'react-redux'
 
-import FilterTag from './FilterTag'
 import filterActions from 'actions/filters'
 import actionTypes from 'actions'
+import FilterTag from './FilterTag'
 
 const FilterTagContainer = connect((state) => ({
   isLoading: state.pending.get(actionTypes.MENU_FETCH_DATA, false),
 }), {
-  collectionFilterChange: filterActions.filterCollectionChange,
-  filterCurrentDietTypesChange: filterActions.filterCurrentDietTypesChange,
   filterDietaryAttributesChange: filterActions.filterDietaryAttributesChange,
-  filterCurrentTotalTimeChange: filterActions.filterCurrentTotalTimeChange,
 })(FilterTag)
 
 export default FilterTagContainer
