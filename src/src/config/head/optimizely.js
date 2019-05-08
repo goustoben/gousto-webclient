eed502528de0b1803278d7d9482440b69ed2ce6b
Nico Hemidy <nicolas@gousto.co.uk>
2019-05-08 14:22:26 +0100

    correct env project id

--- src/src/config/head/optimizely.js
+++ src/src/config/head/optimizely.js
@@ -2,7 +2,7 @@ export default {
   local: '7728010866',
   staging: '7728010866',
   radishes: '14571110311',
-  rockets: "14785300772",
+  rockets: '14785300772',
   haricots: '7728010866',
   production: '3732210624',
 }
