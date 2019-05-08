f017eaf4c13bc1842faafa48ffd173f7da31361b
Alex Kelly <alex.kelly@gousto.co.uk>
2019-05-08 14:57:57 +0100

    Adds in stickyness on mobile

--- src/src/routes/Menu/FilterTagsNav/FilterTagsNav.js
+++ src/src/routes/Menu/FilterTagsNav/FilterTagsNav.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { getScrollOffset } from 'utils/menu'
 import FilterTagsList from './FilterTagsList'
 import css from './FilterTagsNav.css'
 
@@ -14,15 +15,64 @@ const propTypes = {
   ),
 }
 
-const FilterTagsNav = ({ tags, menuFilterExperiment }) => (
-  (menuFilterExperiment) ? (
-    <div className={css.filterTagContainer}>
-      <div className={css.filterListContainer}>
-        <FilterTagsList tags={tags} />
-      </div>
-    </div>
-  ) : null
-)
+class FilterTagsNav extends React.PureComponent {
+  state = {
+    scrolledPastPoint: false 
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll)
+
+    this.checkScroll()
+
+    this.intervals = setInterval(() => {
+      this.checkScroll()
+    }, 50)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll)
+
+    if (this.intervals) {
+      clearInterval(this.intervals)
+      this.intervals = null
+    }
+  }
+
+  onScroll = () => {
+    this.hasScrolled = true
+  }
+
+  checkScroll = () => {
+    if (this.hasScrolled) {
+      this.hasScrolled = false
+      const threshold = 250
+      const animationThreshold = 50
+      const { scrolledPastPoint } = this.state
+      
+      const scrollState = getScrollOffset(threshold, animationThreshold, scrolledPastPoint)
+      scrollState && this.setState({
+        scrolledPastPoint: scrollState.scrolledPastPoint,
+        scrollJumped: scrollState.scrollJumped
+      })
+    }
+  }
+
+  render() {
+    const { tags, menuFilterExperiment } = this.props
+    const className = this.state.scrolledPastPoint ? css.filterTagContainerFixed : css.filterTagContainer
+
+    return (
+      (menuFilterExperiment) ? (
+        <div className={className}>
+          <div className={css.filterListContainer}>
+            <FilterTagsList tags={tags} />
+          </div>
+        </div>
+      ) : null
+    )
+  }
+}
 
 FilterTagsNav.propTypes = propTypes
 
