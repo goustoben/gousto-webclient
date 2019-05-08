3531da838b6b8e35a24cfb52ef1b5f8e252d99b4
Safeer Hayat <saf_hayat94@hotmail.com>
2019-05-08 12:14:27 +0100

    Adds tests

---
+++ src/src/components/Recipe/Detail/__tests__/Detail.test.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Immutable from 'immutable'
+import Helmet from 'react-helmet'
+import DefaultDetail from '../DefaultDetail'
+import FineDineInDetail from '../FineDineInDetail'
+import Detail from '../Detail'
+
+describe('Detail', () => {
+  describe('rendering', () => {
+    let wrapper
+    const media = Immutable.List([
+      Immutable.Map({
+        src: 'testurl',
+        width: 700
+      })
+    ])
+    const range = Immutable.Map({
+      name: 'testBrand'
+    })
+
+    beforeEach(() => {
+      wrapper = shallow(<Detail media={media} range={range}/>)
+    })
+
+    it('should render a DefaultDetail by default', () => {
+      expect(wrapper.find(DefaultDetail).length).toEqual(1)
+    })
+
+    it('should have a Helmet component', () => {
+      expect(wrapper.find(Helmet).length).toEqual(1)
+    })
+
+    describe('views', () => {
+      const views = new Map(
+        Object.entries({
+          detail: DefaultDetail,
+          fineDineInDetail: FineDineInDetail,
+        }
+        ))
+
+      it('should render a different component based on view', () => {
+        for (const [view, component] of views.entries()) {
+          wrapper = shallow(<Detail view={view} range={range} />)
+
+          expect(wrapper.find(component).length).toEqual(1)
+        }
+      })
+    })
+
+    it('should render an overlay which calls the menuRecipeDetailVisibilityChange function prop on click', () => {
+      const menuRecipeDetailVisibilityChangeSpy = jest.fn()
+      wrapper = shallow(<Detail menuRecipeDetailVisibilityChange={menuRecipeDetailVisibilityChangeSpy} range={range}/>)
+      wrapper.find('div').at(0).simulate('click')
+
+      expect(menuRecipeDetailVisibilityChangeSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('should get image link', () => {
+      const imageLink = wrapper.instance().getImageLink()
+      expect(imageLink).toEqual('testurl')
+    })
+  })
+})
