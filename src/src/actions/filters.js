d846ad8f150b844717d2222ff2333ff2c3c914ac
Alex Kelly <alex.kelly@gousto.co.uk>
2019-05-09 17:17:11 +0100

    Keeps the selected fcollection when clear the filter

--- src/src/actions/filters.js
+++ src/src/actions/filters.js
@@ -137,8 +137,8 @@ export const filterCurrentTotalTimeChange = (totalTime) => (
 
 export const clearAllFilters = () => (
   (dispatch, getState) => {
-    const defaultCollection = getAllRecipesCollectionId(getState())
-    dispatch(filtersClearAll(defaultCollection))
+    const getSelectedCollection = getState().filters.get('currentCollectionId')
+    dispatch(filtersClearAll(getSelectedCollection))
     dispatch(trackRecipeFiltersCleared())
   }
 )
