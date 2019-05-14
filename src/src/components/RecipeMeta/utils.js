4b890e9eb213017bf08287f03bba08d43dcbc5d7
Safeer Hayat <saf_hayat94@hotmail.com>
2019-05-14 11:46:00 +0100

    Fixes import link for config file

--- src/src/components/RecipeMeta/utils.js
+++ src/src/components/RecipeMeta/utils.js
@@ -2,7 +2,7 @@ import Immutable from 'immutable'
 
 import { getSurcharge } from 'utils/recipe'
 
-import { defaultMetaImageLink, perPortionPrice } from './config.json'
+import { defaultMetaImageLink, perPortionPrice } from './config.js'
 
 export const getPortionPrice = (recipe) => {
   const surcharge = getSurcharge(recipe.get('meals'), 4)
