bc1e1158a5b40f3b4261d898d3e09b4a9ac352d0
Ellie <ellie.crome@gousto.co.uk>
2019-05-14 09:54:07 +0100

    Adds wait and comment to describe bug

--- tests/e2e/tests/flows/order/transactionOrder.js
+++ tests/e2e/tests/flows/order/transactionOrder.js
@@ -52,6 +52,7 @@ module.exports = {
         done()
       })
       .perform(done => {
+        browser.pause(2000) //BUG: checkout button becomes clickable -> loading -> clickable again
         menu.section.menuContainer.goFromMenuToCheckout()
         done()
       })
