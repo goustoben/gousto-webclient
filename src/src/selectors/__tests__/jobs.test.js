a511627599a30966c8ffb24c8938b50d49355696
Chris Vibert <chris.vibert@gousto.co.uk>
2019-05-21 17:46:25 +0100

    Create selector to get all jobs

--- src/src/selectors/__tests__/jobs.test.js
+++ src/src/selectors/__tests__/jobs.test.js
@@ -1,16 +1,62 @@
 import Immutable from 'immutable'
-import { getSelectedJobsDepartment } from 'selectors/jobs'
+import {
+  getSelectedJobsDepartment,
+  getAllJobs
+} from 'selectors/jobs'
 
 describe('jobs selectors', () => {
+  let state
+  let testJobs
 
   describe('the getSelectedJobsDepartment selector', () => {
     test('returns the selected job department', () => {
-      const state = {
+      state = {
         jobsDepartment: Immutable.fromJS({
           selectedDepartment: 'Food'
         })
       }
       expect(getSelectedJobsDepartment(state)).toEqual('Food')
+    })
+  })
+
+  describe('the getAllJobs selector', () => {
+    describe('when jobs exist', () => {
+      beforeEach(() => {
+        testJobs = Immutable.fromJS({
+          'jobId1': {
+            id: 'jobId1',
+            title: 'Test job 1',
+          },
+          'jobId2': {
+            id: 'jobId2',
+            title: 'Test job 2',
+          }
+        })
+
+        state = {
+          jobs: Immutable.fromJS({
+            jobs: testJobs
+          })
+        }
+      })
+
+      test('returns a list of all jobs in the store', () => {
+        expect(getAllJobs(state)).toBe(testJobs)
+      })
+    })
+
+    describe('when no jobs exist', () => {
+      beforeEach(() => {
+        state = {
+          jobs: Immutable.fromJS({
+            jobs: {}
+          })
+        }
+      })
+
+      test('returns an empty map', () => {
+        expect(getAllJobs(state)).toBe(Immutable.Map())
+      })
     })
   })
 })
--- src/src/selectors/jobs.js
+++ src/src/selectors/jobs.js
@@ -1,1 +1,2 @@
 export const getSelectedJobsDepartment = state => state.jobsDepartment.get('selectedDepartment')
+export const getAllJobs = state => state.jobs.get('jobs')
