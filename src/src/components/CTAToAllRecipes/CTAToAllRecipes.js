c028d36c8c5064ffaefcb7bd54a725a181a83406
Alex Kelly <alex.kelly@gousto.co.uk>
2019-05-22 13:38:40 +0100

    Adds in onClick to take to all recipes collection

--- src/src/components/CTAToAllRecipes/CTAToAllRecipes.js
+++ src/src/components/CTAToAllRecipes/CTAToAllRecipes.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Svg from 'Svg'
 import css from './CTAToAllRecipes.css'
 
-const CTAToAllRecipes = () => {
+const CTAToAllRecipes = ({ collectionFilterChange }) => {
   return (
     <section className={css.ctaWrapper}>
       <Svg fileName="cta-recommendation" className={css.panIcon} />
       <p className={css.ctaText}>Want to see more?</p>
-      <button className={css.ctaButton} type="button">Go to all recipes</button>
+      <button className={css.ctaButton} type="button" onClick={collectionFilterChange}>Go to all recipes</button>
     </section>
   )
 }
