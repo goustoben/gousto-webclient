8114877edd628750cdb319555d25c8df7f39eeae
Alex Kelly <alex.kelly@gousto.co.uk>
2019-05-22 14:48:11 +0100

    Adds test to check filter change function works on click

--- src/src/components/CTAToAllRecipes/__tests__/CTAToAllRecipes.test.js
+++ src/src/components/CTAToAllRecipes/__tests__/CTAToAllRecipes.test.js
@@ -7,10 +7,20 @@ describe('CTA to all Recipes presentation', () => {
   let wrapper
 
   test('show image, text and button ', () => {
-    wrapper = shallow(<CTAToAllRecipes/>)
+    wrapper = shallow(<CTAToAllRecipes collectionFilterChange={jest.fn()}/>)
 
     expect(wrapper.find(Svg)).toHaveLength(1)
     expect(wrapper.find('p')).toHaveLength(1)
     expect(wrapper.find('button')).toHaveLength(1)
+  })
+})
+
+describe('CTA logic', () => {
+  test('should call collectionFilterChange on click', () => {
+    const collectionFilterChangeMock = jest.fn()
+    const wrapper = shallow(<CTAToAllRecipes collectionFilterChange={collectionFilterChangeMock}/>)
+
+    wrapper.find('.ctaButton').simulate('click')
+    expect(collectionFilterChangeMock).toHaveBeenCalledTimes(1)
   })
 })
