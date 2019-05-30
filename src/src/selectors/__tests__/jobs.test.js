e948f147f0e6962041c614b5f3f20f17e3205ea5
Chris Vibert <chris.vibert@gousto.co.uk>
2019-05-30 17:21:07 +0100

    Create a selector to get a filtered list of jobs
    A getJobsToDisplay selector has been built using reselect (memoized by default - good for performance) which takes the full list of jobs (from the getAllJobs selector) and the selectedDepartment (from the getSelectedJobsDepartment selector) and filters the list accordingly.

--- src/src/selectors/__tests__/jobs.test.js
+++ src/src/selectors/__tests__/jobs.test.js
@@ -1,7 +1,8 @@
 import Immutable from 'immutable'
 import {
   getSelectedJobsDepartment,
-  getAllJobs
+  getAllJobs,
+  getJobsToDisplay
 } from 'selectors/jobs'
 
 describe('jobs selectors', () => {
@@ -57,6 +58,98 @@ describe('jobs selectors', () => {
       test('returns an empty map', () => {
         expect(getAllJobs(state)).toBe(Immutable.Map())
       })
+    })
+  })
+
+  describe('the getJobsToDisplay selector', () => {
+    beforeEach(() => {
+      testJobs = Immutable.fromJS({
+        'jobId1': {
+          id: 'jobId1',
+          title: 'Test job 1',
+          department: 'Operations',
+        },
+        'jobId2': {
+          id: 'jobId2',
+          title: 'Test job 2',
+          department: 'Tech'
+        },
+        'jobId3': {
+          id: 'jobId3',
+          title: 'Test job 3',
+          department: 'Operations'
+        },
+        'jobId4': {
+          id: 'jobId4',
+          title: 'Test job 4',
+          department: 'Data'
+        },
+        'jobId5': {
+          id: 'jobId5',
+          title: 'Test job 5',
+          department: 'Analytics'
+        },
+      })
+
+      state = {
+        jobsDepartment: Immutable.fromJS({
+          selectedDepartment: 'All'
+        }),
+        jobs: Immutable.fromJS({
+          jobs: testJobs
+        })
+      }
+    })
+
+    test('returns all jobs when the All filter is selected', () => {
+      expect(getJobsToDisplay(state)).toBe(testJobs)
+    })
+
+    test('returns the correct jobs when a filter is selected', () => {
+      const updatedState = {
+        ...state,
+        jobsDepartment: Immutable.fromJS({
+          selectedDepartment: 'Tech'
+        })
+      }
+      expect(getJobsToDisplay(updatedState)).toEqual(Immutable.fromJS({
+        'jobId2': {
+          id: 'jobId2',
+          title: 'Test job 2',
+          department: 'Tech'
+        }
+      }))
+    })
+
+    test('returns the correct jobs when a filter is selected which contains multiple departments according to config/jobs.js', () => {
+      const updatedState = {
+        ...state,
+        jobsDepartment: Immutable.fromJS({
+          selectedDepartment: 'Data'
+        })
+      }
+      expect(getJobsToDisplay(updatedState)).toEqual(Immutable.fromJS({
+        'jobId4': {
+          id: 'jobId4',
+          title: 'Test job 4',
+          department: 'Data'
+        },
+        'jobId5': {
+          id: 'jobId5',
+          title: 'Test job 5',
+          department: 'Analytics'
+        },
+      }))
+    })
+
+    test('returns an empty map when no jobs exist in the selected department', () => {
+      const updatedState = {
+        ...state,
+        jobsDepartment: Immutable.fromJS({
+          selectedDepartment: 'Brand'
+        })
+      }
+      expect(getJobsToDisplay(updatedState)).toBe(Immutable.Map())
     })
   })
 })
