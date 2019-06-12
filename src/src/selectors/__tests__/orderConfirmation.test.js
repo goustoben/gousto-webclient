937826cee94a94eb0e5e47d097d292875e6a2298
Safeer Hayat <saf_hayat94@hotmail.com>
2019-06-12 15:32:12 +0100

    wraps tests in a top level describe

--- src/src/selectors/__tests__/orderConfirmation.test.js
+++ src/src/selectors/__tests__/orderConfirmation.test.js
@@ -1,34 +1,39 @@
 import Immutable from 'immutable'
 import { isOrderConfirmationPageLoaded } from 'selectors/orderConfirmation'
 
-describe('when any pending action is true', () => {
-  const state = {
-    pending: Immutable.Map({
-      'PRODUCT_CATEGORIES_RECEIVE': false,
-      'PRODUCTS_STOCK_CHANGE': false,
-      'PRODUCTS_RECEIVE': true,
-      'USER_LOAD_REFERRAL_OFFER': false,
-      'USER_LOAD_ORDERS': false,
-    })
-  }
+describe('orderConfirmation selector', () => {
 
-  it('should return true', () => {
-    expect(isOrderConfirmationPageLoaded(state)).toEqual(true)
+  describe('when any pending action is true', () => {
+    const state = {
+      pending: Immutable.Map({
+        'PRODUCT_CATEGORIES_RECEIVE': false,
+        'PRODUCTS_STOCK_CHANGE': false,
+        'PRODUCTS_RECEIVE': true,
+        'USER_LOAD_REFERRAL_OFFER': false,
+        'USER_LOAD_ORDERS': false,
+      })
+    }
+
+    it('should return true', () => {
+      expect(isOrderConfirmationPageLoaded(state)).toEqual(true)
+    })
   })
+
+  describe('when all pending actions are false', () => {
+    const state = {
+      pending: Immutable.Map({
+        'PRODUCT_CATEGORIES_RECEIVE': false,
+        'PRODUCTS_STOCK_CHANGE': false,
+        'PRODUCTS_RECEIVE': false,
+        'USER_LOAD_REFERRAL_OFFER': false,
+        'USER_LOAD_ORDERS': false,
+      })
+    }
+
+    it('should return false', () => {
+      expect(isOrderConfirmationPageLoaded(state)).toEqual(false)
+    })
+  })
+  
 })
 
-describe('when all pending actions are false', () => {
-  const state = {
-    pending: Immutable.Map({
-      'PRODUCT_CATEGORIES_RECEIVE': false,
-      'PRODUCTS_STOCK_CHANGE': false,
-      'PRODUCTS_RECEIVE': false,
-      'USER_LOAD_REFERRAL_OFFER': false,
-      'USER_LOAD_ORDERS': false,
-    })
-  }
-
-  it('should return false', () => {
-    expect(isOrderConfirmationPageLoaded(state)).toEqual(false)
-  })
-})
