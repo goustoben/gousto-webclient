aee7af9dc2689f279740e44cfd6a6477f0cb127d
Alex Kelly <alex.kelly@gousto.co.uk>
2019-06-12 11:05:08 +0100

    Adds selector to get the menu recipe ids from the state

--- src/src/selectors/root.js
+++ src/src/selectors/root.js
@@ -2,6 +2,7 @@
 export const getStock = state => state.menuRecipeStock
 export const getBasket = state => state.basket
 export const getRecipes = state => state.recipes
+export const getMenuRecipeIds = state => state.menuRecipes
 export const getMenuRecipes = state => state.menuCollectionRecipes
 export const getMenuCollections = state => state.menuCollections
 export const getMenuCutoffUntil = state => state.menuCutoffUntil
