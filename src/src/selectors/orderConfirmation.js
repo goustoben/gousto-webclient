3bc341e5a58511a1636beded4eac3887ab806e46
Safeer Hayat <saf_hayat94@hotmail.com>
2019-06-12 10:48:49 +0100

    Adds userloadsorders as pending condition

--- src/src/selectors/orderConfirmation.js
+++ src/src/selectors/orderConfirmation.js
@@ -1,5 +1,5 @@
 export const isOrderConfirmationPageLoaded = state => {
-  const orderConfirmationActions = ['PRODUCT_CATEGORIES_RECEIVE', 'PRODUCTS_STOCK_CHANGE', 'PRODUCTS_RECEIVE', 'USER_LOAD_REFERRAL_OFFER']
+  const orderConfirmationActions = ['PRODUCT_CATEGORIES_RECEIVE', 'PRODUCTS_STOCK_CHANGE', 'PRODUCTS_RECEIVE', 'USER_LOAD_REFERRAL_OFFER', 'USER_LOAD_ORDERS']
 
   return state.pending.some((value, key) => {
 
