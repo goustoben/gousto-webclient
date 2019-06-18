44278724a783fcd9c49992e7b80a2043e3efbd40
Alex Kelly <alex.kelly@gousto.co.uk>
2019-06-18 10:19:08 +0100

    Adds in FoodBrandPage component

---
+++ src/src/routes/Menu/FoodBrandPage/FoodBrandPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import css from './FoodBrandPage.css'
+
+class FoodBrandPage extends React.Component {
+  render() {
+
+    return (
+      <div className={css.foodBrandContainer}>Hello</div>
+    )
+  }
+}
+
+export { FoodBrandPage }
---
+++ src/src/routes/Menu/FoodBrandPage/index.js
@@ -0,0 +1,1 @@
+export { FoodBrandPage } from './FoodBrandPage'
