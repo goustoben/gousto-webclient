2b0b78e362e6da55cb67bd14bfa200d557402af0
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-06-19 17:18:02 +0300

    Add some css plus update structure of foodbrand to include title and description

--- src/src/routes/Menu/FoodBrandPage/FoodBrandPage.js
+++ src/src/routes/Menu/FoodBrandPage/FoodBrandPage.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
 import css from './FoodBrandPage.css'
 
-class FoodBrandPage extends React.Component {
+const propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  borderColor: PropTypes.string,
+}
+class FoodBrandPage extends PureComponent {
+  getFoodBrangeColor = (borderColor) => ({
+    borderBottom: `1px solid ${borderColor}`,
+  })
+  
   render() {
+    const { title, description, borderColor } = this.props
 
     return (
-      <div className={css.foodBrandContainer}>Hello</div>
+      <section className={css.foodBrandContainer}>
+        <div style={this.getFoodBrangeColor(borderColor)}>
+          <div className={css.foodBrandTitleContainer}>
+            <span>&#60; Back to All Recipes</span>
+            <h1>{title}</h1>
+          </div>
+          <p className={css.foodBrandDescription}>{description}</p>
+        </div>
+      </section>
     )
   }
 }
+FoodBrandPage.propTypes = propTypes
 
 export { FoodBrandPage }
