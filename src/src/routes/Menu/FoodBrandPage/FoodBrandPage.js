2396cf3454a7b91de52595e1c0fd726460c0bddc
Alex Kelly <alex.kelly@gousto.co.uk>
2019-06-20 17:34:48 +0100

    Updates logic to add in onScroll and fixed elements

--- src/src/routes/Menu/FoodBrandPage/FoodBrandPage.js
+++ src/src/routes/Menu/FoodBrandPage/FoodBrandPage.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { getScrollOffset } from 'utils/menu'
 
 import css from './FoodBrandPage.css'
 
@@ -7,20 +8,66 @@ const propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   borderColor: PropTypes.string,
+  browser: PropTypes.string,
 }
 class FoodBrandPage extends PureComponent {
   getFoodBrangeColor = (borderColor) => ({
-    borderBottom: `1px solid ${borderColor}`,
+    borderBottom: `2px solid ${borderColor}`,
   })
+
+  constructor(props) {
+    super(props)
+    this.state = { scrolledPastPoint: false }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll)
+
+    this.checkScroll()
+
+    this.intervals = setInterval(() => {
+      this.checkScroll()
+    }, 50)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll)
+
+    if (this.intervals) {
+      clearInterval(this.intervals)
+      this.intervals = null
+    }
+  }
+
+  onScroll = () => {
+    this.hasScrolled = true
+  }
+
+  checkScroll = () => {
+    const { browser } = this.props
+    if (this.hasScrolled) {
+      this.hasScrolled = false
+      const threshold = (browser === 'mobile') ? 53 : 89
+      const animationThreshold = 50
+      const { scrolledPastPoint } = this.state
+      const scrollState = getScrollOffset(threshold, animationThreshold, scrolledPastPoint)
+      scrollState && this.setState({
+        scrolledPastPoint: scrollState.scrolledPastPoint,
+      })
+    }
+  }
   
   render() {
     const { title, description, borderColor } = this.props
 
+    const classNameContainer = this.state.scrolledPastPoint ? css.foodBrandContainerFixed : css.foodBrandContainer
+    const classNameTitle = this.state.scrolledPastPoint ? css.foodBrandTitleContainerFixed : css.foodBrandTitleContainer
+
     return (
-      <section className={css.foodBrandContainer}>
+      <section className={classNameContainer}>
         <div style={this.getFoodBrangeColor(borderColor)}>
-          <div className={css.foodBrandTitleContainer}>
-            <span>&#60; Back to All Recipes</span>
+          <div className={classNameTitle}>
+            <span><span className={css.leftArrow}/>Back</span>
             <h1>{title}</h1>
           </div>
           <p className={css.foodBrandDescription}>{description}</p>
