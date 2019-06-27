281cee0199ee94c2cc19ef837ecd4d65c57e00c1
David Torres <david.torres@gousto.co.uk>
2019-06-27 14:53:02 +0100

    [TECH-11713] Make the BoxProgressMessage accept a className prop

--- src/src/routes/Menu/BoxProgressMessage/BoxProgressMessage.js
+++ src/src/routes/Menu/BoxProgressMessage/BoxProgressMessage.js
@@ -2,25 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import config from 'config/basket'
 
-const BoxProgressMessage = ({ numRecipes }) => {
+const BoxProgressMessage = ({ numRecipes, className }) => {
   const { maxRecipesNum, minRecipesNum } = config
 
   if (numRecipes <= 0) {
     return (
-      <p>Add <strong>{maxRecipesNum}</strong> recipes for the best price per serving</p>
+      <p className={className}>Add <strong>{maxRecipesNum}</strong> recipes for the best price per serving</p>
     )
 
   } else if (numRecipes < minRecipesNum) {
-    return <p>Add more recipes to complete your box</p>
+    return <p className={className}>Add more recipes to complete your box</p>
 
   } else if (numRecipes >= maxRecipesNum) {
-    return <p>Nice! You&apos;ve got the best price per serving</p>
+    return <p className={className}>Nice! You&apos;ve got the best price per serving</p>
 
   } else {
     const remainingRecipes = maxRecipesNum - numRecipes
 
     return (
-      <p>Add <strong>{remainingRecipes}</strong> more recipe{remainingRecipes > 1 ? 's' : ''} for the best price per serving</p>
+      <p className={className}>Add <strong>{remainingRecipes}</strong> more recipe{remainingRecipes > 1 ? 's' : ''} for the best price per serving</p>
     )
   }
 }
