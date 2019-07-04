9824936c1d5ac9ffe2d5970606f23709db60fc17
Alex Kelly <alex.kelly@gousto.co.uk>
2019-07-04 09:45:55 +0100

    Test for the getFilteredRecipes when a foodBrand is selected

--- src/src/routes/Menu/selectors/__tests__/filters.test.js
+++ src/src/routes/Menu/selectors/__tests__/filters.test.js
@@ -4,69 +4,193 @@ import { getFilteredRecipeIds, getFilteredRecipes } from 'routes/Menu/selectors/
 describe('filters', () => {
 
   describe('getFilteredRecipes', () => {
-    const state = {
-      filters: Immutable.Map({
-        currentCollectionId: 'ca8f71be',
-        totalTime: '0',
-        dietTypes: Immutable.Set([]),
-        dietaryAttributes: Immutable.Set([]),
-      }),
-      basket: Immutable.fromJS({
-        numPortions: 2,
-      }),
-      recipes: Immutable.fromJS({
-        327: {
-          id: '327',
-          dietType: 'Meat',
-        },
-        1589: {
-          id: '1589',
-          dietType: 'Fish',
-        },
-        393: {
-          id: '393',
-          dietType: 'Vegetarian',
-        },
-        929: {
-          id: '929',
-          dietType: 'Vegan',
-        },
-        1651: {
-          id: '1651',
-          dietType: 'Meat',
-        },
-      }),
-      menuCollectionRecipes: Immutable.fromJS({
-        ca8f71be: ['327', '1589', '393'],
-        '70c28cb0': ['929', '1651'],
-      }),
-      menuCollections: Immutable.fromJS({
-        ca8f71be: {
-          default: true,
-          id: 'ca8f71be',
-        },
-        '70c28cb0': {
-          defult: false,
-          id: '70c28cb0',
-        },
-      }),
-    }
+    describe('getFilteredRecipes without foodBrand', () => {
+      const state = {
+        filters: Immutable.Map({
+          currentCollectionId: 'ca8f71be',
+          totalTime: '0',
+          dietTypes: Immutable.Set([]),
+          dietaryAttributes: Immutable.Set([]),
+          foodBrand: null,
+        }),
+        basket: Immutable.fromJS({
+          numPortions: 2,
+        }),
+        recipes: Immutable.fromJS({
+          327: {
+            id: '327',
+            dietType: 'Meat',
+          },
+          1589: {
+            id: '1589',
+            dietType: 'Fish',
+          },
+          393: {
+            id: '393',
+            dietType: 'Vegetarian',
+          },
+          929: {
+            id: '929',
+            dietType: 'Vegan',
+          },
+          1651: {
+            id: '1651',
+            dietType: 'Meat',
+          },
+        }),
+        menuCollectionRecipes: Immutable.fromJS({
+          ca8f71be: ['327', '1589', '393'],
+          '70c28cb0': ['929', '1651'],
+        }),
+        menuCollections: Immutable.fromJS({
+          ca8f71be: {
+            default: true,
+            id: 'ca8f71be',
+          },
+          '70c28cb0': {
+            defult: false,
+            id: '70c28cb0',
+          },
+        }),
+        menuRecipes: Immutable.fromJS(['327', '1589', '393', '929', '1651'])
+      }
 
-    test('get all filtered recipe for current collection', () => {
-      expect(getFilteredRecipes(state)).toEqual(Immutable.List([
-        Immutable.Map({
-          id: '327',
-          dietType: 'Meat',
+      test('get all filtered recipe for current collection', () => {
+        expect(getFilteredRecipes(state)).toEqual(Immutable.List([
+          Immutable.Map({
+            id: '327',
+            dietType: 'Meat',
+          }),
+          Immutable.Map({
+            id: '1589',
+            dietType: 'Fish',
+          }),
+          Immutable.Map({
+            id: '393',
+            dietType: 'Vegetarian',
+          }),
+        ]))
+      })
+    })
+
+    describe('getFilteredRecipes with foodBrand', () => {
+      const state = {
+        filters: Immutable.Map({
+          currentCollectionId: 'ca8f71be',
+          totalTime: '0',
+          dietTypes: Immutable.Set([]),
+          dietaryAttributes: Immutable.Set([]),
+          foodBrand: {
+            name: "Takeaway Night",
+            slug: "takeaway-night",
+            borderColor: "#17B7BF",
+          },
         }),
-        Immutable.Map({
-          id: '1589',
-          dietType: 'Fish',
+        basket: Immutable.fromJS({
+          numPortions: 2,
         }),
-        Immutable.Map({
-          id: '393',
-          dietType: 'Vegetarian',
+        recipes: Immutable.fromJS({
+          327: {
+            id: '327',
+            dietType: 'Meat',
+            taxonomy: [
+              {},
+              {
+                id: 2,
+                name: "Food Brands",
+                slug: "food-brands",
+                tags: [
+                  {
+                    id: "9",
+                    name: "Takeaway Night",
+                    slug: "takeaway-night",
+                    properties: {
+                      ribbon_color: "#000000",
+                      border_color: "#17B7BF",
+                      text_color: "#FFFFFF",
+                    },
+                  }
+                ]}]},
+          1589: {
+            id: '1589',
+            dietType: 'Fish',
+            taxonomy: [
+              {},
+              {
+                id: 8,
+                name: "Food Brands",
+                slug: "food-brands",
+                tags: [
+                  {
+                    id: "4",
+                    name: "Modern Bistro",
+                    slug: "modern-bistro",
+                    properties: {
+                      ribbon_color: "#000000",
+                      border_color: "#17B7BF",
+                      text_color: "#FFFFFF",
+                    },
+                  }
+                ]}]
+          },
+          393: {
+            id: '393',
+            dietType: 'Vegetarian',
+          },
+          929: {
+            id: '929',
+            dietType: 'Vegan',
+          },
+          1651: {
+            id: '1651',
+            dietType: 'Meat',
+          },
         }),
-      ]))
+        menuCollectionRecipes: Immutable.fromJS({
+          ca8f71be: ['327', '1589', '393'],
+          '70c28cb0': ['929', '1651'],
+        }),
+        menuCollections: Immutable.fromJS({
+          ca8f71be: {
+            default: true,
+            id: 'ca8f71be',
+          },
+          '70c28cb0': {
+            defult: false,
+            id: '70c28cb0',
+          },
+        }),
+        menuRecipes: Immutable.fromJS(['327', '1589', '393', '929', '1651'])
+      }
+
+      test('should return food brand recipes if a food brand filter is present', () => {
+        expect(getFilteredRecipes(state)).toEqual(Immutable.List([
+          Immutable.Map({
+            id: '327',
+            dietType: 'Meat',
+            taxonomy: Immutable.fromJS([
+              {},
+              {
+                id: 2,
+                name: "Food Brands",
+                slug: "food-brands",
+                tags: [
+                  {
+                    id: "9",
+                    name: "Takeaway Night",
+                    slug: "takeaway-night",
+                    properties: {
+                      ribbon_color: "#000000",
+                      border_color: "#17B7BF",
+                      text_color: "#FFFFFF",
+                    },
+                  }
+                ]
+              }
+            ])
+          }),
+        ]))
+      })
     })
   })
 
