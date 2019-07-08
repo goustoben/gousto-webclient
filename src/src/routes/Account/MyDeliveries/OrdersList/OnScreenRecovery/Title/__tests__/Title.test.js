dfae93aa487ed908e13cce7ee0d1546e8404ca8a
Ellie <ellie.crome@gousto.co.uk>
2019-07-08 10:40:52 +0100

    Makes OSR title component presentational

--- src/src/routes/Account/MyDeliveries/OrdersList/OnScreenRecovery/Title/__tests__/Title.test.js
+++ src/src/routes/Account/MyDeliveries/OrdersList/OnScreenRecovery/Title/__tests__/Title.test.js
@@ -1,45 +1,14 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import renderer from 'react-test-renderer'
-
-import Title from '../Title'
+import { Title } from '../Title'
 
 describe('Order Skip Recovery Model Title', () => {
   let wrapper
 
-  describe('Innitial Render', () => {
-    test('should render snapshot', () => {
-      const tree = renderer.create(
-        <Title />
-      ).toJSON()
+  test('should render title', () => {
+    wrapper = mount(<Title title="Are you sure you want to skip?"/>)
 
-      expect(tree).toMatchSnapshot()
-    })
-
-    test('should render title', () => {
-      wrapper = mount(<Title />)
-
-      expect(wrapper.text()).toBe('Are you sure you want to skip?')
-    })
+    expect(wrapper.text()).toBe('Are you sure you want to skip?')
   })
 
-  describe('Alternative Render', () => {
-    test('should render title to say "cancel" instead of "skip"', () => {
-      wrapper = mount(<Title orderType="pending" />)
-
-      expect(wrapper.text()).toBe('Are you sure you want to cancel?')
-    })
-
-    test('should render custom title', () => {
-      wrapper = mount(<Title title="custom title" />)
-
-      expect(wrapper.text()).toBe('custom title')
-    })
-
-    test('should prioritise title over orderType', () => {
-      wrapper = mount(<Title title="foo bar" orderType="pending" />)
-
-      expect(wrapper.text()).toBe('foo bar')
-    })
-  })
 })
