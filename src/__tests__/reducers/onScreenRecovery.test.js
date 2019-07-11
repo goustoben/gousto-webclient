93ea8a022db3aa87fce2eccd8b52d29117f07826
Ellie <ellie.crome@gousto.co.uk>
2019-07-11 11:36:21 +0100

    Adds OSR reducer triggered test, refactors other  tests to make less brittle

--- src/__tests__/reducers/onScreenRecovery.test.js
+++ src/__tests__/reducers/onScreenRecovery.test.js
@@ -13,6 +13,7 @@ describe('onScreenRecovery reducer', () => {
     deliveryDayId: '',
     boxNumber: '',
     orderType: '',
+    modalType: '',
     orderDate: '',
     title: '',
     offer: null,
@@ -29,19 +30,12 @@ describe('onScreenRecovery reducer', () => {
   describe('onScreenRecovery dispatch ORDER_SKIP_RECOVERY_TRIGGERED', () => {
     state = undefined
     test('onScreenRecovery dispatch trigger with orderId', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: true,
         modalVisibility: false,
         orderId: '1234',
-        deliveryDayId: '',
-        boxNumber: '',
         orderType: 'pending',
-        orderDate: '',
-        title: '',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_TRIGGERED,
         triggered: true,
@@ -50,23 +44,16 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('onScreenRecovery dispatch trigger with deliveryDayId', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: true,
         modalVisibility: false,
-        orderId: '',
         deliveryDayId: '1234',
-        boxNumber: '',
         orderType: 'projected',
-        orderDate: '',
-        title: '',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_TRIGGERED,
         triggered: true,
@@ -75,23 +62,17 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('onScreenRecovery dispatch trigger with orderDate', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: true,
         modalVisibility: false,
-        orderId: '',
         deliveryDayId: '1234',
-        boxNumber: '',
         orderType: 'projected',
         orderDate: '2018-09-18 00:00:00',
-        title: '',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_TRIGGERED,
         triggered: true,
@@ -101,49 +82,49 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
+    })
+
+    test('onScreenRecovery dispatch trigger with modalType', () => {
+      const expected = {
+        triggered: true,
+        modalType: 'order',
+      }
+      const actionToCall = {
+        type: actionTypes.ORDER_SKIP_RECOVERY_TRIGGERED,
+        triggered: true,
+        modalType: 'order',
+      }
+
+      const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
   })
 
   describe('onScreenRecovery dispatch ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE', () => {
     state = undefined
     test('onScreenRecovery dispatch trigger with modalVisibility false', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: false,
-        orderId: '',
-        deliveryDayId: '',
-        boxNumber: '',
-        orderType: '',
-        orderDate: '',
-        title: '',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: false,
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('onScreenRecovery dispatch trigger with modalVisibility true and orderId', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: true,
         orderId: '123',
-        deliveryDayId: '',
-        boxNumber: '',
         orderType: 'pending',
-        orderDate: '',
         title: 'Title',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: true,
@@ -153,23 +134,17 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('onScreenRecovery dispatch trigger with modalVisibility true and deliveryDayId', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: true,
-        orderId: '',
         deliveryDayId: '123',
-        boxNumber: '',
         orderType: 'projected',
-        orderDate: '',
         title: 'Title',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: true,
@@ -179,23 +154,18 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('onScreenRecovery dispatch trigger with modalVisibility true and value proposition', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: true,
-        orderId: '',
         deliveryDayId: '123',
-        boxNumber: '',
         orderType: 'projected',
-        orderDate: '',
         title: 'Title',
-        offer: null,
         valueProposition: 'Value prop',
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: true,
@@ -206,23 +176,18 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('onScreenRecovery dispatch trigger with modalVisibility true and callToActions', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: true,
-        orderId: '',
         deliveryDayId: '123',
-        boxNumber: '',
         orderType: 'projected',
-        orderDate: '',
         title: 'Title',
-        offer: null,
-        valueProposition: null,
         callToActions: 'Call to action',
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: true,
@@ -233,7 +198,7 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('should dispatch trigger with modalVisibility true and offer', () => {
@@ -251,19 +216,15 @@ describe('onScreenRecovery reducer', () => {
           }
         }
       }
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: true,
-        orderId: '',
         deliveryDayId: '123',
-        boxNumber: '',
         orderType: 'projected',
-        orderDate: '',
         title: 'Title',
         offer: offer.details,
-        valueProposition: null,
         callToActions: 'Call to action',
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: true,
@@ -275,7 +236,7 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
 
     test('should not update offer in state when offer.details is undefined', () => {
@@ -283,19 +244,14 @@ describe('onScreenRecovery reducer', () => {
         basis: 'percentage_discount',
         details: undefined
       }
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: true,
-        orderId: '',
         deliveryDayId: '123',
-        boxNumber: '',
         orderType: 'projected',
-        orderDate: '',
         title: 'Title',
-        offer: null,
-        valueProposition: null,
         callToActions: 'Call to action',
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_MODAL_VISIBILITY_CHANGE,
         modalVisibility: true,
@@ -307,32 +263,24 @@ describe('onScreenRecovery reducer', () => {
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
   })
 
   describe('onScreenRecovery dispatch ORDER_SKIP_RECOVERY_BOX_NUMBER_CHANGE', () => {
     test('onScreenRecovery dispatch trigger with orderDate', () => {
-      const expected = Immutable.Map({
+      const expected = {
         triggered: false,
         modalVisibility: false,
-        orderId: '',
-        deliveryDayId: '',
         boxNumber: '5',
-        orderType: '',
-        orderDate: '',
-        title: '',
-        offer: null,
-        valueProposition: null,
-        callToActions: null,
-      })
+      }
       const actionToCall = {
         type: actionTypes.ORDER_SKIP_RECOVERY_BOX_NUMBER_CHANGE,
         boxNumber: '5',
       }
 
       const result = onScreenRecovery.onScreenRecovery(initialState, actionToCall)
-      expect(result).toEqual(expected)
+      expect(result.toJS()).toEqual(expect.objectContaining(expected))
     })
   })
 })
