761ea52a88278909defcc705965ff8a16f2766ef
Safeer Hayat <saf_hayat94@hotmail.com>
2019-07-16 11:00:53 +0100

    Refactors logic for getting star ratings

--- src/src/components/AppBanner/AppBanner.js
+++ src/src/components/AppBanner/AppBanner.js
@@ -13,48 +13,41 @@ const propTypes = {
 }
 
 const showStar = (avg) => {
-  let i
-  const stars = []
+  const stars = Array(5)
   const avgRounded = Math.round(avg)
   const avgFloored = Math.floor(avg)
-  for (i = 0; i < avgFloored; i++) {
-    stars.push(<span className={css.starFull} key={i + 1} />)
-  }
-  if (avgRounded > avgFloored) {
-    stars.push(<span className={css.starHalf} key={avgRounded} />)
-    i++
-  }
-  for (i; i < 5; i++) {
-    stars.push(<span className={css.starEmpty} key={i + 1} />)
-  }
+
+  stars.fill(<span className={css.starFull} />, 0, avgFloored)
+  stars.fill(<span className={css.starHalf} key={avgRounded} />, avgFloored, avgRounded)
+  stars.fill(<span className={css.starEmpty} />, avgRounded, 5)
 
   return stars
 }
 
 const AppBanner = ({ name, averageRating, ratings, showAppBanner, appBannerDismiss }) => (
-  showAppBanner ?
-    (
-      <div className={css.appBannerWrapper}>
-        <button type='button' className={css.closeButton} onClick={() => appBannerDismiss()}>
-          <Svg fileName="icon_dismiss-app-banner" className={css.closeIcon} />
-        </button>
-        <div className={css.appDetails}>
-          <Svg fileName="app-banner-app-icon" className={css.appIcon} />
-          <div className={css.platformSpecificDetails}>
-            <strong>{`Gousto for ${name}`}</strong>
-            <div className={css.rating}>
-              <span className={css.stars}>
-                {showStar(averageRating)}
-              </span>
-              <span>{`(${ratings})`}</span>
-            </div>
+  showAppBanner &&
+  (
+    <div className={css.appBannerWrapper}>
+      <button type='button' className={css.closeButton} onClick={() => appBannerDismiss()}>
+        <Svg fileName="icon_dismiss-app-banner" className={css.closeIcon} />
+      </button>
+      <div className={css.appDetails}>
+        <Svg fileName="app-banner-app-icon" className={css.appIcon} />
+        <div className={css.platformSpecificDetails}>
+          <strong>{`Gousto for ${name}`}</strong>
+          <div className={css.rating}>
+            <span className={css.stars}>
+              {showStar(averageRating)}
+            </span>
+            <span>{`(${ratings})`}</span>
           </div>
         </div>
-        <a className={css.appLink} href="https://gousto.co.uk/apps">
-          <Button noDecoration>Get app</Button>
-        </a>
       </div>
-    ) : null
+      <a className={css.appLink} href="https://gousto.co.uk/apps">
+        <Button noDecoration>Get app</Button>
+      </a>
+    </div>
+  )
 )
 
 AppBanner.propTypes = propTypes
