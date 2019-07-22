a7c484ac1c0cfdfbf9330489fa1e5e8a74c74c4d
Andy Davison <andy.davison@gousto.co.uk>
2019-07-22 09:39:15 +0100

    WIP recipe card; update Image to use lazy prop

--- src/src/components/Image/Image.js
+++ src/src/components/Image/Image.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import Immutable from 'immutable'
-import config from 'config/media'
-import classNames from 'classnames'
-import LazyLoad from 'react-lazyload'
-import css from './Image.css'
+import PropTypes from "prop-types"
+import React from "react"
+import Immutable from "immutable"
+import config from "config/media"
+import classNames from "classnames"
+import LazyLoad from "react-lazyload"
+import css from "./Image.css"
 
 class Image extends React.PureComponent {
   static propTypes = {
@@ -12,7 +12,7 @@ class Image extends React.PureComponent {
     className: PropTypes.string,
     media: PropTypes.oneOfType([
       PropTypes.instanceOf(Immutable.List),
-      PropTypes.string,
+      PropTypes.string
     ]).isRequired,
     onClick: PropTypes.func,
     title: PropTypes.string,
@@ -20,34 +20,37 @@ class Image extends React.PureComponent {
     lazy: PropTypes.bool,
     once: PropTypes.bool,
     offset: PropTypes.number,
-    placeholder: PropTypes.node,
+    placeholder: PropTypes.node
   }
 
   static defaultProps = {
     contain: true,
     onClick: () => {},
-    title: '',
+    title: "",
     lazy: false,
     once: true,
-    offset: 200,
+    offset: 200
   }
 
-  getSrcSet = (srcs) => (
-    srcs.reduce((str, src) => (
-      `${str} ${src.get('src')} ${src.get('width')}w,`
-    ), '')
-  )
+  getSrcSet = srcs =>
+    srcs.reduce(
+      (str, src) => `${str} ${src.get("src")} ${src.get("width")}w,`,
+      ""
+    )
 
-  getSizes = (srcs) => (
-    srcs.reduce((str, src) => (
-      `${str} (max-width: ${config.imageScale[src.get('width')]}px) ${src.get('width')}px,`
-    ), '')
-  )
+  getSizes = srcs =>
+    srcs.reduce(
+      (str, src) =>
+        `${str} (max-width: ${config.imageScale[src.get("width")]}px) ${src.get(
+          "width"
+        )}px,`,
+      ""
+    )
 
   getDefaultImage = (srcs, maxImageSize) => {
     let image = Immutable.Map({})
     if (srcs.size > 0) {
-      const sortedSrcs = srcs.sort((a, b) => b.get('width') - a.get('width'))
+      const sortedSrcs = srcs.sort((a, b) => b.get("width") - a.get("width"))
       if (maxImageSize) {
         image = sortedSrcs.getIn([0])
       } else {
@@ -62,11 +65,13 @@ class Image extends React.PureComponent {
     const { maxMediaSize, media, lazy, once, offset, placeholder } = this.props
     const className = classNames({
       [this.props.className]: this.props.className,
-      [css.contain]: this.props.contain,
+      [css.contain]: this.props.contain
     })
 
-    if (typeof media === 'string') {
-      return (
+    let imageComponent = null
+
+    if (typeof media === "string") {
+      imageComponent = (
         <img
           alt={this.props.title}
           className={className}
@@ -80,33 +85,41 @@ class Image extends React.PureComponent {
       let srcs = media
 
       if (maxMediaSize) {
-        const sortedSrcs = media.sort((a, b) => a.get('width', 0) - b.get('width'), 0)
-        srcs = sortedSrcs.filter(iSrc => iSrc.get('width') <= maxMediaSize)
+        const sortedSrcs = media.sort(
+          (a, b) => a.get("width", 0) - b.get("width"),
+          0
+        )
+        srcs = sortedSrcs.filter(iSrc => iSrc.get("width") <= maxMediaSize)
 
-        if (srcs.size < media.size && srcs.last().get('width') < maxMediaSize) {
+        if (srcs.size < media.size && srcs.last().get("width") < maxMediaSize) {
           srcs = srcs.push(sortedSrcs.get(srcs.size))
         }
       }
 
       const image = this.getDefaultImage(srcs, maxMediaSize)
-      const imageComponent = (
+      imageComponent = (
         <img
           alt={this.props.title}
           className={className}
           onClick={this.props.onClick}
-          src={image.get('src', '')}
+          src={image.get("src", "")}
           srcSet={this.getSrcSet(srcs)}
         />
       )
-
-      return (lazy) ? (
-        <LazyLoad once={once} offset={offset} placeholder={placeholder} height={0}>
-          {imageComponent}
-        </LazyLoad>
-      ) : imageComponent
     }
 
-    return null
+    return lazy ? (
+      <LazyLoad
+        once={once}
+        offset={offset}
+        placeholder={placeholder}
+        height={0}
+      >
+        {imageComponent}
+      </LazyLoad>
+    ) : (
+      imageComponent
+    )
   }
 }
 
--- src/src/routes/Account/MyGousto/Cookbook/RecipeCard/RecipeCard.js
+++ src/src/routes/Account/MyGousto/Cookbook/RecipeCard/RecipeCard.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react"
+import PropTypes from "prop-types"
+import Image from "Recipe/Image"
+import css from "./RecipeCard.css"
 
 const propTypes = {
   link: PropTypes.string,
   image: PropTypes.string,
-  title: PropTypes.string,
-
+  title: PropTypes.string
 }
 
-const RecipeCard = ({link="link", image="image", title="title"}) => (
-  <div>I am a recipe, {link} , {image}, {title} </div>
+const maxMediaSize = 400
 
+const RecipeCard = ({ link = "link", image = "image", title = "title" }) => (
+  <a href={link}>
+    <div className={css.cardContainer}>
+      <Image
+        media={image}
+        alt={title}
+        maxMediaSize={maxMediaSize}
+        // className={css.recipeImg}
+      />
+      <p>{title}</p>
+    </div>
+  </a>
 )
 
 RecipeCard.propTypes = propTypes
