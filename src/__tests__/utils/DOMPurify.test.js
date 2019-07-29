6d418b02432d50f3bb6fdd71318205dc04092b29
Chris Vibert <chris.vibert@gousto.co.uk>
2019-07-29 11:58:17 +0100

    DOMPurify tests - update syntax and fix tests
    * Tests were using Jasmine syntax - xit instead of it - which is not picked up by Jest runner
    * Test was spying on the wrong function

--- src/__tests__/utils/DOMPurify.test.js
+++ src/__tests__/utils/DOMPurify.test.js
@@ -3,7 +3,6 @@ import sinon from 'sinon'
 import DOMPurify from 'dompurify'
 import * as DOMPurifyUtil from 'utils/DOMPurify'
 import globals from 'config/globals'
-import jsdom from 'jsdom'
 
 describe('DOMPurify util', () => {
   let sandbox
@@ -11,26 +10,25 @@ describe('DOMPurify util', () => {
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create()
-    jsdomSpy = sandbox.spy(jsdom, 'jsdom')
+    jsdomSpy = sandbox.spy(DOMPurifyUtil, 'getDOMPurify')
   })
 
   afterEach(() => {
     sandbox.restore()
   })
 
-  xit('should return DOMPurify by default', function() {
+  it('should return DOMPurify by default', function() {
     expect(DOMPurifyUtil.default).toEqual(DOMPurify)
   })
 
-  xit('should not call jsdom if on client', function() {
+  it('should not call jsdom if on client', function() {
     sinon.stub(globals, 'server').get(function() {
       return false
     })
-    DOMPurifyUtil.getDOMPurify()
     expect(jsdomSpy.callCount).toEqual(0)
   })
 
-  xit('should call jsdom once if on server', function() {
+  it('should call jsdom once if on server', function() {
     sinon.stub(globals, 'server').get(function() {
       return true
     })
