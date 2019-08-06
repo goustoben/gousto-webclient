3dbc603bf0b0669308fa86fd68ad3252d404fe61
Razvan Spatariu <razvanspatariu@gmail.com>
2019-08-06 13:36:24 +0100

    [TECH-12223] Display product count where available in the menu for the market
    If count is passed by OrderConfirmation for one of the categories, display it in the menu.

--- src/src/routes/OrderConfirmation/components/Navbar/__tests__/Navbar.test.js
+++ src/src/routes/OrderConfirmation/components/Navbar/__tests__/Navbar.test.js
@@ -8,7 +8,7 @@ describe('Navbar', () => {
   const onClickMock = jest.fn()
 
   beforeEach(() => {
-    items = [{ id: 'Category 1', label: 'Category 1' }, { id: 'Category 2', label: 'Category 2' }]
+    items = [{ id: 'Category 1', label: 'Category 1', count: 45 }, { id: 'Category 2', label: 'Category 2' }]
 
     wrapper = shallow(<Navbar items={items} active='Category 1' onClick={onClickMock} />)
   })
@@ -25,5 +25,13 @@ describe('Navbar', () => {
     wrapper.find('button').at(0).simulate('click')
 
     expect(onClickMock).toHaveBeenCalledWith('Category 1')
+  })
+
+  test('should render the count for categories where it is available', () => {
+    expect(wrapper.find('button').at(0).text()).toBe(`${items[0].id} (${items[0].count})`)
+  })
+
+  test('should not render empty parenthesis when the count for a category is not available', () => {
+    expect(wrapper.find('button').at(1).text()).toBe(items[1].id)
   })
 })
