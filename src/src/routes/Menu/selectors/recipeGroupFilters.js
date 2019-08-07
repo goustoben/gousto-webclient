c0501439e8fbd05832148007cc5758df63396c6f
Alex Kelly <alex.kelly@gousto.co.uk>
2019-08-07 18:26:17 +0100

    New file names and create selector to get recipes filtered by collection slug

---
+++ src/src/routes/Menu/selectors/recipeGroupFilters.js
@@ -0,0 +1,34 @@
+import { createSelector } from 'reselect'
+
+import { getRecipes, getMenuRecipeIds, getMenuCollections, getMenuRecipes } from 'selectors/root'
+import { getRecipeGroupFilter } from 'selectors/filters'
+import { getMenuCollectionRecipeIds } from 'selectors/collections'
+import { getFoodBrand } from 'utils/recipe'
+
+export const getCurrentMenuRecipes = createSelector(
+  [getRecipes, getMenuRecipeIds],
+  (allRecipes, currentMenuIds) => (
+    currentMenuIds && currentMenuIds.map(recipeId => allRecipes.get(recipeId) || null)
+      .filter(recipe => recipe !== null)
+  )
+)
+
+export const getRecipesFilteredByFoodBrand = createSelector(
+  [getCurrentMenuRecipes, getRecipeGroupFilter],
+  (currentMenuRecipes, selectedRecipeGroup) => (
+    selectedRecipeGroup ? currentMenuRecipes.filter(recipe => getFoodBrand(recipe).get('slug') === selectedRecipeGroup.slug) : null
+  )
+)
+
+export const getRecipesFilteredByCollectionSlug = createSelector(
+  [getCurrentMenuRecipes, getRecipeGroupFilter, getMenuCollections, getMenuRecipes],
+  (currentMenuRecipes, selectedRecipeGroup, menuCollections, allMenuCollectionRecipes) => {
+    const recipeGroupSlug = selectedRecipeGroup && selectedRecipeGroup.slug
+    const currentCollectionRecipes = getMenuCollectionRecipeIds(menuCollections , allMenuCollectionRecipes, recipeGroupSlug)
+    const recipesFilteredByThematic = recipeGroupSlug && !!currentCollectionRecipes ? currentMenuRecipes.filter(recipe => currentCollectionRecipes.includes(recipe.get('id'))) : null
+
+    return recipesFilteredByThematic
+  }
+)
+
+//rename file to be recipeGroup or something like this
