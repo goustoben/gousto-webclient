854e0da30a5d2940bbcf560bcf705ce29c468320
Wing Choi <wingchoi@MG625.local>
2019-08-07 12:12:54 +0100

    TECH-11882/queryString support query string in apps redirect

--- src/server/middleware/apps.js
+++ src/server/middleware/apps.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-param-reassign */
 const { appStoreLink, playStoreLink } = require('config/apps')
 
+const converseUtmSource = (querystring) => querystring ? querystring.replace('utm_source', 'referrer') : querystring
+
+const addQueryString = (url, querystring) => querystring ? `${url}?${querystring}` : url
+
 const appsRedirect = async (ctx, next) => {
-  if (ctx.request.url === '/apps') {
+  if (ctx.request.url.startsWith('/apps') || ctx.request.url.startsWith('/apps?')) {
     const userAgent = ctx.request.header['user-agent']
 
     if (/android/i.test(userAgent)) {
-      ctx.redirect(playStoreLink)
+      ctx.redirect(addQueryString(playStoreLink, converseUtmSource(ctx.request.querystring)))
     } else if (/iPad|iPhone|iPod/.test(userAgent)) {
-      ctx.redirect(appStoreLink)
+      ctx.redirect(addQueryString(appStoreLink, ctx.request.querystring))
     } else {
       ctx.redirect('/')
     }
