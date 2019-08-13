31984ae0dc93c4fb97647d2e5154092942a83c95
Renato Ferreira <renatojoey@gmail.com>
2019-08-13 16:37:47 +0100

    [TECH-12436] Add Haricots optimizely ID
    Optimizely ID added for Haricots env, so we can run optimizely preview and test experiments on Haricots env.

--- src/src/config/head/optimizely.js
+++ src/src/config/head/optimizely.js
@@ -3,6 +3,6 @@ export default {
   staging: '7728010866',
   radishes: '14571110311',
   rockets: '14785300772',
-  haricots: '7728010866',
+  haricots: '15910240385',
   production: '3732210624',
 }
