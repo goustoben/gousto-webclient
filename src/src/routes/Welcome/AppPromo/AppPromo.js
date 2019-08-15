2b1c9428c5cc86a715b0ee990ba83dd3e5ec4314
Safeer Hayat <saf_hayat94@hotmail.com>
2019-08-15 17:34:54 +0100

    Refactors ctas into desktop and mobile for clarity

--- src/src/routes/Welcome/AppPromo/AppPromo.js
+++ src/src/routes/Welcome/AppPromo/AppPromo.js
@@ -14,14 +14,15 @@ const defaultProps = {
   device: 'desktop'
 }
 
-const appStoreCTAs = (device, hideOnSmallScreen) => (
-  <div className={classnames({ [css.hideOnLargerScreen]: !hideOnSmallScreen }, { [css.hideOnSmallScreen]: hideOnSmallScreen })}>
-    <a data-testing="appBannerCTA" className={classnames(css.mobileAppLink, { [css.hideElement]: device === 'desktop' })} href="https://gousto.co.uk/apps">
-      <Button className={css.getAppCTA} noDecoration>Get the app now</Button>
-    </a>
-    <div className={classnames(css.desktopAppLink, { [css.hideElement]: device === 'mobile' })}>
-      <AppStoreLinks appStoreId={config.apps.appStoreId} playStoreId={config.apps.playStoreId} />
-    </div>
+const mobileAppStoreCTAs = device => (
+  <a data-testing="appBannerCTA" className={classnames(css.mobileAppLink, { [css.hideElement]: device === 'desktop' })} href="https://gousto.co.uk/apps">
+    <Button className={css.getAppCTA} noDecoration>Get the app now</Button>
+  </a>
+)
+
+const desktopAppStoreCTAs = device => (
+  <div className={classnames(css.desktopAppLink, { [css.hideElement]: device === 'mobile' })}>
+    <AppStoreLinks appStoreId={config.apps.appStoreId} playStoreId={config.apps.playStoreId} />
   </div>
 )
 
@@ -29,17 +30,23 @@ const AppPromo = ({ device }) => {
   return (
     <div className={css.container}>
       <div className={css.contentContainer}>
-        <div className={css.phoneImageContainer}><img className={css.phoneImage} src={require('media/images/app-promo-phone.jpg')} alt="" /></div>
+        <div className={css.phoneImageContainer}><img className={css.phoneImage} src={require('media/images/app_promo_phone.png')} alt="" /></div>
         <div className={css.content}>
           <ul className={css.list}>
             <li><span className={css.bullet}><i className={css.tick} /></span>Get notified when the new menu is out</li>
             <li><span className={css.bullet}><i className={css.tick} /></span>Follow the status of your orders on the go</li>
             <li><span className={css.bullet}><i className={css.tick} /></span>Find all your recipes in your own cookbook</li>
           </ul>
-          {appStoreCTAs(device, true)}
+          <div className={css.hideOnSmallScreen}>
+            {mobileAppStoreCTAs(device)}
+            {desktopAppStoreCTAs(device)}
+          </div>
         </div>
       </div >
-      {appStoreCTAs(device, false)}
+      <div className={css.hideOnLargerScreen}>
+        {mobileAppStoreCTAs(device)}
+        {desktopAppStoreCTAs(device)}
+      </div>
     </div>
   )
 }
