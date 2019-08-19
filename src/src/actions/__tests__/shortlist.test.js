426266b556902753d41d26f4b343adff033d6971
Alex Kelly <alex.kelly@gousto.co.uk>
2019-08-19 11:00:57 +0100

    Updated test to remove objectcontains and include recipeInfo as a param

--- src/src/actions/__tests__/shortlist.test.js
+++ src/src/actions/__tests__/shortlist.test.js
@@ -15,7 +15,10 @@ describe('shortlist actions', () => {
       shortlist: {}
     }),
     menuRecipes: {},
-    menuRecipeStock: {}
+    menuRecipeStock: {},
+    filters: Immutable.fromJS({
+      currentCollectionId: 'ca8f71be-63ac-11e6-a693-068306404bab'
+    })
   })
 
   afterEach(() => {
@@ -23,22 +26,25 @@ describe('shortlist actions', () => {
   })
 
   describe('shortlistRecipeAdd', () => {
+    const recipeInfo = {poisiton: '20'}
+
     describe('if force is true', () => {
+
       test('should dispatch recipeId and action type SHORTLIST_RECIPE_ADD', () => {
-        shortlistRecipeAdd('123', true)(dispatch, getState)
-        expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({ recipeId: "123", type: actionTypes.SHORTLIST_RECIPE_ADD }))
+        shortlistRecipeAdd('123', true, recipeInfo)(dispatch, getState)
+        expect(dispatch).toHaveBeenCalledWith({ recipeId: "123", poisiton: '20', type: actionTypes.SHORTLIST_RECIPE_ADD })
       })
 
       test('should dispatch limitReached to be false and action type SHORTLIST_LIMIT_REACHED', () => {
         shortlistLimitReached.mockReturnValue(false)
-        shortlistRecipeAdd('123', true)(dispatch, getState)
-        expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({ shortlistLimitReached: false, type: actionTypes.SHORTLIST_LIMIT_REACHED }))
+        shortlistRecipeAdd('123', true, recipeInfo)(dispatch, getState)
+        expect(dispatch).toHaveBeenCalledWith({ shortlistLimitReached: false, type: actionTypes.SHORTLIST_LIMIT_REACHED })
       })
 
       test('should dispatch limitReached to be true and action type SHORTLIST_LIMIT_REACHED', () => {
         shortlistLimitReached.mockReturnValue(true)
-        shortlistRecipeAdd('123', true)(dispatch, getState)
-        expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({ shortlistLimitReached: true, type: actionTypes.SHORTLIST_LIMIT_REACHED }))
+        shortlistRecipeAdd('123', true, recipeInfo)(dispatch, getState)
+        expect(dispatch).toHaveBeenCalledWith({ shortlistLimitReached: true, type: actionTypes.SHORTLIST_LIMIT_REACHED })
       })
     })
 
@@ -53,15 +59,15 @@ describe('shortlist actions', () => {
       describe('if limitReached is false', () => {
         test('should dispatch recipeId and action type SHORTLIST_RECIPE_ADD', () => {
           shortlistLimitReached.mockReturnValue(false)
-          shortlistRecipeAdd('123', false)(dispatch, getState)
-          expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({ recipeId: "123", type: actionTypes.SHORTLIST_RECIPE_ADD }))
+          shortlistRecipeAdd('123', false, recipeInfo)(dispatch, getState)
+          expect(dispatch).toHaveBeenCalledWith({ recipeId: "123", poisiton: '20', collection: 'ca8f71be-63ac-11e6-a693-068306404bab', type: actionTypes.SHORTLIST_RECIPE_ADD })
         })
 
         describe('if limitReached is true after adding recipe to shortlist ', () => {
           test('should dispatch limitReached and SHORTLIST_LIMIT_REACHED', () => {
             shortlistLimitReached.mockReturnValueOnce(false).mockReturnValueOnce(true)
-            shortlistRecipeAdd('123', false)(dispatch, getState)
-            expect(dispatch).toHaveBeenCalledWith(expect.objectContaining({ shortlistLimitReached: true, type: actionTypes.SHORTLIST_LIMIT_REACHED }))
+            shortlistRecipeAdd('123', false, recipeInfo)(dispatch, getState)
+            expect(dispatch).toHaveBeenCalledWith({ shortlistLimitReached: true, type: actionTypes.SHORTLIST_LIMIT_REACHED })
           })
         })
       })
