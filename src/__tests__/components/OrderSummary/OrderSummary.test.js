73d73c1323c6da0821f9abb7797d5d1bf280c7a8
Safeer Hayat <saf_hayat94@hotmail.com>
2019-08-21 15:14:57 +0100

    Fixes breaking tests from copy changes

--- src/__tests__/components/OrderSummary/OrderSummary.test.js
+++ src/__tests__/components/OrderSummary/OrderSummary.test.js
@@ -13,8 +13,8 @@ describe('OrderSummary', () => {
   let showProductDetail
 
   beforeEach(() => {
-    removeProduct = function() {}
-    showProductDetail = function() {}
+    removeProduct = function () { }
+    showProductDetail = function () { }
 
     wrapper = shallow(
       <OrderSummary
@@ -236,7 +236,7 @@ describe('OrderSummary SectionHeader', () => {
         products={{}}
         recipes={{}}
         deliveryTotalPrice={'2.50'}
-        onSave={function() {}}
+        onSave={function () { }}
       />,
     )
 
@@ -256,13 +256,13 @@ describe('OrderSummary SectionHeader', () => {
     ).toBe('Your box will arrive Friday, 6th May')
   })
 
-  test('should contain "Here are the details about your box" in second p', () => {
+  test('should contain "Here\'s what\'s inside your box" in second p', () => {
     expect(
       header
         .find('p')
         .last()
         .text(),
-    ).toBe('Here are the details about your box')
+    ).toBe('Here\'s what\'s inside your box')
   })
 })
 
@@ -278,7 +278,7 @@ describe('OrderSummary footer', () => {
         products={{}}
         recipes={{}}
         deliveryTotalPrice={'2.50'}
-        onSave={function() {}}
+        onSave={function () { }}
         orderSummaryCollapsed
       />,
     )
