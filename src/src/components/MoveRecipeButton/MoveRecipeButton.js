d2b41cedecd9636041b70de1313b1efb1c298949
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-09-03 09:20:14 +0100

    change arrow for moveRecipeButton

--- src/src/components/MoveRecipeButton/MoveRecipeButton.js
+++ src/src/components/MoveRecipeButton/MoveRecipeButton.js
@@ -3,12 +3,13 @@ import { PropTypes } from 'prop-types'
 import css from './MoveRecipeButton.css'
 
 const MoveRecipeButton = ({ fromBox, recipeId }) => {
-  const buttonText = fromBox ? '\u2193 Move to shortlist' : '\u2191 Add to box'
+  const arrowStyle = fromBox ? css.arrowDown : css.arrowUp
+  const buttonText = fromBox ? 'Move to shortlist' : 'Add to box'
   const style = fromBox ? css.moveFromButton : css.addToButton
 
   return (
     <div className={css.moveButtonWrapper}>
-      <button type="button" className={style}>{buttonText}</button>
+      <button type="button" className={style}><i className={arrowStyle}></i> {buttonText}</button>
     </div>
   )
 }
--- src/src/components/MoveRecipeButton/__tests__/MoveRecipeButton.test.js
+++ src/src/components/MoveRecipeButton/__tests__/MoveRecipeButton.test.js
@@ -14,11 +14,11 @@ describe('MoveRecipeButton', () => {
 
   test('should render text for move to shortlist if fromBox is true', () => {
     wrapper.setProps({ fromBox: true })
-    expect(wrapper.text()).toContain('\u2193 Move to shortlist')
+    expect(wrapper.text()).toContain('Move to shortlist')
   })
 
   test('should render text for add to box if fromBox is false', () => {
     wrapper.setProps({ fromBox: false })
-    expect(wrapper.text()).toContain('\u2191 Add to box')
+    expect(wrapper.text()).toContain('Add to box')
   })
 })
