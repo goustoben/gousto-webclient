8856d6401e3d442027b14018509e0ee91d95bbba
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-09-03 17:52:05 +0100

    refactor test for MoveRecipeButton

--- src/src/components/MoveRecipeButton/__tests__/MoveRecipeButton.test.js
+++ src/src/components/MoveRecipeButton/__tests__/MoveRecipeButton.test.js
@@ -5,45 +5,57 @@ import { MoveRecipeButton } from '../MoveRecipeButton'
 describe('MoveRecipeButton', () => {
   let wrapper
   beforeEach(() => {
-    wrapper = shallow(<MoveRecipeButton />)
+    wrapper = shallow(<MoveRecipeButton recipeId={"1"} />)
   })
 
   test('should render div', () => {
     expect(wrapper.type()).toEqual('div')
   })
 
-  test('should render text for move to shortlist if fromBox is true', () => {
-    wrapper.setProps({ fromBox: true })
-    expect(wrapper.text()).toContain('Move to shortlist')
+  describe('when fromBox is true', () => {
+    beforeEach(() => {
+      wrapper.setProps({ fromBox: true })
+    })
+
+    test('should render text for move to shortlist', () => {
+      expect(wrapper.text()).toContain('Move to shortlist')
+    })
+
+    test('should render text for shortlist full when limit reached in shortlist', () => {
+      wrapper.setProps({ isShortlistLimitReached: true })
+      expect(wrapper.text()).toContain('Shortlist full')
+    })
+
+    test('should call moveToShortlistSpy when click', () => {
+      const moveToBoxSpy = jest.fn()
+      const moveToShortlistSpy = jest.fn()
+      wrapper.setProps({ recipeId: '123', moveToBox: moveToBoxSpy, moveToShortlist: moveToShortlistSpy })
+      wrapper.find('button').simulate('click')
+      expect(moveToShortlistSpy).toHaveBeenCalled()
+    })
   })
 
-  test('should render text for add to box if fromBox is false', () => {
-    wrapper.setProps({ fromBox: false })
-    expect(wrapper.text()).toContain('Add to box')
+  describe('when fromBox is false', () => {
+    beforeEach(() => {
+      wrapper.setProps({ fromBox: false })
+    })
+
+    test('should render text for add to box', () => {
+      expect(wrapper.text()).toContain('Add to box')
+    })
+
+    test('should render text for box full when limit reached in basket', () => {
+      wrapper.setProps({ isBasketLimitReached: true })
+      expect(wrapper.text()).toContain('Box full')
+    })
+
+    test('should call moveToBox when click', () => {
+      const moveToBoxSpy = jest.fn()
+      const moveToShortlistSpy = jest.fn()
+      wrapper.setProps({ recipeId: '123', moveToBox: moveToBoxSpy, moveToShortlist: moveToShortlistSpy })
+      wrapper.find('button').simulate('click')
+      expect(moveToBoxSpy).toHaveBeenCalled()
+    })
   })
 
-  test('should render text for shortlist full if fromBox is true and limit reached in shortlist', () => {
-    wrapper.setProps({ fromBox: true, isShortlistLimitReached: true })
-    expect(wrapper.text()).toContain('Shortlist full')
-  })
-
-  test('should render text for box full if fromBox is false and limit reached in basket', () => {
-    wrapper.setProps({ fromBox: false, isBasketLimitReached: true })
-    expect(wrapper.text()).toContain('Box full')
-  })
-
-  test('should call moveToBox when click if fromBox false', () => {
-    const moveToBoxSpy = jest.fn()
-    const moveToShortlistSpy = jest.fn()
-    wrapper.setProps({ fromBox: false, recipeId: '123', moveToBox: moveToBoxSpy, moveToShortlist: moveToShortlistSpy })
-    wrapper.find('button').simulate('click')
-    expect(moveToBoxSpy).toHaveBeenCalled()
-  })
-  test('should call moveToShortlistSpy when click if fromBox true', () => {
-    const moveToBoxSpy = jest.fn()
-    const moveToShortlistSpy = jest.fn()
-    wrapper.setProps({ fromBox: true, recipeId: '123', moveToBox: moveToBoxSpy, moveToShortlist: moveToShortlistSpy })
-    wrapper.find('button').simulate('click')
-    expect(moveToShortlistSpy).toHaveBeenCalled()
-  })
 })
