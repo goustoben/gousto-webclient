fcdba45e22e0b42728eb0e1a5a7edbd2e7e5f4a9
Safeer Hayat <saf_hayat94@hotmail.com>
2019-09-10 13:44:34 +0100

    Adds action to track continue to menu cta click

--- src/src/actions/abandonBasket.js
+++ src/src/actions/abandonBasket.js
@@ -20,3 +20,13 @@ export const trackAbandonBasketEligibility = () => (dispatch, getState) => {
     }
   })
 }
+
+export const trackAbandonBasketContinueToMenu = () => (dispatch) => {
+  console.log('gets here') //eslint-disable-line
+  dispatch({
+    type: actionTypes.TRACKING,
+    trackingData: {
+      actionType: 'AbandonedBasket ContinueToMenu',
+    }
+  })
+}
