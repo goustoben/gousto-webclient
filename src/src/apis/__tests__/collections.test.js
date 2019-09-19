dadc2b1297aaa5109c8518fc7c48535c65386c12
James Monger <jameskmonger@hotmail.co.uk>
2019-09-19 13:46:20 +0100

    Collections API tests (#1091)

---
+++ src/src/apis/__tests__/collections.test.js
@@ -0,0 +1,71 @@
+import fetch from 'utils/fetch'
+import { fetchCollections, fetchCollectionBySlug, fetchCollectionRecipes } from '../collections'
+
+const mockFetchResult = { data: [1, 2, 3] }
+jest.mock('utils/fetch', () =>
+  jest.fn().mockImplementation(() => {
+    const getData = async () => ({ data: [1, 2, 3] })
+
+    return getData()
+  })
+)
+
+jest.mock('config/endpoint', () =>
+  jest.fn().mockReturnValue('gousto-endpoint')
+)
+
+jest.mock('config/routes', () => ({
+  version: {
+    recipes: 'v2',
+  },
+  collections: {
+    recipes: '/recipes'
+  }
+}))
+
+describe('collections api', () => {
+  beforeEach(() => {
+    fetch.mockClear()
+  })
+
+  describe('fetchCollections', () => {
+    test('should fetch the correct url', async () => {
+      const reqData = { a: 1, b: 2 }
+      await fetchCollections('token', 'path', reqData)
+      expect(fetch).toHaveBeenCalledTimes(1)
+      expect(fetch).toHaveBeenCalledWith('token', 'gousto-endpoint/collections/path', reqData, 'GET', 'default', {}, null, false, false)
+    })
+
+    test('should return the results of the fetch unchanged', async () => {
+      const result = await fetchCollections('token', 'path', {})
+      expect(result).toEqual(mockFetchResult)
+    })
+  })
+
+  describe('fetchCollectionRecipes', () => {
+    test('should fetch the correct url', async () => {
+      const reqData = { c: 3, d: 4 }
+      await fetchCollectionRecipes('token', 'collection-id', reqData)
+      expect(fetch).toHaveBeenCalledTimes(1)
+      expect(fetch).toHaveBeenCalledWith('token', 'gousto-endpoint/collections/collection-id/recipes', reqData, 'GET', 'default', {}, null, false, false)
+    })
+
+    test('should return the results of the fetch unchanged', async () => {
+      const result = await fetchCollectionRecipes('token')
+      expect(result).toEqual(mockFetchResult)
+    })
+  })
+
+  describe('fetchCollectionBySlug', function () {
+    test('should fetch the correct url', async () => {
+      await fetchCollectionBySlug('slug')
+      expect(fetch).toHaveBeenCalledTimes(1)
+      expect(fetch).toHaveBeenCalledWith(null, 'gousto-endpoint/collections/slug', undefined, 'GET', 'default', {}, null, false, false)
+    })
+
+    test('should return the results of the fetch unchanged', async () => {
+      const result = await fetchCollectionBySlug('slug')
+      expect(result).toEqual(mockFetchResult)
+    })
+  })
+})
