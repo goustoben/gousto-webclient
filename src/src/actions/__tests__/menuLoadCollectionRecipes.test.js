ad3311858cc62075845d5c568280eb3ba2f067f6
Alex Kelly <alex.kelly@gousto.co.uk>
2019-09-20 17:02:15 +0100

    [TR-432] add coverage to menuLoadCollectionRecipes

---
+++ src/src/actions/__tests__/menuLoadCollectionRecipes.test.js
@@ -0,0 +1,75 @@
+import Immutable from 'immutable'
+import { menuLoadCollectionRecipes } from 'actions/menuLoadCollectionRecipes'
+import { fetchCollectionRecipes } from 'apis/collections'
+import { menuReceiveCollectionRecipes, menuReceiveMenu } from 'actions/menu'
+
+jest.mock('apis/collections', () => ({
+  fetchCollectionRecipes: jest.fn().mockResolvedValue({
+    data: {}
+  }),
+}))
+
+jest.mock('actions/menu', () => ({
+  menuReceiveCollectionRecipes: jest.fn(),
+  menuReceiveMenu: jest.fn(),
+}))
+
+describe('menuLoadCollectionRecipes', () => {
+  const dispatch = jest.fn()
+  let getState = jest.fn()
+
+  const state = {
+    auth: Immutable.fromJS({
+      accessToken: 'testAccessToken'
+    }),
+    features: Immutable.fromJS({
+      menu_id: {
+        value: false
+      }
+    })
+  }
+
+  getState = () => state
+
+  test('should call fetchCollectionRecipes with reqData including filters available on date ', async () => {
+    await menuLoadCollectionRecipes('testDate', 'testCollectionId')(dispatch, getState)
+
+    const expectedReqDataParams = {
+      'filters[available_on]': 'testDate',
+      'includes': ['ingredients', 'allergens', 'taxonomy']
+    }
+    expect(fetchCollectionRecipes).toHaveBeenCalledWith('testAccessToken', 'testCollectionId', expectedReqDataParams)
+  })
+
+  test('should call fetchCollectionRecipes with reqData including id', async() => {
+    const addIdField = true
+    await menuLoadCollectionRecipes('testDate', 'testCollectionId', addIdField )(dispatch, getState)
+
+    const expectedReqDataParams = {
+      'filters[available_on]': 'testDate',
+      'includes': ['ingredients', 'allergens', 'taxonomy'],
+      'fields[]': 'id'
+    }
+    expect(fetchCollectionRecipes).toHaveBeenCalledWith('testAccessToken', 'testCollectionId', expectedReqDataParams)
+  })
+
+  test('should dispatch menuReceiveCollectionRecipes when recipes are returned', async () => {
+    const addIdField = true
+
+    fetchCollectionRecipes.mockResolvedValue({
+      data: {
+        recipes: []
+      }
+    })
+
+    await menuLoadCollectionRecipes('testDate', 'testCollectionId', addIdField )(dispatch, getState)
+    expect(menuReceiveCollectionRecipes).toHaveBeenCalled()
+  })
+
+  test('should dispatch menuReceiveMenu when recipes are returned', async () => {
+    const addIdField = false
+
+    await menuLoadCollectionRecipes('testDate', 'testCollectionId', addIdField )(dispatch, getState)
+    expect(menuReceiveMenu).toHaveBeenCalled()
+  })
+})
