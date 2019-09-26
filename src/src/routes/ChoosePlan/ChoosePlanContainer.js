96e01e4f31d37cf8b67f68e9b78d09fb18f46c1e
Safeer Hayat <saf_hayat94@hotmail.com>
2019-09-26 17:44:31 +0100

    Adds action to do redirect on continue button

--- src/src/routes/ChoosePlan/ChoosePlanContainer.js
+++ src/src/routes/ChoosePlan/ChoosePlanContainer.js
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux'
+import { choosePlanContinue } from 'actions/choosePlan'
 import { ChoosePlan } from './ChoosePlan'
 
-const ChoosePlanContainer = connect()(ChoosePlan)
+const mapStateToProps = () => null
+
+const ChoosePlanContainer = connect(mapStateToProps, {
+  choosePlanContinue
+})(ChoosePlan)
 
 export { ChoosePlanContainer }
