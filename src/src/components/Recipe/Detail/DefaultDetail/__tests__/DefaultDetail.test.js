8af8c802cf07f0d0464694753813b327bbbc393e
PasparanGabriel <gabriel.pasparan@gousto.co.uk>
2019-09-30 17:02:30 +0100

    Move tests for DefaultDetails from mocha to jest

--- src/src/components/Recipe/Detail/DefaultDetail/__tests__/DefaultDetail.test.js
+++ src/src/components/Recipe/Detail/DefaultDetail/__tests__/DefaultDetail.test.js
@@ -1,63 +1,352 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import ReactDOM from 'react-dom'
 import Immutable from 'immutable'
+import { shallow } from 'enzyme'
 
-import { CookingInstructions } from 'Recipe/CookingInstructions'
 import DefaultDetail from 'Recipe/Detail/DefaultDetail'
+import { CookingInstructions } from 'Recipe/CookingInstructions'
+import Nutrition from 'Recipe/Detail/Nutrition'
+import { ShortlistButton } from 'Recipe/ShortlistButton'
 
 describe('<DefaultDetail />', () => {
-  let wrapper
-  let menuRecipeDetailVisibilityChangeSpy
-
-  beforeEach(() => {
-    menuRecipeDetailVisibilityChangeSpy = jest.fn()
-
-    wrapper = shallow(<DefaultDetail
-      view="detail"
-      tag="test"
-      media={Immutable.List([{ src: 'src', width: '100' }])}
-      title="title"
-      count={3}
-      average={1}
-      per100Grams={Immutable.Map({ id: 1 })}
-      perPortion={Immutable.Map({ id: 2 })}
-      ingredients={Immutable.List([{}])}
-      allergens={Immutable.List(['test'])}
-      stock={10}
-      inBasket
-      id='123'
-      useWithin="5 days"
+  const DEFAULT_DETAIL = (
+    <DefaultDetail 
+      allergens={Immutable.List(['allergens'])}
+      cuisine='cuisine'
       cookingTime={123}
-      cutoffDate="2019-09-01 10:34:23"
-      description="amazing recipe"
+      dairyFree={false}
+      diet='vegetarian'
+      equipment={Immutable.List(['spoon, mixer'])}
+      id='123'
+      ingredients={Immutable.fromJS([{
+        allergens:'allergens',
+        id:'id',
+        label:'label',
+        media: { images: [{urls: [{width: 10}]}] },
+        name:'name',
+        subIngredients:'subIngredients',
+      }])}
+      per100Grams={Immutable.Map({ 
+        carbs: 1,
+        carbsSugars: 1,
+        energyKj: 1,
+        energyKcal: 1,
+        fat: 1,
+        fatSaturates: 1,
+        fibre: 1,
+        protein: 1,
+        salt: 1,
+      })}
+      perPortion={Immutable.Map({ 
+        carbs: 1,
+        carbsSugars: 1,
+        energyKj: 1,
+        energyKcal: 1,
+        fat: 1,
+        fatSaturates: 1,
+        fibre: 1,
+        protein: 1,
+        salt: 1,
+      })}
+      showCookingInstruction={false}
+      showShortlistButton={false}
+      title='title'
+      useWithin='5 days'
+      view='detail'
       youWillNeed={Immutable.List(['spoon', 'fork'])}
-      cuisine="Fusion"
-      diet="vegetarian"
-      equipment={Immutable.List(['spoon', 'mixer'])}
-      menuRecipeDetailVisibilityChange={menuRecipeDetailVisibilityChangeSpy}
-    />)
+    />
+  )
+
+  test('Render without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(DEFAULT_DETAIL, div)
   })
 
-  describe('<CookingInstructions />', () => {
-    describe('when showCookingInstruction is true', () => {
-      test('should contain CookingInstructions in cookingInstructionsDesktop ', () => {
+  let wrapper
+  
+  beforeEach(() => {
+    wrapper = shallow(DEFAULT_DETAIL)
+  })
+
+  describe('Equipment required', () => {
+    describe('when equipment is not empty', () => {
+      beforeEach(() => {
+        wrapper.setProps({ equipment: Immutable.List(['spoon, mixer']) })
+      })
+
+      test('should return the equipment required', () => {
+        expect(wrapper.text()).toContain('spoon, mixer')
+      })
+    })
+
+    describe('when equipment is empty', () => {
+      beforeEach(() => {
+        wrapper.setProps({ equipment: Immutable.List([]) })
+      })
+
+      test('should not return the equipment required', () => {
+        expect(wrapper.text()).not.toContain('spoon, mixer')
+      })
+    })
+  })
+
+  describe('You will need', () => {
+    describe('when youWillNeed is not empty', () => {
+      beforeEach(() => {
+        wrapper.setProps({ youWillNeed: Immutable.List(['spoon', 'fork']) })
+      })
+
+      test('should return the youWillNeed', () => {
+        expect(wrapper.text()).toContain('spoon, fork')
+      })
+    })
+
+    describe('when youWillNeed is empty', () => {
+      beforeEach(() => {
+        wrapper.setProps({ youWillNeed: Immutable.List([]) })
+      })
+      
+      test('should not return the youWillNeed', () => {
+        expect(wrapper.text()).not.toContain('spoon, fork')
+      })
+    })
+  })
+
+  describe('<Ingredients />', () => {
+    describe('when ingredients is not empty', () => {
+      beforeEach(() => {
+        wrapper.setProps({ 
+          ingredients: Immutable.fromJS([{
+            allergens:'allergens',
+            id:'id',
+            label:'label',
+            media: { images: [{urls: [{width: 10}]}] },
+            name:'name',
+            subIngredients:'subIngredients',
+          }])
+        })
+      })
+
+      test('should return the <Ingredients />', () => {
+        expect(wrapper.find('Ingredients')).toHaveLength(1)
+      })
+    })
+
+    describe('when ingredients is empty', () => {
+      beforeEach(() => {
+        wrapper.setProps({ ingredients: Immutable.fromJS([]) })
+      })
+      
+      test('should not return the <Ingredients />', () => {
+        expect(wrapper.find('Ingredients').exists()).toBe(false)
+      })
+    })
+  })
+
+  describe('When perPortion is not empty', () => {
+    beforeEach(() => {
+      wrapper.setProps({ 
+        perPortion: Immutable.Map({ 
+          carbs: 1,
+          carbsSugars: 1,
+          energyKj: 1,
+          energyKcal: 1,
+          fat: 1,
+          fatSaturates: 1,
+          fibre: 1,
+          protein: 1,
+          salt: 1,
+        })
+      })
+    })
+
+    test('should return the <Nutrition />', () => {
+      expect(wrapper.find(Nutrition)).toHaveLength(1)
+    })
+
+    describe('and showCookingInstruction is true', () => {
+      beforeEach(() => {
         wrapper.setProps({ showCookingInstruction: true })
+      })
+
+      test('should return the <CookingInstructions />', () => {
         expect(wrapper.find('.cookingInstructionsDesktop').find(CookingInstructions)).toHaveLength(1)
       })
-    
-      test('should contain CookingInstructions in cookingInstructionsMobile', () => {
+    })
+
+    describe('and showCookingInstruction is false', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showCookingInstruction: false })
+      })
+
+      test('should not return the <CookingInstructions />', () => {
+        expect(wrapper.find('.cookingInstructionsDesktop').find(CookingInstructions).exists()).toBe(false)
+      })
+    })
+  })
+
+  describe('When perPortion is empty', () => {
+    beforeEach(() => {
+      wrapper.setProps({ perPortion: Immutable.fromJS([]) })
+    })
+
+    test('should not return the <Nutrition />', () => {
+      expect(wrapper.find(Nutrition).exists()).toBe(false)
+    })
+
+    test('should not return the <CookingInstructions />', () => {
+      expect(wrapper.find('.cookingInstructionsDesktop').find(CookingInstructions).exists()).toBe(false)
+    })
+  })
+
+  describe('When allergens is not empty', () => {
+    beforeEach(() => {
+      wrapper.setProps({ allergens: Immutable.List(['allergens']) })
+    })
+
+    test('should return the <IngredientsList />', () => {
+      expect(wrapper.find('IngredientsList')).toHaveLength(1)
+    })
+
+    test('should return the <Allergens />', () => {
+      expect(wrapper.find('Allergens')).toHaveLength(1)
+    })
+
+    describe('and showCookingInstruction is true', () => {
+      beforeEach(() => {
         wrapper.setProps({ showCookingInstruction: true })
+      })
+
+      test('should return <CookingInstructions />', () => {
         expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions)).toHaveLength(1)
       })
     })
-  
-    describe('when showCookingInstruction is false', () => {
-      test('should contain CookingInstructions in cookingInstructionsDesktop ', () => {
-        expect(wrapper.find('.cookingInstructionsDesktop').find(CookingInstructions).exists()).toBe(false)
-      })
-    
-      test('should contain CookingInstructions in cookingInstructionsMobile', () => {
+
+    describe('and showCookingInstruction is false', () => {
+      test('should not return <CookingInstructions />', () => {
         expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions).exists()).toBe(false)
+      })
+    })
+  })
+
+  describe('When ingredients is not empty', () => {
+    beforeEach(() => {
+      wrapper.setProps({ 
+        ingredients: Immutable.fromJS([{
+          allergens:'allergens',
+          id:'id',
+          label:'label',
+          media: { images: [{urls: [{width: 10}]}] },
+          name:'name',
+          subIngredients:'subIngredients',
+        }])
+      })
+    })
+
+    test('should return the <IngredientsList />', () => {
+      expect(wrapper.find('IngredientsList')).toHaveLength(1)
+    })
+
+    test('should return the <Allergens />', () => {
+      expect(wrapper.find('Allergens')).toHaveLength(1)
+    })
+
+    describe('and showCookingInstruction is true', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showCookingInstruction: true })
+      })
+
+      test('should return <CookingInstructions />', () => {
+        expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions)).toHaveLength(1)
+      })
+    })
+
+    describe('and showCookingInstruction is false', () => {
+      test('should not return <CookingInstructions />', () => {
+        expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions).exists()).toBe(false)
+      })
+    })
+  })
+
+  describe('When allergens and ingredients is not empty', () => {
+    beforeEach(() => {
+      wrapper.setProps({ 
+        allergens: Immutable.List(['allergens']),
+        ingredients: Immutable.fromJS([{
+          allergens:'allergens',
+          id:'id',
+          label:'label',
+          media: { images: [{urls: [{width: 10}]}] },
+          name:'name',
+          subIngredients:'subIngredients',
+        }])
+      })
+    })
+
+    test('should return the <IngredientsList />', () => {
+      expect(wrapper.find('IngredientsList')).toHaveLength(1)
+    })
+
+    test('should return the <Allergens />', () => {
+      expect(wrapper.find('Allergens')).toHaveLength(1)
+    })
+
+    describe('and showCookingInstruction is true', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showCookingInstruction: true })
+      })
+
+      test('should return <CookingInstructions />', () => {
+        expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions)).toHaveLength(1)
+      })
+    })
+
+    describe('and showCookingInstruction is false', () => {
+      test('should not return <CookingInstructions />', () => {
+        expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions).exists()).toBe(false)
+      })
+    })
+  })
+
+  describe('When allergens and ingredients is empty', () => {
+    beforeEach(() => {
+      wrapper.setProps({ 
+        allergens: Immutable.List([]),
+        ingredients: Immutable.fromJS([])
+      })
+    })
+
+    test('should not return the <IngredientsList />', () => {
+      expect(wrapper.find('IngredientsList').exists()).toBe(false)
+    })
+
+    test('should not return the <Allergens />', () => {
+      expect(wrapper.find('Allergens').exists()).toBe(false)
+    })
+
+    test('should not return the <CookingInstructions />', () => {
+      expect(wrapper.find('.cookingInstructionsMobile').find(CookingInstructions).exists()).toBe(false)
+    })
+  })
+
+  describe('<ShortlistButton />', () => {
+    describe('when showShortlistButton is true', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showShortlistButton: true })
+      })
+
+      test('should return the <ShortlistButton />', () => {
+        expect(wrapper.find(ShortlistButton)).toHaveLength(1)
+      })
+    })
+
+    describe('when showShortlistButton is false', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showShortlistButton: false })
+      })
+
+      test('should not return the <ShortlistButton />', () => {
+        expect(wrapper.find(ShortlistButton).exists()).toBe(false)
       })
     })
   })
