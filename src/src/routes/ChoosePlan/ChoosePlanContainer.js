54e4c5cb8db89b87184ce5e957214eba49a1dcb8
Safeer Hayat <saf_hayat94@hotmail.com>
2019-10-02 09:42:39 +0100

    Conditionally renders alert to show excl extras message

--- src/src/routes/ChoosePlan/ChoosePlanContainer.js
+++ src/src/routes/ChoosePlan/ChoosePlanContainer.js
@@ -2,7 +2,16 @@ import { connect } from 'react-redux'
 import { choosePlanContinue } from 'actions/choosePlan'
 import { ChoosePlan } from './ChoosePlan'
 
-const mapStateToProps = () => null
+const mapStateToProps = () => {
+  const surchargeTotal = '5.99'
+  const deliveryTotal = '2.99'
+  const grossTotal = '40.73'
+  const recipeTotal = '31.75'
+
+  return {
+    extrasIncluded: surchargeTotal || deliveryTotal || grossTotal !== recipeTotal,
+  }
+}
 
 const ChoosePlanContainer = connect(mapStateToProps, {
   choosePlanContinue
