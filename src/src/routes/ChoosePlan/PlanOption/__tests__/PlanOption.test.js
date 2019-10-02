27137ba7406881052c6ab88e146e58e33ffc27ec
Safeer Hayat <saf_hayat94@hotmail.com>
2019-10-02 09:43:09 +0100

    Adds unit tests

--- src/src/routes/ChoosePlan/PlanOption/__tests__/PlanOption.test.js
+++ src/src/routes/ChoosePlan/PlanOption/__tests__/PlanOption.test.js
@@ -4,16 +4,28 @@ import { PlanOption } from '../PlanOption'
 
 describe('PlanOption', () => {
   test('when totalPriceDiscounted is provided, should render both prices with full price struck through', () => {
-    const wrapper = shallow(<PlanOption totalPrice='20.00' totalPriceDiscounted='10.00'/>)
+    const wrapper = shallow(<PlanOption totalPrice='20.00' totalPriceDiscounted='10.00' />)
 
     expect(wrapper.find('.totalPriceDiscounted').text()).toEqual('£20.00£10.00')
     expect(wrapper.find('s').text()).toEqual('£20.00')
   })
 
   test('when totalPriceDiscounted is not provided, should render the full price', () => {
-    const wrapper = shallow(<PlanOption totalPrice='20.00'/>)
+    const wrapper = shallow(<PlanOption totalPrice='20.00' />)
 
     expect(wrapper.find('.totalPriceDiscounted').exists()).toEqual(false)
     expect(wrapper.find('.totalPrice').text()).toEqual('£20.00')
+  })
+
+  test('should show excl extras message if any extras have been chosen', () => {
+    const wrapper = shallow(<PlanOption totalPrice='20.00' showExclExtras />)
+
+    expect(wrapper.find('.exclExtras').length).toEqual(1)
+  })
+
+  test('should NOT show excl extras message if no extras have been chosen', () => {
+    const wrapper = shallow(<PlanOption totalPrice='20.00' showExclExtras={false} />)
+
+    expect(wrapper.find('.exclExtras').length).toEqual(0)
   })
 })
