b6ec01d37f73ba5af1e480712eb396b93f1f6c66
David Torres <david.torres@gousto.co.uk>
2019-10-03 22:51:52 +0100

    Extract the logic that sets the port and path in endpoint.js

--- src/src/config/endpoint.js
+++ src/src/config/endpoint.js
@@ -10,14 +10,49 @@ const getProtocol = (service, isServerSide, environment) => {
   return 'http'
 }
 
+const getPath = (service, isServerSide, environment, version) => {
+  const isCore = service === 'core'
+
+  if (service === 'webclient') {
+    return ''
+  }
+
+  if (environment === 'local' && !isCore) {
+    return `/${service}/${version}`
+  }
+
+  if (isServerSide) {
+    return ''
+  }
+
+  let path = ''
+  if (!isCore) {
+    path += `/${service}`
+  }
+  if (version) {
+    path += `/${version}`
+  }
+
+  return path
+}
+
+const getPort = (service, environment, isClientSide) => {
+  if (service === 'webclient' || environment !== 'local' || isClientSide) {
+    return ''
+  }
+
+  return ':80'
+}
+
 function endpoint(service, version = '') {
   const protocol = getProtocol(service, __SERVER__, __ENV__)
+  const path = getPath(service, __SERVER__, __ENV__, version)
+  const port = getPort(service, __ENV__, __CLIENT__)
   let domain
 
   if (service === 'webclient') {
     domain = `${__ENV__}-${service}.${__DOMAIN__}`
-    if (__SERVER__) {
-    } else if (__ENV__ === 'production') {
+    if (!__SERVER__ && __ENV__ === 'production') {
       domain = `www.${__DOMAIN__}`
     }
   } else {
@@ -27,31 +62,13 @@ function endpoint(service, version = '') {
         domain += `.${__DOMAIN__}`
       } else {
         domain = `${__ENV__}-api.${__DOMAIN__}`
-        if (service !== 'core') {
-          domain += `/${service}`
-        }
-        if (version) {
-          domain += `/${version}`
-        }
       }
     } else {
-      if (service === 'core') {
-        if (__CLIENT__) {
-          domain = `api.${__DOMAIN__}`
-        } else {
-          domain = `api.${__DOMAIN__}:80`
-        }
-      } else {
-        if (__CLIENT__) {
-          domain = `api.${__DOMAIN__}/${service}/${version}`
-        } else {
-          domain = `api.${__DOMAIN__}:80/${service}/${version}`
-        }
-      }
+      domain = `api.${__DOMAIN__}`
     }
   }
 
-  const url = `${protocol}://${domain}`
+  const url = `${protocol}://${domain}${port}${path}`
 
   return url
 }
