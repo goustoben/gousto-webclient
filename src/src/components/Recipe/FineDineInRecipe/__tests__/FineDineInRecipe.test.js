07c0b8be9140c2fc3890e9d40c222b4c5cda8cb3
PasparanGabriel <gabriel.pasparan@gousto.co.uk>
2019-10-04 14:15:15 +0100

    Update test for FineDineInRecipe

--- src/src/components/Recipe/FineDineInRecipe/__tests__/FineDineInRecipe.test.js
+++ src/src/components/Recipe/FineDineInRecipe/__tests__/FineDineInRecipe.test.js
@@ -1,90 +1,51 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import { shallow } from 'enzyme'
 import Immutable from 'immutable'
-
-import Title from 'Recipe/Title'
-import RangeBadge from 'Recipe/RangeBadge'
-import AddButton from 'Recipe/AddButton'
-import StockBadge from 'Recipe/StockBadge'
-import TasteScore from 'Recipe/TasteScore'
 
 import FineDineInRecipe from 'Recipe/FineDineInRecipe'
 import { ShortlistButton } from 'Recipe/ShortlistButton'
 
 describe('<FineDineInRecipe />', () => {
-  describe('rendering', () => {
-    let recipe
-    let view
-    beforeEach(() => {
-      recipe = Immutable.fromJS({
-        id: 1,
-        title: 'test',
-        rating: {
-          count: 1,
-          average: 4,
-        },
-        url: '',
-        cookingTime: 1,
-        cookingTimeFamily: 1,
-        shelfLifeDays: '',
-        media: {
-          images: [
-            {
-              urls: [
-                {},
-                {},
-                {
-                  src: 'test',
-                },
-              ],
-            },
-          ],
-        },
+  let wrapper
+    
+  const FINE_DINE_IN_RECIPE = <FineDineInRecipe 
+    id='id'
+    cookingTime={1}
+    features={Immutable.Map({})}
+    useWithin='3 days'
+    title='title'
+  />
+
+  beforeEach(() => {
+    wrapper = shallow(FINE_DINE_IN_RECIPE)
+  })
+
+  test('Render without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(FINE_DINE_IN_RECIPE, div)
+  })
+
+  describe('<ShortlistButton />', () => {
+    describe('when showShortlistButton is true', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showShortlistButton: true })
       })
-      view = 'grid'
+
+      test('should return the <ShortlistButton />', () => {
+        expect(wrapper.find(ShortlistButton).exists()).toBe(true)
+      })
     })
 
-    test('should return a <div>', () => {
-      const wrapper = shallow(<FineDineInRecipe recipe={recipe} view={view} />)
-      expect(wrapper.type()).toBe('div')
-    })
+    describe('when showShortlistButton is false', () => {
+      beforeEach(() => {
+        wrapper.setProps({ showShortlistButton: false })
+      })
 
-    test('should contain one Title component', () => {
-      const wrapper = shallow(<FineDineInRecipe recipe={recipe} view={view} />)
-
-      expect(wrapper.find(Title).length).toEqual(1)
-    })
-
-    test('should contain one Range Badge component', () => {
-      const wrapper = shallow(<FineDineInRecipe recipe={recipe} view={view} />)
-
-      expect(wrapper.find(RangeBadge).length).toEqual(1)
-    })
-
-    test('should contain one AddButton component', () => {
-      const wrapper = shallow(<FineDineInRecipe recipe={recipe} view={view} />)
-
-      expect(wrapper.find(AddButton).length).toEqual(1)
-    })
-
-    test('should contain one StockBadge component', () => {
-      const wrapper = shallow(<FineDineInRecipe recipe={recipe} view={view} />)
-
-      expect(wrapper.find(StockBadge).length).toEqual(1)
-    })
-
-    test('should contain a TasteScore component', () => {
-      const wrapper = shallow(<FineDineInRecipe tasteScore={95} view={view} />)
-
-      expect(wrapper.find(TasteScore)).toHaveLength(1)
-      expect(wrapper.find(TasteScore).prop('score')).toEqual(95)
-    })
-
-    test('should contain one ShortlistButton if feature flag is showShortlistButton is true and onMobile is true', () => {
-      const wrapper = shallow(<FineDineInRecipe />)
-      wrapper.setProps({ showShortlistButton: true })
-      expect(wrapper.find(ShortlistButton).length).toBe(1)
+      test('should not return the <ShortlistButton />', () => {
+        expect(wrapper.find(ShortlistButton).exists()).toBe(false)
+      })
     })
   })
 })
