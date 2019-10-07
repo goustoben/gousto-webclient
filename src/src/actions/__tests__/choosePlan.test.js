8bf4735611bfbe9bd5405e05c8349c2bd00066ba
Andy Davison <andy.davison@gousto.co.uk>
2019-10-07 16:34:02 +0100

    Add tests for choosePlan actions

--- src/src/actions/__tests__/choosePlan.test.js
+++ src/src/actions/__tests__/choosePlan.test.js
@@ -1,16 +1,84 @@
+import actionTypes from 'actions/actionTypes'
+import { basketPromoCodeChange } from 'actions/basket'
 import { redirect } from 'actions/redirect'
 import routes from 'config/routes'
-import { choosePlanContinue } from '../choosePlan'
+import { choosePlanContinue, trackSubscriptionOptionSelected, clearTempPromoCode, stashTempPromoCode } from '../choosePlan'
+
+jest.mock('actions/basket', () => ({
+  basketPromoCodeChange: jest.fn()
+}))
 
 jest.mock('actions/redirect', () => ({
   redirect: jest.fn()
 }))
 
-describe('choosePlanContinue', () => {
+describe('choosePlan actions', () => {
+  const dispatch = jest.fn()
 
-  it('should call the redirect action', () => {
-    choosePlanContinue()
+  describe('trackSubscriptionOptionSelected', () => {
+    const selectedOption = 'subscription'
 
-    expect(redirect).toHaveBeenCalledWith(routes.client['check-out'])
+    it('should call the tracking action with SubscriptionOption Selected', () => {
+      trackSubscriptionOptionSelected(selectedOption)(dispatch)
+
+      expect(dispatch).toHaveBeenCalledWith({
+        type: actionTypes.TRACKING,
+        trackingData: {
+          actionType: 'SubscriptionOption Selected',
+          subscriptionOption: selectedOption
+        }
+      })
+    })
+  })
+
+  describe('clearTempPromoCode', () => {
+
+    it('should dispatch a temp action with a blank promoCode', () => {
+      clearTempPromoCode()(dispatch)
+
+      expect(dispatch).toHaveBeenCalledWith({
+        type: actionTypes.TEMP,
+        promoCode: ''
+      })
+    })
+  })
+
+  describe('stashTempPromoCode', () => {
+    const testPromoCode = 'TEST-PROMO'
+
+    it('should dispatch a temp action with the given promoCode', () => {
+      stashTempPromoCode(testPromoCode)(dispatch)
+
+      expect(dispatch).toHaveBeenCalledWith({
+        type: actionTypes.TEMP,
+        promoCode: testPromoCode
+      })
+    })
+  })
+
+  describe('choosePlanContinue', () => {
+    const subscriptionOption = 'subscription'
+    const chosenPromoCode = 'TEST-PROMO'
+
+    it('should call basketPromoCodeChange action with given promoCode', () => {
+      choosePlanContinue(subscriptionOption, chosenPromoCode)(dispatch)
+      expect(basketPromoCodeChange).toHaveBeenCalledWith(chosenPromoCode)
+    })
+    it('should call the choose plan continue action with given subscriptionOption', () => {
+      choosePlanContinue(subscriptionOption, chosenPromoCode)(dispatch)
+
+      expect(dispatch).toHaveBeenCalledWith({
+        type: actionTypes.CHOOSE_PLAN_CONTINUE,
+        subscriptionOption,
+        trackingData: {
+          actionType: 'SubscriptionOption Chosen',
+          subscriptionOption
+        }
+      })
+    })
+    it('should call the redirect action', () => {
+      choosePlanContinue(subscriptionOption, chosenPromoCode)(dispatch)
+      expect(redirect).toHaveBeenCalledWith(routes.client['check-out'])
+    })
   })
 })
