7e68bd10a646070a991aedf57c77ebf3462bdc23
Artiom Nistrean <artiom.nistrean@gmail.com>
2019-10-14 15:03:30 +0100

    fixed confirmation text to read should cutoff at
    removed comments

--- src/src/routes/OrderConfirmation/helper.js
+++ src/src/routes/OrderConfirmation/helper.js
@@ -1,12 +1,12 @@
 import moment from 'moment'
 
 export const getHeaderDetails = (order) => {
-  const { humanDeliveryDate, whenCutoff, deliverySlot } = order.toJS()
+  const { humanDeliveryDate, deliverySlot, shouldCutoffAt } = order.toJS()
   const { deliveryStart, deliveryEnd } = deliverySlot
   const deliveryStartFormat = moment(deliveryStart, 'HH:mm:ss').format('ha')
   const deliveryEndFormat = moment(deliveryEnd, 'HH:mm:ss').add(1, 'seconds').format('ha')
-  const cutOffTimeFormat = moment(whenCutoff).add(1, 'seconds').format('ha')
-  const cutoffDayFormat = moment(whenCutoff).format('dddd Do MMMM')
+  const cutOffTimeFormat = moment(shouldCutoffAt).add(1, 'seconds').format('ha')
+  const cutoffDayFormat = moment(shouldCutoffAt).format('dddd Do MMMM')
 
   return ({
     deliveryDate: humanDeliveryDate,
