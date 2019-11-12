d5e5d09166563456a840d22561db5df51b180ca7
Ashley Wharton <ashley.wharton@gousto.co.uk>
2019-11-12 14:13:24 +0000

    Adaped pricing API to handle DSLT id and delivery tariff id

--- src/src/apis/pricing.js
+++ src/src/apis/pricing.js
@@ -2,13 +2,24 @@ import fetch from 'utils/fetch'
 import endpoint from 'config/endpoint'
 import routes from 'config/routes'
 
-function pricing(accessToken, items, deliveryDate, deliverySlotId, promocode, tariffId) {
+function pricing(
+  accessToken,
+  items,
+  deliveryDate,
+  deliverySlotId,
+  promocode,
+  daySlotLeadTimeId,
+  deliveryTariffId,
+  tariffId
+) {
   return fetch(accessToken, `${endpoint('core')}${routes.core.prices}`, {
     items,
     promo_code: promocode,
     delivery_slot_id: deliverySlotId,
     delivery_date: deliveryDate,
     tariff_id: tariffId,
+    day_slot_lead_time_id: daySlotLeadTimeId,
+    delivery_tariff_id: deliveryTariffId
   }, 'GET')
 }
 
