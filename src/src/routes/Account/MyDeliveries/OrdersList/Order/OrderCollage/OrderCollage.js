1a1efd68b96a6a7af4612e32a1f4aa6c614acb94
Mark Gono <markgono@gmail.com>
2019-11-20 17:45:38 +0000

    edit mydeliveries ordercollagemake functional component

--- src/src/routes/Account/MyDeliveries/OrdersList/Order/OrderCollage/OrderCollage.js
+++ src/src/routes/Account/MyDeliveries/OrdersList/Order/OrderCollage/OrderCollage.js
@@ -1,34 +1,33 @@
-import PropTypes from 'prop-types'
 import React from 'react'
+import PropTypes from 'prop-types'
 import Immutable from 'immutable'
+
 import Image from 'Image'
 import placeholderSrc from 'media/images/recipe-placeholder.png'
 import css from './OrderCollage.css'
 
-class OrderCollage extends React.PureComponent {
-  render() {
-    const recipes = this.props.recipes
-    const maxRecipes = ['confirmed', 'dispatched'].indexOf(this.props.orderState) > -1 ? recipes.size : 4
-    const blankCards = []
-    const recipeCollage = recipes.map((recipe) =>
-      <div className={css.collageItem} key={recipe.get('id')}>
-        <Image className={css.collageImage} media={recipe.get('image') || placeholderSrc} />
-      </div>
-    )
-    for (let i = recipes.size; i < maxRecipes; i++) {
-      blankCards.push(<div className={css.empty} key={i}>
+const OrderCollage = ({ recipes, orderState }) => {
+  const maxRecipes = ['confirmed', 'dispatched'].indexOf(orderState) > -1 ? recipes.size : 4
+  const blankCards = []
+  const recipeCollage = recipes.map((recipe) =>
+    <div className={css.collageItem} key={recipe.get('id')}>
+      <Image className={css.collageImage} media={recipe.get('image') || placeholderSrc} />
+    </div>
+  )
+  for (let i = recipes.size; i < maxRecipes; i++) {
+    blankCards.push(
+      <div className={css.empty} key={i}>
         <div className={css.emptyItem}></div>
-                      </div>)
-    }
-
-    return (
-      <div className={css.collageContainer}>
-        {recipeCollage}
-        {blankCards}
       </div>
     )
   }
 
+  return (
+    <div className={css.collageContainer}>
+      {recipeCollage}
+      {blankCards}
+    </div>
+  )
 }
 
 OrderCollage.propTypes = {
