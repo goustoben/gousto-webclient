345159432aeef205b413827531b70a2bfbe06254
Ana Pasparan <ana.pasparan@gousto.co.uk>
2019-11-22 16:32:06 +0000

    run tests in paralel

---
+++ scripts/validate-mobile.sh
@@ -0,0 +1,9 @@
+
+AWS_ACCESS_KEY_ID=$BETA_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$BETA_AWS_SECRET_ACCESS_KEY \
+gousto env sglease $ENVIRONMENT --sgs platform-tools -p 4444
+AWS_ACCESS_KEY_ID=$BETA_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$BETA_AWS_SECRET_ACCESS_KEY \
+gousto env sglease $ENVIRONMENT
+
+cd tests/e2e
+chmod a+x runtestsmobile.sh
+./runtestsmobile.sh
