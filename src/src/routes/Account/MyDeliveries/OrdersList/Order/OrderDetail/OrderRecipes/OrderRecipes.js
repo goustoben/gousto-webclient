c06495a338f163b80174a2315d9788a321f16e95
Safeer Hayat <saf_hayat94@hotmail.com>
2019-11-25 11:56:07 +0000

    Moves edit recipes to top right and restyles it

--- src/src/routes/Account/MyDeliveries/OrdersList/Order/OrderDetail/OrderRecipes/OrderRecipes.js
+++ src/src/routes/Account/MyDeliveries/OrdersList/Order/OrderDetail/OrderRecipes/OrderRecipes.js
@@ -3,7 +3,6 @@ import React from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import Immutable from 'immutable'
 
-import { Button } from 'goustouicomponents'
 import Link from 'Link'
 import routes from 'config/routes'
 import Content from 'containers/Content'
@@ -17,33 +16,31 @@ const OrderRecipes = ({
   orderState,
   whenCutoff,
 }) => (
-  <div>
-    <div className={`${css.header} ${css.hideInMobile}`}>
-      <Content contentKeys="mydeliveriesOrderOrderrecipesTitle" >
-        <span>Recipe box</span>
-      </Content>
+    <div>
+      <div className={`${css.headerRow} ${css.hideInMobile}`}>
+        <Content contentKeys="mydeliveriesOrderOrderrecipesTitle" >
+          <span className={css.header}>Recipe box</span>
+        </Content>
+        {['menu open', 'recipes chosen'].indexOf(orderState) > -1 ?
+          <div className={css.buttonRow}>
+            <Link className={css.editLink} to={`${routes.client.menu}/${orderId}`} clientRouted={false}>
+              {orderState === 'recipes chosen' ? 'Edit recipes' : 'Choose recipes'}
+            </Link>
+          </div>
+          : null}
+      </div>
+      <OrderSideSwipe recipes={recipes} orderState={orderState} />
+      {orderState === 'menu open' ?
+        <div className={css.textRow}>
+          <p className={css.subHeader}>
+            <Content contentKeys="mydeliveriesOrderOrderrecipesMessage" >
+              <span>You haven't chosen any recipes yet.</span>
+            </Content>
+          </p>
+          <p>If you do not choose by <strong>{whenCutoff}</strong>, Gousto will send you a selection of recipes based on your subscription settings.</p>
+        </div>
+        : null}
     </div>
-    <OrderSideSwipe recipes={recipes} orderState={orderState} />
-    {orderState === 'menu open' ?
-      <div className={css.textRow}>
-        <p className={css.subHeader}>
-          <Content contentKeys="mydeliveriesOrderOrderrecipesMessage" >
-            <span>You haven't chosen any recipes yet.</span>
-          </Content>
-        </p>
-        <p>If you do not choose by <strong>{whenCutoff}</strong>, Gousto will send you a selection of recipes based on your subscription settings.</p>
-      </div>
-      : null}
-    {['menu open', 'recipes chosen'].indexOf(orderState) > -1 ?
-      <div className={css.buttonRow}>
-        <Link to={`${routes.client.menu}/${orderId}`} clientRouted={false}>
-          <Button color={orderState === 'recipes chosen' ? 'secondary' : 'primary'} noDecoration>
-            {orderState === 'recipes chosen' ? 'Edit recipes' : 'Choose recipes'}
-          </Button>
-        </Link>
-      </div>
-      : null}
-  </div>
 )
 
 OrderRecipes.propTypes = {
