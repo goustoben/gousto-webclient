ce1a501d53a24cf5c933820ae15fe1b3418c6221
Chris Vibert <chris.vibert@gousto.co.uk>
2019-12-02 15:11:09 +0000

    [TC-1059] Refactor selectors for page loading
    The isOrderConfirmationPage selector has been split into two - one which deals with loading referral offers, and one which deals with loading orderDetails. This has been done so that the latter selector (isOrderDetailsLoading) can be reused by the OrderAddOns page in a following commit.

--- src/src/selectors/__tests__/orderConfirmation.test.js
+++ src/src/selectors/__tests__/orderConfirmation.test.js
@@ -1,22 +1,25 @@
 import Immutable from 'immutable'
 
-import { isOrderConfirmationPageLoading } from 'selectors/orderConfirmation'
+import {
+  isOrderConfirmationPageLoading,
+  isOrderDetailsLoading,
+  isReferralOfferLoading,
+} from 'selectors/orderConfirmation'
 
 describe('orderConfirmation selectors', () => {
-  describe('isOrderConfirmationPageLoading', () => {
+  describe('isOrderDetailsLoading', () => {
     describe('when any pending action is true', () => {
       const state = {
         pending: Immutable.Map({
           'PRODUCT_CATEGORIES_RECEIVE': false,
           'PRODUCTS_STOCK_CHANGE': false,
           'PRODUCTS_RECEIVE': true,
-          'USER_LOAD_REFERRAL_OFFER': false,
           'USER_LOAD_ORDERS': false,
         })
       }
 
-      it('should return true', () => {
-        expect(isOrderConfirmationPageLoading(state)).toEqual(true)
+      test('returns true', () => {
+        expect(isOrderDetailsLoading(state)).toEqual(true)
       })
     })
 
@@ -26,13 +29,114 @@ describe('orderConfirmation selectors', () => {
           'PRODUCT_CATEGORIES_RECEIVE': false,
           'PRODUCTS_STOCK_CHANGE': false,
           'PRODUCTS_RECEIVE': false,
-          'USER_LOAD_REFERRAL_OFFER': false,
           'USER_LOAD_ORDERS': false,
         })
       }
 
-      it('should return false', () => {
-        expect(isOrderConfirmationPageLoading(state)).toEqual(false)
+      test('returns false', () => {
+        expect(isOrderDetailsLoading(state)).toEqual(false)
+      })
+    })
+  })
+
+  describe('isReferralOfferLoading', () => {
+    let state
+
+    describe('when the referral offer is loading', () => {
+      beforeEach(() => {
+        state = {
+          pending: Immutable.Map({
+            'USER_LOAD_REFERRAL_OFFER': true,
+          })
+        }
+      })
+
+      test('returns true', () => {
+        expect(isReferralOfferLoading(state)).toBe(true)
+      })
+    })
+
+    describe('when the referral offer is not loading', () => {
+      describe('and the piece of store exists', () => {
+        beforeEach(() => {
+          state = {
+            pending: Immutable.Map({
+              'USER_LOAD_REFERRAL_OFFER': false,
+            })
+          }
+        })
+
+        test('returns false', () => {
+          expect(isReferralOfferLoading(state)).toBe(false)
+        })
+      })
+
+      describe('and the piece of store does not exist', () => {
+        beforeEach(() => {
+          state = {
+            pending: Immutable.Map({})
+          }
+        })
+
+        test('returns false', () => {
+          expect(isReferralOfferLoading(state)).toBe(false)
+        })
+      })
+    })
+  })
+
+  describe('isOrderConfirmationPageLoading', () => {
+    let orderDetailsLoading
+    let referralOfferLoading
+    let resultingSelector
+
+    describe('when the page is loading', () => {
+      describe('and the order details are loading', () => {
+        beforeEach(() => {
+          orderDetailsLoading = true
+          referralOfferLoading = false
+          resultingSelector = isOrderConfirmationPageLoading.resultFunc(orderDetailsLoading, referralOfferLoading)
+        })
+
+        test('returns true', () => {
+          expect(resultingSelector).toBe(true)
+        })
+      })
+
+      describe('and the referral offer is loading', () => {
+        beforeEach(() => {
+          orderDetailsLoading = false
+          referralOfferLoading = true
+          resultingSelector = isOrderConfirmationPageLoading.resultFunc(orderDetailsLoading, referralOfferLoading)
+        })
+
+        test('returns true', () => {
+          expect(resultingSelector).toBe(true)
+        })
+      })
+
+      describe('and the order details and referral offer are loading', () => {
+        beforeEach(() => {
+          orderDetailsLoading = true
+          referralOfferLoading = true
+          resultingSelector = isOrderConfirmationPageLoading.resultFunc(orderDetailsLoading, referralOfferLoading)
+        })
+
+        test('returns true', () => {
+          expect(resultingSelector).toBe(true)
+        })
+      })
+    })
+
+    describe('when the page is not loading', () => {
+      beforeEach(() => {
+        orderDetailsLoading = false
+        referralOfferLoading = false
+        resultingSelector = isOrderConfirmationPageLoading.resultFunc(orderDetailsLoading, referralOfferLoading)
+      })
+
+      test('returns false', () => {
+        expect(resultingSelector).toBe(false)
       })
     })
   })
--- src/src/selectors/orderConfirmation.js
+++ src/src/selectors/orderConfirmation.js
@@ -1,8 +1,16 @@
-export const isOrderConfirmationPageLoading = state => {
-  const orderConfirmationActions = ['PRODUCT_CATEGORIES_RECEIVE', 'PRODUCTS_STOCK_CHANGE', 'PRODUCTS_RECEIVE', 'USER_LOAD_REFERRAL_OFFER', 'USER_LOAD_ORDERS']
+import { createSelector } from 'reselect'
+
+export const isOrderDetailsLoading = state => {
+  const orderDetailsActions = ['PRODUCT_CATEGORIES_RECEIVE', 'PRODUCTS_STOCK_CHANGE', 'PRODUCTS_RECEIVE', 'USER_LOAD_ORDERS']
 
   return state.pending.some((value, key) => {
-
-    return value && orderConfirmationActions.includes(key)
+    return value && orderDetailsActions.includes(key)
   })
 }
+
+export const isReferralOfferLoading = state => !!state.pending.get('USER_LOAD_REFERRAL_OFFER')
+
+export const isOrderConfirmationPageLoading = createSelector(
+  [isOrderDetailsLoading, isReferralOfferLoading],
+  (orderDetailsLoading, referralOfferLoading) => orderDetailsLoading || referralOfferLoading
+)
