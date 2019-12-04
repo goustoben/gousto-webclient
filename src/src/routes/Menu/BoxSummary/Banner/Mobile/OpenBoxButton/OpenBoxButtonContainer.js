20edaa80b180c13a4fcb5651a50ad57c02db51a3
James Monger <jameskmonger@hotmail.co.uk>
2019-12-04 10:37:04 +0000

    [TR-2385] Remove text from OpenBoxButton

--- src/src/routes/Menu/BoxSummary/Banner/Mobile/OpenBoxButton/OpenBoxButtonContainer.js
+++ src/src/routes/Menu/BoxSummary/Banner/Mobile/OpenBoxButton/OpenBoxButtonContainer.js
@@ -1,18 +1,10 @@
 import { connect } from 'react-redux'
-import { basketSum } from 'utils/basket'
-import { getBasketRecipes } from 'selectors/basket'
-import { getCurrentBoxSummaryView, boxSummaryViews } from 'utils/boxSummary'
 import { isMobile } from 'utils/view'
-import { OpenBoxButton } from './OpenBoxButton.logic'
+import { OpenBoxButton } from './OpenBoxButton'
 
 const mapStateToProp = (state) => {
-  const boxSummaryCurrentView = getCurrentBoxSummaryView(state)
-  const isDetailsView = (boxSummaryCurrentView === boxSummaryViews.DETAILS)
-
   return ({
-    recipeNumber: basketSum(getBasketRecipes(state)),
-    showTextOnButton: isDetailsView,
-    showDetails: state.boxSummaryShow.get('show') && isMobile(state.boxSummaryShow.get('view')),
+    arrowUp: state.boxSummaryShow.get('show') && isMobile(state.boxSummaryShow.get('view')),
   })
 
 }
--- src/src/routes/Menu/BoxSummary/Banner/Mobile/OpenBoxButton/__tests__/OpenBoxButton.test.js
+++ src/src/routes/Menu/BoxSummary/Banner/Mobile/OpenBoxButton/__tests__/OpenBoxButton.test.js
@@ -1,48 +1,19 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { OpenBoxButton } from '../OpenBoxButton.logic'
+import { shallow } from 'enzyme'
+import { OpenBoxButton } from '../OpenBoxButton'
 
 describe('OpenBoxButton', () => {
-  const props = {
-    showDetails: false,
-    showTextOnButton: false,
-    recipeNumber: 0,
-  }
-  const wrapper = mount(<OpenBoxButton {...props} />)
-  describe('render', () => {
-
-    describe('when showTextOnButton false', () => {
-      test('should render arrowUp icon', () => {
-        expect(wrapper.find('.arrowUp')).toHaveLength(1)
-      })
-
-      test('should render arrowDown icon', () => {
-        wrapper.setProps({ showDetails: true })
-        expect(wrapper.find('.arrowDown')).toHaveLength(1)
-      })
+  describe('when arrowUp is true', () => {
+    test('should render arrowUp icon', () => {
+      const wrapper = shallow(<OpenBoxButton arrowUp />)
+      expect(wrapper.find('.arrowUp')).toHaveLength(1)
     })
+  })
 
-    describe('when showTextOnButton true', () => {
-      wrapper.setProps({ showTextOnButton: true })
-      describe('and no recipes in or basket', () => {
-        test('should render text Box Summary on the button', () => {
-          expect(wrapper.find('.openButtonText').text()).toEqual('BOX SUMMARY')
-        })
-      })
-
-      describe('and less then 4 recipes in basket', () => {
-        test('should render text Review Recipes on the button', () => {
-          wrapper.setProps({ recipeNumber: 3 })
-          expect(wrapper.find('.openButtonText').text()).toEqual('REVIEW RECIPES')
-        })
-      })
-
-      describe('and 4 recipes in basket', () => {
-        test('should render text Review Recipes on the button', () => {
-          wrapper.setProps({ recipeNumber: 4 })
-          expect(wrapper.find('.openButtonText').text()).toEqual('REVIEW RECIPES')
-        })
-      })
+  describe('when arrowUp is false', () => {
+    test('should render arrowDown icon', () => {
+      const wrapper = shallow(<OpenBoxButton arrowUp={false} />)
+      expect(wrapper.find('.arrowDown')).toHaveLength(1)
     })
   })
 })
