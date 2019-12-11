4fe34057e8243140b58317889e378a5ab4eda4ce
James Monger <jameskmonger@hotmail.co.uk>
2019-12-11 17:34:08 +0000

    [TECH-13452] store eslint results in code health file

--- src/scripts/get-code-health.js
+++ src/scripts/get-code-health.js
@@ -2,6 +2,12 @@
 const fs = require('fs')
 const path = require('path')
 
+const sanitiseEslintFileResult = (file) => ({ 
+  filePath: file.filePath, 
+  errorCount: file.errorCount, 
+  warningCount: file.warningCount
+})
+
 module.exports = () => {
   const coveragePath = path.resolve(__dirname, '../', 'coverage', 'coverage-summary.json')
 
@@ -9,10 +15,19 @@ module.exports = () => {
     console.log('No code coverage file could be found.')
     process.exit(0)
   }
+
+  const eslintResultsPath = path.resolve(__dirname, '../', 'eslint-results.json')
+
+  if (fs.existsSync(eslintResultsPath) === false) {
+    console.log('No eslint results file could be found.')
+    process.exit(0)
+  }
   
   const coverageSummary = require(coveragePath)
+  const eslintResults = require(eslintResultsPath)
   
   return {
-    coveragePercent: coverageSummary.total.lines.pct
+    coveragePercent: coverageSummary.total.lines.pct,
+    lintResults: eslintResults.map(sanitiseEslintFileResult)
   }
 }
