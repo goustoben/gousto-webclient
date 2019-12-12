cd8740b2ede920667ca8f94ad542f0ae0570ea8f
James Monger <jameskmonger@hotmail.co.uk>
2019-12-12 10:22:56 +0000

    [TECH-13452] Find most recent artifact build

--- src/scripts/code-health-utils/get-code-health.js
+++ src/scripts/code-health-utils/get-code-health.js
@@ -10,14 +10,14 @@ const sanitiseEslintFileResult = (file) => ({
 })
 
 module.exports = () => {
-  const coveragePath = path.resolve(__dirname, '../', 'coverage', 'coverage-summary.json')
+  const coveragePath = path.resolve(__dirname, '../../', 'coverage', 'coverage-summary.json')
 
   if (fs.existsSync(coveragePath) === false) {
     console.log('No code coverage file could be found.')
     process.exit(0)
   }
 
-  const eslintResultsPath = path.resolve(__dirname, '../', 'eslint-results.json')
+  const eslintResultsPath = path.resolve(__dirname, '../../', 'eslint-results.json')
 
   if (fs.existsSync(eslintResultsPath) === false) {
     console.log('No eslint results file could be found.')
--- src/scripts/compare-code-health.js
+++ src/scripts/compare-code-health.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-const request = require('request')
-const getCodeHealth = require('./get-code-health')
-const sanitiseFilePath = require('./sanitise-file-path')
+const getCodeHealthBenchmark = require('./code-health-utils/get-benchmark-file')
+const getCodeHealth = require('./code-health-utils/get-code-health')
+const sanitiseFilePath = require('./code-health-utils/sanitise-file-path')
 
 if (process.argv.length !== 5) {
   console.log('error! missing arguments')
@@ -11,58 +11,18 @@ if (process.argv.length !== 5) {
 
 const [, , argToken, argBranch, argCodeHealthFile] = process.argv
 
-const getCircleCIArtifactsUrl = (username, project, token, branch) => {
-  return `https://circleci.com/api/v1.1/project/github/${username}/${project}/latest/artifacts?circle-token=${token}&branch=${branch}&filter=successful`
-}
-
-const getCodeHealthBenchmark = (token, branch, filePath) => {
-  return new Promise((resolve, reject) => {
-    const getCodeHealthFileCallback = (error, response, body) => {
-      if (error) {
-        return reject(error)
-      }
-
-      return resolve(body)
-    }
-
-    const getArtifactListCallback = (error, response, body) => {
-      if (error) {
-        return reject(error)
-      }
-
-      const codeHealthFile = body.find(file => file.path === filePath)
-
-      if (!codeHealthFile) {
-        return reject(`No file '${filePath}' found`)
-      }
-
-      request(
-        `${codeHealthFile.url}?circle-token=${token}`,
-        { json: true },
-        getCodeHealthFileCallback
-      )
-    }
-
-    request(
-      getCircleCIArtifactsUrl('Gousto', 'gousto-webclient', token, branch),
-      { json: true },
-      getArtifactListCallback
-    )
-  })
-}
-
 const compareCoverage = (benchmark, codeHealth) => {
   console.log('=== Comparing code coverage ===')
-  console.log(`Benchmark coverage: ${benchmark.coveragePercent.toFixed(2)}%`)
-  console.log(`New coverage: ${codeHealth.coveragePercent.toFixed(2)}%`)
+  console.log(`Benchmark coverage: ${benchmark.coveragePercent.toFixed(3)}%`)
+  console.log(`New coverage: ${codeHealth.coveragePercent.toFixed(3)}%`)
   console.log('')
 
   const difference = codeHealth.coveragePercent - benchmark.coveragePercent
 
-  console.log(`Difference in coverage: ${difference.toFixed(2)}%`)
+  console.log(`Difference in coverage: ${difference.toFixed(3)}%`)
   console.log('')
 
-  const pass = (difference < 0)
+  const pass = (difference >= 0)
 
   if (pass) {
     console.log(`Coverage did not decrease, check PASSED`)
@@ -77,6 +37,11 @@ const compareCoverage = (benchmark, codeHealth) => {
 }
 
 const compareEslintResults = (benchmark, codeHealth) => {
+  if (Array.isArray(benchmark.lintResults) === false) {
+    console.log('No eslint results in benchmark, check PASSED')
+    return true
+  }
+
   const increasedWarnings = []
 
   codeHealth.lintResults.forEach(result => {
