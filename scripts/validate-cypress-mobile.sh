1e5c448c9fe3a6d2cadde7cdcb68572ee66d1768
wingleungchoi <wingleungchoi@gmail.com>
2019-12-17 11:56:36 +0000

    TG-533/cypress-poc set baseUrl in with CI variable instead of hardcode

--- scripts/validate-cypress-mobile.sh
+++ scripts/validate-cypress-mobile.sh
@@ -5,6 +5,8 @@ gousto env sglease $ENVIRONMENT --sgs platform-tools -p 4444
 AWS_ACCESS_KEY_ID=$BETA_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$BETA_AWS_SECRET_ACCESS_KEY \
 gousto env sglease $ENVIRONMENT
 
+CYPRESS_baseURL="${ENVIRONMENT}-frontend.${DOMAIN}"
+
 cd tests/e2e
 chmod a+x runcypresstestsmobile.sh
 ./runcypresstestsmobile.sh
