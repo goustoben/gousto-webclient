75fe4c5030fc645020ef138c767b49e706e3850b
wingleungchoi <wingleungchoi@gmail.com>
2019-12-17 14:30:42 +0000

    TG-533/cypress-poc pipeline order to test

--- scripts/validate-cypress.sh
+++ scripts/validate-cypress.sh
@@ -5,7 +5,7 @@ gousto env sglease $ENVIRONMENT --sgs platform-tools -p 4444
 AWS_ACCESS_KEY_ID=$BETA_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$BETA_AWS_SECRET_ACCESS_KEY \
 gousto env sglease $ENVIRONMENT
 
-export CYPRESS_baseUrl="${ENVIRONMENT}-frontend.${DOMAIN}"
+export CYPRESS_baseUrl="https://${ENVIRONMENT}-frontend.gousto.info"
 
 cd tests/e2e
 chmod a+x runcypresstests.sh
