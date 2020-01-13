e849f33dc049823996bd784a01708a1750b641b8
Rianne <rianne.mccartney@gousto.co.uk>
2020-01-13 14:13:09 +0000

    [TR-2654] New test for loginfailed

---
+++ src/src/middlewares/tracking/snowplow/__tests__/loginFailed.test.js
@@ -0,0 +1,24 @@
+import Immutable from 'immutable'
+import { actionTypes } from 'actions/actionTypes'
+import loginFailed from '../global/loginFailed.js'
+
+describe('loginFailed', () => {
+  test('should return the correct action', () => {
+    const state = {
+      error: Immutable.fromJS({
+        [actionTypes.USER_LOGIN]: 'test error reason'
+      })
+    }
+    const action = {
+      type: 'TEST_ACTION_TYPE'
+    }
+
+    const result = loginFailed(action, state)
+    expect(result).toEqual({
+      type: 'TEST_ACTION_TYPE',
+      data: {
+        reason: 'test error reason'
+      }
+    })
+  })
+})
