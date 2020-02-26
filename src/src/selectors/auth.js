b9369bec107702e1760632bdca2b91fc7adc81f9
Ellie Crome <ellie.crome@gousto.co.uk>
2020-02-26 10:19:45 +0000

    [TG-850] Check for auth cookies in checkValidSession (#1761)
    * updated account routes to check for auth cookie and validate it
    
    * adds tests to the routes utils
    
    * Fixes eslint warnings
    
    * Updates check valid session
    
    * Updates authToken to accessToken  in route utils
    
    * Updates test names, makes addTargetToRedirect take in an object
    
    * Updates checkValidSession to make error case more prominent
    
    * Removes async from checkValidSession

--- src/src/selectors/auth.js
+++ src/src/selectors/auth.js
@@ -1,5 +1,9 @@
+
 export const getIsAdmin = state => state.auth.get('isAdmin')
 export const getIsAuthenticated = state => state.auth.get('isAuthenticated')
+export const getAccessToken = state => state.auth.get('accessToken')
+export const getRefreshToken = state => state.auth.get('refreshToken')
+export const getExpiresAt = state => state.auth.get('expiresAt')
 
 export default {
   getIsAdmin,
---
+++ src/src/utils/__tests__/routes.test.js
@@ -0,0 +1,158 @@
+import Immutable from 'immutable'
+import authActions from 'actions/auth'
+import { checkValidSession, addTargetToRedirect } from '../routes'
+
+jest.mock('actions/auth', () => ({
+  authValidate: jest.fn(),
+}))
+
+describe('routes', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('checkValidSession', () => {
+    const next = jest.fn()
+    const replace = jest.fn()
+    const location = {
+      pathname: '/somewhere',
+    }
+
+    describe('when isAuthenticated is true', () => {
+      beforeEach(() => {
+        const store = {
+          getState: () => ({
+            auth: Immutable.Map({
+              isAuthenticated: true,
+            })
+          })
+        }
+
+        checkValidSession(store)({location}, replace, next)
+      })
+
+      test('then next should be called', () => {
+        expect(next).toHaveBeenCalled()
+      })
+
+      test('then replace should NOT be called', () => {
+        expect(replace).not.toHaveBeenCalled()
+      })
+    })
+
+    describe('when isAuthenticated is false', () => {
+      beforeEach(() => {
+        const store = {
+          getState: () => ({
+            auth: Immutable.Map({
+              isAuthenticated: false,
+              accessToken: '',
+              refreshToken: '',
+              expiresAt: ''
+            })
+          })
+        }
+
+        checkValidSession(store)({location}, replace, next)
+      })
+
+      describe('and access token exists', () => {
+        describe('and the token is valid', () => {
+          beforeEach(() => {
+            const store = {
+              getState: () => ({
+                auth: Immutable.Map({
+                  isAuthenticated: false,
+                  accessToken: 'accessToken',
+                  refreshToken: '',
+                  expiresAt: ''
+                })
+              })
+            }
+
+            checkValidSession(store)({location}, replace, next)
+          })
+
+          test('then auth validate should be called with correct parameters', () => {
+            expect(authActions.authValidate).toHaveBeenCalledWith('accessToken', '', '')
+          })
+
+          test('then next should be called', () => {
+            expect(next).toHaveBeenCalled()
+          })
+        })
+
+        describe('and the token is invalid', () => {
+          beforeEach(() => {
+            const store = {
+              getState: () => ({
+                auth: Immutable.Map({
+                  isAuthenticated: false,
+                  accessToken: 'accessToken',
+                  refreshToken: '',
+                  expiresAt: ''
+                })
+              })
+            }
+            authActions.authValidate.mockImplementation(() => {
+              throw new Error()
+            })
+            checkValidSession(store)({location}, replace, next)
+          })
+
+          test('then auth validate should be called with correct parameters', () => {
+            expect(authActions.authValidate).toHaveBeenCalledWith('accessToken', '', '')
+          })
+
+          test('then replace should be called', () => {
+            expect(replace).toHaveBeenCalled()
+          })
+        })
+      })
+    })
+  })
+
+  describe('addTargetToRedirect', () => {
+    let url
+    describe('when target exists', () => {
+      beforeEach(() => {
+        const location = {
+          pathname: '/somewhere',
+        }
+
+        delete global.window.location
+        global.window = Object.create(window)
+        global.window.location = {}
+        url = addTargetToRedirect({target: true, location})
+      })
+
+      test('then the returned url should contain the target correctly encoded', () => {
+        expect(url).toContain('?target=%2Fsomewhere')
+      })
+
+      test('then the returned url should contain the login hash', () => {
+        expect(url).toContain('#login')
+      })
+    })
+
+    describe("when target doesn't exist", () => {
+      beforeEach(() => {
+        url = addTargetToRedirect({target: false, redirectUrl: '/menu'})
+      })
+
+      test('should return the redirectUrl passed in as a parameter', () => {
+        expect(url).toEqual('/menu')
+      })
+
+      describe('and no redirectURL is passed in ', () => {
+        beforeEach(() => {
+          url = addTargetToRedirect({target: false})
+        })
+
+        test('then "/" is returned', () => {
+          expect(url).toEqual('/')
+        })
+      })
+    })
+  })
+})
