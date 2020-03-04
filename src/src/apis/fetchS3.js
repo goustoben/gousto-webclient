78f76f5077a4aec702b2174997c5fe5aed642fb4
James Carson <jamesscarson@gmail.com>
2020-03-04 20:43:02 +0100

    Fix envName for the fetchS3 (#1827)

--- src/src/apis/fetchS3.js
+++ src/src/apis/fetchS3.js
@@ -1,5 +1,5 @@
 import fetch from 'utils/fetch'
-const envName = 'staging' || __ENV__ || 'production'
+const envName = __ENV__ || 'production'
 const S3FILE = `https://s3-gousto-${envName}-media.s3-eu-west-1.amazonaws.com/features.json`
 
 export const fetchFeatures = async () => fetch(null, S3FILE, { }, 'GET', 'no-cache')
