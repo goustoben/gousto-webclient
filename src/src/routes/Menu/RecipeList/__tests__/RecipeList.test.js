4598f925a02a4c29d619c3907c36eb7ebc1ec2e4
Rianne <46559738+riannemcc@users.noreply.github.com>
2020-03-20 09:55:04 +0000

    [TR-3038] refactor trackRecipeOrderDisplayed (#1902)
    * [TR-3038] refactor trackRecipeOrderDisplayed
    
    * [TR-3038] Removing eslint warning from test file
    
    * [TR-3038] Address PR comments

--- src/src/routes/Menu/RecipeList/__tests__/RecipeList.test.js
+++ src/src/routes/Menu/RecipeList/__tests__/RecipeList.test.js
@@ -104,30 +104,97 @@ describe('RecipeList', () => {
     })
   })
 
-  test('should dispatch trackRecipeOrderDisplayed with original and final sorting order', () => {
-    const filteredRecipeIds = Immutable.List(['1', '2', '3'])
-    const recipes = Immutable.fromJS([
-      { id: '3' },
-      { id: '1' }
-    ])
+  describe('trackRecipeOrderDisplayed', () => {
+    describe('when the recipe list is initially rendered', () => {
+      test('should dispatch trackRecipeOrderDisplayed once', () => {
+        const filteredRecipeIds = Immutable.List(['1', '2', '3'])
+        const recipes = Immutable.fromJS([
+          { id: '3' },
+          { id: '1' }
+        ])
 
-    const wrapper = shallow(
-      <RecipeList
-        filteredRecipeIds={filteredRecipeIds}
-        recipes={recipes}
-      />,
-      { context },
-    )
-    wrapper.instance().componentDidUpdate(wrapper.props())
+        shallow(
+          <RecipeList
+            filteredRecipeIds={filteredRecipeIds}
+            recipes={recipes}
+          />,
+          { context },
+        )
 
-    expect(context.store.dispatch).toHaveBeenCalledTimes(2)
-    expect(context.store.dispatch).toHaveBeenCalledWith(
-      'trackRecipeOrderDisplayed return value',
-    )
-    expect(trackRecipeOrderDisplayed).toHaveBeenCalledTimes(2)
-    expect(trackRecipeOrderDisplayed).toHaveBeenCalledWith(
-      ['1', '2', '3'],
-      ['3', '1'],
-    )
+        expect(context.store.dispatch).toHaveBeenCalledTimes(1)
+        expect(context.store.dispatch).toHaveBeenCalledWith(
+          'trackRecipeOrderDisplayed return value',
+        )
+        expect(trackRecipeOrderDisplayed).toHaveBeenCalledTimes(1)
+        expect(trackRecipeOrderDisplayed).toHaveBeenCalledWith(
+          ['1', '2', '3'],
+          ['3', '1'],
+        )
+      })
+    })
+
+    describe('when the recipe collection selection is changed', () => {
+      test('should dispatch trackRecipeOrderDisplayed an additional time', () => {
+        const filteredRecipeIds = Immutable.List(['1', '2', '3'])
+        const recipes = Immutable.fromJS([
+          { id: '3' },
+          { id: '1' }
+        ])
+
+        const wrapper = shallow(
+          <RecipeList
+            filteredRecipeIds={filteredRecipeIds}
+            recipes={recipes}
+            currentCollectionId="123"
+          />,
+          { context },
+        )
+        wrapper.setProps({
+          currentCollectionId: '321',
+        })
+
+        expect(context.store.dispatch).toHaveBeenCalledTimes(2)
+        expect(context.store.dispatch).toHaveBeenCalledWith(
+          'trackRecipeOrderDisplayed return value',
+        )
+        expect(trackRecipeOrderDisplayed).toHaveBeenCalledTimes(2)
+        expect(trackRecipeOrderDisplayed).toHaveBeenCalledWith(
+          ['1', '2', '3'],
+          ['3', '1'],
+        )
+      })
+    })
+
+    describe('when the recipe collection remains the same', () => {
+      test('should not dispatch trackRecipeOrderDisplayed after initial render', () => {
+        const filteredRecipeIds = Immutable.List(['1', '2', '3'])
+        const recipes = Immutable.fromJS([
+          { id: '3' },
+          { id: '1' }
+        ])
+
+        const wrapper = shallow(
+          <RecipeList
+            filteredRecipeIds={filteredRecipeIds}
+            recipes={recipes}
+            currentCollectionId="123"
+          />,
+          { context },
+        )
+        wrapper.setProps({
+          currentCollectionId: '123',
+        })
+
+        expect(context.store.dispatch).toHaveBeenCalledTimes(1)
+        expect(context.store.dispatch).toHaveBeenCalledWith(
+          'trackRecipeOrderDisplayed return value',
+        )
+        expect(trackRecipeOrderDisplayed).toHaveBeenCalledTimes(1)
+        expect(trackRecipeOrderDisplayed).toHaveBeenCalledWith(
+          ['1', '2', '3'],
+          ['3', '1'],
+        )
+      })
+    })
   })
 })
