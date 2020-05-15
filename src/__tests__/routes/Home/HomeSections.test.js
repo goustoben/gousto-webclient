b8cc966891110e2ff0323d5b7e3e56b6ef4da0a1
trunham <todd.runham@gmail.com>
2020-05-15 10:30:08 +0100

    [TECH-XXXX] - Put signup queue form behind flag (#2126)
    * Put signup queue form behind flag
    
    * fixed lint health
    
    * added test coverage for email form

--- src/__tests__/routes/Home/HomeSections.test.js
+++ src/__tests__/routes/Home/HomeSections.test.js
@@ -43,11 +43,17 @@ describe('rendered output', () => {
     expect(wrapper.find('Storystream')).toHaveLength(0)
   })
 
+  test('should have an EmailForm element when signup flag is on', () => {
+    wrapper = shallow(<HomeSections isSignupReductionEnabled />)
+    expect(wrapper.find('EmailForm')).toHaveLength(1)
+  })
+
   test('should show only specified modules from the modules props', () => {
     wrapper = shallow(<HomeSections modules={['hero', 'testimonials']} />)
     expect(wrapper.find('Connect(Hero)')).toHaveLength(1)
     expect(wrapper.find('Connect(Testimonials)')).toHaveLength(1)
     expect(wrapper.find('Connect(InYourBox)')).toHaveLength(0)
     expect(wrapper.find('TestedLoved')).toHaveLength(0)
+    expect(wrapper.find('EmailForm')).toHaveLength(0)
   })
 })
--- src/src/routes/Home/HomeContainer.js
+++ src/src/routes/Home/HomeContainer.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux'
 import { knownVariants, defaultVariant } from 'config/home'
 import actions from 'actions/auth'
+import { getIsSignupReductionEnabled } from 'selectors/features'
+import { getIsAuthenticated } from 'selectors/auth'
 
 import Home from './Home'
 
@@ -13,6 +15,7 @@ const mapStateToProps = (state, props) => ({
   moduleOrder: state.features.getIn(['hp_module_order', 'value']),
   isAuthenticated: state.auth.get('isAuthenticated'),
   variant: (props.location && props.location.query) ? getKnownVariant(props.location.query.variant) : defaultVariant,
+  isSignupReductionEnabled: getIsSignupReductionEnabled(state) && !getIsAuthenticated(state),
 })
 
 const mapDispatchToProps = {
