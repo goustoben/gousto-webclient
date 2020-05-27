c9b2b0fd8f680fb38dfdf6cbed1b9cc509c6aa9b
zinkevichav <62094540+zinkevichav@users.noreply.github.com>
2020-05-27 15:54:34 +0300

    [TG-1327] change header link to sustainability (#2174)
    * [TG-1327] change header link to sustainability
    
    * [TG-1327] update unit tests
    
    * [TG-1327] remove console

--- src/src/components/Header/Header.js
+++ src/src/components/Header/Header.js
@@ -120,14 +120,14 @@ class Header extends React.PureComponent {
       !isAuthenticated && menuItems.boxPrices,
       menuItems.menu,
       isAuthenticated && menuItems.referFriend,
-      menuItems.charityCampaign,
+      menuItems.sustainability,
       menuItems.faq,
     ].filter(item => item)
 
     const mobileItems = [
       !isAuthenticated && menuItems.boxPrices,
       menuItems.menu,
-      menuItems.charityCampaign,
+      menuItems.sustainability,
       menuItems.faq,
     ].filter(item => item)
 
