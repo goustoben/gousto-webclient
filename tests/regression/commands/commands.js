fc3c7bae4bb820de2a8b2e41a98ea5c9329a4388
Renato Ferreira <renaferr.social@gmail.com>
2020-06-01 13:06:38 +0100

    Revert "[TG-1324] signup/delivery step regression test (#2192)" (#2200)
    This reverts commit 3d8e167629055f7fd249abcf4ba7d839ddb10883.

--- tests/regression/commands/commands.js
+++ tests/regression/commands/commands.js
@@ -29,7 +29,7 @@ Cypress.Commands.add('checkoutLoggedOut', ({ withDiscount }) => {
   const pricesFixtureFile = withDiscount
     ? 'prices/2person2portionDiscount'
     : 'prices/2person2portionNoDiscount'
-
+    
   const DATE = new Date(2020, 4, 1).getTime()
   cy.server()
   cy.clearCookies()
