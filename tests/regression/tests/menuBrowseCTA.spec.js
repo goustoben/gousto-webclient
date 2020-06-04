cccfac66955e6616b6adf55858cc6e383515c36c
AnaPasparan <ana.pasparan@gousto.co.uk>
2020-06-04 18:35:53 +0100

    change menu regression tests (#2219)

--- tests/regression/tests/menuBrowseCTA.spec.js
+++ tests/regression/tests/menuBrowseCTA.spec.js
@@ -1,8 +1,10 @@
 import { withPlatformTags, MOBILE, WEB } from '../utils/tags'
 
 describe("Menu-BrowseCTA", () => {
-  beforeEach(() => {
+  before(() => {
     cy.server()
+    const DATE = new Date(2020, 4, 1).getTime()
+    cy.clock(DATE, ['Date'])
     cy.route('GET', 'boxPrices', 'fixture:boxPrices/priceNoPromocode.json').as('getBoxPrice')
     cy.route('GET', 'brand/v1/theme', 'fixture:brand/brand.json').as('getBrand')
     cy.route('GET', 'brand/v1/menu-headers', 'fixture:brand/brandHeaders.json')
@@ -10,25 +12,16 @@ describe("Menu-BrowseCTA", () => {
     cy.route('GET', 'delivery_day/**/stock', 'fixture:stock/deliveryDayStock.json').as('getStock')
     cy.route('/menu/v1/**', 'fixture:menu/twoWeeksDetails.json').as('getMenu')
     cy.visit('/menu')
-    const DATE = new Date(2020, 4, 1).getTime()
-    cy.clock(DATE, ['Date'])
     cy.wait(['@getMenu', '@getBrand', '@getStock', '@getDeliveries'])
   })
-  
-  afterEach(() => {
+  after(() => {
     cy.clock().then((clock) => {
       clock.restore()
     })
   })
 
   describe('add recipe', () => {
-    withPlatformTags(WEB).it('should open browse CTA overlay', () => {
-      cy.wait('@getDeliveries')
-
-      cy.get('[data-testing="menuRecipeAdd"]').first().click()
-      cy.get('[data-testing="menuBrowseCTAOverlay"]').should('be.visible')
-    })
-    withPlatformTags(MOBILE).it('should open browse CTA overlay', () => {
+    it('should open browse CTA overlay', () => {
       cy.wait('@getDeliveries')
 
       cy.get('[data-testing="menuRecipeAdd"]').first().click()
@@ -37,19 +30,14 @@ describe("Menu-BrowseCTA", () => {
   })
   
   describe('click on browseCTA', () => {
-    withPlatformTags(WEB).it('should open box summary desktop', () => {
-      cy.wait('@getDeliveries')
-
-      cy.get('[data-testing="menuRecipeAdd"]').first().click()
+    beforeEach(() => {
       cy.get('[data-testing="menuBrowseCTAOverlay"]').click()
+    })
+    withPlatformTags(WEB).it('should open box summary desktop', () => {
       cy.get('[data-testing="boxSummaryDesktop"]').should('be.visible')
     })
     
     withPlatformTags(MOBILE).it('should open box summary mobile', () => {
-      cy.wait('@getDeliveries')
-
-      cy.get('[data-testing="menuRecipeAdd"]').first().click()
-      cy.get('[data-testing="menuBrowseCTAOverlay"]').click()
       cy.get('[data-testing="boxSummaryMobile"]').should('be.visible')
     })
   })
--- tests/regression/tests/menuCollectionHeaders.spec.js
+++ tests/regression/tests/menuCollectionHeaders.spec.js
@@ -1,20 +1,4 @@
-import { withPlatformTags, MOBILE, WEB } from '../utils/tags'
-
 describe("MenuCollectionHeaders", () => {
-  beforeEach(() => {
-    cy.server()
-    cy.route('GET', 'boxPrices', 'fixture:boxPrices/priceNoPromocode.json').as('getBoxPrice')
-    cy.route('GET', 'brand/v1/theme', 'fixture:brand/brand.json').as('getBrand')
-    cy.route('GET', 'brand/v1/menu-headers', 'fixture:brand/brandHeaders.json')
-    cy.route('GET', 'deliveries/v1.0/**', 'fixture:deliveries/deliveryDays.json').as('getDeliveries')
-    cy.route('GET', 'delivery_day/**/stock', 'fixture:stock/deliveryDayStock.json').as('getStock')
-    cy.route('/menu/v1/**', 'fixture:menu/twoWeeksDetails.json').as('getMenu')
-    cy.visit('/menu')
-    const DATE = new Date(2020, 4, 1).getTime()
-    cy.clock(DATE, ['Date'])
-    cy.wait(['@getMenu', '@getBrand', '@getStock', '@getDeliveries'])
-  })
-  
   afterEach(() => {
     cy.clock().then((clock) => {
       clock.restore()
@@ -23,6 +7,17 @@ describe("MenuCollectionHeaders", () => {
 
   describe('click on waveLinkHeader', () => {
     it('should change collection', () => {
+      cy.server()
+      cy.route('GET', 'boxPrices', 'fixture:boxPrices/priceNoPromocode.json').as('getBoxPrice')
+      cy.route('GET', 'brand/v1/theme', 'fixture:brand/brand.json').as('getBrand')
+      cy.route('GET', 'brand/v1/menu-headers', 'fixture:brand/brandHeaders.json')
+      cy.route('GET', 'deliveries/v1.0/**', 'fixture:deliveries/deliveryDays.json').as('getDeliveries')
+      cy.route('GET', 'delivery_day/**/stock', 'fixture:stock/deliveryDayStock.json').as('getStock')
+      cy.route('/menu/v1/**', 'fixture:menu/twoWeeksDetails.json').as('getMenu')
+      const DATE = new Date(2020, 4, 1).getTime()
+      cy.clock(DATE, ['Date'])
+      cy.visit('/menu')
+      cy.wait(['@getMenu', '@getBrand', '@getStock', '@getDeliveries'])
       cy.wait('@getDeliveries')
       cy.get('[data-testing="waveLinkHeader"]').click()
       cy.url().should('include', 'collection=healthy-choices')
