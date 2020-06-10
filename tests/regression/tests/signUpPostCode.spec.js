0f36c068c06340746acf52eb400c38b9dfa288ca
Renato Ferreira <renaferr.social@gmail.com>
2020-06-10 10:12:05 +0100

    [TG-1084] signup/postcode step regression test (#2194)
    * [TG-1084] Test when entering a valid postcode
    
    * [TG-1084] Install cypress-pipe package
    
    This is a helper to avoid racing conditions when getting something from the store
    
    * [TG-1084] Test when entering invalid postcodes
    
    * [TG-1084] Create mockDate command
    
    * [TG-1084] CR | Make getBoxSummaryError reusable
    
    Also, move the `.clock` to run after `.visit` as it was causing a known issue when all tests are running.
    
    * [TG-1084] Add postcode as part of the `.visit` URL

---
+++ tests/regression/tests/signUpPostCode.spec.js
@@ -0,0 +1,73 @@
+const PAGE_URL = '/signup/?steps=postcode'
+const POSTCODE_6_CHARS = 'W140EE'
+const POSTCODE_5_CHARS = 'W140E'
+const POSTCODE_8_CHARS = 'W140EE14'
+
+describe('Given I land on postcode slide of the wizard', () => {
+  const getBoxSummaryError = (win) => (
+    win.__store__.getState().error.get('BOXSUMMARY_DELIVERY_DAYS_RECEIVE')
+  )
+
+  before(() => {
+    cy.server()
+    cy.route('/menu/v1/**', 'fixture:menu/twoWeeksDetails.json')
+    cy.route('GET', 'brand/v1/theme', 'fixture:brand/brand.json')
+    cy.route('GET', 'brand/v1/menu-headers', 'fixture:brand/brandHeaders.json')
+    cy.route('GET', 'delivery_day/**/stock', 'fixture:stock/deliveryDayStock.json')
+    cy.route('GET', /^(?=.*\bdeliveries\/v1.0\/days\b)(?!.*\bpostcode\b).*$/, 'fixture:deliveries/deliveryDays.json')
+    cy.visit(PAGE_URL)
+    cy.mockDate()
+  })
+
+  describe('And I entered a valid postcode', () => {
+    before(() => {
+      cy.get('[data-testing="signupPostcodeInput"]').type(POSTCODE_6_CHARS)
+    })
+
+    it('Then the CTA is enabled', () => {
+      cy.get('[data-testing="signupPostcodeInput"]').should('not.have.class', 'disabled')
+    })
+
+    it('And the postcode is stored in the basket object of the redux store', () => {
+      const getTempPostCode = (win) => {
+        return win.__store__.getState().temp.get('postcode')
+      }
+
+      cy.window().then(getTempPostCode).should('equal', POSTCODE_6_CHARS)
+    })
+  })
+
+  describe('And I entered a postcode with less than 6 characters ', () => {
+    before(() => {
+      cy.server()
+      cy.route('GET', /^(?=.*\bdeliveries\/v1.0\/days\b)(?=.*\bpostcode\b).*$/, 'fixture:deliveries/postcode/invalid.json')
+      cy.get('[data-testing="signupPostcodeInput"]')
+        .clear()
+        .type(POSTCODE_5_CHARS)
+    })
+
+    it('Then I’m not able to proceed to the next wizard step', () => {
+      cy.get('[data-testing="signupPostcodeCTADesktop"]').click()
+
+      cy.window().pipe(getBoxSummaryError).should('equal', 'do-not-deliver')
+      cy.url().should('not.include', 'signup/delivery-options')
+    })
+  })
+
+  describe('And I entered a postcode with more than 6 characters ', () => {
+    before(() => {
+      cy.server()
+      cy.route('GET', /^(?=.*\bdeliveries\/v1.0\/days\b)(?=.*\bpostcode\b).*$/, 'fixture:deliveries/postcode/invalid.json')
+      cy.get('[data-testing="signupPostcodeInput"]')
+        .clear()
+        .type(POSTCODE_8_CHARS)
+    })
+
+    it('Then I’m not able to proceed to the next wizard step', () => {
+      cy.get('[data-testing="signupPostcodeCTADesktop"]').click()
+
+      cy.window().pipe(getBoxSummaryError).should('equal', 'do-not-deliver')
+      cy.url().should('include', 'signup/postcode?steps=postcode')
+    })
+  })
+})
