3f266c251d50d57c07ff698c324fd6c715d2feca
Rianne <46559738+riannemcc@users.noreply.github.com>
2020-06-23 09:14:00 +0100

    Tr 3686/use border color (#2282)
    * Removing default border for tags
    
    * Moving logic into variable
    
    * extracting logic into separate function

--- src/src/routes/Menu/Recipe/InfoBadge/InfoBadge.js
+++ src/src/routes/Menu/Recipe/InfoBadge/InfoBadge.js
@@ -2,11 +2,13 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import css from './InfoBadge.css'
 
+const getTagStyle = (theme) => ({ ...theme, border: theme.borderColor ? '1px solid' : 'none' })
+
 const InfoBadge = ({ type = 'span', brandTag }) => {
   if (brandTag) {
     const { theme, text } = brandTag
 
-    return React.createElement(type, { className: css.badge, style: theme }, text)
+    return React.createElement(type, { className: css.badge, style: getTagStyle(theme) }, text)
   }
 
   return React.createElement(type)
