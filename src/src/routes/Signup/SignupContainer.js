4aa6e0b6890d713ca0ed16f52ebc6abcacf03354
AnaPasparan <ana.pasparan@gousto.co.uk>
2020-08-04 17:15:09 +0100

    TG-1672 change step size if tastePreferences enabled (#2437)
    * TG-1672 change step size if tastePreferences enabled
    
    * add test for signup

--- src/src/routes/Signup/SignupContainer.js
+++ src/src/routes/Signup/SignupContainer.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux'
 import actions from 'actions'
+import { getIsTastePreferencesEnabled } from 'selectors/features'
 import { Signup } from './Signup'
 
 const mapStateToProps = (state, ownProps) => ({
   stepName: ownProps.params.stepName,
   currentStepName: state.signup.getIn(['wizard', 'currentStepName']),
   steps: state.signup.getIn(['wizard', 'steps']),
+  isTastePreferencesEnabled: getIsTastePreferencesEnabled(state)
 })
 
 const SignupContainer = connect(mapStateToProps, {
