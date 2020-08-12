00d2d401612a3497c88ddd1f782fb59d0ae528cf
AnaPasparan <ana.pasparan@gousto.co.uk>
2020-08-12 10:32:19 +0100

    fix auth user id name (#2458)

--- src/src/containers/OptimizelyRollouts/OptimizelyRolloutsContainer.js
+++ src/src/containers/OptimizelyRollouts/OptimizelyRolloutsContainer.js
@@ -3,7 +3,7 @@ import { OptimizelyRollouts } from './OptimizelyRollouts'
 import { getAuthUserId } from '../../selectors/auth'
 
 const mapDispatchToProps = state => ({
-  userId: getAuthUserId(state)
+  authUserId: getAuthUserId(state)
 })
 
 export const OptimizelyRolloutsContainer = connect(mapDispatchToProps)(OptimizelyRollouts)
