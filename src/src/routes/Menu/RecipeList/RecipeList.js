69165a73742a7142a4fde1f9886cbfb63cfc4618
Rianne <46559738+riannemcc@users.noreply.github.com>
2020-08-17 10:43:02 +0100

    Removing store.dispatch RecipeList.js (#2474)
    * Removing store.dispatch RecipeList.js
    
    * Addressing PR comms and adding tests

--- src/src/routes/Menu/RecipeList/RecipeList.js
+++ src/src/routes/Menu/RecipeList/RecipeList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Immutable from 'immutable'
-import actions from 'actions/tracking'
 
 import { ThreeColumnRecipeList } from './ThreeColumnRecipeList'
 import { SingleColumnRecipeList } from './SingleColumnRecipeList'
@@ -21,15 +20,14 @@ class RecipeList extends React.PureComponent {
   }
 
   trackRecipeOrderDisplayed() {
-    const { originalOrderRecipeIds, recipes } = this.props
-    const { store } = this.context
+    const { originalOrderRecipeIds, recipes, trackRecipeOrderDisplayed } = this.props
 
     const recipeIds = recipes.map(({ recipe }) => recipe.get('id'))
 
-    store.dispatch(actions.trackRecipeOrderDisplayed(
+    trackRecipeOrderDisplayed(
       originalOrderRecipeIds.toJS(),
       recipeIds.toJS()
-    ))
+    )
   }
 
   render() {
@@ -81,15 +79,13 @@ RecipeList.propTypes = {
   recipes: PropTypes.instanceOf(Immutable.List).isRequired,
   browserType: PropTypes.string.isRequired,
   currentCollectionId: PropTypes.string.isRequired,
+  trackRecipeOrderDisplayed: PropTypes.func.isRequired,
 }
 
 RecipeList.defaultProps = {
   originalOrderRecipeIds: Immutable.List([]),
   deliveryDate: null,
-  thematicName: '',
+  thematicName: ''
 }
 
-RecipeList.contextTypes = {
-  store: PropTypes.object.isRequired,
-}
 export { RecipeList }
