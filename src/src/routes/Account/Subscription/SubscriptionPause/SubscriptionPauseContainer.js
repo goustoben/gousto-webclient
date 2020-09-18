3aad1b3d0c75de4beb52525d453b708def78b061
trunham <todd.runham@gmail.com>
2020-09-18 12:06:31 +0100

    [TECH-10] - Removed store from subscription pause in prep for upgrade (#2450)
    Force merging as the code health fail is a false positive

--- src/src/routes/Account/Subscription/SubscriptionPause/SubscriptionPauseContainer.js
+++ src/src/routes/Account/Subscription/SubscriptionPause/SubscriptionPauseContainer.js
@@ -1,11 +1,25 @@
 import { connect } from 'react-redux'
+import actions, { fetchData } from 'actions/subscriptionPause'
 import SubscriptionPause from './SubscriptionPause'
 
-const mapStateToProps = state => ({
-  dataLoaded: state.subscriptionPause.get('reasons').size > 0,
-  showModal: state.subscriptionPause.get('inProgress'),
-})
+const mapStateToProps = state => {
+  const { subscriptionPause } = state
+  const reasons = subscriptionPause.get('reasons')
 
-const SubscriptionContainer = connect(mapStateToProps)(SubscriptionPause)
+  return {
+    dataLoaded: reasons.size > 0,
+    showModal: subscriptionPause.get('inProgress'),
+  }
+}
+
+const mapDispatchToProps = {
+  fetchData,
+  subscriptionPauseFetchReasons: actions.subscriptionPauseFetchReasons,
+}
+
+const SubscriptionContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SubscriptionPause)
 
 export default SubscriptionContainer
