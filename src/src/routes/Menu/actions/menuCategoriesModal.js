741b438a3fb4093c8867dfb052f0496dffd0152c
Rianne <46559738+riannemcc@users.noreply.github.com>
2020-09-25 09:30:36 +0100

    Tr 4770/eme2 snowplow (#2650)
    * Adding tracking data to categories modal actions
    
    * Adding tests

--- src/src/routes/Menu/actions/menuCategoriesModal.js
+++ src/src/routes/Menu/actions/menuCategoriesModal.js
@@ -1,9 +1,16 @@
 import { actionTypes } from '../../../actions/actionTypes'
+import * as trackingKeys from '../../../actions/trackingKeys'
 
 export const showCategoriesModal = () => ({
   type: actionTypes.MENU_SHOW_CATEGORIES_MODAL,
+  trackingData: {
+    actionType: trackingKeys.clickShowAllRecipeCategories,
+  },
 })
 
 export const hideCategoriesModal = () => ({
   type: actionTypes.MENU_HIDE_CATEGORIES_MODAL,
+  trackingData: {
+    actionType: trackingKeys.clickBackToMenu,
+  },
 })
