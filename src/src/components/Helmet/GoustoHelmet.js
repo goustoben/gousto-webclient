28071f140560d257f1fe21ff0177f64f162f6e9c
zinkevichav <62094540+zinkevichav@users.noreply.github.com>
2020-10-26 11:39:42 +0300

    [TG-2381] Add canonical link for new menu redirect page (#2788)
    * Add canonical link for /menu2
    
    * Change canonical href to menu
    
    * Remove helmet from menu redirect and update GoustoHelmet
    
    * Fix eslint warnings
    
    * Import routes from config

--- src/src/components/Helmet/GoustoHelmet.js
+++ src/src/components/Helmet/GoustoHelmet.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import templateConfig from 'config/template'
 import globals from 'config/globals'
+import client from 'config/routes'
 import CanonicalLink from './CanonicalLink'
 import Facebook from './Facebook'
 import OpenGraph from './OpenGraph'
@@ -9,9 +10,10 @@ import SiteVerification from './SiteVerification'
 import Twitter from './Twitter'
 
 export const generateHref = url => {
-  const hostname = `${globals.env === 'production' ? 'www.' : `${globals.env}-webclient.`}${globals.domain}`
+  const hostname = `${globals.env === 'production' ? 'www.' : `${globals.env}-frontend.`}${globals.domain}`
+  const resultURL = url === client.menu2 ? client.menu : url
 
-  return `${globals.protocol}://${hostname}${url || '/'}`
+  return `${globals.protocol}://${hostname}${resultURL || '/'}`
 }
 
 const propTypes = {
