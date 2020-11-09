6544d3fa46d6f5e0146210e0f90f538f65b58da6
AnaPasparan <ana.pasparan@gousto.co.uk>
2020-11-09 15:58:45 +0000

    TG-2541/Fix_fixture_missing_for_my_deliveries (#2853)

--- src/src/routes/Account/MyDeliveries/__regression__/myDeliveries.spec.js
+++ src/src/routes/Account/MyDeliveries/__regression__/myDeliveries.spec.js
@@ -1,5 +1,5 @@
 
-describe("My Deliveries", () => {
+describe('My Deliveries', () => {
   beforeEach(() => {
     cy.server()
     cy.fixture('user/userCurrent').as('userCurrent')
@@ -12,7 +12,8 @@ describe("My Deliveries", () => {
     cy.route('GET', /user\/current\/address/, '@userCurrentAddress').as('currentAddress')
     cy.fixture('user/userCurrentSubscription').as('userCurrentSubscription')
     cy.route('GET', /user\/current\/subscription/, '@userCurrentSubscription').as('currentSubscription')
-    cy.route('GET', /customers\/v1\/customers\/\/addresses/, {})
+    cy.fixture('user/userAddresses').as('userAdresses')
+    cy.route('GET', '/customers/v1/customers/17247344/addresses', '@userAdresses')
     cy.fixture('orderSkipRecovery').as('orderSkipRecovery')
     cy.route('GET', /orderskiprecovery/, '@orderSkipRecovery')
     cy.fixture('user/userCurrentSubscriptionDelivery').as('userCurrentSubscriptionDelivery')
@@ -27,14 +28,14 @@ describe("My Deliveries", () => {
   })
 
   describe('add box button', () => {
-    it("should take the user to the menu", () => {
+    it('should take the user to the menu', () => {
       cy.get('[data-testing="addBoxButton"]').click()
       cy.url().should('include', 'menu')
     })
   })
 
   describe('pending orders', () => {
-    it("should be cancellable", () => {
+    it('should be cancellable', () => {
       cy.get('[data-testing="pendingOrder"]').first().click()
       cy.get('[data-testing="cancelButton"]').click()
       cy.wait('@cancelPendingOrder')
@@ -44,7 +45,7 @@ describe("My Deliveries", () => {
   })
 
   describe('projected deliveries', () => {
-    it("should be cancellable and restorable", () => {
+    it('should be cancellable and restorable', () => {
       cy.get('[data-testing="projectedDelivery"]').first().get('[data-testing="orderState"]').contains('Scheduled')
       cy.get('[data-testing="projectedDelivery"]').first().click()
       cy.get('[data-testing="cancelButton"]').click()
@@ -67,14 +68,14 @@ describe("My Deliveries", () => {
         cy.wait(['@currentOrders','@projectedDeliveries','@currentAddress', '@currentSubscription'])
       })
 
-      it("should show the modal to prompt a user to pause", () => {
+      it('should show the modal to prompt a user to pause', () => {
         cy.get('[data-testing="projectedDelivery"]').first().get('[data-testing="orderState"]').contains('Scheduled')
         cy.get('[data-testing="projectedDelivery"]').first().click()
         cy.get('[data-testing="cancelButton"]').click()
         cy.wait('@cancelProjectedDelivery')
         cy.contains('Cancelled').should('exist')
 
-        cy.get('[data-testing="cancelledAllBoxesModal"]').should("be.visible")
+        cy.get('[data-testing="cancelledAllBoxesModal"]').should('be.visible')
       })
     })
   })
