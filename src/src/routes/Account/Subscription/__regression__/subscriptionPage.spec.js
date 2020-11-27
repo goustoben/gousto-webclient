00d7ca64eacc59a38f7d36d12cca5ac79069011a
AnaPasparan <ana.pasparan@gousto.co.uk>
2020-11-27 13:10:32 +0000

    TG-2471 fix regression tests for subscription page (#2909)

--- src/src/routes/Account/Subscription/__regression__/subscriptionPage.spec.js
+++ src/src/routes/Account/Subscription/__regression__/subscriptionPage.spec.js
@@ -8,41 +8,48 @@ describe('Given I am logged in', () => {
       cy.server()
       cy.fixture('user/userCurrent').as('userCurrent')
       cy.route('GET', /user\/current/, '@userCurrent')
+
+      cy.fixture('user/userCurrentSubscription').as('userCurrentSubscription')
+      cy.route('GET', /user\/current\/subscription/, '@userCurrentSubscription').as('currentSubscription')
+
       cy.fixture('user/userCurrentOrders').as('userCurrentOrders')
       cy.route('GET', /user\/current\/orders/, '@userCurrentOrders').as('currentOrders')
+
+      cy.fixture('user/userAddresses').as('userAdresses')
+      cy.route('GET', '/customers/v1/customers/17247344/addresses', '@userAdresses')
+
+      cy.route('GET', /boxPrices|prices/, 'fixture:prices/2person2portionNoDiscount.json').as('getPrices')
+
+      cy.route('GET', /^(?=.*\bdeliveries\/v1.0\/days\b).*$/, 'fixture:deliveries/deliveryDays.json').as('deliveryDays')
+
       cy.fixture('user/userCurrentProjectedDeliveries').as('userCurrentProjectedDeliveries')
       cy.route('GET', /user\/current\/projected-deliveries/, '@userCurrentProjectedDeliveries').as('projectedDeliveries')
-      cy.fixture('user/userCurrentAddress').as('userCurrentAddress')
-      cy.fixture('user/userCurrentSubscription').as('userCurrentSubscription')
 
-      cy.route('GET', /user\/current\/address/, '@userCurrentAddress').as('currentAddress')
-      cy.fixture('user/userCurrentSubscription').as('userCurrentSubscription')
+      cy.fixture('user/userCurrentAddress').as('userCurrentAddress')
+      cy.route('GET', /user\/current\/address/, '@userCurrentAddress')
 
       cy.route('GET', /user\/current\/subscription/, '@userCurrentSubscription').as('currentSubscription')
       cy.fixture('orderSkipRecovery').as('orderSkipRecovery')
       cy.fixture('user/userCurrentSubscriptionDelivery').as('userCurrentSubscriptionDelivery')
 
-      cy.route('GET', /^(?=.*\bdeliveries\/v1.0\/days\b)(?!.*\bpostcode\b).*$/, 'fixture:deliveries/deliveryDays.json').as('deliveryDays')
-
       cy.visit('/subscription-settings')
-      cy.wait(['@currentOrders',
-        '@projectedDeliveries',
-        '@currentAddress',
+      cy.wait([
         '@currentSubscription',
-        '@deliveryDays'])
+        '@deliveryDays'
+      ])
     })
 
-    it.skip('Then I should see Subscription Settings Page', () => {
+    it('Then I should see Subscription Settings Page', () => {
       cy.get('[data-testing="subscriptionSettingsPage"]')
         .should('be.visible')
     })
 
-    it.skip('Then I should see the current delivery day', () => {
+    it('Then I should see the current delivery day', () => {
       cy.get('[data-testing="current-delivery-day"]')
         .should('have.text', 'Saturday')
     })
 
-    it.skip('Then I should see the current delivery time', () => {
+    it('Then I should see the current delivery time', () => {
       cy.get('[data-testing="current-delivery-time"]')
         .should('have.text', '8am - 7pm')
     })
@@ -73,7 +80,7 @@ describe('Given I am logged in', () => {
           cy.wait(['@updateCurrentSubscription'])
         })
 
-        it.skip('Then I should see the updated delivery day and time', () => {
+        it('Then I should see the updated delivery day and time', () => {
           cy.get('[data-testing="current-delivery-day"]')
             .should('have.text', 'Monday')
 
