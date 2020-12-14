a23ce530c3331d5ac06504ef426939fcd9ab183c
Jason Lafferty <jasonlafferty@users.noreply.github.com>
2020-12-14 10:11:54 +0000

    Refactor SaveButton to use hooks instead of deprecated componentWillReceiveProps (#2950)

--- src/src/components/OrderSummary/SaveButton.js
+++ src/src/components/OrderSummary/SaveButton.js
@@ -1,68 +1,88 @@
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import React from 'react'
+import usePrevious from 'react-use/lib/usePrevious'
 import { Button } from 'goustouicomponents'
 import classnames from 'classnames'
 import css from './OrderSummary.css'
 
-class SaveButton extends React.PureComponent {
-  static propTypes = {
-    error: PropTypes.bool,
-    onClick: PropTypes.func,
-    onOrderConfirmationMobile: PropTypes.bool,
-    saving: PropTypes.bool,
-    saveRequired: PropTypes.bool.isRequired,
-  }
+const useSafePrevious = (value) => {
+  const prevValue = usePrevious(value)
 
-  static defaultProps = {
-    error: false,
-    onClick: () => { },
-    onOrderConfirmationMobile: false,
-    saving: false,
-    saveRequired: false,
-  }
+  return prevValue === undefined ? value : prevValue
+}
 
-  state = {
-    showButton: this.props.saveRequired,
-    showError: false,
-    showSuccess: false,
-  }
+export const useSaving = (saveRequired, saving, error) => {
+  const [showButton, setShowButton] = useState(saveRequired)
+  const [showError, setShowError] = useState(false)
+  const [showSuccess, setShowSuccess] = useState(false)
+  const prevSaving = useSafePrevious(saving)
+  const prevSaveRequired = useSafePrevious(saveRequired)
 
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.saveRequired && nextProps.saveRequired) {
-      this.setState({ showButton: true })
+  useEffect(() => {
+    if (!prevSaveRequired && saveRequired) {
+      setShowButton(true)
+    }
+  }, [saveRequired, prevSaveRequired])
+
+  useEffect(() => {
+    if (prevSaving === saving) {
+      return
     }
 
-    if (this.props.saving && !nextProps.saving) {
-      if (!nextProps.error) {
-        this.setState({ showSuccess: true, showButton: false }, () => {
-          setTimeout(() => { this.setState({ showSuccess: false }) }, 5000)
-        })
-      } else {
-        this.setState({ showError: true }, () => {
-          setTimeout(() => { this.setState({ showError: false }) }, 5000)
-        })
-      }
+    if (saving) {
+      return
     }
-  }
 
-  render = () => (
-    <div className={classnames({ [css.updateOrderButton]: this.props.onOrderConfirmationMobile })}>
-      {!!this.state.showButton && (
+    setShowButton(false)
+
+    if (error) {
+      setShowError(true)
+      setTimeout(() => setShowError(false), 5000)
+    } else {
+      setShowSuccess(true)
+      setTimeout(() => setShowSuccess(false), 5000)
+    }
+  }, [saving, prevSaving, error])
+
+  return { showButton, showSuccess, showError }
+}
+
+export const SaveButton = ({ saveRequired, onClick, saving, onOrderConfirmationMobile, error }) => {
+  const { showButton, showSuccess, showError } = useSaving(saveRequired, saving, error)
+
+  return (
+    <div className={classnames({ [css.updateOrderButton]: onOrderConfirmationMobile })}>
+      {showButton && (
         <div className={css.button}>
           <Button
-            disabled={this.props.saving}
-            onClick={this.props.onClick}
-            pending={this.props.saving}
+            disabled={saving}
+            onClick={onClick}
+            pending={saving}
             width="full"
           >
             Update Order
           </Button>
         </div>
       )}
-      {(this.state.showSuccess && !this.state.showButton) ? <div className={css.success}>SAVED</div> : ''}
-      {(this.state.showError && !this.state.showButton) ? <div className={css.error}>ERROR SAVING CHOICES</div> : ''}
+      {(showSuccess && !showButton) ? <div className={css.success}>SAVED</div> : null}
+      {(showError && !showButton) ? <div className={css.error}>ERROR SAVING CHOICES</div> : null}
     </div>
   )
+}
+
+SaveButton.propTypes = {
+  error: PropTypes.bool,
+  onClick: PropTypes.func,
+  onOrderConfirmationMobile: PropTypes.bool,
+  saving: PropTypes.bool,
+  saveRequired: PropTypes.bool.isRequired,
+}
+
+SaveButton.defaultProps = {
+  error: false,
+  onClick: () => { },
+  onOrderConfirmationMobile: false,
+  saving: false,
 }
 
 export default SaveButton
