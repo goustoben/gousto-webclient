5a997e71f931dfc9f69f0fca1c23846707e0c099
zinkevichav <62094540+zinkevichav@users.noreply.github.com>
2021-02-17 16:22:14 +0300

    [TG-3380] Make marketing checkout ticked by default (#3188)

--- src/src/routes/Checkout/Components/AboutYou/AboutYouContainer.js
+++ src/src/routes/Checkout/Components/AboutYou/AboutYouContainer.js
@@ -48,7 +48,7 @@ export function addInitialValues(Component, { sectionName }) {
           ...initialValues,
           [sectionName]: {
             title: 'miss',
-            allowEmail: !getIsCheckoutOverhaulEnabled(state),
+            allowEmail: true,
           },
         },
       }
