bc705126e57454282eaf963887f6653a7fae43e1
ashkandeel <Astridax93@googlemail.com>
2021-05-17 02:02:57 +0100

    Rewrote logInfo.js to output all variables exported by config automatically.
    This now requires no maintenance and won't become out of sync with config, assuming config is only defined in config.js (in the webpack folder).
    
    Strings are truncated at 100 characters.

--- src/config/webpack/logInfo.js
+++ src/config/webpack/logInfo.js
@@ -1,20 +1,15 @@
-const {
-  BUILD,
-  ENV_NAME,
-  DOMAIN,
-  CLIENT_PROTOCOL,
-  PUBLIC_PATH,
-} = require('./config')
+const config = require('./config')
 
-// eslint-disable-next-line no-console
-module.exports = ({ mode }) => console.log(`
+module.exports = ({ mode }) =>
+  console.log(`
 =====================================================
-  ${mode} BUILD: ${BUILD}
-  ENVIRONMENT: ${ENV_NAME}
-  DOMAIN: ${DOMAIN}
-  CLIENT PROTOCOL: ${CLIENT_PROTOCOL}
-  PUBLIC PATH: ${PUBLIC_PATH}
+  MODE: ${mode}
   NODE_APP_INSTANCE: ${process.env.NODE_APP_INSTANCE}
   NODE_CONFIG_ENV: ${process.env.NODE_CONFIG_ENV}
+  ${Object.entries(config)
+    .map(([key, value = '']) => {
+      return `${key}: ${JSON.stringify(value).substr(0, 100)} \n  `
+    })
+    .join('')}
 =====================================================
 `)
