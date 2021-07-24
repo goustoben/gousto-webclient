01fe599cb7b5a792486945e7dbaa58f6dcf40855
astridax <Astridax93@googlemail.com>
2021-07-24 09:59:21 +0100

    Removed redundant old env vars from scripts
    These vars were mapped to unused npm config vars which would at one point have been referenced somewhere. The only one I could find still necessary was the goustowebclient_asset_bucket which is used by the Gousto Gulp upload task to select which S3 bucket to upload the FE assets to.
    
    Brief conversations have been had with Potatoes to determine whether storing these files on S3 is even required.
    A) It used gousto gulp, something barely anyone knows about and isn't actively maintained.
    B) We have a CF distro in front of the whole site that can cache assets, especially after the changes in [FEF-102] which brings it to lower environments. So why do we need to store them on S3 to reduce server load? They can be served by the Express server like in development and then cached by CF.
    
    This work will probably come in another ticket.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,21 +6,8 @@ echo "${ASSETS_BUCKET}"
 
 cd src
 
-# Sets env dependant variables
-envNameUppercase=$(echo $ENVIRONMENT | tr [a-z] [A-Z])
-checkoutComEnvName="CHECKOUTCOM_PK_$envNameUppercase"
-export CHECKOUTCOM_PK=${!checkoutComEnvName}
-
 # set npm environment variables
 npm config set goustowebclient_asset_bucket "${ASSETS_BUCKET}"
-npm config set goustowebclient_environment_name "${ENVIRONMENT}"
-npm config set goustowebclient_products_domain "${PRODUCT_SERVICE_DOMAIN}"
-npm config set goustowebclient_api_domain "${PRODUCT_SERVICE_DOMAIN}"
-npm config set goustowebclient_api_token ""
-npm config set goustowebclient_products_domain_path "/products/v2.0"
-npm config set goustowebclient_deliveries_domain "${DELIVERY_SERVICE_DOMAIN}"
-npm config set goustowebclient_deliveries_domain_path "/deliveries/v1.0"
-npm config set goustowebclient_checkoutcom_pk "${CHECKOUTCOM_PK}"
 # end set npm environment variables
 
 npm run deploy
