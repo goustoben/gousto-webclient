757df1930e503986cf6df15028cdb2f1c0ec36d1
Max Hilliard <max.hilliard@gousto.co.uk>
2021-07-29 11:13:00 +0100

    Fef 102/add waf header for cypress tests (#3768)
    * FEF-102 Cherry-pick adding waf header for regression tests from FEF-100
    
    * Add missing circle secret for mobile regression tests

--- scripts/validate-regression-mobile.sh
+++ scripts/validate-regression-mobile.sh
@@ -4,6 +4,7 @@ AWS_ACCESS_KEY_ID=$BETA_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$BETA_AWS_SECRET
 gousto env sglease $ENVIRONMENT -t 30
 
 export CYPRESS_baseUrl="https://${ENVIRONMENT}-frontend.gousto.info"
+export CYPRESS_WAF_ACCESS_TOKEN="${WAF_ACCESS_TOKEN}"
 
 cd tests/regression
 chmod a+x runtestsmobile.sh
