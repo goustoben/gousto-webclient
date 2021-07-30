0edea91ed4c9db4e98432bb8197f9fc80ad262ff
maxhilliard <max.hilliard@gousto.co.uk>
2021-07-30 12:01:39 +0100

    Number coverage reports by circle node index

---
+++ src/scripts/merge-parallel-jest-coverage-reports.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+const fs = require('fs')
+const path = require('path')
+
+// Require all reports
+// Iterate over all keys
+// Pick the report result with the highest coverage
+// Write file
+
+() => {
+
+}()
