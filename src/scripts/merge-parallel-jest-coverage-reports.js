6bfda95558fc774ea1f0ad944c14b8c6b845bac9
maxhilliard <max.hilliard@gousto.co.uk>
2021-07-30 12:10:51 +0100

    Experiment with storing parallel reports in different dirs

--- src/scripts/merge-parallel-jest-coverage-reports.js
+++ src/scripts/merge-parallel-jest-coverage-reports.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
 const fs = require('fs')
-const path = require('path')
+const path = require('path')(
+  // Require all reports
+  // Iterate over all keys
+  // Pick the report result with the highest coverage
+  // Write file
 
-// Require all reports
-// Iterate over all keys
-// Pick the report result with the highest coverage
-// Write file
-
-() => {
-
-}()
+  () => {}
+)()
