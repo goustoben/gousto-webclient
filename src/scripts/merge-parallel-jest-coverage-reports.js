6e7dbd5db81ec0f878343e644f1c9d253ed28ae8
maxhilliard <max.hilliard@gousto.co.uk>
2021-07-30 13:11:03 +0100

    Persist tests-to-run file for use in merging

--- src/scripts/merge-parallel-jest-coverage-reports.js
+++ src/scripts/merge-parallel-jest-coverage-reports.js
@@ -1,10 +1,33 @@
 /* eslint-disable */
 const fs = require('fs')
-const path = require('path')(
-  // Require all reports
-  // Iterate over all keys
-  // Pick the report result with the highest coverage
-  // Write file
+const path = require('path')
 
-  () => {}
-)()
+;(() => {
+  // Require all reports
+  const allFiles = fs.readdirSync(process.cwd())
+  const allReports = allFiles.filter((file) => {
+    return path.extname(file).toLowerCase() === '.json' && file.includes('coverage-summary')
+  })
+
+  console.log('all report files:', JSON.stringify(allReports))
+
+  const [firstReport, ...restReports] = allReports.map((filename) => require(filename))
+
+  // Iterate over all keys
+  const mergedReport = Object.entries(firstReport).reduce((_merged, [path, file], idx) => {
+    // Pick the report result with the highest coverage
+    if (path === 'total') {
+      return {
+        ...merged,
+        [path]: file,
+      }
+    }
+
+    // Get report values from all reports for this idx
+    const reportValuesForIdx = restReports.reduce((vals, currentReport) => {
+      return [...vals, Object.values(currentReport)[idx]]
+    }, [])
+  }, {})
+
+  // Write file
+})()
