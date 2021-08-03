a049561258e57b7d425d5607806c90bcead81978
maxhilliard <max.hilliard@gousto.co.uk>
2021-08-03 15:24:53 +0100

    FEF-149 remove unused scripts

--- src/scripts/compare-code-health.sh
+++ src/scripts/compare-code-health.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-npm run test:jest:ci:coverage
-
 ./scripts/run-eslint-ci.sh
 
 git fetch origin develop && git diff --name-status origin/develop | node ./scripts/compare-code-health.js develop tmp/artifacts/code_health.json
