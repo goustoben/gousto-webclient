79d71cf306e2a3cb88c2f3cfdd1e303a4feca4e2
Gavin Boulton <gavboulton@gmail.com>
2021-09-09 10:20:20 +0100

    [WIP] Start `addRecipe` method in `useBasket` hook

--- src/src/routes/Menu/apis/basket.hook.js
+++ src/src/routes/Menu/apis/basket.hook.js
@@ -1,1 +1,23 @@
-export const useBasket = () => ({})
+import { useDispatch, useSelector } from 'react-redux'
+import { getBasketPostcode } from '../../../selectors/basket'
+import { menuBrowseCTAVisibilityChange } from '../../../actions/menu'
+
+export const useBasket = () => {
+  const dispatch = useDispatch()
+  const state = useSelector(s => s)
+
+  const addRecipe = (recipeId) => {
+    const basketPostcode = getBasketPostcode(state)
+
+    // check postcode is set, if not dispatch postcode CTA popup
+    if (!basketPostcode) {
+      return dispatch(menuBrowseCTAVisibilityChange(true))
+    }
+
+    // TODO: should close details screen
+  }
+
+  return {
+    addRecipe,
+  }
+}
--- src/src/routes/Menu/apis/basket.hook.test.js
+++ src/src/routes/Menu/apis/basket.hook.test.js
@@ -1,5 +1,5 @@
 describe('useBasket hook', () => {
   describe('addRecipe', () => {
-    it.skip('adds a recipes to redux state', () => {})
+    it('shows postcode input when not set', () => {})
   })
 })
