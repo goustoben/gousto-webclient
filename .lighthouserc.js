b87d98a0b29a85468b5fedbcd2e1d8314f3146d5
maxhilliard <max.hilliard@gousto.co.uk>
2021-10-08 18:19:42 +0100

    feat: simple lighthouse performance budget
    fix: add missing workflow step

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,9 +2,21 @@ module.exports = {
   ci: {
     collect: {
       url: ["https://staging-www.gousto.info/"],
+      numberOfRuns: 3,
+      onlyCategories: ["performance"],
+      settings: {
+        preset: "desktop",
+      },
+    },
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["error", { maxNumericValue: 500 }],
+        interactive: ["error", { maxNumericValue: 1000 }],
+      },
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "filesystem",
+      outputDir: "./performance-budget-results",
     },
   },
 };
