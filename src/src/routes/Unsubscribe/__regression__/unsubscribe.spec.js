d7f35cbb9bf5bbbe2cd59fbefcc8589fb3b96e62
Stefan Smith <stefan.smith@gousto.co.uk>
2021-10-11 13:51:50 +0100

    Retrofit regression tests for Unsubscribe feature

---
+++ src/src/routes/Unsubscribe/__regression__/unsubscribe.spec.js
@@ -0,0 +1,77 @@
+const CONFIRM_UNSUBSCRIBE_ELEMENT_SELECTOR = '[data-testing="confirm-unsubscribe"]'
+
+describe('Unsubscribing from marketing emails', () => {
+  before(() => {
+    cy.stubAll3rdParties()
+    cy.server()
+
+    cy.route({
+      method: 'DELETE',
+      url: unsubscribeApiPathFor({
+        authUserId: 'the-auth-user-id',
+        marketingType: 'the-market-type'
+      }),
+      status: 200,
+      response: {}
+    })
+
+    cy.route({
+      method: 'DELETE',
+      url: unsubscribeApiPathFor({
+        authUserId: 'non-existent-auth-user-id',
+        marketingType: 'the-market-type'
+      }),
+      status: 404,
+      response: {}
+    })
+  })
+
+  describe('Given I click an unsubscribe link that Gousto provided to me', () => {
+    before(() => {
+      cy.visit(unsubscribeUrlPathFor({
+        authUserId: 'the-auth-user-id',
+        marketingType: 'the-market-type',
+        marketingUnsubscribeToken: 'the-marketing-unsubscribe-token'
+      }))
+    })
+
+    describe('And I confirm I want to unsubscribe', () => {
+      before(() => {
+        cy.get(CONFIRM_UNSUBSCRIBE_ELEMENT_SELECTOR).click()
+      })
+
+      it('Then I see the post-unsubscribe call to action', () => {
+        cy.get('[data-testing="post-unsubscribe-cta"]')
+      })
+    })
+  })
+
+  describe('Given I click an unsubscribe link for a non-existent user', () => {
+    before(() => {
+      cy.visit(unsubscribeUrlPathFor({
+        authUserId: 'non-existent-auth-user-id',
+        marketingType: 'the-market-type',
+        marketingUnsubscribeToken: 'the-marketing-unsubscribe-token'
+      }))
+    })
+
+    describe('And I confirm I want to unsubscribe', () => {
+      before(() => {
+        cy.get(CONFIRM_UNSUBSCRIBE_ELEMENT_SELECTOR).click()
+      })
+
+      it('Then I see an error message rather than the post-unsubscribe call to action', () => {
+        cy.contains('error')
+        cy.get('[data-testing="post-unsubscribe-cta"]').should('not.exist')
+      })
+    })
+  })
+})
+
+function unsubscribeApiPathFor({authUserId, marketingType}) {
+  return `/user/${authUserId}/marketing/${marketingType}`
+}
+
+function unsubscribeUrlPathFor({authUserId, marketingType, marketingUnsubscribeToken}) {
+  return `/unsubscribe?authUserId=${authUserId}&marketingType=${marketingType}&marketingUnsubscribeToken=${marketingUnsubscribeToken}`
+}
