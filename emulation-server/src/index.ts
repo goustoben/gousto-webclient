c7f75d0791f4aa11df167a75df2270b176e07dce
James Sawle <james.sawle@gousto.co.uk>
2021-10-13 11:44:18 +0100

    [FDE-1] Add body parser to handle DELETEs with bodys

--- emulation-server/src/index.ts
+++ emulation-server/src/index.ts
@@ -1,13 +1,20 @@
 import Router from '@koa/router';
 import Koa from 'koa';
+import bodyParser from 'koa-body';
 import logger from 'koa-logger';
 
 const app = new Koa();
 
 const router = new Router();
-router.delete('/user/the-user-id/marketing/the-marketing-type', (ctx) => ctx.body = { status: 'ok' });
+router.delete('/user/the-user-id/marketing/the-marketing-type', (ctx) => {
+    console.log(ctx.request.body)
+    ctx.body = { status: 'ok' }
+});
 
 app.use(logger())
+    .use(bodyParser({
+        parsedMethods: ['POST', 'PUT', 'PATCH', 'GET', 'HEAD', 'DELETE'],
+    }))
     .use(router.routes())
     .use(router.allowedMethods());
 
