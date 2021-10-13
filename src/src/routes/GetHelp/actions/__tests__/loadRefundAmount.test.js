0c19207661de1d0f1ff20300bd64620a5595004d
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2021-10-13 11:32:39 +0100

    [TC-2897] Hide auto accept credit in GetHelp behind a feature flag  (#4126)
    * [TC-2897] Add isAutoAcceptEnabled feature flag
    
    * [TC-2897] Use feature flag in loadRefundAmount
    
    * [TC-2897] Refactor loadRefundAmount test to use mocked selectors
    
    Co-authored-by: Jess Karia <jesskaria1@gmail.com>

--- src/src/routes/GetHelp/actions/__tests__/loadRefundAmount.test.js
+++ src/src/routes/GetHelp/actions/__tests__/loadRefundAmount.test.js
@@ -1,21 +1,40 @@
-import Immutable from 'immutable'
-import { fetchRefundAmount } from 'apis/getHelp'
+import { safeJestMock } from '_testing/mocks'
+import * as authSelectors from 'selectors/auth'
+import * as userSelectors from 'selectors/user'
+import * as fetchRefundApi from 'apis/getHelp'
 import { actionTypes as webClientActionTypes } from 'actions/actionTypes'
 import { loadRefundAmount } from '../loadRefundAmount'
+import * as getHelpSelectors from '../../selectors/selectors'
+import * as AutoAcceptEnabledFeature from '../../../../selectors/features'
 import * as getHelpActionsUtils from '../utils'
 
-jest.mock('apis/getHelp')
+const getAccessToken = safeJestMock(authSelectors, 'getAccessToken')
+const getUserId = safeJestMock(userSelectors, 'getUserId')
+const fetchRefundAmount = safeJestMock(fetchRefundApi, 'fetchRefundAmount')
+const getOrderId = safeJestMock(getHelpSelectors, 'getOrderId')
+const getSelectedIngredients = safeJestMock(getHelpSelectors, 'getSelectedIngredients')
+const getIsAutoAcceptEnabled = safeJestMock(AutoAcceptEnabledFeature, 'getIsAutoAcceptEnabled')
 
+const ACCESS_TOKEN = 'adfjlakjds13'
+const ACTION_TYPE = 'GET_HELP_LOAD_REFUND_AMOUNT'
 const AMOUNT = 2.4
 const LOW_AMOUNT = 1.9
+const INGREDIENT_UUID = [
+  '3c07d126-f655-437c-aa1d-c38dbbae0398',
+]
+const INGREDIENT_UUIDS = [
+  '3c07d126-f655-437c-aa1d-c38dbbae0398',
+  '488d5751-dcff-4985-88c0-bf745ff54904',
+]
+const ORDER_ID = '4455'
 const TYPE = 'credit'
-const ACCESS_TOKEN = 'adfjlakjds13'
+const USER_ID = '1234'
 
 const RESPONSE = {
   status: 'ok',
   data: {
     value: AMOUNT,
-    multiComplaintTotalValue: 0,
+    multiComplaintTotalValue: 5,
     type: TYPE,
   }
 }
@@ -29,212 +48,198 @@ const RESPONSE_WITH_AUTO_ACCEPT_AMOUNT = {
   }
 }
 
-const USER_ID = '1234'
-const ORDER_ID = '4455'
-const ACTION_TYPE = 'GET_HELP_LOAD_REFUND_AMOUNT'
-const INGREDIENT_UUIDS = [
-  '3c07d126-f655-437c-aa1d-c38dbbae0398',
-  '488d5751-dcff-4985-88c0-bf745ff54904',
-]
-
-const STATE = {
-  auth: Immutable.fromJS({ accessToken: ACCESS_TOKEN }),
-  user: Immutable.fromJS({
-    id: USER_ID,
-  }),
-  getHelp: Immutable.fromJS({
-    order: {
-      id: ORDER_ID,
-    },
-    selectedIngredients: {
-      '385&3c07d126-f655-437c-aa1d-c38dbbae0398': {
-        recipeId: '385',
-        ingredientUuid: '3c07d126-f655-437c-aa1d-c38dbbae0398',
-        label: '8ml soy sauce',
-        recipeGoustoReference: '408',
-        issueId: '4',
-        issueName: 'Wrong Ingredients',
-        issueDescription: 'assd'
-      },
-      '2223&488d5751-dcff-4985-88c0-bf745ff54904': {
-        recipeId: '2223',
-        ingredientUuid: '488d5751-dcff-4985-88c0-bf745ff54904',
-        label: '40g Cornish clotted cream',
-        recipeGoustoReference: '2093',
-        issueId: '3',
-        issueName: 'Missing Ingredients',
-        issueDescription: 'ssss'
-      },
-    }
-  })
+const SELECTED_INGREDIENTS = {
+  '385&3c07d126-f655-437c-aa1d-c38dbbae0398': {
+    recipeId: '385',
+    ingredientUuid: '3c07d126-f655-437c-aa1d-c38dbbae0398',
+    label: '8ml soy sauce',
+    recipeGoustoReference: '408',
+    issueId: '4',
+    issueName: 'Wrong Ingredients',
+    issueDescription: 'assd'
+  },
 }
-
-const STATE_WITH_ONLY_ONE_INGREDIENT = {
-  auth: Immutable.fromJS({ accessToken: ACCESS_TOKEN }),
-  user: Immutable.fromJS({
-    id: USER_ID,
-  }),
-  getHelp: Immutable.fromJS({
-    order: {
-      id: ORDER_ID,
-    },
-    selectedIngredients: {
-      '385&3c07d126-f655-437c-aa1d-c38dbbae0398': {
-        recipeId: '385',
-        ingredientUuid: '3c07d126-f655-437c-aa1d-c38dbbae0398',
-        label: '8ml soy sauce',
-        recipeGoustoReference: '408',
-        issueId: '4',
-        issueName: 'Wrong Ingredients',
-        issueDescription: 'assd'
-      },
-    }
-  })
+const MULTIPLE_SELECTED_INGREDIENTS = {
+  ...SELECTED_INGREDIENTS,
+  '2223&488d5751-dcff-4985-88c0-bf745ff54904': {
+    recipeId: '2223',
+    ingredientUuid: '488d5751-dcff-4985-88c0-bf745ff54904',
+    label: '40g Cornish clotted cream',
+    recipeGoustoReference: '2093',
+    issueId: '3',
+    issueName: 'Missing Ingredients',
+    issueDescription: 'ssss'
+  },
 }
 
 const dispatch = jest.fn()
-const getState = jest.fn().mockReturnValue(STATE)
+const getState = jest.fn()
 const asyncAndDispatchSpy = jest.spyOn(getHelpActionsUtils, 'asyncAndDispatch')
 
-describe('Given loadRefundAmount action is called', () => {
+describe('loadRefundAmount', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
 
-  describe('When api call returns successfully', () => {
-    beforeEach(async () => {
-      fetchRefundAmount.mockResolvedValueOnce(RESPONSE)
-      await loadRefundAmount()(dispatch, getState)
+  beforeEach(async () => {
+    getAccessToken.mockReturnValue(ACCESS_TOKEN)
+    getUserId.mockReturnValue(USER_ID)
+    getOrderId.mockReturnValue(ORDER_ID)
+    getSelectedIngredients.mockReturnValue(SELECTED_INGREDIENTS)
+  })
+
+  test('fetchRefundAmount is called with the right params', async () => {
+    await loadRefundAmount()(dispatch, getState)
+
+    expect(fetchRefundAmount).toHaveBeenCalledWith(
+      ACCESS_TOKEN,
+      {
+        customer_id: Number(USER_ID),
+        order_id: Number(ORDER_ID),
+        ingredient_ids: INGREDIENT_UUID,
+      }
+    )
+  })
+
+  describe('When isAutoAcceptEnabled flag is false', () => {
+    beforeEach(() => {
+      getIsAutoAcceptEnabled.mockReturnValue(false)
     })
 
-    test('the refund amount and the refund type are dispatched', () => {
-      expect(dispatch).toHaveBeenCalledWith({
-        type: ACTION_TYPE,
-        payload: {
-          amount: AMOUNT,
-          isAutoAccept: false,
-          type: TYPE,
-          totalAmount: 0,
-        },
+    describe('and isMultiComplaint is false', () => {
+      beforeEach(async () => {
+        fetchRefundAmount.mockResolvedValue(RESPONSE_WITH_AUTO_ACCEPT_AMOUNT)
+
+        await loadRefundAmount()(dispatch, getState)
+      })
+
+      test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept and isMultiComplaint false ', () => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: webClientActionTypes.TRACKING,
+          trackingData: {
+            actionType: 'ssr_ingredients_auto_accept_check',
+            auto_accept: false,
+            is_second_complaint: false,
+            seCategory: 'help',
+          }
+        })
       })
     })
 
-    test('fetchRefundAmount is called with the right params', () => {
-      expect(fetchRefundAmount).toHaveBeenCalledWith(
-        ACCESS_TOKEN,
-        {
-          customer_id: Number(USER_ID),
-          order_id: Number(ORDER_ID),
-          ingredient_ids: INGREDIENT_UUIDS,
-        }
-      )
-    })
+    describe('and isMultiComplaint is true', () => {
+      beforeEach(async () => {
+        fetchRefundAmount.mockResolvedValue(RESPONSE)
 
-    test('asyncAndDispatch is called with the right params', () => {
-      expect(asyncAndDispatchSpy).toHaveBeenCalledWith(
-        expect.objectContaining({
-          dispatch,
-          actionType: ACTION_TYPE,
-          errorMessage: `Failed to loadRefundAmount for orderId: ${ORDER_ID}, userId: ${USER_ID}, ingredientIds: ${INGREDIENT_UUIDS}`,
+        await loadRefundAmount()(dispatch, getState)
+      })
+
+      test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept false and isMultiComplaint true ', () => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: webClientActionTypes.TRACKING,
+          trackingData: {
+            actionType: 'ssr_ingredients_auto_accept_check',
+            auto_accept: false,
+            is_second_complaint: true,
+            seCategory: 'help',
+          }
         })
-      )
+      })
     })
   })
 
-  describe('When api call returns successfully', () => {
-    describe('and the refund amount and number of ingredients are lower than the max auto accept amount', () => {
-      beforeEach(async () => {
-        getState.mockReturnValueOnce(STATE_WITH_ONLY_ONE_INGREDIENT)
-
-        fetchRefundAmount.mockResolvedValueOnce(RESPONSE_WITH_AUTO_ACCEPT_AMOUNT)
-        await loadRefundAmount()(dispatch, getState)
-      })
-
-      test('isAutoAccept is dispatched as true', () => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: ACTION_TYPE,
-          payload: {
-            amount: LOW_AMOUNT,
-            isAutoAccept: true,
-            type: TYPE,
-            totalAmount: 0,
-          },
-        })
-      })
-
-      test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept true', () => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: webClientActionTypes.TRACKING,
-          trackingData: {
-            actionType: 'ssr_ingredients_auto_accept_check',
-            auto_accept: true,
-            is_second_complaint: false,
-            seCategory: 'help',
-          }
-        })
-      })
+  describe('When isAutoAcceptEnabled flag is true', () => {
+    beforeEach(() => {
+      getIsAutoAcceptEnabled.mockReturnValue(true)
     })
 
-    describe('and the refund amount is higher than the max auto accept amount ', () => {
-      beforeEach(async () => {
-        fetchRefundAmount.mockResolvedValueOnce(RESPONSE)
-        await loadRefundAmount()(dispatch, getState)
-      })
-
-      test('isAutoAccept is dispatched as false', () => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: ACTION_TYPE,
-          payload: {
-            amount: AMOUNT,
-            isAutoAccept: false,
-            type: TYPE,
-            totalAmount: 0,
-          },
-        })
-      })
-
-      test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept false', () => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: webClientActionTypes.TRACKING,
-          trackingData: {
-            actionType: 'ssr_ingredients_auto_accept_check',
-            auto_accept: false,
-            is_second_complaint: false,
-            seCategory: 'help',
-          }
-        })
-      })
-    })
-
-    describe('and number of ingredients are more than the max auto accept amount', () => {
-      beforeEach(async () => {
-        getState.mockReturnValueOnce(STATE)
-
+    describe('and api call returns successfully with auto accept amount', () => {
+      beforeEach(() => {
         fetchRefundAmount.mockResolvedValueOnce(RESPONSE_WITH_AUTO_ACCEPT_AMOUNT)
-        await loadRefundAmount()(dispatch, getState)
       })
 
-      test('isAutoAccept is dispatched as false', () => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: ACTION_TYPE,
-          payload: {
-            amount: LOW_AMOUNT,
-            isAutoAccept: false,
-            type: TYPE,
-            totalAmount: 0,
-          },
+      test('asyncAndDispatch is called with the right params', async () => {
+        await loadRefundAmount()(dispatch, getState)
+
+        expect(asyncAndDispatchSpy).toHaveBeenCalledWith(
+          expect.objectContaining({
+            dispatch,
+            actionType: ACTION_TYPE,
+            errorMessage: `Failed to loadRefundAmount for orderId: ${ORDER_ID}, userId: ${USER_ID}, ingredientIds: ${INGREDIENT_UUID}`,
+          })
+        )
+      })
+
+      describe('and the number of ingredients is lower than the max auto accept amount', () => {
+        beforeEach(async () => {
+          getSelectedIngredients.mockReturnValue(SELECTED_INGREDIENTS)
+
+          await loadRefundAmount()(dispatch, getState)
+        })
+
+        test('correct payload with isAutoAccept set to true is dispatched', async () => {
+          expect(dispatch).toHaveBeenCalledWith({
+            type: ACTION_TYPE,
+            payload: {
+              amount: LOW_AMOUNT,
+              isAutoAccept: true,
+              type: TYPE,
+              totalAmount: 0,
+            },
+          })
+        })
+
+        test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept true', () => {
+          expect(dispatch).toHaveBeenCalledWith({
+            type: webClientActionTypes.TRACKING,
+            trackingData: {
+              actionType: 'ssr_ingredients_auto_accept_check',
+              auto_accept: true,
+              is_second_complaint: false,
+              seCategory: 'help',
+            }
+          })
         })
       })
 
-      test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept false', () => {
-        expect(dispatch).toHaveBeenCalledWith({
-          type: webClientActionTypes.TRACKING,
-          trackingData: {
-            actionType: 'ssr_ingredients_auto_accept_check',
-            auto_accept: false,
-            is_second_complaint: false,
-            seCategory: 'help',
-          }
+      describe('and the number of ingredients is more than the max auto accept amount', () => {
+        beforeEach(async () => {
+          getSelectedIngredients.mockReturnValue(MULTIPLE_SELECTED_INGREDIENTS)
+
+          await loadRefundAmount()(dispatch, getState)
+        })
+
+        test('correct payload with isAutoAccept set to false is dispatched', async () => {
+          expect(dispatch).toHaveBeenCalledWith({
+            type: ACTION_TYPE,
+            payload: {
+              amount: LOW_AMOUNT,
+              isAutoAccept: false,
+              type: TYPE,
+              totalAmount: 0,
+            },
+          })
+        })
+
+        test('trackIngredientsAutoAcceptCheck is dispatched with isAutoAccept false', () => {
+          expect(dispatch).toHaveBeenCalledWith({
+            type: webClientActionTypes.TRACKING,
+            trackingData: {
+              actionType: 'ssr_ingredients_auto_accept_check',
+              auto_accept: false,
+              is_second_complaint: false,
+              seCategory: 'help',
+            }
+          })
+        })
+
+        test('asyncAndDispatch is called with the right params', async () => {
+          expect(asyncAndDispatchSpy).toHaveBeenCalledWith(
+            expect.objectContaining({
+              dispatch,
+              actionType: ACTION_TYPE,
+              errorMessage: `Failed to loadRefundAmount for orderId: ${ORDER_ID}, userId: ${USER_ID}, ingredientIds: ${INGREDIENT_UUIDS}`,
+            })
+          )
         })
       })
     })
