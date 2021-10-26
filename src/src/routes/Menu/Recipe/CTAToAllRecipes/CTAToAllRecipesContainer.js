934f235f4bb95811d9c2912d7b483ccee4dc2279
Rianne <46559738+riannemcc@users.noreply.github.com>
2021-10-26 09:30:23 +0100

    [NP-1322] Add "see all recipes" button to CFY (#4181)
    * Deleting CTAThematic
    
    * Deleting CTACard
    
    * Adding CTAToAllRecipes to RecipeGrid
    
    * Updating CTAToAllRecipes
    
    * exporting CTAToAllRecipesContainer
    
    * Adding CTAToAllRecipes back into RecipeList

--- src/src/routes/Menu/Recipe/CTAToAllRecipes/CTAToAllRecipesContainer.js
+++ src/src/routes/Menu/Recipe/CTAToAllRecipes/CTAToAllRecipesContainer.js
@@ -1,9 +1,18 @@
 import { connect } from 'react-redux'
 import { changeCollectionToAllRecipesViaCTA } from 'actions/filters'
+import { getCurrentCollectionId } from '../../selectors/collections'
 import { CTAToAllRecipes } from './CTAToAllRecipes'
 
-const CTAToAllRecipesContainer = connect(null, {
-  collectionFilterChange: changeCollectionToAllRecipesViaCTA
+function mapStateToProps(state) {
+  const collectionId = getCurrentCollectionId(state)
+
+  return {
+    menuCurrentCollectionId: collectionId,
+  }
+}
+
+const CTAToAllRecipesContainer = connect(mapStateToProps, {
+  collectionFilterChange: changeCollectionToAllRecipesViaCTA,
 })(CTAToAllRecipes)
 
 export { CTAToAllRecipesContainer }
