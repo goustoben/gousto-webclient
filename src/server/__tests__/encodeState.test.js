96ec16fa8cfd656d645b866d2375e651a25ebe66
zinkevichav <alexandra.zinkevich@gousto.co.uk>
2021-10-27 18:08:46 +0300

    [TG-5241] Serialise state

--- src/server/__tests__/encodeState.test.js
+++ src/server/__tests__/encodeState.test.js
@@ -1,21 +1,45 @@
 import Immutable from 'immutable'
-import transit from 'transit-immutable-js'
-import encodeState from 'server/encodeState'
+import { PaymentMethod } from 'config/signup'
+
+const encodeState = require('server/encodeState')
 
 describe('encodeState', () => {
-  test('should serialise the immutablejs state tree', () => {
-    const input = Immutable.fromJS({
-      object: {
-        nested: {
-          value: 'value1',
-          value2: 'value2',
+  test('should serialise state tree', () => {
+    const state = {
+      form: {},
+      serverError: '404',
+      payment: Immutable.Map({
+        challengeUrl: null,
+        isModalVisible: false,
+        paymentMethod: PaymentMethod.Card,
+      }),
+      menuCollections: Immutable.OrderedMap(),
+    }
+
+    const serialised = encodeState(state)
+    const expected = JSON.stringify({
+      form: {
+        type: 'plain',
+        value: {}
+      },
+      serverError: {
+        type: 'plain',
+        value: '404',
+      },
+      payment: {
+        type: 'isMap',
+        value: {
+          challengeUrl: null,
+          isModalVisible: false,
+          paymentMethod: PaymentMethod.Card,
         },
+      },
+      menuCollections: {
+        type: 'isOrderedMap',
+        value: {},
       },
     })
 
-    const serialised = encodeState(input)
-    const deserialised = transit.fromJSON(JSON.parse(serialised))
-
-    expect(deserialised).toEqual(input)
+    expect(serialised).toEqual(expected)
   })
 })
--- src/server/encodeState.js
+++ src/server/encodeState.js
@@ -1,3 +1,31 @@
-const transit = require('transit-immutable-js')
+import Immutable from 'immutable'
 
-module.exports = state => JSON.stringify(transit.toJSON(state))
+const defineType = (value) => {
+  if (value instanceof Immutable.OrderedMap) {
+    return 'isOrderedMap'
+  }
+
+  if (value instanceof Immutable.Map) {
+    return 'isMap'
+  }
+
+  if (value instanceof Immutable.List) {
+    return 'isList'
+  }
+
+  return 'plain'
+}
+
+const transformState = (state) => {
+  const transformedState = {}
+  Object.entries(state).forEach(([key, value]) => {
+    transformedState[key] = {
+      type: defineType(value),
+      value,
+    }
+  })
+
+  return transformedState
+}
+
+module.exports = state => JSON.stringify(transformState(state))
