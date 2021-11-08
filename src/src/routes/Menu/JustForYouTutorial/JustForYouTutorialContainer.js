e92e9dd6e13636a11f1b5dc4f83f0527818c184c
Pavlo <pavel.vodopyan@gmail.com>
2021-11-08 16:28:49 +0000

    Disable CFY Tutorial for all customer (#4240)
    * Disable CFY Tutorial for all customer
    
    There is ongoing experiment where we trialing new way of
    representing Chosen For You (CFY) collection.
    
    The CFY tutorial is not aware of new presentation.
    
    Product ownership wants to disable the CFY tutorial experiment
    for the entire customer base.
    
    The reason why the "dead" code remains here (temporary):
    to allow easy way of revert the experiment.
    
    * Diactivate the showTutorial
    
    * Kepp linter happy
    
    * Make code health happy
    
    * Deactivate the getBrowser as well.
    
    As it is not used elssewhere, so there is no point to keep it.
    
    Co-authored-by: Pavlo Vodopyan <pavlo.vodopyan@gousto.co.uk>

--- src/src/routes/Menu/JustForYouTutorial/JustForYouTutorialContainer.js
+++ src/src/routes/Menu/JustForYouTutorial/JustForYouTutorialContainer.js
@@ -2,10 +2,11 @@ import { connect } from 'react-redux'
 
 import { incrementTutorialViewed, tutorialTracking } from 'actions/tutorial'
 
-import { getUserAgent } from 'selectors/root'
 import { getRecommendationShortName } from '../selectors/collections'
 import { JustForYouTutorial } from './JustForYouTutorial'
 
+// TODO: revisit when the `kales_remove_cfy_collection` experiment is over
+/*
 const getBrowser = (userAgent) => {
   if (userAgent.indexOf('Edge') >= 0) {
     return 'Edge'
@@ -16,8 +17,22 @@ const getBrowser = (userAgent) => {
 
   return null
 }
+*/
 
+// eslint-disable-next-line arrow-body-style, no-unused-vars
 const showTutorial = (state) => {
+  return false
+
+  // TODO: revisit when the `kales_remove_cfy_collection` experiment is over
+  //
+  // There is ongoing experiment where we trialing new way of
+  // representing Chosen For You (CFY) collection.
+  // The CFY tutorial is not aware of new presentation.
+  // Product ownership wants to disable the CFY tutorial experiment
+  // for the entire customer base.
+  // The reason why the "dead" code remains here (temporary):
+  // to allow easy way of revert the experiment.
+  /**
   const userAgentFromState = getUserAgent(state)
   const browser = getBrowser(userAgentFromState)
   const jfyPresent = state.tutorial && state.tutorial.getIn(['visible', 'justforyou'], false)
@@ -27,6 +42,7 @@ const showTutorial = (state) => {
   }
 
   return true
+   */
 }
 
 const mapStateToProps = (state) => ({
