f850ed9317cb0bb024871399f1f0582ae7c6ea03
Eliza Plowden <elizaplowden@gmail.com>
2021-11-15 17:56:24 +0000

    navbar links

--- src/src/components/Header/menuItemsHelper.js
+++ src/src/components/Header/menuItemsHelper.js
@@ -4,17 +4,17 @@ import { deepCloneObject } from 'utils/deepClone'
 
 export const defaultMenuItems = {
   home: { name: 'Home', url: clientRoutes.home, clientRouted: true },
-  boxPrices: { name: 'Prices', url: clientRoutes.boxPrices, clientRouted: true, tracking: 'BoxPricingNavigation Clicked' },
-  menu: { name: 'Take Quiz', url: clientRoutes.menu, tracking: trackingKeys.clickRecipeNavigation },
+  boxPrices: { name: 'Subscrirption', url: clientRoutes.boxPrices, clientRouted: true, tracking: 'BoxPricingNavigation Clicked' },
+  menu: { name: 'Online Shop', url: clientRoutes.menu, tracking: trackingKeys.clickRecipeNavigation },
   help: {
     name: 'Help',
     url: clientRoutes.helpCentre,
     clientRouted: false,
     tracking: trackingKeys.clickHelpNavigation,
   },
-  myGousto: { name: 'My Nourished', url: clientRoutes.myGousto, clientRouted: true, tracking: 'MyGoustoNavigation Clicked' },
-  referFriend: { name: 'Nourished Kids', url: clientRoutes.referFriend, clientRouted: true, tracking: 'ReferAFriendNavigation Clicked' },
-  rateMyRecipes: { name: 'Rate My Vitamins', url: clientRoutes.rateMyRecipes, clientRouted: false, tracking: 'RateMyRecipes Clicked' },
+  myGousto: { name: 'My Craft', url: clientRoutes.myGousto, clientRouted: true, tracking: 'MyGoustoNavigation Clicked' },
+  referFriend: { name: 'Refer a Friend', url: clientRoutes.referFriend, clientRouted: true, tracking: 'ReferAFriendNavigation Clicked' },
+  rateMyRecipes: { name: 'Rate My Gins', url: clientRoutes.rateMyRecipes, clientRouted: false, tracking: 'RateMyRecipes Clicked' },
   deliveries: { name: 'Upcoming Deliveries', url: clientRoutes.myDeliveries, clientRouted: false, tracking: 'DeliveriesNavigation Clicked' },
   subscription: { name: 'Subscription Settings', url: clientRoutes.mySubscription, clientRouted: true, tracking: 'SubscriptionSettingsNavigation Clicked' },
   details: { name: 'Account Details', url: clientRoutes.myDetails, clientRouted: false, tracking: 'DetailsNavigation Clicked' },
@@ -22,7 +22,7 @@ export const defaultMenuItems = {
 }
 
 export const showcaseMenuItem = {
-  name: 'This week’s menu',
+  name: "This month's menu",
   url: clientRoutes.menu,
   tracking: trackingKeys.clickRecipeNavigation
 }
