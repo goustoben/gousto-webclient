4e01845819c6160da25366f2a42292de83b644f2
Stefan Smith <stefan.smith@gousto.co.uk>
2021-11-17 13:30:31 +0000

    Stop unnecessarily stubbing GET /user/current

--- src/src/routes/GetHelp/__regression__/recipe-cards.spec.js
+++ src/src/routes/GetHelp/__regression__/recipe-cards.spec.js
@@ -1,12 +1,10 @@
 describe('Given the customer is logged in', () => {
   beforeEach(() => {
     cy.server()
-    cy.fixture('user/userCurrent').as('userCurrent')
-    cy.route('GET', /user\/current/, '@userCurrent').as('userCurrentRequest')
 
-    document.cookie = `v1_oauth_token=${encode({access_token: '8tebopinE7WiWTgDDGl92NhMYXLMCD9AUHfEsWcH'})};path=/;`
-    document.cookie = `v1_oauth_expiry=${encode({expires_at: '2030-01-31T22:15:01.593Z'})};path=/`
-    document.cookie = `v1_oauth_refresh=${encode({refresh_token: '5lJLjJ67tJ5n8RIW2ZYohXTes4F47qEMtzZSI4HM'})};path=/`
+    document.cookie = `v1_oauth_token=${encode({access_token: 'ANY_ACCESS_TOKEN'})};path=/;`
+    document.cookie = `v1_oauth_expiry=${encode({expires_at: '1970-01-01T00:00:00.000Z'})};path=/`
+    document.cookie = `v1_oauth_refresh=${encode({refresh_token: 'ANY_REFRESH_TOKEN'})};path=/`
   })
 
   describe('When their order is eligible for ingredients refund and they visit the Recipe Cards URL directly', () => {
