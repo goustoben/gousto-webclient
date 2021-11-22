84ab8d1ed401e4d9146015520b34e43bb7411285
Stefan Smith <stefan.smith@gousto.co.uk>
2021-11-22 21:53:19 +0000

    FDE-12 Simplify emulation server authorization handling

--- emulation-server/src/utils/authorization.ts
+++ emulation-server/src/utils/authorization.ts
@@ -2,21 +2,19 @@ import Application from "koa";
 import {State} from "../state";
 
 export function isAuthorizedRequest(ctx: Application.DefaultContext, state: State) {
-    return state.sessions && state.sessions.some(session => {
-        const authorizationCookie = ctx.get('authorization');
+    const authorizationCookie = ctx.get('authorization');
 
-        if (!authorizationCookie) {
-            return false
-        }
+    if (!authorizationCookie) {
+        return false
+    }
 
-        const match = authorizationCookie.match(/^Bearer (\S+)$/);
+    const match = authorizationCookie.match(/^Bearer (\S+)$/);
 
-        if (!match) {
-            return false
-        }
+    if (!match) {
+        return false
+    }
 
-        const accessToken = match[1];
+    const accessToken = match[1];
 
-        return session.accessToken === accessToken;
-    });
+    return state.sessions && state.sessions.some(session => session.accessToken === accessToken);
 }
