c9813414f2039f5ff222ad3da4ac09efd63d5261
Naveen Venkat <naveen-venkat@users.noreply.github.com>
2021-11-24 13:30:49 +0000

    Kal-92- Defining MenuCollection type and converting useCurrentCollection hook to TS (#4307)
    * converting useCurrentCollection JS to TS implementation
    
    * fixing TS errors in types
    
    * fixing review comments
    
    * fixing review comments
    
    * fixing review comments
    
    * fixed ts error
    
    * add example file
    
    * Adding exposing MenuCollection and removing MenuCollectionIMap, MenuCollections, MenuCollectionsIOrderedMap
    
    * Moving ImmutableMap out of MenuColleciton and deleted example.ts
    
    * swapping MenuCollection with MenuCollectionIMap
    
    * swapping MenuCollection with MenuCollectionProperties
    
    * swapping interface with type
    
    Co-authored-by: Naveen Venkat <naveenvenkat@ovmg207.home>
    Co-authored-by: Naveen Venkat <naveenvenkat@OVMG207.local>
    Co-authored-by: James Monger <jameskmonger@hotmail.co.uk>

---
+++ src/src/routes/Menu/types/immutableMap.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+export type ImmutableMap<T> = {
+  get<K extends keyof T>(name: K): T[K]
+  set<S>(o: S): ImmutableMap<T & S>
+}
---
+++ src/src/routes/Menu/types/menuCollection.ts
@@ -0,0 +1,34 @@
+import { fromJS } from 'immutable'
+import { ImmutableMap } from './immutableMap'
+
+type MenuCollectionProperties = {
+  carouselConfig?:
+    | {
+        [k: string]: unknown
+      }[]
+    | null
+  featuredCategoryOrder: number
+  recipesInCollection: string[]
+  slug: string
+  colour: string
+  published: boolean
+  order: number
+  requirements?:
+    | {
+        [k: string]: unknown
+      }[]
+    | null
+  isFeaturedCategory: boolean
+  id: string
+  shortTitle: string
+  description: string
+  default: boolean
+}
+
+export function createMenuCollection(
+  o: MenuCollectionProperties
+): ImmutableMap<MenuCollectionProperties> {
+  return fromJS(o)
+}
+
+export type MenuCollection = ImmutableMap<MenuCollectionProperties>
