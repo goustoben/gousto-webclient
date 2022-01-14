goustoben/gousto-webclient

commit 199d9e63e7ed005b868218c15e4ec8cf67bbc981

Upload sourcemaps to DataDog (#4528)
* feat: add build version to RUM and DD log sdks

* chore: enable source-map generation for client builds

* feat: upload sourcemaps to datadog at build time

* chore: package.lock updates

* fix: downgrade terser-webpack-plugin, move CIRCLE_BUILD_NUM to common config

src/config/webpack.client.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const { getClientPlugins } = require('./build/libs/plugins')
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const UIComponentsAlias = require('../libs/goustouicomponents/setup/webpackAlias')
 const { build, publicPath, clientDevServerEnabled, hmrEnabled } = require('./build/libs/webpack-env-vars.js')
 const { logBuildInfo } = require('./build/libs/logs')
@@ -13,7 +14,7 @@ logBuildInfo(isDevelopmentBuild)
 
 const baseConfig = {
   context: path.resolve(__dirname, '..'),
-  devtool: false,
+  devtool: 'source-map',
   entry: {
     main: ['./src/client.js'],
     legacy: ['./src/legacy.js'],
@@ -34,6 +35,15 @@ const baseConfig = {
     publicPath,
   },
   optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      parallel: true,
+      sourceMap: true,
+      terserOptions: {
+        mangle: true,
+        compress: true,
+      },
+    })],
     splitChunks: {
       cacheGroups: {
         vendor: {

.circleci/config.yml
@@ -184,6 +184,17 @@ jobs:
           paths:
             - mnt/ramdisk/src
 
+  upload-source-maps-to-datadog:
+    docker: *circleci_node_image
+    working_directory: /mnt/ramdisk
+    steps:
+      - attach_workspace:
+          at: /
+      - run:
+          name: Upload sourcemaps to datadog
+          working_directory: src
+          command: source ~/.circlerc && DATADOG_API_KEY="${DATADOG_SOURCE_MAPS_TOKEN}" DATADOG_SITE="datadoghq.eu" BUILD_DIR="./public" CIRCLE_BUILD_NUM="${CIRCLE_BUILD_NUM}" ASSET_URL_PREFIX="https://${ENVIRONMENT}-assets.gousto.info/build/latest/" npm run datadog:upload-sourcemaps
+
   upload-assets-to-s3:
     docker: *gousto_platform_image
     resource_class: medium
@@ -764,6 +775,14 @@ workflows:
           context: gousto-prod-aws-app-circleci
           requires:
             - install-src-dependencies
+          filters:
+            branches:
+              only:
+                - master
+      - upload-source-maps-to-datadog:
+          context: gousto-beta-aws-app-circleci
+          requires:
+            - build-webclient
           filters:
             branches:
               only:

src/config/build/libs/plugins.js
@@ -6,7 +6,6 @@ const ExtractPlugin = require('extract-text-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
@@ -43,26 +42,12 @@ const getProductionClientPlugins = () => {
       },
       canPrint: true,
     }),
-    new TerserPlugin({
-      parallel: true,
-      sourceMap: false,
-      terserOptions: {
-        mangle: true,
-        compress: true,
-        warnings: false,
-        output: {
-          comments: false,
-        },
-      },
-    }),
     ExitCodePlugin,
     new webpack.IgnorePlugin(/^@axe-core/),
   ]
 
   if (process.env.GW_ENABLE_BUNDLE_ANALYZER) {
-    result.push(
-      new BundleAnalyzerPlugin({ analyzerMode: 'disabled', generateStatsFile: true })
-    )
+    result.push(new BundleAnalyzerPlugin({ analyzerMode: 'disabled', generateStatsFile: true }))
   }
 
   return result
@@ -71,14 +56,16 @@ const getProductionClientPlugins = () => {
 const getDevelopmentClientPlugins = (isHmrEnabled) => {
   const result = [
     new SimpleProgressWebpackPlugin({
-      format: 'compact'
+      format: 'compact',
     }),
   ]
 
   if (isHmrEnabled) {
-    result.push(new ReactRefreshWebpackPlugin({
-      overlay: false,
-    }))
+    result.push(
+      new ReactRefreshWebpackPlugin({
+        overlay: false,
+      })
+    )
   }
 
   return result

src/config/build/libs/webpack-env-vars.js
@@ -42,6 +42,7 @@ const webpackEnvVarsBase = {
   __RECAPTCHA_RAF_PUBK__: JSON.stringify(recaptchaReferralPublicKey),
   __RUNNING_ENV__: JSON.stringify(runningEnv),
   __TEST__: false,
+  __CIRCLE_BUILD_NUM__: JSON.stringify(process.env.CIRCLE_BUILD_NUM),
 }
 
 const webpackEnvVarsDev = {
@@ -78,7 +79,6 @@ const webpackEnvVarsServer = {
   __SERVER__: true,
   __TEST__: false,
   'process.env.NODE_ENV': JSON.stringify(build),
-  __CIRCLE_BUILD_NUM__: JSON.stringify(process.env.CIRCLE_BUILD_NUM),
 }
 
 module.exports = {

src/src/middlewares/datadog/config.ts
@@ -6,6 +6,7 @@ export const commonConfig: Partial<LogsInitConfiguration & RumInitConfiguration>
   sampleRate: 1,
   service: 'gousto-webclient',
   env: __ENV__,
+  version: __CIRCLE_BUILD_NUM__,
 }
 
 export const browserLogsConfig: LogsInitConfiguration = {

src/src/middlewares/datadog/mocks.js
@@ -3,4 +3,5 @@ export const getMockedCommonConfig = ({ env }) => ({
   sampleRate: 1,
   service: 'gousto-webclient',
   env,
+  version: 'MOCK_CIRCLE_BUILD_NUM',
 })

commit 33603882e626f5ded9d1a0174a339bf0480b2051

[TG-5844] disable minification in dev builds to increase incremental build speed (#4551)
This fixes a regression in dev build speeds introduced in 199d9e63e.

src/config/webpack.client.js
@@ -109,6 +109,9 @@ const addOverridesForDevBuildConfig = (webpackConfig, _clientDevServerEnabled = 
       filename: '[name].bundle.js',
       publicPath,
     },
+    optimization: {
+      minimize: false,
+    },
   }
 
   const devConfigWithServer = {