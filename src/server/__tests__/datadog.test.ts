cd9ec07e6c850734f79bae837aa61ca5848c2ed4
maxhilliard <max.hilliard@gousto.co.uk>
2022-01-14 13:05:30 +0000

    FEF-309 only init dd-tracer when dd is enabled

--- src/server/__tests__/datadog.test.ts
+++ src/server/__tests__/datadog.test.ts
@@ -14,22 +14,40 @@ describe('datadog', () => {
   describe('configureDDTracer', () => {
     beforeEach(() => {
       jest.clearAllMocks()
-      configureDDTracer()
     })
 
-    it('initializes dd-trace as expected', () => {
-      expect(mockInit).toHaveBeenCalledWith({
-        env: 'production',
-        sampleRate: 1,
-        service: 'gousto-webclient',
-        version: 'MOCK_CIRCLE_BUILD_NUM',
+    describe('when datadog is enabled', () => {
+      beforeEach(() => {
+        global.__DATADOG_ENABLED__ = true
+        configureDDTracer()
+      })
+
+      it('initializes dd-trace as expected', () => {
+        expect(mockInit).toHaveBeenCalledWith({
+          env: 'production',
+          sampleRate: 1,
+          service: 'gousto-webclient',
+          version: 'MOCK_CIRCLE_BUILD_NUM',
+        })
+      })
+  
+      it('configures dd-trace as expected', () => {
+        expect(mockUse).toHaveBeenCalledWith('http', {
+          blocklist: ['/ping'],
+        })
+      })
+    })
+    
+    describe('when datadog is not enabled', () => {
+      beforeEach(() => {
+        global.__DATADOG_ENABLED__ = false
+        configureDDTracer()
+      })
+
+      it('does not initialize dd-trace', () => {
+        expect(mockInit).not.toHaveBeenCalled()
       })
     })
 
-    it('configures dd-trace as expected', () => {
-      expect(mockUse).toHaveBeenCalledWith('http', {
-        blocklist: ['/ping'],
-      })
-    })
   })
 })
