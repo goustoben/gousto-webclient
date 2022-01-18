d823d27bb1d3f4e23019f188f0c179fab97608b9
Jason Lafferty <jasonlafferty@users.noreply.github.com>
2022-01-18 17:02:46 +0000

    Add HotJar component for 5 Recipes (#4560)

--- src/src/types/index.d.ts
+++ src/src/types/index.d.ts
@@ -1,1 +1,8 @@
 export type Nullable<T> = T | null;
+
+declare global {
+    interface Window {
+        // eslint-disable-next-line no-unused-vars
+        hj?: (action: string, name: string) => void
+    }
+}
