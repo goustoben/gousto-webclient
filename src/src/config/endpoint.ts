b1490a4e525a9b03ced1082ab8e7dc7182e56787
Lewis Barclay <lewis.barclay@googlemail.com>
2022-01-20 17:20:07 +0000

    refactor: fix linting issues

--- src/src/config/endpoint.ts
+++ src/src/config/endpoint.ts
@@ -1,14 +1,15 @@
-declare var __SERVER__: boolean;
-declare var __API_ENV__: string;
-declare var __RUNNING_ENV__: ApiTarget;
-declare var __ENDPOINTS__: { [key: string]: any };
+/* eslint-disable no-underscore-dangle */
+declare let __SERVER__: boolean
+declare let __API_ENV__: string
+declare let __RUNNING_ENV__: ApiTarget
+declare let __ENDPOINTS__: { [key: string]: never }
 
 type EndpointUris = {
   live: string,
   local: string,
 }
-type EndpointRequestOrigin = "serverSide" | "clientSide";
-type ApiTarget = "local" | "live";
+type EndpointRequestOrigin = 'serverSide' | 'clientSide';
+type ApiTarget = 'local' | 'live';
 type ServiceLocations = {
   [version: string]: {
     versionString: string,
@@ -23,10 +24,10 @@ function validateEndpointParams(
   apiEnvironmentToPointTo: string,
   appInstanceEnvironment: string
 ): void {
-  if (service === undefined) throw new Error("Please specify a valid service.");
-  if (isCallFromServer === undefined) throw new Error("Please specify whether this call is coming from the server or client browser for routing purposes.");
-  if (apiEnvironmentToPointTo === undefined) throw new Error("Please specify which environment to point this call to.");
-  if (appInstanceEnvironment === undefined) throw new Error("Please specify whether this app is hosted 'locally' or 'live' on AWS.");
+  if (service === undefined) throw new Error('Please specify a valid service.')
+  if (isCallFromServer === undefined) throw new Error('Please specify whether this call is coming from the server or client browser for routing purposes.')
+  if (apiEnvironmentToPointTo === undefined) throw new Error('Please specify which environment to point this call to.')
+  if (appInstanceEnvironment === undefined) throw new Error("Please specify whether this app is hosted 'locally' or 'live' on AWS.")
 }
 
 function getServiceVersionsFromEndpointsConfig(
@@ -39,9 +40,9 @@ function getServiceVersionsFromEndpointsConfig(
         [service]: serviceVersions
       }
     }
-  } = __ENDPOINTS__;
+  } = __ENDPOINTS__
 
-  return serviceVersions;
+  return serviceVersions
 }
 
 function getEndpointFromServiceVersions(
@@ -50,17 +51,17 @@ function getEndpointFromServiceVersions(
   appInstanceEnvironment: ApiTarget,
   clientOrServer: EndpointRequestOrigin
 ): string {
-  let checkedVersion = serviceVersions[version] ? version : 1;
+  const checkedVersion = serviceVersions[version] ? version : 1
 
   const {
     [checkedVersion]: {
       [clientOrServer]: {
-        [appInstanceEnvironment]: endpoint
+        [appInstanceEnvironment]: serviceEndpoint
       }
     }
-  } = serviceVersions;
+  } = serviceVersions
 
-  return endpoint;
+  return serviceEndpoint
 }
 
 /**
@@ -73,17 +74,19 @@ function getEndpointFromServiceVersions(
  */
 function endpoint(
   service: string,
-  version: number = 1,
+  version = 1,
   {
     isCallFromServer = __SERVER__,
     apiEnvironmentToPointTo = __API_ENV__,
     appInstanceEnvironment = __RUNNING_ENV__
   } = {}
 ): string {
-  validateEndpointParams(service, isCallFromServer, apiEnvironmentToPointTo, appInstanceEnvironment);
+  validateEndpointParams(service, isCallFromServer, apiEnvironmentToPointTo, appInstanceEnvironment)
 
-  const serviceVersions = getServiceVersionsFromEndpointsConfig(apiEnvironmentToPointTo, service);
-  return getEndpointFromServiceVersions(version, serviceVersions, appInstanceEnvironment, isCallFromServer ? "serverSide" : "clientSide");
+  const serviceVersions = getServiceVersionsFromEndpointsConfig(apiEnvironmentToPointTo, service)
+
+  return getEndpointFromServiceVersions(version, serviceVersions, appInstanceEnvironment, isCallFromServer ? 'serverSide' : 'clientSide')
 }
 
-export default endpoint;
+// eslint-disable-next-line import/no-default-export
+export default endpoint
