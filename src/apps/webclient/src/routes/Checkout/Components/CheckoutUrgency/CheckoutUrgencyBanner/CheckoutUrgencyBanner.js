9ad3b8f3a79c299be0f2f270b30241f942ef6ff3
Eliza Plowden <elizaplowden@gmail.com>
2022-01-27 15:56:17 +0000

    added space between icon and text

--- src/apps/webclient/src/routes/Checkout/Components/CheckoutUrgency/CheckoutUrgencyBanner/CheckoutUrgencyBanner.js
+++ src/apps/webclient/src/routes/Checkout/Components/CheckoutUrgency/CheckoutUrgencyBanner/CheckoutUrgencyBanner.js
@@ -3,22 +3,47 @@ import { CheckoutUrgencyContext } from 'routes/Checkout/Components/CheckoutUrgen
 import Svg from 'Svg'
 import { formatSeconds } from '../checkoutUrgencyUtils'
 import css from './CheckoutUrgencyBanner.css'
-import { Badge, Box, Color } from '@gousto-internal/citrus-react'
+import {
+  Badge,
+  Box,
+  Color,
+  Space,
+  Display,
+  AlignItems,
+  JustifyContent,
+} from '@gousto-internal/citrus-react'
+
+// export const CheckoutUrgencyBanner = () => {
+//   const remainingSeconds = useContext(CheckoutUrgencyContext)
+//   const formattedTime = formatSeconds(remainingSeconds)
+
+//   return (
+//     <div className={css.container}>
+//       <div className={css.banner}>
+//         <div className={css.iconContainer}>
+//           <Svg className={css.icon} fileName="icon-timer-red" />
+//         </div>
+//         <div className={css.content}>
+//           Checkout within {formattedTime} to avoid losing your recipes
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export const CheckoutUrgencyBanner = () => {
   const remainingSeconds = useContext(CheckoutUrgencyContext)
   const formattedTime = formatSeconds(remainingSeconds)
 
   return (
-    <div className={css.container}>
-      <div className={css.banner}>
-        <div className={css.iconContainer}>
+    <Box bg={Color.Warning_50} borderWidth={3} borderColor={Color.Warning_200}>
+      <Box className={css.container} paddingV={1} display="flex" alignItems={AlignItems.Center}>
+        <Box>
           <Svg className={css.icon} fileName="icon-timer-red" />
-        </div>
-        <div className={css.content}>
-          Checkout within {formattedTime} to avoid losing your recipes
-        </div>
-      </div>
-    </div>
+        </Box>
+        <Space size={1} direction="horizontal" />
+        <Box paddingV={(0, 1)}> Checkout within {formattedTime} to avoid losing your recipes</Box>
+      </Box>
+    </Box>
   )
 }
