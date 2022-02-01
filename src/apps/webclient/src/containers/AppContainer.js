643de285ed9245e1bdd446588b0cde2fdb8d415e
Anthony Donovan <anthonydonovan@OVMG221.local>
2022-02-01 16:15:23 +0000

    Adds ThemeProvider and Reset to AppContainer

--- src/apps/webclient/src/containers/AppContainer.js
+++ src/apps/webclient/src/containers/AppContainer.js
@@ -10,27 +10,31 @@ import { hashLinkScroll } from 'routes/hashLinkScroll'
 import fetchContentOnChange from 'routes/fetchContentOnChange'
 import { documentLocation } from 'utils/window'
 import { SetupOptimizelyOverride } from 'containers/OptimizelyRollouts'
+import { ThemeProvider, Reset } from '@gousto-internal/citrus-react'
 
 export const AppContainer = ({ history, routes, store }) => (
   <Provider store={store}>
     <SetupOptimizelyOverride />
     <SWRConfig
       value={{
-        revalidateOnFocus: false
+        revalidateOnFocus: false,
       }}
     >
-      <Router
-        history={history}
-        // eslint-disable-next-line
-        render={__CLIENT__ ? applyRouterMiddleware(useScroll(shouldScroll)) : undefined}
-        onUpdate={() => {
-          trackPageChange(store)
-          hashLinkScroll()
-          fetchContentOnChange(documentLocation().pathname, store)
-        }}
-      >
-        {routes}
-      </Router>
+      <ThemeProvider>
+        <Reset />
+        <Router
+          history={history}
+          // eslint-disable-next-line
+          render={__CLIENT__ ? applyRouterMiddleware(useScroll(shouldScroll)) : undefined}
+          onUpdate={() => {
+            trackPageChange(store)
+            hashLinkScroll()
+            fetchContentOnChange(documentLocation().pathname, store)
+          }}
+        >
+          {routes}
+        </Router>
+      </ThemeProvider>
     </SWRConfig>
   </Provider>
 )
