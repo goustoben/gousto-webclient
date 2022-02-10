7787a4119b4b10d27bdc6a3ea427692ff4917383
James Monger <jameskmonger@hotmail.co.uk>
2022-02-10 10:52:18 +0000

    [KAL-XXX] Simplify MenuRecipesPage (#4731)
    * remove ternaries from render method
    
    * remove TastePreferences
    
    * move CapacityInfo
    
    * move Modals into function
    
    * remove unnecessary check
    
    * remove unused import

--- src/apps/webclient/src/routes/Menu/MenuRecipesPage/BannerTastePreferences/index.js
+++
@@ -1,1 +0,0 @@
-export { BannerTastePreferencesContainer } from './BannerTastePreferencesContainer'
--- src/apps/webclient/src/routes/Menu/selectors/tastePreferences.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect'
-import { getIsAuthenticated } from '../../../selectors/auth'
-import { getIsTastePreferencesEnabled } from '../../../selectors/features'
-
-export const getShouldBannerTastePreferencesShow = createSelector(
-  getIsTastePreferencesEnabled,
-  getIsAuthenticated,
-  (isTastePreferencesEnabled, isAuthenticated) => isTastePreferencesEnabled && !isAuthenticated
-)
