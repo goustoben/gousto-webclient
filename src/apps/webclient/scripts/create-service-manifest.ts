6ebc7395abaf0bbabb7cc60a0dade6aab18db0ae
Lewis Barclay <lewis.barclay@googlemail.com>
2022-02-14 21:12:30 +0000

    refactor: tidy up create manifest script

--- src/apps/webclient/scripts/create-service-manifest.ts
+++ src/apps/webclient/scripts/create-service-manifest.ts
@@ -1,41 +1,86 @@
 import * as fs from 'fs'
 import { resolve } from 'path'
 
+// old types for the service configuration
+export type ConfigEndpoints = {
+  [serviceName: string]: ConfigEndpointServiceVersions
+}
+
+export type ServiceDetails = {
+  versionString: EndpointVersionString;
+  clientSide: ServiceUrls;
+  serverSide: ServiceUrls;
+}
+
+export type ServiceUrls = {
+  live: string;
+  local: string;
+}
+
+export type EndpointVersionString = '' | 'v1' | 'v1.0' | 'v1.0.0' | 'v2' | 'v2.0';
+
+export type ConfigEndpointServiceVersions = {
+  [version: string]: ServiceDetails;
+}
+
+// new types for the new service manifest
+export type ServiceVersion = {
+  version: number,
+  basePath: string
+}
+
+export type ServiceManifest = {
+  [key: string]: ServiceVersion[]
+}
+
 const JSON5_FILEPATH = '../config/default.json5'
 const ENCODING = 'utf8'
 const URL_PREFIX = 'https://production-api.gousto.co.uk'
 const OUTPUT_FILEPATH = '../src/config/service-environment/service-manifest.json'
+const ENV_PRODUCTION = 'production'
 
-const stripComments = (str: string) => {
-  return str.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, "");
-};
+const pipe = (...fns: any[]) => (init: unknown) => fns.reduce((acc, fn) => fn(acc), init)
 
-const configJson = JSON.parse(
-  stripComments(fs.readFileSync(resolve(__dirname, JSON5_FILEPATH), ENCODING))
-);
+const readFile = (filePath: string = JSON5_FILEPATH) => fs.readFileSync(resolve(__dirname, filePath), ENCODING)
 
-const endpoints = (configJson: any) => configJson.endpoints;
-const servicesForEnvironment = (environment: string) => Object.keys(endpoints(configJson)[environment].services);
-const serviceVersionsForService = (environment: string) => (serviceName: string) => Object.keys(endpoints(configJson)[environment].services[serviceName]);
+const stripComments = (str: string) => str.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, "")
 
-const getUrl = (environment: string, serviceName, version: string) =>
-  endpoints(configJson)[environment].services[serviceName][version].clientSide.live.replace(URL_PREFIX, "")
+const parseJson = (jsonString: string) => JSON.parse(jsonString)
 
-const addServiceToManifest = (environment: string) => (serviceManifest, serviceName) => {
-  return {
-    ...serviceManifest,
-    [serviceName]: serviceVersionsForService(environment)(serviceName).map(version => {
-      return {
-        version: Number(version),
-        basePath: getUrl(environment, serviceName, version)
-      }
-    })
-  }
+const getEndpointsForEnvironment = (environment: string) => (json: any): ConfigEndpoints => json.endpoints[environment].services
+
+const transformServiceVersions = (serviceVersions: ConfigEndpointServiceVersions): ServiceVersion[] => {
+  return Object.keys(serviceVersions).map(version => ({
+    version: Number(version),
+    basePath: serviceVersions[version].clientSide.live.replace(URL_PREFIX, "")
+  }))
 }
 
-const buildManifestForEnvironmentServices = (environment: string) => (services: string[]) => services.reduce(addServiceToManifest(environment), {})
+const transformEndpointsToManifest = (endpoints: ConfigEndpoints): ServiceManifest => {
+  return Object.keys(endpoints).reduce((acc: ServiceManifest, serviceName: string) => {
+    return {
+      ...acc,
+      [serviceName]: transformServiceVersions(endpoints[serviceName])
+    }
+  }, {})
+}
 
-const serviceManifest = (environment: string) =>
-  buildManifestForEnvironmentServices(environment)(servicesForEnvironment(environment));
+const writeManifestFile = (serviceManifest: ServiceManifest) => {
+  const filePath = resolve(__dirname, OUTPUT_FILEPATH)
 
-fs.writeFileSync(resolve(__dirname, OUTPUT_FILEPATH), JSON.stringify(serviceManifest('production'), null, 2)+"\n",ENCODING)
+  return fs.writeFileSync(
+    filePath,
+    JSON.stringify(serviceManifest, null, 2)+"\n",ENCODING
+  )
+}
+
+const createManifestFromConfig = pipe(
+  readFile,
+  stripComments,
+  parseJson,
+  getEndpointsForEnvironment(ENV_PRODUCTION),
+  transformEndpointsToManifest,
+  writeManifestFile
+)
+
+createManifestFromConfig(JSON5_FILEPATH)
