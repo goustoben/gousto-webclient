db18558c806714325d3e164b5b1d7f6d5168071f
Anthony Donovan <95023310+anthony-donovan@users.noreply.github.com>
2022-02-14 14:06:13 +0000

    Fix failing ReceiptLine test

--- src/apps/webclient/src/components/Receipt/ReceiptLine/__tests__/ReceiptLine.test.js
+++ src/apps/webclient/src/components/Receipt/ReceiptLine/__tests__/ReceiptLine.test.js
@@ -1,11 +1,11 @@
-import { shallow } from 'enzyme'
+import { render } from 'enzyme'
 import React from 'react'
 
 import { ReceiptLine } from 'Receipt/ReceiptLine'
 
 describe('ReceiptLine', () => {
   test('should render label and children', () => {
-    const wrapper = shallow(<ReceiptLine label="Total">11.99</ReceiptLine>)
+    const wrapper = render(<ReceiptLine label="Total">11.99</ReceiptLine>)
 
     expect(wrapper.text()).toEqual('Total11.99')
   })
