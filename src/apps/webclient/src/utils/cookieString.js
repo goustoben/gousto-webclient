4a27acf0b0432f1c6f35f1a3b06eb94f6f070cb2
maxhilliard <max.hilliard@gousto.co.uk>
2022-02-14 11:30:29 +0000

    FEF-344 - guild-frontend - Implement canUseWindow in utils/cookieString

--- src/apps/webclient/src/utils/cookieString.js
+++ src/apps/webclient/src/utils/cookieString.js
@@ -1,11 +1,12 @@
 import globals from 'config/globals'
+import { canUseWindow } from './browserEnvironment'
 
 export function cookieString(reqCookies) {
   let cookies
   if (globals.server && reqCookies.request && reqCookies.request.headers && reqCookies.request.headers.cookie) {
     cookies = reqCookies.request.headers.cookie.split(';')
   }
-  if (globals.client) {
+  if (canUseWindow()) {
     cookies = document.cookie.split(';')
   }
 
