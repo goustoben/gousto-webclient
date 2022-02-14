ca3d6a99eebf5ffcff3be9544a067159e8cbd580
maxhilliard <max.hilliard@gousto.co.uk>
2022-02-14 11:05:23 +0000

    FEF-344 - guild-frontend - Implement canUseWindow in utils/loadFeatures

---
+++ src/apps/webclient/src/utils/__tests__/loadFeatures.test.ts
@@ -0,0 +1,78 @@
+import { getWindow, canUseWindow } from 'utils/browserEnvironment'
+import { loadFeatures } from '../loadFeatures'
+
+jest.mock('utils/browserEnvironment')
+
+let win: typeof window
+
+const mockStore = {
+  // eslint-disable-next-line
+  dispatch: () => {},
+}
+
+const mockSnowplow = jest.fn()
+
+describe('loadFeatures util', () => {
+  beforeAll(() => {
+    win = window
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    (getWindow as jest.Mock).mockReturnValue({
+      __snowPlowGetOptimizelyExperimentsData: () => [
+        { data: { experiment_id: 'mock-experiment-id' } },
+      ],
+      snowplow: mockSnowplow,
+    })
+  })
+
+  afterAll(() => {
+    window = win
+  })
+
+  describe('when window is available', () => {
+    beforeEach(() => {
+      (canUseWindow as jest.Mock).mockReturnValue(true)
+
+      loadFeatures(
+        // This data structure just enables the fn to run without error,
+        // It is not an accurate representation
+        {
+          enable: ['something'],
+          disable: ['something else'],
+          set: { setSomething: 123 },
+          features: { someFeatures: 456 },
+        },
+        mockStore
+      )
+    })
+
+    test('the snowplow method on window is invoked', () => {
+      expect(mockSnowplow).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('when window is not available', () => {
+    beforeEach(() => {
+      (canUseWindow as jest.Mock).mockReturnValue(false)
+
+      loadFeatures(
+        // This data structure just enables the fn to run without error,
+        // It is not an accurate representation
+        {
+          enable: ['something'],
+          disable: ['something else'],
+          set: { setSomething: 123 },
+          features: { someFeatures: 456 },
+        },
+        mockStore
+      )
+    })
+
+    test('the snowplow method on window is not invoked', () => {
+      expect(mockSnowplow).not.toHaveBeenCalled()
+    })
+  })
+})
