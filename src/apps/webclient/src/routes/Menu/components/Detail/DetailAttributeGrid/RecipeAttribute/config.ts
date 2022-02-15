d8ca3dd2bcf2d1518b4d0a32da15d3b03fc85a92
Pavlo <pavel.vodopyan@gmail.com>
2022-02-15 10:22:06 +0000

    [KAL-273] Move RecipeAttribute under Menu/components namespace (#4754)
    * A sketch
    
    * Fix types for config.ts
    
    * Convert RecipeAttribute to TS
    
    * Better typing
    
    * Remove unused parameters from RecipeAttibutes related helper functions
    
    * Add space between label and list of equipments
    
    * Convert index into TS
    
    * Update tests to reflect changes in helper functions
    
    Neither show nor equepment as a list is thing now
    
    * Move RecipeAttribute under Menu/components/Detail/DetailAttributeGrid
    
    Co-authored-by: Pavlo Vodopyan <pavlo.vodopyan@gousto.co.uk>

---
+++ src/apps/webclient/src/routes/Menu/components/Detail/DetailAttributeGrid/RecipeAttribute/config.ts
@@ -0,0 +1,37 @@
+import { getCookingTime } from 'utils/recipe'
+
+const getUseWithinFormatted = (useWithin: string) =>
+  String(useWithin).replace(/(\d)-(\d)/, '$1 - $2')
+
+const capitalizeFirstLetter = (string: string) =>
+  string.charAt(0).toUpperCase() + string.toLowerCase().slice(1)
+
+const asterisk = String.fromCharCode(42)
+
+type funcString = (_: string | boolean | number) => string | boolean
+
+const descriptions = {
+  cookingTime: (time: string) => `Takes ${getCookingTime(time)}`,
+  useWithin: (useWithin: string) => `Use within ${getUseWithinFormatted(useWithin)} days`,
+  equipmentRequired: (equipment: boolean) => equipment && 'Equipment required',
+  fiveADay: (fiveADay: string) => `${fiveADay}/5 a day`,
+  cuisine: (cuisine: string) => `${cuisine} Cuisine`,
+  cals: (cals: number) => `${Math.round(cals)} cals / serving${asterisk}`,
+  diet: (diet: string) => (diet === 'vegan' ? 'Plant-based' : capitalizeFirstLetter(diet)),
+  glutenFree: (glutenFree: boolean) => glutenFree && 'Gluten free',
+  dairyFree: (dairyFree: boolean) => dairyFree && 'Dairy free',
+  numPortions: (numPortions: boolean) => `${numPortions} servings`,
+}
+
+export const getDescription = <
+  TName extends keyof typeof descriptions,
+  TFunction extends funcString = funcString,
+  TValue extends Parameters<TFunction>[0] = Parameters<TFunction>[0]
+>(
+  name: TName,
+  value: TValue
+) => {
+  const f = descriptions[name] as TFunction
+
+  return f(value) || ''
+}
