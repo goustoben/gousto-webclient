7581f24d95c44cd9b5d48308e7de93b8255813ae
Lewis Barclay <lewis.barclay@googlemail.com>
2022-02-16 10:55:23 +0000

    feat: Add types for Environment config

--- src/apps/webclient/scripts/create-service-manifest.ts
+++ src/apps/webclient/scripts/create-service-manifest.ts
@@ -2,36 +2,102 @@ import * as fs from 'fs'
 import { resolve } from 'path'
 
 /**
- * @deprecated
- * old types for the service configuration
+ * @type {EnvironmentConfig} old types for the service configuration
 */
-export type ConfigEndpoints = {
-  [serviceName: string]: ConfigEndpointServiceVersions
+export interface EnvironmentConfig {
+  build:                          string;
+  environment_name:               string;
+  api_name:                       string;
+  domain:                         string;
+  client_dev_server_enabled:      boolean;
+  client_protocol:                string;
+  cloudfront_url:                 string;
+  checkout_pk:                    string;
+  recaptcha_referral_public_key:  string;
+  recaptcha_referral_private_key: string;
+  running_env:                    string;
+  api_token:                      string;
+  auth_client_id:                 string;
+  auth_client_secret:             string;
+  endpoints:                      EnvironmentConfigEndpoints;
 }
 
-export type ServiceDetails = {
-  versionString: EndpointVersionString;
-  clientSide: ServiceUrls;
-  serverSide: ServiceUrls;
+export type EnvironmentConfigEndpoints = {
+  [key: string]: EnvironmentConfigServices;
 }
 
-export type ServiceUrls = {
-  live: string;
+export type EnvironmentConfigServices ={
+  envDomain: string;
+  services:  EnvironmentConfigServiceList;
+}
+
+export type EnvironmentConfigServiceList = {
+  auth:                EnvironmentConfigServiceVersion;
+  brand:               EnvironmentConfigServiceVersion;
+  clientmetrics:       EnvironmentConfigServiceVersion;
+  collections:         EnvironmentConfigServiceVersion;
+  complaints:          EnvironmentConfigServiceVersion;
+  content:             EnvironmentConfigServiceVersion;
+  core:                EnvironmentConfigServiceVersion;
+  customers:           EnvironmentConfigServiceVersion;
+  deliveries:          EnvironmentConfigServiceVersion;
+  felogging:           EnvironmentConfigServiceVersion;
+  loggingmanager:      EnvironmentConfigServiceVersion;
+  menu:                EnvironmentConfigServiceVersion;
+  order:               EnvironmentConfigServiceVersion;
+  orders:              EnvironmentConfigServiceVersion;
+  orderskiprecovery:   EnvironmentConfigServiceVersion;
+  payments:            EnvironmentConfigServiceVersion;
+  products:            EnvironmentConfigServiceVersion;
+  recipes:             EnvironmentConfigServiceVersion;
+  ssr:                 EnvironmentConfigServiceVersion;
+  ssrdeliveries:       EnvironmentConfigServiceVersion;
+  ssrrecipecards:      EnvironmentConfigServiceVersion;
+  subpauseosr:         EnvironmentConfigServiceVersion;
+  subscriptioncommand: EnvironmentConfigServiceVersion;
+  subscriptionquery:   EnvironmentConfigServiceVersion;
+  tastepreferences:    EnvironmentConfigServiceVersion;
+  userbucketing:       EnvironmentConfigServiceVersion;
+  userfeedback:        EnvironmentConfigServiceVersion;
+  webclient:           EnvironmentConfigServiceVersion;
+  workable:            EnvironmentConfigServiceVersion;
+}
+
+export type EnvironmentConfigServiceUrlGroup = {
+  versionString: VersionString;
+  clientSide:    EnvironmentConfigServiceUrls;
+  serverSide:    EnvironmentConfigServiceUrls;
+}
+
+export type EnvironmentConfigServiceUrls ={
+  live:  string;
   local: string;
 }
 
-export type EndpointVersionString = '' | `v${number}` | `v${number}.${number}` | `v${number}.${number}.${number}`
-
-export type ConfigEndpointServiceVersions = {
-  [version: string]: ServiceDetails;
+export enum VersionString {
+  Empty = "",
+  V1 = "v1",
+  V10 = "v1.0",
+  V100 = "v1.0.0",
+  V2 = "v2",
+  V20 = "v2.0",
 }
 
-// new types for the new service manifest
+export type EnvironmentConfigServiceVersion = {
+  [version: string]: EnvironmentConfigServiceUrlGroup
+}
+
+/*
+ * @type {ServiceVersion} - type for describing the service and its version in the service manifest
+ */
 export type ServiceVersion = {
   version: number,
   basePath: string
 }
 
+/*
+ * @type {ServiceManifest} - type for describing the service manifest
+ */
 export type ServiceManifest = {
   [key: string]: ServiceVersion[]
 }
@@ -50,16 +116,16 @@ const stripComments = (str: string) => str.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\
 
 const parseJson = (jsonString: string) => JSON.parse(jsonString)
 
-const getEndpointsForEnvironment = (environment: string) => (json: any): ConfigEndpoints => json.endpoints[environment].services
+const getEndpointsForEnvironment = (environment: string) => (json: EnvironmentConfig): EnvironmentConfigServiceList => json.endpoints[environment].services
 
-const transformServiceVersions = (serviceVersions: ConfigEndpointServiceVersions): ServiceVersion[] => {
+const transformServiceVersions = (serviceVersions: EnvironmentConfigServiceVersion): ServiceVersion[] => {
   return Object.keys(serviceVersions).map(version => ({
     version: Number(version),
     basePath: serviceVersions[version].clientSide.live.replace(URL_PREFIX, "")
   }))
 }
 
-const transformEndpointsToManifest = (endpoints: ConfigEndpoints): ServiceManifest => {
+const transformEndpointsToManifest = (endpoints: EnvironmentConfigServiceList): ServiceManifest => {
   return Object.entries(endpoints).reduce((acc: ServiceManifest, [serviceName, serviceVersion]) => {
     return {
       ...acc,
