53a7c1f91531e7d3df1f7759c04787a65efdc3fc
Anthony Donovan <95023310+anthony-donovan@users.noreply.github.com>
2022-02-17 15:32:34 +0000

    fix Alert component tests

--- src/apps/webclient/src/routes/Checkout/Components/Alert/Alert.js
+++ src/apps/webclient/src/routes/Checkout/Components/Alert/Alert.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Svg from 'Svg'
 import {
   Box,
   Color,
@@ -10,32 +9,23 @@ import {
   FlexDirection,
   Space,
 } from '@gousto-internal/citrus-react'
-import css from './Alert.css'
 
 export const Alert = ({ children }) => (
-  <>
-    <Box
-      bg={Color.Error_50}
-      borderRadius={2}
-      borderWidth={0.5}
-      borderStyle={BorderStyle.Solid}
-      borderColor={Color.Error_800}
-      display="flex"
-      flexDirection={FlexDirection.Row}
-      paddingV={4}
-      paddingH={4}
-    >
-      <Icon name="error" variant={IconVariant.Error} />
-      <Space size={2} direction="horizontal" />
-      <Box>{children}</Box>
-    </Box>
-    <div className={css.alert}>
-      <div className={css.iconContainer}>
-        <Svg className={css.icon} fileName="icon-danger" />
-      </div>
-      <div className={css.content}>{children}</div>
-    </div>
-  </>
+  <Box
+    bg={Color.Error_50}
+    borderRadius={2}
+    borderWidth={0.5}
+    borderStyle={BorderStyle.Solid}
+    borderColor={Color.Error_800}
+    display="flex"
+    flexDirection={FlexDirection.Row}
+    paddingV={4}
+    paddingH={4}
+  >
+    <Icon name="error" variant={IconVariant.Error} />
+    <Space size={2} direction="horizontal" />
+    <Box>{children}</Box>
+  </Box>
 )
 
 Alert.propTypes = {
--- src/apps/webclient/src/routes/Checkout/Components/Alert/__tests__/Alert.test.js
+++ src/apps/webclient/src/routes/Checkout/Components/Alert/__tests__/Alert.test.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render } from 'enzyme'
 import { Alert } from '../Alert'
 
 describe('Given Alert', () => {
   let wrapper
-  const children = <div>children</div>
+  const children = <div>Error message content</div>
 
   beforeEach(() => {
-    wrapper = shallow(<Alert>{children}</Alert>)
+    wrapper = render(<Alert>{children}</Alert>)
   })
 
-  test('should be rendered correctly', () => {
-    expect(wrapper.find('.alert').exists()).toBeTruthy()
-    expect(wrapper.find('Svg').exists()).toBeTruthy()
-    expect(wrapper.find('.content').exists()).toBeTruthy()
+  test('should render child prorps', () => {
+    expect(wrapper.text()).toBe('Error message content')
+  })
+
+  test('should render an error icon', () => {
+    expect(wrapper.find('svg').length).toBe(1)
+    expect(wrapper.find('svg').prop('data-testid')).toBe('error')
   })
 })
