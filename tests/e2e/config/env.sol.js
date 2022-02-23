1ad56cafcf70fd976ca122d93ccbd34a591e2d69
Eliza Plowden <71760740+elizaplowden@users.noreply.github.com>
2022-02-23 09:48:28 +0000

    end to end test

---
+++ tests/e2e/config/env.sol.js
@@ -0,0 +1,8 @@
+module.exports = () => ({
+  launch_url: "https://sol-frontend.gousto.info",
+  desiredCapabilities: {
+    browserName: "chrome",
+    acceptSslCerts: true,
+    acceptInsecureCerts: true,
+  },
+});
