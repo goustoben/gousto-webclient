ae65ff201db1467326c69184fa2643dc13ea999f
Eliza Plowden <71760740+elizaplowden@users.noreply.github.com>
2022-03-01 15:14:20 +0000

    implement citrus for subscriptiontransparency component

--- src/apps/webclient/src/routes/Checkout/Components/SubscriptionTransparency/SubscriptionTransparency.js
+++ src/apps/webclient/src/routes/Checkout/Components/SubscriptionTransparency/SubscriptionTransparency.js
@@ -1,13 +1,36 @@
+import {
+  Box,
+  Color,
+  Icon,
+  IconVariant,
+  Text,
+  FontFamily,
+  Space,
+} from '@gousto-internal/citrus-react'
+
 import React from 'react'
 import Svg from 'Svg'
 import css from './SubscriptionTransparency.css'
 
 export const SubscriptionTransparency = () => (
-  <div className={css.container}>
-    <Svg className={css.tick} fileName="icon-success-tick" />
-    <div className={css.text}>
-      <span className={css.highlighted}>No commitment. No cancellation fees.</span>{' '}
-      <span>Skip a box or cancel your subscription online at anytime.</span>
+  <>
+    <div className={css.container}>
+      <Svg className={css.tick} fileName="icon-success-tick" />
+      <div className={css.text}>
+        <span className={css.highlighted}>No commitment. No cancellation fees.</span>{' '}
+        <span>Skip a box or cancel your subscription online at anytime.</span>
+      </div>
     </div>
-  </div>
+
+    <Box paddingH={[0, 2]} display="flex">
+      <Icon name="tick" variant={IconVariant.Confirmation} />
+      <Space size={3} direction="horizontal" />
+      <Box display="flex">
+        <Text color={Color.Success_600} size={2} fontFamily={FontFamily.Bold}>
+          No commitment. No cancellation fees.{' '}
+        </Text>
+        <Text size={2}>Skip a box or cancel your subscription online at anytime.</Text>
+      </Box>
+    </Box>
+  </>
 )
