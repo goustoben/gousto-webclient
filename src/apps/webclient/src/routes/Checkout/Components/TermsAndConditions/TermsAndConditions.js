8cc0cd259e943e68c4df229a382fa2b09d17b396
Eliza Plowden <71760740+elizaplowden@users.noreply.github.com>
2022-03-01 14:51:52 +0000

    implement citrus for terms and conditions component

--- src/apps/webclient/src/routes/Checkout/Components/TermsAndConditions/TermsAndConditions.js
+++ src/apps/webclient/src/routes/Checkout/Components/TermsAndConditions/TermsAndConditions.js
@@ -1,20 +1,45 @@
+import {
+  Box,
+  Link,
+  Text,
+  Space,
+  Color,
+  TextAlign,
+  JustifyContent,
+} from '@gousto-internal/citrus-react'
 import React from 'react'
 import configRoutes from 'config/routes'
 import { checkoutConfig } from 'config/checkout'
-import Link from 'Link'
+// import Link from 'Link'
 import css from './TermsAndConditions.css'
 
 export const TermsAndConditions = () => (
-  <div className={css.termsAndConditions}>
-    <span>{checkoutConfig.terms}</span>
-    <br />
-    <Link
-      to={configRoutes.client.termsAndConditions}
-      className={css.link}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Terms and Conditions
-    </Link>
-  </div>
+  <>
+    <div className={css.termsAndConditions}>
+      <span>{checkoutConfig.terms}</span>
+      <br />
+      <Link
+        to={configRoutes.client.termsAndConditions}
+        className={css.link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Terms and Conditions
+      </Link>
+    </div>
+
+    <Box display="flex" flexDirection="column" justifyContent={JustifyContent.Center}>
+      <Space size={[4, 6]} direction="vertical" />
+      <Text size={0}>{checkoutConfig.terms}</Text>
+      <Text>
+        <Link
+          href={configRoutes.client.termsAndConditions}
+          color={Color.Secondary_400}
+          target="_blank"
+        >
+          Terms and Conditions
+        </Link>
+      </Text>
+    </Box>
+  </>
 )
