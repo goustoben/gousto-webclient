4b24735023a53e5ac38f48d88181ab3e742d1a33
Eliza Plowden <71760740+elizaplowden@users.noreply.github.com>
2022-03-03 17:53:47 +0000

    fix unit tests

--- src/apps/webclient/src/routes/Checkout/Components/SubscriptionTransparency/SubscriptionTransparency.js
+++ src/apps/webclient/src/routes/Checkout/Components/SubscriptionTransparency/SubscriptionTransparency.js
@@ -6,43 +6,37 @@ import {
   Text,
   FontFamily,
   Space,
+  Display,
   JustifyContent,
   AlignItems,
 } from '@gousto-internal/citrus-react'
 
 import React from 'react'
-import Svg from 'Svg'
-import css from './SubscriptionTransparency.css'
 
 export const SubscriptionTransparency = () => (
-  <>
-    <div className={css.container}>
-      <Svg className={css.tick} fileName="icon-success-tick" />
-      <div className={css.text}>
-        <span className={css.highlighted}>No commitment. No cancellation fees.</span>{' '}
-        <span>Skip a box or cancel your subscription online at anytime.</span>
-      </div>
-    </div>
-
-    <Box
-      paddingH={[0]}
-      display="flex"
-      justifyContent={JustifyContent.Center}
-      maxWidth={['25.6rem']}
-      alignItems={AlignItems.Center}
-      textAlign="center"
-    >
-      <Box>
-        <Icon name="tick" variant={IconVariant.Confirmation} />
-        <Space size={1} direction="horizontal" />
-      </Box>
-
-      <Text size={2} textAlign="center">
-        <Text fontWeight={FontFamily.Bold} color={Color.Success_600} display="inline-block">
-          No commitment. No cancellation fees.
-        </Text>{' '}
-        Skip a box or cancel your subscription online at anytime.
-      </Text>
+  <Box
+    display={Display.Flex}
+    justifyContent={JustifyContent.Center}
+    AlignItems={AlignItems.Center}
+    paddingH={['0.75rem', 0]}
+    maxwidth="25.6rem"
+    data-testing="container"
+  >
+    <Box>
+      <Icon name="tick" variant={IconVariant.Confirmation} />
+      <Space size={1} direction="horizontal" />
     </Box>
-  </>
+
+    <Text size={2} textAlign="center">
+      <Text
+        fontWeight={FontFamily.Bold}
+        color={Color.Success_600}
+        display="inline-block"
+        data-testing="highlighted"
+      >
+        No commitment. No cancellation fees.
+      </Text>{' '}
+      Skip a box or cancel your subscription online at anytime.
+    </Text>
+  </Box>
 )
