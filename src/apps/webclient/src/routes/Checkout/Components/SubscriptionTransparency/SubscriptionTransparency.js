354b1962e0cdaeac371199b0fd49e39f71eccf70
Eliza Plowden <71760740+elizaplowden@users.noreply.github.com>
2022-03-04 14:29:30 +0000

    fix width issue

--- src/apps/webclient/src/routes/Checkout/Components/SubscriptionTransparency/SubscriptionTransparency.js
+++ src/apps/webclient/src/routes/Checkout/Components/SubscriptionTransparency/SubscriptionTransparency.js
@@ -14,29 +14,32 @@ import {
 import React from 'react'
 
 export const SubscriptionTransparency = () => (
-  <Box
-    display={Display.Flex}
-    justifyContent={JustifyContent.Center}
-    AlignItems={AlignItems.Center}
-    paddingH={['0.75rem', 0]}
-    maxwidth="25.6rem"
-    data-testing="container"
-  >
-    <Box>
-      <Icon name="tick" variant={IconVariant.Confirmation} />
-      <Space size={1} direction="horizontal" />
-    </Box>
+  <Box width="100%" display={Display.Flex} justifyContent={JustifyContent.Center}>
+    <Box
+      display={Display.Flex}
+      justifyContent={JustifyContent.Center}
+      AlignItems={AlignItems.Center}
+      paddingH={['0.75rem', 0]}
+      maxWidth="72%"
+      flexGrow={0}
+      data-testing="container"
+    >
+      <Box>
+        <Icon name="tick" variant={IconVariant.Confirmation} />
+        <Space size={1} direction="horizontal" />
+      </Box>
 
-    <Text size={2} textAlign="center">
-      <Text
-        fontWeight={FontFamily.Bold}
-        color={Color.Success_600}
-        display="inline-block"
-        data-testing="highlighted"
-      >
-        No commitment. No cancellation fees.
-      </Text>{' '}
-      Skip a box or cancel your subscription online at anytime.
-    </Text>
+      <Text size={2} textAlign="center">
+        <Text
+          fontWeight={FontFamily.Bold}
+          color={Color.Success_600}
+          display="inline-block"
+          data-testing="highlighted"
+        >
+          No commitment. No cancellation fees.
+        </Text>{' '}
+        Skip a box or cancel your subscription online at anytime.
+      </Text>
+    </Box>
   </Box>
 )
