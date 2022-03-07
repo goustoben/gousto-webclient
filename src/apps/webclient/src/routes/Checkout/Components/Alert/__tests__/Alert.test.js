bce542aa889a58c06bbf3589d09bbacab6b3b3fd
Anthony Donovan <95023310+anthony-donovan@users.noreply.github.com>
2022-03-07 17:53:28 +0000

    Adds test for rendering cta button

--- src/apps/webclient/src/routes/Checkout/Components/Alert/__tests__/Alert.test.js
+++ src/apps/webclient/src/routes/Checkout/Components/Alert/__tests__/Alert.test.js
@@ -5,6 +5,7 @@ import { Alert } from '../Alert'
 describe('Given Alert', () => {
   let wrapper
   const children = <div>Error message content</div>
+  const ctaButton = <cta-button>Log in</cta-button>
 
   beforeEach(() => {
     wrapper = render(<Alert>{children}</Alert>)
@@ -17,5 +18,21 @@ describe('Given Alert', () => {
   test('should render an error icon', () => {
     expect(wrapper.find('svg').length).toBe(1)
     expect(wrapper.find('svg').prop('data-testid')).toBe('error')
+  })
+
+  describe('when a cta button is not provided', () => {
+    test('should not render a cta button', () => {
+      expect(wrapper.find('cta-button').length).toBe(0)
+    })
+  })
+
+  describe('when a cta button is provided', () => {
+    beforeEach(() => {
+      wrapper = render(<Alert ctaButton={ctaButton}>{children}</Alert>)
+    })
+
+    test('should render a cta button', () => {
+      expect(wrapper.find('cta-button').length).toBe(1)
+    })
   })
 })
