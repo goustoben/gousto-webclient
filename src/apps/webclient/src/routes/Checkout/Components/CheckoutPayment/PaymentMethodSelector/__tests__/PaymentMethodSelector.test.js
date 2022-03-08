0f560a7aaa611c4d03020dbbfb283fa6459b94b4
Anthony Donovan <95023310+anthony-donovan@users.noreply.github.com>
2022-03-08 12:19:56 +0000

    Adds tests for PaymentMethodSelector component

--- src/apps/webclient/src/routes/Checkout/Components/CheckoutPayment/PaymentMethodSelector/__tests__/PaymentMethodSelector.test.js
+++ src/apps/webclient/src/routes/Checkout/Components/CheckoutPayment/PaymentMethodSelector/__tests__/PaymentMethodSelector.test.js
@@ -17,9 +17,15 @@ describe('given PaymentMethodSelector', () => {
   })
 
   test('should be rendered correctly', () => {
+    expect(wrapper.find('fieldset[role="group"]')).toHaveLength(1)
     expect(wrapper.find('input[type="radio"]')).toHaveLength(2)
     expect(wrapper.find('input[type="radio"][id="Card"]').length).toBe(1)
     expect(wrapper.find('input[type="radio"][id="PayPal"]').length).toBe(1)
+  })
+
+  test('should have the current payment method checked', () => {
+    expect(wrapper.find('input[id="Card"]').prop('checked')).toBe(true)
+    expect(wrapper.find('input[id="PayPal"]').prop('checked')).toBe(false)
   })
 
   describe('when payment method is selected', () => {
@@ -29,6 +35,28 @@ describe('given PaymentMethodSelector', () => {
 
     test('then setCurrentPaymentMethod should be called with proper parameter', () => {
       expect(setCurrentPaymentMethod).toHaveBeenCalledWith('PayPal')
+    })
+  })
+
+  describe('when is paypal ready', () => {
+    test('should render the correct paypal label', () => {
+      expect(wrapper.find('label[htmlFor="PayPal"]').text().includes('Connected')).toBe(true)
+    })
+  })
+
+  describe('when is not paypal ready', () => {
+    beforeEach(() => {
+      wrapper = mount(
+        <PaymentMethodSelector
+          isPayPalReady={false}
+          setCurrentPaymentMethod={setCurrentPaymentMethod}
+          currentPaymentMethod="Card"
+        />
+      )
+    })
+
+    test('should render the correct paypal label', () => {
+      expect(wrapper.find('label[htmlFor="PayPal"]').text().includes('Connected')).toBe(false)
     })
   })
 })
