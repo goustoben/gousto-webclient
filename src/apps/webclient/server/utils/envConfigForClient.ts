cb224e1e097add6d9933f7ac274f3c9440c2590f
maxhilliard <max.hilliard@gousto.co.uk>
2022-04-06 17:39:37 +0100

    FEF-325 - pass ENVIRONMENT variable from server to client

--- src/apps/webclient/server/utils/envConfigForClient.ts
+++ src/apps/webclient/server/utils/envConfigForClient.ts
@@ -2,7 +2,7 @@ import { getEnvConfig, ParsedProcessEnv } from '../../src/utils/processEnv'
 
 export type WindowEnvConfig = Pick<
   ParsedProcessEnv,
-  'RECAPTCHA_PUBK' | 'RECAPTCHA_RAF_PUBK' | 'CHECKOUT_COM_PUBK'
+  'RECAPTCHA_PUBK' | 'RECAPTCHA_RAF_PUBK' | 'CHECKOUT_COM_PUBK' | 'ENVIRONMENT'
 >
 
 /**
@@ -10,12 +10,13 @@ export type WindowEnvConfig = Pick<
  * Enables passing of env config (only a PUBLIC subset) from server to client
  */
 export const createWindowEnvConfig = () => {
-  const { RECAPTCHA_PUBK, RECAPTCHA_RAF_PUBK, CHECKOUT_COM_PUBK } = getEnvConfig()
+  const { RECAPTCHA_PUBK, RECAPTCHA_RAF_PUBK, CHECKOUT_COM_PUBK, ENVIRONMENT } = getEnvConfig()
 
   const windowEnvConfig: WindowEnvConfig = {
     RECAPTCHA_PUBK,
     RECAPTCHA_RAF_PUBK,
     CHECKOUT_COM_PUBK,
+    ENVIRONMENT,
   }
 
   return JSON.stringify(windowEnvConfig)
