3bb9f4e461c0807d6b14b78ae67356415931cbc7
Alexander Rymasheusky <77661746+alexander-rymasheusky-gousto@users.noreply.github.com>
2022-04-15 11:49:51 +0300

    [TG-6284] extract `userSubscribe` closer to the place where it's used (#5079)
    * [TG-6284] correct log levels and wording
    
    * [TG-6284] extract userSubscribe to checkoutActions
    
    This is an action that is called only from the checkoutSignup process,
    therefore it should be owned by Beetroots.
    
    It is problematic because its "stop early when boxId is missing" logic doesn't
    actually prevent the rest of checkoutSignup: so the app continues blindly into
    the signup flow after the first attempt failed and user presses the CTA again;
    misleading the users with the lack of errors.
    
    Before the actual functional changes, though, this commit just extracts it for
    clearer ownership.
    
    * [TG-6284] move unit test
    
    * [TG-6284] increase coverage

--- src/apps/webclient/src/actions/user.js
+++ src/apps/webclient/src/actions/user.js
@@ -4,55 +4,39 @@ import { datadogLogs } from '@datadog/browser-logs'
 import { datadogRum } from '@datadog/browser-rum'
 
 import * as userApi from 'apis/user'
-import { customerSignup } from 'apis/customers'
 import { fetchDeliveryConsignment } from 'apis/deliveries'
 import * as orderV2 from 'routes/Menu/apis/orderV2'
 import { get3DSCompliantToken } from 'apis/payments'
 import * as prospectApi from 'apis/prospect'
 
-import { signupConfig } from 'config/signup'
-
 import {
   accountFormName,
   deliveryFormName,
-  getPasswordValue,
-  getFormattedPhoneNumber,
-  getSignupE2ETestName,
 } from 'selectors/checkout'
 import {
-  getNDDFeatureValue,
   getIsGoustoOnDemandEnabled,
 } from 'selectors/features'
-import { isCardPayment } from 'selectors/payment'
 import { getUserRecentRecipesIds, getUserId } from 'selectors/user'
 
 import logger from 'utils/logger'
 import GoustoException from 'utils/GoustoException'
-import { getAddress } from 'utils/checkout'
-import { getDeliveryTariffId } from 'utils/deliveries'
 import { transformPendingOrders, transformProjectedDeliveries } from 'utils/myDeliveries'
 import { getAuthUserId } from 'selectors/auth'
 import { getPreviewOrderId, getPromoCode } from 'selectors/basket'
-import { getSessionId } from 'selectors/cookies'
 import { skipDates, fetchProjectedDeliveries } from 'routes/Account/apis/subscription'
 import { deleteOrder } from 'routes/Account/MyDeliveries/apis/orderV2'
 import { canUseWindow } from 'utils/browserEnvironment'
 
 import { actionTypes } from './actionTypes'
 // eslint-disable-next-line import/no-cycle
-import { basketAddressChange, basketChosenAddressChange, basketPostcodeChangePure, basketPreviewOrderChange } from './basket'
-import { feLoggingLogEvent, logLevels, trackFailedCheckoutFlow, trackSuccessfulCheckoutFlow } from './log'
+import { basketAddressChange, basketChosenAddressChange, basketPostcodeChangePure } from './basket'
+import { feLoggingLogEvent, logLevels } from './log'
 import recipeActions from './recipes'
 import statusActions from './status'
 // eslint-disable-next-line import/no-cycle
 import { subscriptionLoadData } from './subscription'
-import { placeOrder } from './trackingKeys'
 import {
-  trackFirstPurchase,
   trackUserAttributes,
-  trackNewUser,
-  trackNewOrder,
-  trackUnexpectedSignup,
 } from './tracking'
 
 const fetchShippingAddressesPending = pending => ({
@@ -632,144 +616,6 @@ function userUnsubscribe({ authUserId, marketingType, marketingUnsubscribeToken 
   }
 }
 
-function buildSignupRequestData(state) {
-  const { form, basket, promoAgeVerified } = state
-
-  const isGoustoOnDemandEnabled = getIsGoustoOnDemandEnabled(state)
-
-  const isCard = isCardPayment(state)
-
-  const account = Immutable.fromJS(form[accountFormName].values).get('account')
-  const delivery = Immutable.fromJS(form[deliveryFormName].values).get('delivery')
-  const payment = Immutable.fromJS(form.payment.values).get('payment')
-
-  const deliveryAddress = getAddress(delivery)
-  const billingAddress = isCard && payment.get('isBillingAddressDifferent')
-    ? getAddress(payment)
-    : deliveryAddress
-
-  const intervalId = delivery.get('interval_id', 1)
-  const promoCode = getPromoCode(state) || ''
-
-  return {
-    order_id: getPreviewOrderId(state),
-    promocode: promoCode,
-    session_id: getSessionId(),
-    customer: {
-      tariff_id: basket.get('tariffId', ''),
-      phone_number: getFormattedPhoneNumber(state),
-      email: account.get('email'),
-      name_first: delivery.get('firstName').trim(),
-      name_last: delivery.get('lastName').trim(),
-      promo_code: promoCode,
-      password: getPasswordValue(state),
-      age_verified: Number(promoAgeVerified || false),
-      marketing_do_allow_email: Number(account.get('allowEmail') || false),
-      marketing_do_allow_thirdparty: Number(account.get('allowThirdPartyEmail') || false),
-      delivery_tariff_id: getDeliveryTariffId(null, getNDDFeatureValue(state)),
-      gousto_ref: state.checkout.get('goustoRef')
-    },
-    addresses: {
-      shipping_address: {
-        type: signupConfig.address_types.shipping,
-        delivery_instructions: delivery.get('deliveryInstructionsCustom') || delivery.get('deliveryInstruction'),
-        ...deliveryAddress
-      },
-      billing_address: {
-        type: signupConfig.address_types.billing,
-        ...billingAddress
-      }
-    },
-    subscription: {
-      interval_id: intervalId,
-      delivery_slot_id: basket.get('slotId'),
-      box_id: basket.get('boxId'),
-      ...(isGoustoOnDemandEnabled && { paused: Number(isGoustoOnDemandEnabled || false) }),
-    },
-    decoupled: {
-      payment: 1,
-    },
-  }
-}
-
-export function userSubscribe({ pricing }) {
-  return async (dispatch, getState) => {
-    const state = getState()
-    const boxId = state.basket.get('boxId')
-    if (!boxId) {
-      dispatch(feLoggingLogEvent(logLevels.error, 'userSubscribe called with missing boxId: unexpected signup'))
-      dispatch(trackUnexpectedSignup())
-
-      return
-    }
-    dispatch(feLoggingLogEvent(logLevels.error, 'userSubscribe started', { boxId }))
-    dispatch(statusActions.error(actionTypes.USER_SUBSCRIBE, null))
-    dispatch(statusActions.pending(actionTypes.USER_SUBSCRIBE, true))
-
-    const signupTestName = getSignupE2ETestName(state)
-    try {
-      const reqData = buildSignupRequestData(state)
-
-      const { data } = await customerSignup(null, reqData)
-
-      if (data.customer && data.addresses && data.subscription && data.orderId) {
-        const { customer, addresses, subscription, orderId, customer: { id: customerId }} = data
-        let user = Immutable.fromJS({
-          ...customer,
-          ...addresses,
-          subscription
-        })
-        user = user.set('goustoReference', user.get('goustoReference').toString())
-
-        dispatch(trackNewUser(customerId))
-
-        dispatch({
-          type: actionTypes.CHECKOUT_ORDER_PLACED,
-          trackingData: {
-            actionType: placeOrder,
-            order_id: orderId,
-            order_total: pricing?.total,
-            promo_code: getPromoCode(state),
-            signup: true,
-            subscription_active: data.subscription.status ? data.subscription.status.slug : true,
-            interval_id: reqData.subscription.interval_id,
-          }
-        })
-
-        dispatch(trackFirstPurchase(orderId, pricing))
-        dispatch(trackNewOrder(orderId, customerId))
-        dispatch(basketPreviewOrderChange(orderId, state.basket.get('boxId')))
-        dispatch({ type: actionTypes.USER_SUBSCRIBE, user })
-        dispatch(trackSuccessfulCheckoutFlow('userSubscribe: customerSignup successful', { testName: signupTestName, data }))
-      } else {
-        throw new GoustoException(actionTypes.USER_SUBSCRIBE)
-      }
-    } catch (err) {
-      dispatch(statusActions.error(actionTypes.USER_SUBSCRIBE, err.message))
-      dispatch(trackNewUser())
-
-      const previewOrderId = getPreviewOrderId(state)
-
-      dispatch({
-        type: actionTypes.CHECKOUT_ORDER_FAILED,
-        trackingData: {
-          actionType: 'Order Failed',
-          order_id: previewOrderId,
-          promo_code: getPromoCode(state),
-          signup: true,
-          error_reason: err.message
-        }
-      })
-      dispatch(trackNewOrder(previewOrderId))
-      logger.error({ message: err.message, errors: [err] })
-      dispatch(trackFailedCheckoutFlow('userSubscribe: customerSignup failed', err, { testName: signupTestName }))
-      throw err
-    } finally {
-      dispatch(statusActions.pending(actionTypes.USER_SUBSCRIBE, false))
-    }
-  }
-}
-
 export const userReferAFriend = (email, recaptchaToken = null) => async () => {
   try {
     await userApi.serverReferAFriend(email, recaptchaToken)
@@ -883,7 +729,6 @@ const userActions = {
   userFetchCredit,
   userOrderCancelNext,
   userOrderSkipNextProjected,
-  userSubscribe,
   userLoadData,
   userLoadReferralDetails,
   userFetchShippingAddresses,
