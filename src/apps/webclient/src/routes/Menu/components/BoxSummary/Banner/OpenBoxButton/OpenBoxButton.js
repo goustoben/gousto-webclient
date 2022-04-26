a2b1124651003e9af47eb60a78b075ee92a17ca3
Alexander Rymasheusky <alexander.rymashevskiy@gousto.co.uk>
2022-04-26 16:37:21 +0300

    [TG-6412] productionize for OpenBoxButton

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/Banner/OpenBoxButton/OpenBoxButton.js
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/Banner/OpenBoxButton/OpenBoxButton.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import css from './OpenBoxButton.css'
 
-const OpenBoxButton = ({ arrowUp, isSimplifyBasketBarEnabled }) => (
-  <div
-    className={classNames(css.iconMobile, {
-      [css.isSimplifyBasketBarEnabled]: isSimplifyBasketBarEnabled,
-    })}
-  >
+const OpenBoxButton = ({ arrowUp }) => (
+  <div className={css.iconMobile}>
     <div>
       <span
         className={arrowUp ? css.arrowUp : css.arrowDown}
@@ -21,7 +16,6 @@ const OpenBoxButton = ({ arrowUp, isSimplifyBasketBarEnabled }) => (
 
 OpenBoxButton.propTypes = {
   arrowUp: PropTypes.bool.isRequired,
-  isSimplifyBasketBarEnabled: PropTypes.bool.isRequired,
 }
 
 export { OpenBoxButton }
