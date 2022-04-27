53cafe48702728b049d7eb49984b0bd3ecdf8632
Alexander Rymasheusky <77661746+alexander-rymasheusky-gousto@users.noreply.github.com>
2022-04-27 14:31:37 +0300

    [TG-6304] fix typo in comment (#5119)

--- src/apps/webclient/src/routes/Signup/constants/SignupSteps.ts
+++ src/apps/webclient/src/routes/Signup/constants/SignupSteps.ts
@@ -1,5 +1,5 @@
 /**
- * Steps in Wizard. If experiment steps should be added, add them there.
+ * Steps in Wizard. If experiment steps should be added, add them here.
  */
 // eslint-disable-next-line no-shadow
 export enum SignupSteps {
