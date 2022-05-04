26f3ab78201afb1196d8e8499afc032d2e981d2b
Alexander Rymasheusky <alexander.rymashevskiy@gousto.co.uk>
2022-05-04 14:26:55 +0300

    [TG-6412] inline Title
    As it's only used in one place, it doesn't make sense to have a separate file
    high up in the hierarchy.  Also some logic is repeated in Title&Description,
    and it'll be clearer when they're both in the same place.

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/Banner/PriceAndDiscountTip/PriceAndDiscountTip.tsx
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/Banner/PriceAndDiscountTip/PriceAndDiscountTip.tsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-import { Title } from '../../Title'
+import basketConfig from 'config/basket'
+import { usePricing } from 'routes/Menu/domains/pricing'
+import { useDiscountTip } from 'routes/Menu/components/BoxSummary/utilHooks'
+
+import { Price } from '../../Price'
 import { Description } from '../../Description'
 
 import css from './PriceAndDiscountTip.css'
 
-export const PriceAndDiscountTip = ({ numRecipes }: any) => (
-  <div className={css.buttonTextWrapper}>
-    <Title numRecipes={numRecipes} />
-    <Description numRecipes={numRecipes} />
-  </div>
-)
+const deliveryTip = <b>Free UK delivery,</b>
+
+export const PriceAndDiscountTip = ({ numRecipes }: any) => {
+  const { pricing } = usePricing()
+  const discountTip = useDiscountTip()
+
+  const titleContent =
+    numRecipes < basketConfig.minRecipesNum ? (
+      <div className={css.discountTip}>{discountTip || deliveryTip}</div>
+    ) : (
+      <Price
+        recipeTotal={parseFloat(pricing?.grossTotal || '0')}
+        recipeDiscount={parseFloat(pricing?.totalDiscount || '0')}
+        recipeTotalDiscounted={parseFloat(pricing?.total || '0')}
+      />
+    )
+
+  return (
+    <div className={css.buttonTextWrapper}>
+      <div className={classNames(css.titleWrapperVariant)}>{titleContent}</div>
+      <Description numRecipes={numRecipes} />
+    </div>
+  )
+}
 
 PriceAndDiscountTip.propTypes = {
   numRecipes: PropTypes.number.isRequired,
