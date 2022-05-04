cbf0321a4980aa8946dad05433d508319da6f63a
Alexander Rymasheusky <alexander.rymashevskiy@gousto.co.uk>
2022-05-04 14:58:40 +0300

    [TG-6412] inline Description

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/Banner/PriceAndDiscountTip/PriceAndDiscountTip.tsx
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/Banner/PriceAndDiscountTip/PriceAndDiscountTip.tsx
@@ -4,18 +4,40 @@ import classNames from 'classnames'
 
 import basketConfig from 'config/basket'
 import { usePricing } from 'routes/Menu/domains/pricing'
-import { useDiscountTip } from 'routes/Menu/components/BoxSummary/utilHooks'
+import { useCheckoutPrices, useDiscountTip } from 'routes/Menu/components/BoxSummary/utilHooks'
 
 import { Price } from '../../Price'
-import { Description } from '../../Description'
 
 import css from './PriceAndDiscountTip.css'
 
 const deliveryTip = <b>Free UK delivery,</b>
 
+const useSimplifyBasketBarContent = (canCheckout: boolean) => {
+  const discountTip = useDiscountTip()
+  const { isDiscountEnabled } = useCheckoutPrices()
+
+  let content
+  if (canCheckout) {
+    if (isDiscountEnabled) {
+      content = discountTip
+    } else {
+      content = 'Free UK delivery'
+    }
+  } else if (isDiscountEnabled) {
+    content = '+ Free UK delivery'
+  } else {
+    content = '7 days a week'
+  }
+
+  return content
+}
+
 export const PriceAndDiscountTip = ({ numRecipes }: any) => {
   const { pricing } = usePricing()
   const discountTip = useDiscountTip()
+
+  const canCheckout = numRecipes >= basketConfig.minRecipesNum
+  const content = useSimplifyBasketBarContent(canCheckout)
 
   const titleContent =
     numRecipes < basketConfig.minRecipesNum ? (
@@ -31,7 +53,13 @@ export const PriceAndDiscountTip = ({ numRecipes }: any) => {
   return (
     <div className={css.buttonTextWrapper}>
       <div className={classNames(css.titleWrapperVariant)}>{titleContent}</div>
-      <Description numRecipes={numRecipes} />
+      <p
+        className={classNames(css.description, {
+          [css.canCheckout]: canCheckout,
+        })}
+      >
+        {content}
+      </p>
     </div>
   )
 }
