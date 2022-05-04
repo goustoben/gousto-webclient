8a970a6c4aa48d749ecfdcc056f456901f38df54
Alexander Rymasheusky <alexander.rymashevskiy@gousto.co.uk>
2022-05-04 12:59:43 +0300

    [TG-6412] set flag to true in Description

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/Description/Description.js
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/Description/Description.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { useSelector } from 'react-redux'
 import classNames from 'classnames'
-import { getIsSimplifyBasketBarEnabled } from 'routes/Menu/selectors/features'
 import basketConfig from 'config/basket'
 import { useCheckoutPrices, useDiscountTip } from 'routes/Menu/components/BoxSummary/utilHooks'
 import css from './Description.css'
@@ -27,52 +25,26 @@ const useSimplifyBasketBarContent = (canCheckout) => {
   return content
 }
 
-const Description = ({ numPortions, numRecipes, view, deliveryOptions, warning }) => {
-  const isSimplifyBasketBarEnabled = useSelector(getIsSimplifyBasketBarEnabled)
+const Description = ({ numRecipes, view }) => {
   const canCheckout = numRecipes >= basketConfig.minRecipesNum
-  const simplifyBasketBarContent = useSimplifyBasketBarContent(canCheckout)
+  const content = useSimplifyBasketBarContent(canCheckout)
 
-  if (isSimplifyBasketBarEnabled) {
-    return (
-      <p
-        className={classNames(css[`description${view}`], css.isSimplifyBasketBarEnabled, {
-          [css.canCheckout]: canCheckout,
-        })}
-      >
-        {simplifyBasketBarContent}
-      </p>
-    )
-  }
-
-  let statusText = `Choose 2, 3 or 4 meals for ${numPortions} people`
-  if (numRecipes > 0) {
-    statusText = `${numRecipes} meal${numRecipes > 1 ? 's' : ''} for ${numPortions} people added`
-  }
-  if (deliveryOptions) {
-    statusText = 'Select delivery options'
-  }
-  if (warning) {
-    statusText = (
-      <span>
-        <span className={css.warningIcon} /> There&apos;s been a change in your box
-      </span>
-    )
-  }
-
-  return <p className={css[`description${view}`]}>{statusText}</p>
+  return (
+    <p
+      className={classNames(css.description, {
+        [css.canCheckout]: canCheckout,
+      })}
+    >
+      {content}
+    </p>
+  )
 }
 
 Description.propTypes = {
   view: PropTypes.string.isRequired,
-  numPortions: PropTypes.number.isRequired,
   numRecipes: PropTypes.number.isRequired,
-  deliveryOptions: PropTypes.bool,
-  warning: PropTypes.bool,
 }
 
-Description.defaultProps = {
-  deliveryOptions: false,
-  warning: false,
-}
+Description.defaultProps = {}
 
 export { Description }
