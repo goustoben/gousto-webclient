74415c4970f3a00c0e17d0413f9c91d0f50e320d
Alexander Rymasheusky <alexander.rymashevskiy@gousto.co.uk>
2022-05-04 12:16:53 +0300

    [TG-6412] set flag to true in Title

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/Title/Title.js
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/Title/Title.js
@@ -1,48 +1,26 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import classNames from 'classnames'
-import moment from 'moment'
 import basketConfig from 'config/basket'
 import { usePricing } from 'routes/Menu/domains/pricing'
-import { Spinner } from 'goustouicomponents'
-import { isMobile } from 'utils/view'
 import { useDiscountTip } from 'routes/Menu/components/BoxSummary/utilHooks'
-import { useSelector } from 'react-redux'
-import { getIsSimplifyBasketBarEnabled } from 'routes/Menu/selectors/features'
 import { Price } from '../Price'
 import css from './Title.css'
 
-const minRecipesToCheckout = basketConfig.minRecipesNum
 const propTypes = {
-  date: PropTypes.string,
-  view: PropTypes.string,
-  spinnerClassName: PropTypes.string,
-  spinnerContainerClassName: PropTypes.string,
-  slotTime: PropTypes.string,
   numRecipes: PropTypes.number,
 }
 
 const deliveryTip = <b>Free UK delivery,</b>
 const defaultProps = {
-  date: '',
-  view: '',
-  spinnerClassName: '',
-  spinnerContainerClassName: '',
-  slotTime: '',
   numRecipes: 0,
 }
 
-export function Title({ slotTime, date, view, spinnerClassName, spinnerContainerClassName, numRecipes }) {
-  const { pricing, isPending } = usePricing()
-  const spinnerClassNames = {
-    [css.spinnerContainer]: true,
-    spinnerContainerClassName,
-    [css.spinnerShow]: isPending,
-  }
+export function Title({ numRecipes }) {
+  const { pricing } = usePricing()
   const discountTip = useDiscountTip()
-  const isSimplifyBasketBarEnabled = useSelector(getIsSimplifyBasketBarEnabled)
   const titleContent =
-    numRecipes < minRecipesToCheckout ? (
+    numRecipes < basketConfig.minRecipesNum ? (
       <div className={css.discountTip}>{discountTip || deliveryTip}</div>
     ) : (
       <Price
@@ -52,33 +30,7 @@ export function Title({ slotTime, date, view, spinnerClassName, spinnerContainer
       />
     )
 
-  return isSimplifyBasketBarEnabled ? (
-    <div className={classNames(css.titleWrapperVariant)}>{titleContent}</div>
-  ) : (
-    <div className={css.titleWrapper}>
-      <p className={css[`title${view}`]}>
-        {date ? <span className={css.showDate}>{moment(date).format('ddd D MMM')}</span> : null}
-        {slotTime ? <span className={css.showDate}> {slotTime}</span> : null}
-      </p>
-      <div className={classNames(css[`title${view}`], css.price)}>
-        {isPending ? (
-          isMobile(view) && (
-            <div className={classNames(spinnerClassNames)}>
-              <span className={classNames(css.spinner, spinnerClassName)}>
-                <Spinner />
-              </span>
-            </div>
-          )
-        ) : (
-          <Price
-            recipeTotal={parseFloat(pricing?.grossTotal || 0)}
-            recipeDiscount={parseFloat(pricing?.totalDiscount || 0)}
-            recipeTotalDiscounted={parseFloat(pricing?.total || 0)}
-          />
-        )}
-      </div>
-    </div>
-  )
+  return <div className={classNames(css.titleWrapperVariant)}>{titleContent}</div>
 }
 
 Title.defaultProps = defaultProps
