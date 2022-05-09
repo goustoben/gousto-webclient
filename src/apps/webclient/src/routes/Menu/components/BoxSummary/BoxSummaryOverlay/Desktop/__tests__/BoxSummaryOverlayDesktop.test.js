4df5bfd96fe98b056e5eb17e2ca28701f2eff93a
Alexander Rymasheusky <alexander.rymashevskiy@gousto.co.uk>
2022-05-09 17:42:54 +0300

    [TG-6412] update unit test for BoxSummaryOverlayDesktop

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/BoxSummaryOverlay/Desktop/__tests__/BoxSummaryOverlayDesktop.test.js
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/BoxSummaryOverlay/Desktop/__tests__/BoxSummaryOverlayDesktop.test.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import Immutable from 'immutable'
 import { shallow } from 'enzyme'
-import { useSelector } from 'react-redux'
 import { BoxSummaryOverlayDesktop } from '../BoxSummaryOverlayDesktop'
-
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
-  useSelector: jest.fn(),
-  useDispatch: jest.fn(),
-}))
 
 describe('BoxSummaryOverlayDesktop', () => {
   let wrapper
+
   const defaultProps = {
     onCloseClick: () => {},
     onToggleVisibility: () => {},
-    view: '',
-    date: '',
-    numPortions: 0,
-    recipes: Immutable.Map(),
     showDetails: false,
+    date: '',
+    recipes: Immutable.Map(),
+    numPortions: 0,
     orderSaveError: '',
-    shouldDisplayFullScreenBoxSummary: false,
   }
+
+  describe('when rendered', () => {
+    beforeEach(() => {
+      wrapper = shallow(<BoxSummaryOverlayDesktop {...defaultProps} />)
+    })
+
+    test('then it should render correctly', () => {
+      expect(wrapper.find('Connect(BoxSummaryContent)')).toHaveLength(1)
+    })
+  })
 })
