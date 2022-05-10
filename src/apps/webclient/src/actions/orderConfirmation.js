99bda6e799700f40648916e0067d558da2682903
Naveen Venkat <naveen-venkat@users.noreply.github.com>
2022-05-10 09:59:18 +0100

    migrate v1 to v2 update market place items (#5141)
    * migrate v1 to v2 update market place items

--- src/apps/webclient/src/actions/orderConfirmation.js
+++ src/apps/webclient/src/actions/orderConfirmation.js
@@ -4,7 +4,13 @@ import { push } from 'react-router-redux'
 import config from 'config/routes'
 import logger from 'utils/logger'
 import { getUserOrderRecipeUuIds } from 'utils/user'
-import { basketOrderLoad } from 'actions/basket'
+import {
+  basketDateChange,
+  basketNumPortionChange,
+  basketPostcodeChange,
+  basketChosenAddressChange,
+  basketOrderLoad,
+} from 'actions/basket'
 import * as orderV2 from 'routes/Menu/apis/orderV2'
 import { productsLoadCategories, productsLoadProducts, productsLoadStock } from 'actions/products'
 import { orderCheckPossibleDuplicate } from './order'
@@ -13,40 +19,41 @@ import { fetchSimpleMenu } from '../routes/Menu/fetchData/menuApi'
 import recipeActions from './recipes'
 import { actionTypes } from './actionTypes'
 
-export const orderConfirmationRedirect = (orderId, orderAction) => (
-  (dispatch) => {
-    const confirmationUrl = config.client.orderConfirmation.replace(':orderId', orderId)
-    dispatch(orderCheckPossibleDuplicate(orderId))
-    dispatch(push((orderAction) ? `${confirmationUrl}?order_action=${orderAction}` : confirmationUrl))
+export const orderConfirmationRedirect = (orderId, orderAction) => (dispatch) => {
+  const confirmationUrl = config.client.orderConfirmation.replace(':orderId', orderId)
+  dispatch(orderCheckPossibleDuplicate(orderId))
+  dispatch(push(orderAction ? `${confirmationUrl}?order_action=${orderAction}` : confirmationUrl))
+}
+
+export const orderDetails = (orderId) => async (dispatch, getState) => {
+  const accessToken = getAccessToken(getState())
+  const userId = getAuthUserId(getState())
+
+  try {
+    dispatch(productsLoadCategories())
+    dispatch(productsLoadStock())
+    const { data: order } = await orderV2.fetchOrder(dispatch, getState, orderId)
+    const { data: menus } = await fetchSimpleMenu(accessToken, userId)
+    const immutableOrderDetails = Immutable.fromJS(order)
+    const orderRecipeIds = getUserOrderRecipeUuIds(immutableOrderDetails)
+    dispatch(recipeActions.recipesLoadFromMenuRecipesById(orderRecipeIds))
+    await dispatch(productsLoadProducts(order.whenCutoff, order.periodId, { reload: true }, menus))
+
+    dispatch(basketOrderLoad(orderId, immutableOrderDetails))
+    dispatch(basketDateChange(order.deliveryDate))
+    dispatch(basketNumPortionChange(order.box.numPortions, orderId))
+    dispatch(basketChosenAddressChange(order.shippingAddress))
+    await dispatch(basketPostcodeChange(order.shippingAddress.postcode))
+
+    dispatch({
+      type: actionTypes.BASKET_ORDER_DETAILS_LOADED,
+      orderId,
+      orderDetails: immutableOrderDetails,
+    })
+  } catch (err) {
+    logger.error(err)
   }
-)
-
-export const orderDetails = (orderId) => (
-  async (dispatch, getState) => {
-    const accessToken = getAccessToken(getState())
-    const userId = getAuthUserId(getState())
-
-    try {
-      dispatch(productsLoadCategories())
-      dispatch(productsLoadStock())
-      const { data: order } = await orderV2.fetchOrder(dispatch, getState, orderId)
-      const { data: menus } = await fetchSimpleMenu(accessToken, userId)
-      const immutableOrderDetails = Immutable.fromJS(order)
-      const orderRecipeIds = getUserOrderRecipeUuIds(immutableOrderDetails)
-      dispatch(recipeActions.recipesLoadFromMenuRecipesById(orderRecipeIds))
-      await dispatch(productsLoadProducts(order.whenCutoff, order.periodId, {reload: true}, menus))
-
-      dispatch(basketOrderLoad(orderId, immutableOrderDetails))
-      dispatch({
-        type: actionTypes.BASKET_ORDER_DETAILS_LOADED,
-        orderId,
-        orderDetails: immutableOrderDetails,
-      })
-    } catch (err) {
-      logger.error(err)
-    }
-  }
-)
+}
 
 export const orderConfirmationProductTracking = (trackingData) => (
   (dispatch) => {
