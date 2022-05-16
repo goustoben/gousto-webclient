fc6edc9c30cec9368f54a3c8a4ad9daf49ecdd2f
Tatiana-KorzunGousto <104076919+Tatiana-KorzunGousto@users.noreply.github.com>
2022-05-16 17:15:07 +0300

    [TG 6423] Remove merchandising banner experiment implementation (#5177)
    * [TG-6423] Remove merchandising banner experiment implementation: the banner is not shown for the categories exclude Calorie controlled one for the prospects when experiment is on
    
    * [TG-6423] refactoring - useRemoveMerchandisingBanner logic is moved to a custom hook
    
    * [TG-6423]: a test-case for the removing of a merchandising banner is added, code is formatted with prettier
    
    * [TG-6423]: test typechecks fix
    
    * [TG-6423] code review: renaming of a custom hook, which returns a boolean value
    
    * refactor: mock useShouldRemoveMerchandisingBanner rather than Optimizely
    
    * refactor: fix lint issue
    
    * [TG-6423]: useSelector mock is changed to the certain selectors and hooks mocks
    
    * [TG-6423] tests for the new value of isAuthenticated for a useAuth hook are written
    
    * [TG-6423] useShouldRemoveMerchandisingBanner hook is covered with tests for all the cases
    
    * [TG-6423] prettier problems fix
    
    * [TG-6423] 'calorie-controlled' category title is moved to constants
    
    * [TG-6423] a condition for the useShouldRemoveMerchandisingBanner is optimized (the check if the isRemoveMerchandisingBannerWebEnabled is on should go first, because there is no sense to check other parts of the condition if it is off)
    
    * [TG-6423] a comment to clarify the condition is added
    
    Co-authored-by: tkorzun <tkorzun@ap-korzunt.local>
    Co-authored-by: James Monger <jameskmonger@hotmail.co.uk>

---
+++ src/apps/webclient/src/routes/Menu/MenuRecipesPage/CollectionHeader/__tests__/useShouldRemoveMerchandisingBanner.test.js
@@ -0,0 +1,179 @@
+import { useIsOptimizelyFeatureEnabled } from 'containers/OptimizelyRollouts'
+import * as useCollectionQuerySlug from '../../../domains/collections/internal/useCollectionQuerySlug'
+import * as useAuth from '../../../domains/auth/useAuth'
+import { useShouldRemoveMerchandisingBanner } from '../useShouldRemoveMerchandisingBanner'
+import { CollectionSlug } from '../../../domains/collections'
+
+jest.mock('containers/OptimizelyRollouts', () => ({
+  ...jest.requireActual('containers/OptimizelyRollouts'),
+  isOptimizelyFeatureEnabledFactory: jest.fn(),
+  useIsOptimizelyFeatureEnabled: jest.fn('beetroots_remove_merchandising_banner_web_enabled'),
+}))
+const useCollectionQuerySlugMock = jest.spyOn(useCollectionQuerySlug, 'useCollectionQuerySlug')
+const useAuthMock = jest.spyOn(useAuth, 'useAuth')
+
+describe('useShouldRemoveMerchandisingBanner', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('when an experiment is on', () => {
+    beforeEach(() => {
+      useIsOptimizelyFeatureEnabled.mockReturnValue(true)
+    })
+
+    describe('when user is not authenticated', () => {
+      beforeEach(() => {
+        useAuthMock.mockReturnValue({ isAuthenticated: false })
+      })
+
+      describe('when category is not calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.AllRecipes)
+        })
+
+        it('returns true', () => {
+          expect(
+            useIsOptimizelyFeatureEnabled('beetroots_remove_merchandising_banner_web_enabled'),
+          ).toBe(true)
+          expect(useIsOptimizelyFeatureEnabled).toHaveBeenCalledTimes(1)
+
+          expect(useAuthMock().isAuthenticated).toBe(false)
+          expect(useAuthMock).toHaveBeenCalledTimes(1)
+
+          expect(useCollectionQuerySlugMock()).toBe(CollectionSlug.AllRecipes)
+          expect(useCollectionQuerySlugMock).toHaveBeenCalledTimes(1)
+
+          expect(useShouldRemoveMerchandisingBanner()).toBe(true)
+        })
+      })
+
+      describe('when category is calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.CalorieControlled)
+        })
+
+        it('returns false', () => {
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+    })
+
+    describe('when user is authenticated', () => {
+      beforeEach(() => {
+        useAuthMock.mockReturnValue({ isAuthenticated: true })
+      })
+
+      describe('when category is not calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.AllRecipes)
+        })
+
+        it('returns false', () => {
+          expect(
+            useIsOptimizelyFeatureEnabled('beetroots_remove_merchandising_banner_web_enabled'),
+          ).toBe(true)
+          expect(useIsOptimizelyFeatureEnabled).toHaveBeenCalledTimes(1)
+
+          expect(useAuthMock().isAuthenticated).toBe(true)
+          expect(useAuthMock).toHaveBeenCalledTimes(1)
+
+          expect(useCollectionQuerySlugMock()).toBe(CollectionSlug.AllRecipes)
+          expect(useCollectionQuerySlugMock).toHaveBeenCalledTimes(1)
+
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+
+      describe('when category is calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.CalorieControlled)
+        })
+
+        it('returns false', () => {
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+    })
+  })
+
+  describe('when an experiment is off', () => {
+    beforeEach(() => {
+      useIsOptimizelyFeatureEnabled.mockReturnValue(false)
+    })
+
+    describe('when user is not authenticated', () => {
+      beforeEach(() => {
+        useAuthMock.mockReturnValue({ isAuthenticated: true })
+      })
+
+      describe('when category is not calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.AllRecipes)
+        })
+
+        it('returns false', () => {
+          expect(
+            useIsOptimizelyFeatureEnabled('beetroots_remove_merchandising_banner_web_enabled'),
+          ).toBe(false)
+          expect(useIsOptimizelyFeatureEnabled).toHaveBeenCalledTimes(1)
+
+          expect(useAuthMock().isAuthenticated).toBe(true)
+          expect(useAuthMock).toHaveBeenCalledTimes(1)
+
+          expect(useCollectionQuerySlugMock()).toBe(CollectionSlug.AllRecipes)
+          expect(useCollectionQuerySlugMock).toHaveBeenCalledTimes(1)
+
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+
+      describe('when category is calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.CalorieControlled)
+        })
+
+        it('returns false', () => {
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+    })
+
+    describe('when user is authenticated', () => {
+      beforeEach(() => {
+        useAuthMock.mockReturnValue({ isAuthenticated: true })
+      })
+
+      describe('when category is not calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.AllRecipes)
+        })
+
+        it('returns false', () => {
+          expect(
+            useIsOptimizelyFeatureEnabled('beetroots_remove_merchandising_banner_web_enabled'),
+          ).toBe(false)
+          expect(useIsOptimizelyFeatureEnabled).toHaveBeenCalledTimes(1)
+
+          expect(useAuthMock().isAuthenticated).toBe(true)
+          expect(useAuthMock).toHaveBeenCalledTimes(1)
+
+          expect(useCollectionQuerySlugMock()).toBe(CollectionSlug.AllRecipes)
+          expect(useCollectionQuerySlugMock).toHaveBeenCalledTimes(1)
+
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+
+      describe('when category is calorie-controlled', () => {
+        beforeEach(() => {
+          useCollectionQuerySlugMock.mockReturnValue(CollectionSlug.CalorieControlled)
+        })
+
+        it('returns false', () => {
+          expect(useShouldRemoveMerchandisingBanner()).toBe(false)
+        })
+      })
+    })
+  })
+})
