35713adf12a5bb522c818b368a13fbefbf88cf01
Pavlo <pavel.vodopyan@gmail.com>
2022-05-17 11:38:50 +0100

    [KL-142] Clone `VariantHeader` into `RecipeTile` module (#5191)
    * Initial sketch
    
    * Basic story
    
    * Story is dynamic
    
    * Move recipe context into context directory
    
    * Better typings
    
    * Move hook bearing contexts to the central place
    
    * Add dependencies injector for RecipeTile
    
    * Convert test
    
    * Initial scetach of Emotion based CSS
    
    * Some improvements for Emotion
    
    * Move styles out into its own file
    
    * Move common styles to the root of the RecipeTile module
    
    * Move common styles to the root of the RecipeTile module
    
    * Some explenatory comments
    
    * Fix typo in the file name
    
    * Make code more DRY-er
    
    * Tests to have setup outside testing steps
    
    Co-authored-by: Pavlo Vodopyan <pavlo.vodopyan@gousto.co.uk>

---
+++ src/modules/features/recipe-tile/src/components/VariantHeader/VariantHeader.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+
+import {
+  useRecipe,
+  useStockHook,
+  useGetAlternativeOptionsForRecipeHook,
+} from "../../model/context";
+import { cssPositionTop, cssTextLeft, cssThemeBlue, cssVariantHeader } from "./styles";
+
+export const VariantHeader: React.FC<{
+  categoryId: string;
+  originalId: string;
+}> = ({ categoryId, originalId }) => {
+  const useStock = useStockHook();
+  const useGetAlternativeOptionsForRecipe =
+    useGetAlternativeOptionsForRecipeHook();
+
+  const { isRecipeOutOfStock } = useStock();
+  const getAlternativeOptionsForRecipe = useGetAlternativeOptionsForRecipe();
+  const recipe = useRecipe();
+  const recipeId = recipe?.id;
+
+  if (!recipeId) {
+    return null;
+  }
+
+  const isOutOfStock = isRecipeOutOfStock(recipeId);
+
+  if (isOutOfStock) {
+    return null;
+  }
+
+  const alternatives = getAlternativeOptionsForRecipe({
+    recipeId,
+    originalId,
+    categoryId,
+    isOnDetailScreen: false,
+  });
+
+  // alternatives includes recipe itself, hence checking against `1`
+  if (!alternatives || alternatives.length <= 1) {
+    return null;
+  }
+
+  const alternativeCount = alternatives.length;
+  const text = `${alternativeCount} options available`;
+
+  return (
+    <div
+      css={css(cssVariantHeader, cssTextLeft, cssThemeBlue, cssPositionTop)}
+    >
+      {text}
+    </div>
+  );
+};
+
+VariantHeader.propTypes = {
+  categoryId: PropTypes.string.isRequired,
+  originalId: PropTypes.string.isRequired,
+};
---
+++ src/modules/features/recipe-tile/src/model/context/useGetAlternativeOptionsForRecipe.ts
@@ -0,0 +1,49 @@
+import { createContext, useContext } from 'react'
+
+export type Recipe = {
+  id: string
+  coreRecipeId: string
+  displayName: string
+}
+
+export type GetAlternativeOptionsForRecipe<T extends Recipe = Recipe> = (args: {
+  /**
+   * ID of the original recipe
+   */
+  originalId: string
+  /**
+   * ID of recipe (alternative option) that is already selected
+   */
+  recipeId: string
+  isOnDetailScreen: boolean
+
+  categoryId?: string
+  closeOnSelection?: boolean
+}) => {
+  // This is what would be fed into Alternative Option menu item
+  recipeId: T['id']
+  recipeName: string
+  // eslint-disable-next-line no-unused-vars
+  changeCheckedRecipe: (checkedRecipeId: Recipe['id'], isOutOfStock: boolean) => void
+  isChecked: boolean
+  isOnDetailScreen: boolean
+  isOutOfStock: boolean
+  surcharge?: number | null
+}[]
+
+type useGetAlternativeOptionsForRecipe = () => GetAlternativeOptionsForRecipe
+
+const UseGetAlternativeOptionsForRecipeContext = createContext<useGetAlternativeOptionsForRecipe | null>(null)
+UseGetAlternativeOptionsForRecipeContext.displayName = 'UseGetAlternativeOptionsForRecipeContext'
+
+export const UseGetAlternativeOptionsForRecipeContextProvider = UseGetAlternativeOptionsForRecipeContext.Provider
+
+export const useGetAlternativeOptionsForRecipeHook = () => {
+  const hook = useContext(UseGetAlternativeOptionsForRecipeContext)
+
+  if (hook === null) {
+    throw new Error('There is no useGetAlternativeOptionsForRecipe hook in context!')
+  }
+
+  return hook
+}
