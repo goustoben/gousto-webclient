60cc499b2cc2cb208345ce0bf26effed7385702c
OlegBelski <oleg.belski@gousto.co.uk>
2022-06-09 08:12:56 +0200

    [TG-6543]: removing comment

--- src/apps/webclient/src/config/signup.js
+++ src/apps/webclient/src/config/signup.js
@@ -12,9 +12,6 @@ const applyVoucherPageSlug = 'apply-voucher'
 const enterPromoCodeManuallyPageSlug = 'enter-discount-code'
 const postcodeSlug = 'postcode'
 
-/*
-interface?
- */
 export const signupConfig = {
   /**
    * If experiment steps should be added:
