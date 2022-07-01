8f33b90d6d29088b302f620738a807808a0502b2
OlegBelski <oleg.belski@gousto.co.uk>
2022-07-01 10:40:19 +0200

    [TG-6651] modifying tests

--- src/apps/webclient/src/routes/Signup/Steps/__tests__/BoxSizeStep.test.js
+++ src/apps/webclient/src/routes/Signup/Steps/__tests__/BoxSizeStep.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import { shallow } from 'enzyme'
+import Immutable from 'immutable'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
 import { BoxSizeStep } from '../BoxSize/BoxSizeStep'
 
@@ -9,8 +12,21 @@ jest.mock('containers/OptimizelyRollouts', () => ({
   useIsOptimizelyFeatureEnabled: jest.fn().mockReturnValue(false),
 }))
 
+const store = configureStore()
+const mockedStore = store({
+  auth: Immutable.fromJS({}),
+  basket: Immutable.fromJS({}),
+  menu: Immutable.fromJS({
+    menuLimits: [],
+  }),
+  menuRecipeDetails: Immutable.fromJS({}),
+  tracking: Immutable.fromJS({}),
+  menuCollections: Immutable.fromJS({}),
+})
+
 describe('given the user is at the Box Size Step', () => {
   let wrapper
+  let boxSizeWrapper
   const numPortionChange = jest.fn()
   const numPortionChangeTracking = jest.fn()
   const trackSignupWizardAction = jest.fn()
@@ -18,26 +34,30 @@ describe('given the user is at the Box Size Step', () => {
 
   beforeEach(() => {
     wrapper = shallow(
-      <BoxSizeStep
-        numPortionChange={numPortionChange}
-        numPortionChangeTracking={numPortionChangeTracking}
-        next={next}
-        trackSignupWizardAction={trackSignupWizardAction}
-      />,
+      <Provider store={mockedStore}>
+        <BoxSizeStep
+          numPortionChange={numPortionChange}
+          numPortionChangeTracking={numPortionChangeTracking}
+          next={next}
+          trackSignupWizardAction={trackSignupWizardAction}
+        />
+      </Provider>,
     )
+    boxSizeWrapper = wrapper.find('BoxSizeStep')
+    console.log(boxSizeWrapper.debug({ verbose: true }))
   })
 
   test('then the component renders correctly', () => {
-    expect(wrapper.find('Heading').exists()).toBeTruthy()
-    expect(wrapper.find('.subtitle').exists()).toBeTruthy()
-    expect(wrapper.find('.boxSizeInner').exists()).toBeTruthy()
-    expect(wrapper.find('.carouselItem')).toHaveLength(2)
-    expect(wrapper.find('PrimaryButton')).toHaveLength(2)
+    expect(boxSizeWrapper.find('Heading').exists()).toBeTruthy()
+    expect(boxSizeWrapper.find('.subtitle').exists()).toBeTruthy()
+    expect(boxSizeWrapper.find('.boxSizeInner').exists()).toBeTruthy()
+    expect(boxSizeWrapper.find('.carouselItem')).toHaveLength(2)
+    expect(boxSizeWrapper.find('PrimaryButton')).toHaveLength(2)
   })
 
   describe('when the user chooses the box size', () => {
     beforeEach(() => {
-      wrapper.find('PrimaryButton').at(0).prop('onPrimaryButtonClick')(2)
+      boxSizeWrapper.find('PrimaryButton').at(0).prop('onPrimaryButtonClick')(2)
     })
 
     test('then proper amount of portions are set correctly', () => {
