2b8c5a7ae70233de33f0cf428d3d8e28b27557a4
James Monger <jameskmonger@hotmail.co.uk>
2022-07-01 19:39:49 +0100

    create transformMenu function

---
+++ src/modules/library/menu-service/internal/transformer/index.ts
@@ -0,0 +1,1 @@
+export { transformMenu, TransformedRecipe, TransformedCollection } from './transformMenu'
---
+++ src/modules/library/menu-service/internal/transformer/transformMenu.ts
@@ -0,0 +1,52 @@
+import { MenuAPIResponseDataItem, MenuServiceData } from '../http'
+import { collectionsTransformer } from './collections'
+import { ingredientTransformer } from './ingredientTransformer'
+import { recipeTransformer } from './recipes'
+import { formatIngredients } from './recipeHelpers'
+
+function transformRecipes(menuServiceData: MenuServiceData, menu: MenuAPIResponseDataItem) {
+  if (!menuServiceData) {
+    return []
+  }
+
+  return menu.relationships.recipes.data.map((recipe, index) => {
+    const individualRecipeId = recipe.core_recipe_id
+
+    const currentRecipe = menuServiceData.recipe[individualRecipeId]
+    const normalisedAttributes = currentRecipe && currentRecipe.attributes
+    const normalisedRelationships = currentRecipe && currentRecipe.relationships
+
+    const formattedIngredients = formatIngredients(normalisedRelationships, menuServiceData)
+    const finalIngredients = formattedIngredients.map(ingredientTransformer)
+
+    // use the first recipe as the featured one (base on recommendations)
+    const isFeaturedRecipe = index === 0
+
+    return recipeTransformer({
+      normalisedAttributes,
+      finalIngredients,
+      activeMenu: menu,
+      individualRecipeId,
+      isFeaturedRecipe,
+      recipeMeta: recipe.meta,
+    })
+  })
+}
+
+/**
+ * Get the inner type of an array
+ */
+type Unpacked<T> = T extends (infer U)[] ? U : T
+
+export type TransformedRecipe = Unpacked<ReturnType<typeof transformRecipes>>
+export type TransformedCollection = Unpacked<ReturnType<typeof collectionsTransformer>>
+
+export function transformMenu(menuServiceData: MenuServiceData, menu: MenuAPIResponseDataItem) {
+  const transformedCollections: TransformedCollection[] = collectionsTransformer(menuServiceData, menu)
+  const transformedRecipes: TransformedRecipe[] = transformRecipes(menuServiceData, menu)
+
+  return {
+    collections: transformedCollections,
+    recipes: transformedRecipes,
+  }
+}
