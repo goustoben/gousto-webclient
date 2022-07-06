73baf7470c04bcdebe6725c54ee9e07c137aca35
Edeman George <edeman.george@gousto.co.uk>
2022-07-06 14:05:11 +0200

    lint fix

--- src/apps/webclient/src/components/FiveRecipesAwareness/FiveRecipesAwarenessBanner/FiveRecipesAwarenessBanner.test.tsx
+++ src/apps/webclient/src/components/FiveRecipesAwareness/FiveRecipesAwarenessBanner/FiveRecipesAwarenessBanner.test.tsx
@@ -7,7 +7,7 @@ import * as clientMetrics from 'routes/Menu/apis/clientMetrics'
 
 import { JestSpyInstance } from '../../../types/jest'
 import * as FiveRecipeHooks from '../use5RecipesAwareness'
-import { FiveRecipesAwarenessBanner } from '../../../components/FiveRecipesAwareness/FiveRecipesAwarenessBanner/FiveRecipesAwarenessBanner'
+import { FiveRecipesAwarenessBanner } from './FiveRecipesAwarenessBanner'
 
 describe('<FiveRecipesAwarenessBanner /> ', () => {
   let use5RecipesAwarenessSpy: JestSpyInstance<typeof FiveRecipeHooks.use5RecipesAwareness>
