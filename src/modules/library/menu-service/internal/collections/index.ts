cc6d54f21cf16f0b2645280e3cd9119f09b2d3c8
James Monger <jameskmonger@hotmail.co.uk>
2022-07-07 12:27:26 +0100

    delete unused

--- src/modules/library/menu-service/internal/collections/index.ts
+++
@@ -1,8 +0,0 @@
-import Immutable from 'immutable'
-import { useSelector } from 'react-redux'
-
-const getMenuCollections = (state: any) => state.menuCollections
-
-export function use_legacy_AllCollections(): Immutable.Map<string, any> {
-  return useSelector(getMenuCollections)
-}
