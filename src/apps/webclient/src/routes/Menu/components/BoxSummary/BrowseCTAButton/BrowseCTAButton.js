161e0f4200cd5af8615ab029698fd1c27316228a
Pavlo <pavel.vodopyan@gmail.com>
2022-07-21 14:55:55 +0100

    [KL-713] Ensure `boxDetailsVisibilityChange` has second argument (#5644)
    * Ensure second arg is passed to boxDetailsVisibilityChange
    
    * Remove test as looks like it was not develop
    
    * Pass removeRecipeFromBasket into Details component
    
    * Update the test
    
    Co-authored-by: Pavlo Vodopyan <pavlo.vodopyan@gousto.co.uk>

--- src/apps/webclient/src/routes/Menu/components/BoxSummary/BrowseCTAButton/BrowseCTAButton.js
+++ src/apps/webclient/src/routes/Menu/components/BoxSummary/BrowseCTAButton/BrowseCTAButton.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import classnames from 'classnames'
 import { Button, Segment } from 'goustouicomponents'
 import PropTypes from 'prop-types'
 
+import { useBasket } from 'routes/Menu/domains/basket'
 import { isMobile } from 'utils/view'
 
 import css from './BrowseCTAButton.css'
@@ -12,12 +13,13 @@ const handleSetDeliveryDateClick = (
   menuBrowseCTAVisibilityChange,
   boxDetailsVisibilityChange,
   boxSummaryShow,
+  removeRecipe,
 ) => {
   if (!boxSummaryShow) {
     menuBrowseCTAVisibilityChange(false)
-    boxDetailsVisibilityChange(true)
+    boxDetailsVisibilityChange(true, removeRecipe)
   } else {
-    boxDetailsVisibilityChange(false)
+    boxDetailsVisibilityChange(false, removeRecipe)
   }
 }
 
@@ -34,21 +36,28 @@ const BrowseCTAButton = ({
     [css.fullWidth]: isMobileView && fullWidth,
   })
 
+  const { removeRecipe } = useBasket()
+
+  const onClick = useCallback(() => {
+    if (!disable) {
+      handleSetDeliveryDateClick(
+        menuBrowseCTAVisibilityChange,
+        boxDetailsVisibilityChange,
+        boxSummaryShow,
+        removeRecipe,
+      )
+    }
+  }, [
+    boxDetailsVisibilityChange,
+    boxSummaryShow,
+    disable,
+    menuBrowseCTAVisibilityChange,
+    removeRecipe,
+  ])
+
   return (
     <Button className={classes} data-testing="menuBrowseCTAButton">
-      <Segment
-        onClick={() => {
-          if (!disable) {
-            handleSetDeliveryDateClick(
-              menuBrowseCTAVisibilityChange,
-              boxDetailsVisibilityChange,
-              boxSummaryShow,
-              view,
-            )
-          }
-        }}
-        className={css[view]}
-      >
+      <Segment onClick={onClick} className={css[view]}>
         {isMobileView ? 'Set Date' : 'Set Delivery Date'}
       </Segment>
     </Button>
