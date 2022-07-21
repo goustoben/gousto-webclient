983c8f373faaba9e4521cb86597cf3d123520d43
OlegBelski <oleg.belski@gousto.co.uk>
2022-07-21 13:59:47 +0200

    [TG-6651] restoring changes

--- src/apps/webclient/src/hooks/useIsFiveRecipesEnabledForProspects.ts
+++ src/apps/webclient/src/hooks/useIsFiveRecipesEnabledForProspects.ts
@@ -1,20 +1,21 @@
+import { useMemo } from 'react'
+
 import { useIsOptimizelyFeatureEnabled } from 'containers/OptimizelyRollouts'
 import { useAuth } from 'routes/Menu/domains/auth'
 
 export const useIsFiveRecipesEnabledForProspects = (numPortions?: number) => {
-  const fiveRecipesExperimentEnabled =
-    useIsOptimizelyFeatureEnabled('beetroots_five_recipes_web_enabled') || false
+  const fiveRecipesExperimentEnabled = useIsOptimizelyFeatureEnabled(
+    'beetroots_five_recipes_web_enabled',
+  )
   const { isAuthenticated } = useAuth()
 
-  // FYI: This should be used only on BoxSizeStep
-  const isFiveRecipesExperimentEnabled = !isAuthenticated && fiveRecipesExperimentEnabled
-  // FYI: This should be used at any other step except BoxSize
-  const isFiveRecipesEnabled = numPortions
-    ? isFiveRecipesExperimentEnabled && numPortions === 2
-    : false
+  return useMemo(() => {
+    const isFiveRecipesExperimentEnabled = !isAuthenticated && fiveRecipesExperimentEnabled
+    // FYI: This should be used at any other step except BoxSize
+    const isFiveRecipesEnabled = numPortions
+      ? isFiveRecipesExperimentEnabled && numPortions === 2
+      : false
 
-  return {
-    isFiveRecipesExperimentEnabled,
-    isFiveRecipesEnabled,
-  }
+    return { isFiveRecipesEnabled, isFiveRecipesExperimentEnabled }
+  }, [fiveRecipesExperimentEnabled, isAuthenticated, numPortions])
 }
