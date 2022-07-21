2f6c1619b236a335fac4d70d863f51ab5751e602
James Monger <jameskmonger@hotmail.co.uk>
2022-07-21 16:00:31 +0100

    refactor: more menu memoisation (pulled from SWR PR) (#5646)
    * feat: set up MenuQueryContextProvider
    
    * feat: expose isPending from menu service
    
    * more memoisation
    
    * fix test

---
+++ src/apps/webclient/src/routes/Menu/domains/menu/internal/query.ts
@@ -0,0 +1,37 @@
+import { MenuQueryParams, useMenuQueryParams } from 'routes/Menu/context/menuQueryContext'
+
+const getTasteProfileIdFromQuery = (query: MenuQueryParams) => {
+  if (query && query.tasteProfileId) {
+    return query.tasteProfileId
+  }
+
+  return null
+}
+
+export function useMenuRequestArgs() {
+  const query = useMenuQueryParams()
+
+  if (!query) {
+    throw Error('Attempted to read menu request args outside of context provider')
+  }
+
+  let adminLinkData
+  if (query && query['preview[auth_user_id]']) {
+    adminLinkData = {
+      'preview[menu_id]': query['preview[menu_id]'],
+      'preview[auth_user_id]': query['preview[auth_user_id]'],
+      'preview[expiry]': query['preview[expiry]'],
+      'preview[signature]': query['preview[signature]'],
+    }
+  }
+
+  const tasteProfileId = getTasteProfileIdFromQuery(query)
+
+  return {
+    include: 'ingredients',
+    addAlternatives: true,
+    ...adminLinkData,
+
+    ...(tasteProfileId ? { tasteProfileId } : {}),
+  }
+}
