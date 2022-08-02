57ab5e061dacc9182a1b3d90beb7733a80797046
maxhilliard <max.hilliard@gousto.co.uk>
2022-08-02 15:38:52 +0100

    Set cookie to bypass tutorials

--- tests/e2e-cypress/support/hooks.ts
+++ tests/e2e-cypress/support/hooks.ts
@@ -1,6 +1,12 @@
 beforeEach(() => {
+  // TODO - add this to utility
   cy.setCookie(
     "v1_cookie_policy_v2",
-    encodeURIComponent(JSON.stringify({ isAccepted: true })) // Move this encode/decode into command
+    encodeURIComponent(JSON.stringify({ isAccepted: true }))
+  );
+
+  cy.setCookie(
+    "v1_tutorial_viewed",
+    encodeURIComponent(JSON.stringify({ justforyou: 1, likedislikerecipes: 1 }))
   );
 });
