af87763e5d3b6fbc4eae4ab61c467b6ae3e4a100
maxhilliard <max.hilliard@gousto.co.uk>
2022-08-02 11:01:40 +0100

    bootstrap

---
+++ tests/e2e-cypress/types/index.d.ts
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to get input by label text
+     * @example cy.getInputByLabel('First Name')
+     */
+    getInputByLabel(label: string): Chainable<Element>;
+
+    /**
+     * Custom command to get select nth select option
+     * @example cy.selectNthOption('[data-testing="select"]', 2)
+     */
+    selectNthOption(
+      selectSelector: string,
+      n: number,
+      opts?: { force: boolean }
+    ): Chainable<Element>;
+
+    /**
+     * Custom command to get parse iframe body
+     * to enable Cypress to interact with it
+     * @example cy.getIframeBody('iframe[data-testing="some-iframe"]')
+     */
+    getIframeBody(iframeSelector: string): Chainable<Element>;
+
+    /**
+     * Custom command to configure interceptors, act and then wait for interceptors
+     * Helps wait until page is stable for actions that trigger multiple side effects
+     */
+    actAndWaitForInterceptors(
+      action: () => void,
+      interceptors: Array<Partial<Cypress.RouteOptions>>
+    ): void;
+
+    /**
+     * Custom command wait until element's attachment to DOM is stable
+     * Helps work around "Cypress requires elements be attached in the DOM to interact with them." errors
+     * @example cy.waitUntilDOMAttachmentStable('some-element')
+     */
+    waitUntilDOMAttachmentStable(selector: string): Chainable<Element>;
+  }
+}
