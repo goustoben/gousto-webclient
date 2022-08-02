bd8298a1fa7d3c36f0e351fbe2a8a3fc35391e20
maxhilliard <max.hilliard@gousto.co.uk>
2022-08-02 19:27:17 +0100

    FEF-583 - upgrade to Cypress 10

---
+++ tests/e2e-cypress/cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  screenshotsFolder: "screenshots",
+  videosFolder: "videos",
+  video: false,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 15000,
+  env: {
+    WAF_ACCESS_TOKEN: "MOCK_TOKEN_VALUE",
+  },
+  e2e: {
+    baseUrl: "https://staging-webclient.gousto.info/",
+    specPattern: "./**/*.spec.*",
+    supportFile: "support/index.ts",
+  },
+});
