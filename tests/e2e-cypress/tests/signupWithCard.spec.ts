c005eefc16187f2a145ae443d7aee67cd43964a0
maxhilliard <max.hilliard@gousto.co.uk>
2022-08-02 11:02:42 +0100

    add card signup spec

---
+++ tests/e2e-cypress/tests/signupWithCard.spec.ts
@@ -0,0 +1,39 @@
+import { home, signup, menu, checkout } from "../pages";
+
+describe("Signup", () => {
+  beforeEach(() => {
+    home.visit();
+  });
+
+  it("With Card", () => {
+    home.clickGetStarted();
+
+    signup.clickClaimDiscount();
+    signup.clickChooseBox("regular");
+    signup.enterPostcode();
+    signup.clickConfirmDeliveryDay();
+    signup.clickSeeThisWeeksMenu();
+
+    menu.addNRecipes();
+    menu.clickCheckout();
+
+    checkout.waitForOrderSummary();
+    checkout.enterSignupEmail();
+    checkout.enterSignupPassword();
+    checkout.clickContinueToDelivery();
+    checkout.enterFirstName();
+    checkout.enterLastName();
+    checkout.selectAddress();
+    checkout.enterPhoneNumber();
+    checkout.selectDeliveryDetailsInstruction();
+    checkout.clickContinueToPayment();
+    checkout.enterCardNumber();
+    checkout.enterNameOnCard();
+    checkout.enterCardExpiry();
+    checkout.enterCVV();
+    checkout.clickStartYourSubscription({ type: "card" });
+    checkout.enter3DSPassword();
+
+    checkout.assertCheckoutConfirmation();
+  });
+});
